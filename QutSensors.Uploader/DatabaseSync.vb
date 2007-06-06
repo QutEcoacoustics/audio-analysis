6c2ae377ed3730ffb2db3838173de091a47ec0f2
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2007-06-06 10:57:31 +1000

    Added command line facility to uploader service. Original hash: fce183b40ca68756ad0d3a1334af260fa6d00c9f

--- QutSensors.Uploader/DatabaseSync.vb
+++ QutSensors.Uploader/DatabaseSync.vb
@@ -3,9 +3,45 @@ Imports QutSensors
 
 Public Class DatabaseSync
 	WithEvents timer As New System.Timers.Timer
+	Declare Function AllocConsole Lib "kernel32.dll" () As Boolean
+
+	' The main entry point for the process (Normally in Designer.vb but then you can't edit it...)
+	<MTAThread()> _
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Shared Sub Main(ByVal args() As String)
+		If args.Length > 0 And args(0).ToLower() = "debug" Then
+			DebugRun()
+		Else
+			Dim ServicesToRun() As System.ServiceProcess.ServiceBase
+			ServicesToRun = New System.ServiceProcess.ServiceBase() {New DatabaseSync}
+
+			System.ServiceProcess.ServiceBase.Run(ServicesToRun)
+		End If
+	End Sub
+
+	Shared Sub DebugRun()
+		Dim service As New DatabaseSync
+
+		AllocConsole()
+
+		service.DebugStart()
+
+		Console.WriteLine("Running database sync service in debug mode." + Environment.NewLine + "Hit ENTER to stop.")
+		Console.ReadLine()
+
+		service.DebugStop()
+	End Sub
+
+	Sub DebugStart()
+		OnStart(Nothing)
+	End Sub
+
+	Sub DebugStop()
+		OnStop()
+	End Sub
 
 	Protected Overrides Sub OnStart(ByVal args() As String)
-		timer.Interval = 10000 '1000 = 1sec
+		timer.Interval = My.Settings.SyncRate '1000 = 1sec
 		timer.Enabled = True
 		timer.AutoReset = True
 	End Sub
@@ -21,19 +57,26 @@ Public Class DatabaseSync
 		Dim service As New QutSensors.Services.Service
 
 		For Each _sensor As Sensor In Sensor.GetAllSensors()
+			Console.WriteLine("Synchronising sensor: {0} ({1})", _sensor.Name, _sensor.ID)
 			For Each reading As AudioReading In _sensor.GetAudioReadingsNotUploaded()
 				Try
+					Console.Write("Synchronising audio reading @{0:g}...", reading.Time)
 					service.AddAudioReading(_sensor.ID.ToString(), reading.ID.ToString(), reading.Time, reading.GetData())
 					reading.MarkAsUploaded()
-				Catch
+					Console.WriteLine("Success")
+				Catch e As Exception
+					Console.WriteLine("Failed - {0}", e)
 				End Try
 			Next
 
 			For Each reading As PhotoReading In _sensor.GetPhotoReadingsNotUploaded()
 				Try
+					Console.Write("Synchronising image reading @{0:g}...", reading.Time)
 					service.AddPhotoReading(_sensor.ID.ToString(), reading.ID.ToString(), reading.Time, reading.GetData())
 					reading.MarkAsUploaded()
-				Catch
+					Console.WriteLine("Success")
+				Catch e As Exception
+					Console.WriteLine("Failed - {0}", e)
 				End Try
 			Next
 		Next
