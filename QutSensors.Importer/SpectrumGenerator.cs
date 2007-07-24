566ff1976ad9ee320fdff462022d8f8248a2b325
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2007-07-25 09:03:26 +1000

    Added noise reduction to web frontend. Fixed bug in spectrum generator.

--- QutSensors.Importer/SpectrumGenerator.cs
+++ QutSensors.Importer/SpectrumGenerator.cs
@@ -89,70 +89,80 @@ namespace QutSensors.Importer
 				if (Stopping) // Indicates we're supposed to stop
 					return;
 				Console.WriteLine("Processing reading {0}/{1} - {2}", index++, readings.Count, reading.ID);
-
-				byte[] buffer = reading.GetData();
-				string basePath = Path.GetTempFileName();
-				File.Delete(basePath);
-				basePath = Path.Combine(System.Configuration.ConfigurationManager.AppSettings["SpectrumGeneratorPath"], Path.GetFileNameWithoutExtension(basePath));
-				string wavPath = basePath + ".wav";
-				string freqSpectrumPath = basePath + "-freqspectrum.jpg";
-				string spectrogramPath = basePath + "-spectrogram.png";
-				string dataPath = basePath + "-values.txt";
-
 				try
 				{
-					using (FileStream stream = new FileStream(wavPath, FileMode.Create))
-						stream.Write(buffer, 0, buffer.Length);
-					ProcessStartInfo psi = new ProcessStartInfo("java", string.Format("-classpath .;jfreechart-1.0.5.jar;jcommon-1.0.9.jar;jl1.0.jar SpectrumGenerator {0}", wavPath));
-					psi.WorkingDirectory = System.Configuration.ConfigurationManager.AppSettings["SpectrumGeneratorPath"];
-					psi.CreateNoWindow = true;
-					Process process = Process.Start(psi);
-					process.WaitForExit();
+					byte[] buffer = reading.GetData();
+					if (buffer != null)
+					{
+						string basePath = Path.GetTempFileName();
+						File.Delete(basePath);
+						basePath = Path.Combine(System.Configuration.ConfigurationManager.AppSettings["SpectrumGeneratorPath"], Path.GetFileNameWithoutExtension(basePath));
+						string wavPath = basePath + ".wav";
+						string freqSpectrumPath = basePath + "-freqspectrum.jpg";
+						string spectrogramPath = basePath + "-spectrogram.png";
+						string dataPath = basePath + "-values.txt";
 
-					using (FileStream stream = File.Open(freqSpectrumPath, FileMode.Open))
-					using (BinaryReader reader = new BinaryReader(stream))
-						buffer = reader.ReadBytes(5000 * 1024);
-					if (buffer.Length > 0)
-					{
-						Console.WriteLine("Spectrum image generated.");
-						reading.UpdateSpectrumData(buffer);
-					}
+						try
+						{
+							using (FileStream stream = new FileStream(wavPath, FileMode.Create))
+								stream.Write(buffer, 0, buffer.Length);
+							ProcessStartInfo psi = new ProcessStartInfo("java", string.Format("-classpath .;jfreechart-1.0.5.jar;jcommon-1.0.9.jar;jl1.0.jar SpectrumGenerator {0}", wavPath));
+							psi.WorkingDirectory = System.Configuration.ConfigurationManager.AppSettings["SpectrumGeneratorPath"];
+							psi.CreateNoWindow = true;
+							Process process = Process.Start(psi);
+							process.WaitForExit();
 
-					Thread.Sleep(20000); // Spectrogram takes time to be created!?!
-				RetrySpectrogram:
-					using (FileStream stream = File.Open(spectrogramPath, FileMode.Open))
-					using (BinaryReader reader = new BinaryReader(stream))
-						buffer = reader.ReadBytes(5000 * 1024);
-					if (buffer.Length > 0)
-					{
-						Console.WriteLine("Spectrogram image generated.");
-						reading.UpdateSpectrogramData(buffer);
-					}
-					else
-					{
-						Thread.Sleep(5000);
-						goto RetrySpectrogram;
+							using (FileStream stream = File.Open(freqSpectrumPath, FileMode.Open))
+							using (BinaryReader reader = new BinaryReader(stream))
+								buffer = reader.ReadBytes(5000 * 1024);
+							if (buffer.Length > 0)
+							{
+								Console.WriteLine("Spectrum image generated.");
+								reading.UpdateSpectrumData(buffer);
+							}
+
+							Thread.Sleep(20000); // Spectrogram takes time to be created!?!
+						RetrySpectrogram:
+							using (FileStream stream = File.Open(spectrogramPath, FileMode.Open))
+							using (BinaryReader reader = new BinaryReader(stream))
+								buffer = reader.ReadBytes(5000 * 1024);
+							if (buffer.Length > 0)
+							{
+								Console.WriteLine("Spectrogram image generated.");
+								reading.UpdateSpectrogramData(buffer);
+							}
+							else
+							{
+								Thread.Sleep(5000);
+								goto RetrySpectrogram;
+							}
+						}
+						catch (Exception e)
+						{
+							Console.WriteLine("Failed generation... will retry later.");
+							Console.WriteLine(e);
+						}
+						finally
+						{
+							try
+							{
+								if (File.Exists(wavPath))
+									File.Delete(wavPath);
+								if (File.Exists(freqSpectrumPath))
+									File.Delete(freqSpectrumPath);
+								if (File.Exists(spectrogramPath))
+									File.Delete(spectrogramPath);
+								if (File.Exists(dataPath))
+									File.Delete(dataPath);
+							}
+							catch { }
+						}
 					}
 				}
 				catch (Exception e)
 				{
 					Console.WriteLine("Failed generation... will retry later.");
 					Console.WriteLine(e);
-				}
-				finally
-				{
-					try
-					{
-						if (File.Exists(wavPath))
-							File.Delete(wavPath);
-						if (File.Exists(freqSpectrumPath))
-							File.Delete(freqSpectrumPath);
-						if (File.Exists(spectrogramPath))
-							File.Delete(spectrogramPath);
-						if (File.Exists(dataPath))
-							File.Delete(dataPath);
-					}
-					catch { }
 				}
 			}
 		}
