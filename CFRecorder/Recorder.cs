8a3aac8db4beb9a805e35981972a1f2ae0dd119e
Andy Lau <qut.bioacoustics.research+old.laua@gmail.com>
2007-09-03 13:46:25 +1000

    Original hash: 282787ac463d1ee501cbac6f8dd07eb5b2a106ca

---
+++ CFRecorder/Recorder.cs
@@ -0,0 +1,13 @@
+namespace QUT
+{
+    /// <summary>
+    /// This is the main Recorder class to use with no GUI
+    /// </summary>
+    public class Recorder
+    {
+        public Recorder()
+        {            
+            
+        }
+    }
+}
