835d0ee41b42b54c59606f604231a1618f9f0407
Andy Lau <qut.bioacoustics.research+old.laua@gmail.com>
2007-09-06 11:27:11 +1000

    Original hash: 2d754e54af895e6a4e13cbd6f1f8acfd793e51f7

--- CFRecorder/Uploader.cs
+++ CFRecorder/Uploader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using CFRecorder;
+using System.Text.RegularExpressions;
 
 namespace CFRecorder
 {
@@ -38,7 +39,7 @@ namespace CFRecorder
                     //System.Windows.Forms.MessageBox.Show("Data upload done");
                     //if (!silent)
                     //    MainForm.Log("Upload complete.");
-
+                    
                     File.Delete(file.FullName); // To delete the audio recording once the file is uploaded.
                 }
                 catch (Exception e)
@@ -49,6 +50,23 @@ namespace CFRecorder
                 }
             }
 
+        public static void ProcessFailures()
+        {
+            Regex fileRegex = new Regex(Settings.SensorName + @"_(?<date>\d{8}-\d{6})");
+
+            foreach (string file in Directory.GetFiles(Settings.SensorDataPath))
+            {
+                Match m = fileRegex.Match(file);
+                if (m.Success)
+                {
+                    DateTime time = DateTime.ParseExact(m.Groups["date"].Value, "yyyyMMdd-HHmmss", System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat);
+                    Recording recording = new Recording(time);
+                    Upload(recording, true);
+                }
+            }
+
+
+        }
 
             }
         }
