6de5a3cc6649654a0d981d6b4c4be95cdb3434cf
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2008-02-27 14:15:38 +1000

    New db schema. Added unit tests. Original hash: 388bfec23c95638094accb7ffdd80e65ba39e965

---
+++ Tests/AudioReadingTest.cs
@@ -0,0 +1,90 @@
+﻿using QutSensors.Data.ActiveRecords;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace WebsiteTests
+{
+	[TestClass()]
+	public class AudioReadingTest
+	{
+		public TestContext TestContext { get; set; }
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		[TestInitialize()]
+		public void MyTestInitialize()
+		{
+			Utilities.InitialiseActiveRecord();
+		}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+		[TestMethod()]
+		public void GetReadingsSqlTest()
+		{
+			var actual = AudioReading.GetReadingsSql(null, ReadStatus.Both, Guid.Empty, null, null, null, 0, 20);
+			Assert.IsNotNull(actual);
+		}
+
+		[TestMethod()]
+		public void GetReadingsWithSensorTag()
+		{
+			var actual = AudioReading.GetReadingsSql(new string[] {"Active"}, ReadStatus.Both, Guid.Empty, null, null, null, 0, 20);
+			Assert.IsNotNull(actual);
+		}
+
+		[TestMethod()]
+		public void GetReadingsWithReadRestriction()
+		{
+			var rr = ReadReading.FindFirst();
+			Assert.IsNotNull(rr, "This test requires a reading to have been read on the database. You must set this up manually");
+
+			var actual = AudioReading.GetReadingsSql(null, ReadStatus.Read, rr.AspNetProfilesUserId, null, null, null, 0, 20);
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Count > 0);
+		}
+
+		[TestMethod()]
+		public void GetReadingsWithMinDateRestriction()
+		{
+			var actual = AudioReading.GetReadingsSql(null, ReadStatus.Both, Guid.Empty, System.Data.SqlTypes.SqlDateTime.MinValue.Value, null, null, 0, 20);
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Count > 0, "This test requires there to be some readings in the database. You must set this up manually.");
+		}
+
+		[TestMethod()]
+		public void GetReadingsWithMaxDateRestriction()
+		{
+			var actual = AudioReading.GetReadingsSql(null, ReadStatus.Both, Guid.Empty, null, System.Data.SqlTypes.SqlDateTime.MaxValue.Value, null, 0, 20);
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(actual.Count > 0, "This test requires there to be some readings in the database. You must set this up manually.");
+		}
+
+		[TestMethod()]
+		public void GetReadingsWithAudioTags()
+		{
+			var actual = AudioReading.GetReadingsSql(null, ReadStatus.Both, Guid.Empty, null, null, new string[] {"Bird"}, 0, 20);
+			Assert.IsNotNull(actual);
+		}
+	}
+}
---
+++ Tests/OldService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QutSensors.Data.ActiveRecords;
+
+namespace WebsiteTests
+{
+	[TestClass]
+	public class OldServiceTests
+	{
+		const string TestSensorID = "TestSensor";
+		const string TestDeploymentName = "TestDeploymentName";
+		OldService.ServiceSoapClient service = new WebsiteTests.OldService.ServiceSoapClient();
+
+		public TestContext TestContext
+		{
+			get; set;
+		}
+
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+			
+		//}
+
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+
+		[TestInitialize()]
+		public void MyTestInitialize()
+		{
+			Utilities.InitialiseActiveRecord();
+
+			Hardware hardware = EnsureHardwareExists(TestSensorID);
+			hardware.AddDeployment(TestDeploymentName);
+		}
+
+		[TestCleanup()]
+		public void MyTestCleanup()
+		{
+			Hardware hardware = EnsureHardwareExists(TestSensorID);
+			foreach (Deployment d in hardware.Deployments)
+				d.Delete();
+			foreach (var r in AudioReading.FindAllByProperty("Hardware", hardware))
+				r.Delete();
+			hardware.Delete();
+		}
+		#endregion
+
+		[TestMethod]
+		public void TestConnection()
+		{
+			Assert.IsTrue(service.TestConnection());
+		}
+
+		[TestMethod]
+		public void GetInvalidDeployment()
+		{
+			Assert.IsNull(service.GetLatestDeployment("InvalidSensorID"));
+		}
+
+		[TestMethod]
+		public void GetTestDeployment()
+		{
+			var d = service.GetLatestDeployment(TestSensorID);
+			Assert.IsNotNull(d);
+			Assert.AreEqual(TestDeploymentName, d.Name);
+		}
+
+		[TestMethod]
+		public void AddAudioReading()
+		{
+			var d = service.GetLatestDeployment(TestSensorID);
+			Assert.IsNotNull(d);
+			Assert.IsTrue(service.AddAudioReading(d.DeploymentID, null, DateTime.Now, new byte[0]));
+			Hardware hardware = EnsureHardwareExists(TestSensorID);
+			var readings = AudioReading.FindAllByProperty("Hardware", hardware);
+			Assert.IsNotNull(readings);
+			Assert.AreEqual(1, readings.Length);
+		}
+
+		#region Utilities
+		static Hardware EnsureHardwareExists(string sensorID)
+		{
+			Hardware hardware = Hardware.GetByID(sensorID);
+			if (hardware == null)
+			{
+				hardware = new Hardware();
+				hardware.UniqueID = sensorID;
+				hardware.Create();
+			}
+			return hardware;
+		}
+		#endregion
+	}
+}
