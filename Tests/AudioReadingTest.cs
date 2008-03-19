4612bb55fc9fd9d39f1ce3a7d2e5073e20181336
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2008-03-19 12:17:25 +1000

    Added test deployment flag. Added ability to hide test deployments. Original hash: 04f07f9e43641aa59d94022b004dbfdde517bc2f

--- Tests/AudioReadingTest.cs
+++ Tests/AudioReadingTest.cs
@@ -26,7 +26,7 @@ namespace WebsiteTests
 		//public static void MyClassCleanup()
 		//{
 		//}
-		[TestInitialize()]
+		[TestInitialize]
 		public void MyTestInitialize()
 		{
 			Utilities.InitialiseActiveRecord();
@@ -40,56 +40,56 @@ namespace WebsiteTests
 		//
 		#endregion
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsSqlTest()
 		{
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithSensorTag()
 		{
-			var actual = AudioReading.GetReadingsSql(new string[] {"Active"}, ReadStatus.Both, null, null, null, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql(new string[] { "Active" }, ReadStatus.Both, null, null, null, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithReadRestriction()
 		{
 			var rr = ReadReading.FindFirst();
 			Assert.IsNotNull(rr, "This test requires a reading to have been read on the database. You must set this up manually");
 
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, rr.UserName, null, null, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, rr.UserName, null, null, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 			Assert.IsTrue(actual.Count > 0);
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithUnreadRestriction()
 		{
 			var rr = ReadReading.FindFirst();
 			Assert.IsNotNull(rr, "This test requires a reading to have been read on the database. You must set this up manually");
 
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Unread, rr.UserName, null, null, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Unread, rr.UserName, null, null, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 			//Assert.IsTrue(actual.Count > 0);
 			foreach (var reading in actual)
 				Assert.AreNotEqual(reading.ID, rr.AudioReading.ID);
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithBothAnonymous()
 		{
 			var rr = ReadReading.FindFirst();
 			Assert.IsNotNull(rr, "This test requires a reading to have been read on the database. You must set this up manually");
 
-			int anonymousCount = AudioReading.GetReadingsCountSql((string[])null, ReadStatus.Both, null, null, null, null);
-			int userCount = AudioReading.GetReadingsCountSql((string[])null, ReadStatus.Both, rr.UserName, null, null, null);
+			int anonymousCount = AudioReading.GetReadingsCountSql((string[])null, ReadStatus.Both, null, null, null, null, null);
+			int userCount = AudioReading.GetReadingsCountSql((string[])null, ReadStatus.Both, rr.UserName, null, null, null, null);
 			Assert.AreEqual(anonymousCount, userCount);
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithReadMultipleUsers()
 		{
 			// Checks that read readings are filtered per user, not all users have all readings marked as read
@@ -98,8 +98,8 @@ namespace WebsiteTests
 			var r2 = ReadReading.FindFirst(Expression.Not(Expression.Eq("UserName", r1.UserName)));
 			Assert.IsNotNull(r2, "This test requires a different readings to have been read on the database by different users. You must set this up manually");
 
-			var rr1 = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, r1.UserName, null, null, null, 0, int.MaxValue);
-			var rr2 = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, r2.UserName, null, null, null, 0, int.MaxValue);
+			var rr1 = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, r1.UserName, null, null, null, null, 0, int.MaxValue);
+			var rr2 = AudioReading.GetReadingsSql((string[])null, ReadStatus.Read, r2.UserName, null, null, null, null, 0, int.MaxValue);
 			if (rr1.Count == rr2.Count)
 			{
 				int removedCount = 0;
@@ -109,27 +109,87 @@ namespace WebsiteTests
 			}
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithMinDateRestriction()
 		{
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, System.Data.SqlTypes.SqlDateTime.MinValue.Value, null, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, System.Data.SqlTypes.SqlDateTime.MinValue.Value, null, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 			Assert.IsTrue(actual.Count > 0, "This test requires there to be some readings in the database. You must set this up manually.");
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithMaxDateRestriction()
 		{
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, System.Data.SqlTypes.SqlDateTime.MaxValue.Value, null, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, System.Data.SqlTypes.SqlDateTime.MaxValue.Value, null, null, 0, 20);
 			Assert.IsNotNull(actual);
 			Assert.IsTrue(actual.Count > 0, "This test requires there to be some readings in the database. You must set this up manually.");
 		}
 
-		[TestMethod()]
+		[TestMethod]
 		public void GetReadingsWithAudioTags()
 		{
-			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, new string[] { "Bird" }, 0, 20);
+			var actual = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, new string[] { "Bird" }, null, 0, 20);
 			Assert.IsNotNull(actual);
+		}
+
+		[TestMethod]
+		public void GetTestReadings()
+		{
+			var testDeployments = Deployment.FindFirst(Expression.Eq("IsTest", true));
+			Assert.IsNotNull(testDeployments, "This test requires a test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, testDeployments.GetAudioReadings().Length, "This test requires a test deployment with readings on the database. You must set this up manually");
+			var nontestDeployments = Deployment.FindFirst(Expression.Eq("IsTest", false));
+			Assert.IsNotNull(nontestDeployments, "This test requires a non-test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, nontestDeployments.GetAudioReadings().Length, "This test requires a non-test deployment with readings on the database. You must set this up manually");
+
+			var results = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, null, true, 0, int.MaxValue);
+			Assert.IsNotNull(results);
+			Assert.IsTrue(results.Count > 0);
+
+			foreach (var r in testDeployments.GetAudioReadings())
+				Assert.IsTrue(results.Find(r2 => r2.ID == r.ID) != null);
+			foreach (var r in nontestDeployments.GetAudioReadings())
+				Assert.IsFalse(results.Find(r2 => r2.ID == r.ID) != null);
+		}
+
+		[TestMethod]
+		public void GetNonTestReadings()
+		{
+			var testDeployments = Deployment.FindFirst(Expression.Eq("IsTest", true));
+			Assert.IsNotNull(testDeployments, "This test requires a test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, testDeployments.GetAudioReadings().Length, "This test requires a test deployment with readings on the database. You must set this up manually");
+			var nontestDeployments = Deployment.FindFirst(Expression.Eq("IsTest", false));
+			Assert.IsNotNull(nontestDeployments, "This test requires a non-test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, nontestDeployments.GetAudioReadings().Length, "This test requires a non-test deployment with readings on the database. You must set this up manually");
+
+			var results = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, null, false, 0, int.MaxValue);
+			Assert.IsNotNull(results);
+			Assert.IsTrue(results.Count > 0);
+
+			foreach (var r in testDeployments.GetAudioReadings())
+				Assert.IsFalse(results.Find(r2 => r2.ID == r.ID) != null);
+			foreach (var r in nontestDeployments.GetAudioReadings())
+				Assert.IsTrue(results.Find(r2 => r2.ID == r.ID) != null);
+		}
+
+		[TestMethod]
+		public void GetNoTestFilterReadings()
+		{
+			var testDeployments = Deployment.FindFirst(Expression.Eq("IsTest", true));
+			Assert.IsNotNull(testDeployments, "This test requires a test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, testDeployments.GetAudioReadings().Length, "This test requires a test deployment with readings on the database. You must set this up manually");
+			var nontestDeployments = Deployment.FindFirst(Expression.Eq("IsTest", false));
+			Assert.IsNotNull(nontestDeployments, "This test requires a non-test deployment on the database. You must set this up manually");
+			Assert.AreNotEqual(0, nontestDeployments.GetAudioReadings().Length, "This test requires a non-test deployment with readings on the database. You must set this up manually");
+
+			var results = AudioReading.GetReadingsSql((string[])null, ReadStatus.Both, null, null, null, null, null, 0, int.MaxValue);
+			Assert.IsNotNull(results);
+			Assert.IsTrue(results.Count > 0);
+
+			foreach (var r in testDeployments.GetAudioReadings())
+				Assert.IsTrue(results.Find(r2 => r2.ID == r.ID) != null);
+			foreach (var r in nontestDeployments.GetAudioReadings())
+				Assert.IsTrue(results.Find(r2 => r2.ID == r.ID) != null);
 		}
 	}
 }
