1a542bd9ef614616528e0ed572e0fb1b9abd673f
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2008-04-09 09:33:30 +1000

    Fixed bad checkin. Added start of manual sensor support. Original hash: 25223ed7fd65fee016e0a50b3c19ee8d66fb26db

---
+++ DataUtilities/LocalDataStore/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace LocalDataStore
+{
+	static class Settings
+	{
+		public static string DataFolder
+		{
+			get { return (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\QUT\Sensors\LocalDataStore", "DataFolder", null); }
+			set { Registry.SetValue(@"HKEY_CURRENT_USER\Software\QUT\Sensors\LocalDataStore", "DataFolder", value); }
+		}
+
+		public static string DeselectedDeployments
+		{
+			get { return (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\QUT\Sensors\LocalDataStore", "DeselectedDeployments", null); }
+			set { Registry.SetValue(@"HKEY_CURRENT_USER\Software\QUT\Sensors\LocalDataStore", "DeselectedDeployments", value); }
+		}
+	}
+}
