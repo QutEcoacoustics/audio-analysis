cec6c01e6261ae107d14725623619fa72335aab4
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2008-04-14 11:11:02 +1000

    Included Fallback for audio recordings Added Wireless connection Functionality

---
+++ AudioPhotoWirelessMobileC++/MobileAudio/resourceppc.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Confirmationppc.rc
+//
+#define IDS_APP_TITLE                   1
+#define IDC_ASFWRITER                   2
+#define IDI_ASFWRITER                   101
+#define IDS_OK                          103
+#define IDS_HELP                        104
+#define IDD_ABOUTBOX                    105
+#define IDD_ABOUTBOX_WIDE               106
+#define IDR_MENU1                       130
+#define IDR_MENU                        130
+#define IDB_BITMAP1                     131
+#define IDB_BITMAP2                     132
+#define IDB_BITMAP3                     133
+#define IDB_BITMAP4                     134
+#define IDB_BITMAP5                     135
+#define IDB_BITMAP6                     136
+#define IDB_BITMAP7                     137
+#define IDC_STATIC_1                    201
+#define IDC_STATIC_2                    202
+#define IDC_STATIC_3                    203
+#define IDM_OK                          40000
+#define IDM_HELP                        40001
+#define IDM_HELP_ABOUT                  40002
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
