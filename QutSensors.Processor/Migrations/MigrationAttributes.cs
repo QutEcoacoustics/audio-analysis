13a7f097668eeae9329f71a533cd5471568ebbb1
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2009-02-02 08:48:18 +1000

    Moved Processor

--- QutSensors.Processor/Migrations/MigrationAttributes.cs
+++
@@ -1,6 +0,0 @@
-﻿using RikMigrations;
-using QutSensors.Processor.Migrations;
-
-[assembly: Migration(typeof(JobsInitialiser), 1, "QutSensors.Processor")]
-[assembly: Migration(typeof(JobItemsInitialiser), 2, "QutSensors.Processor")]
-[assembly: Migration(typeof(ResultsInitialiser), 3, "QutSensors.Processor")]
