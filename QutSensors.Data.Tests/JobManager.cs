6299fb948130f19dc41e7795675fb1cbbc5c058b
Richard Mason <qut.bioacoustics.research+old.masonr@gmail.com>
2009-03-03 14:07:53 +1000

    NewData now loads current tags. NewData supports filtering.

--- QutSensors.Data.Tests/JobManager.cs
+++ QutSensors.Data.Tests/JobManager.cs
@@ -20,7 +20,7 @@ namespace QutSensors.Data.Tests
 			var template = JobManager.Instance.AddTemplate(new DummyTemplateParameters(), "TEST TEMPLATE", "This is a template used for testing.");
 
 			var filter = new ReadingsFilter() {FromDate = DateTime.Today};
-			var job = JobManager.Instance.Add(db, filter, "TEST JOB", user.UserName, template);
+			var job = JobManager.Instance.Add(db, filter, "TEST JOB", user.UserName, template, true, true);
 
 			Assert.AreEqual(0, job.JobItems.Count());
 
@@ -42,9 +42,9 @@ namespace QutSensors.Data.Tests
 			var template = JobManager.Instance.AddTemplate(new DummyTemplateParameters(), "TEST TEMPLATE", "This is a template used for testing.");
 
 			var filter1 = new ReadingsFilter() { FromDate = DateTime.Today };
-			var job1 = JobManager.Instance.Add(db, filter1, "TEST JOB 1", user.UserName, template);
+			var job1 = JobManager.Instance.Add(db, filter1, "TEST JOB 1", user.UserName, template, true, true);
 			var filter2 = new ReadingsFilter() { ToDate = DateTime.Today };
-			var job2 = JobManager.Instance.Add(db, filter2, "TEST JOB 2", user.UserName, template);
+			var job2 = JobManager.Instance.Add(db, filter2, "TEST JOB 2", user.UserName, template, true, true);
 
 			Assert.AreEqual(0, job1.JobItems.Count());
 			Assert.AreEqual(0, job2.JobItems.Count());
@@ -63,6 +63,30 @@ namespace QutSensors.Data.Tests
 			Assert.AreEqual(1, job1.JobItems.Count());
 			Assert.AreEqual(1, job2.JobItems.Count());
 			Assert.AreEqual(reading2.AudioReadingID, job2.JobItems.First().AudioReadingID);
+		}
+
+		[TestMethod]
+		public void ProcessReadingOnlyForFlaggedJobs()
+		{
+			var user = CreateUser();
+			var hardware = CreateHardware(user);
+			var deployment = CreateDeployment(user, hardware);
+			var template = JobManager.Instance.AddTemplate(new DummyTemplateParameters(), "TEST TEMPLATE", "This is a template used for testing.");
+
+			var filter1 = new ReadingsFilter() { };
+			var job1 = JobManager.Instance.Add(db, filter1, "TEST JOB 1", user.UserName, template, false, false);
+			var filter2 = new ReadingsFilter() { };
+			var job2 = JobManager.Instance.Add(db, filter2, "TEST JOB 2", user.UserName, template, true, true);
+
+			Assert.AreEqual(0, job1.JobItems.Count());
+			Assert.AreEqual(0, job2.JobItems.Count());
+
+			var reading1 = AddAudioReading(hardware, DateTime.Now.AddDays(1));
+			Assert.AreEqual(1, filter1.GetAudioReadings(db).Count());
+			Assert.AreEqual(1, filter2.GetAudioReadings(db).Count());
+			Assert.AreEqual(0, job1.JobItems.Count());
+			Assert.AreEqual(1, job2.JobItems.Count());
+			Assert.AreEqual(reading1.AudioReadingID, job2.JobItems.First().AudioReadingID);
 		}
 
 		[Serializable]
