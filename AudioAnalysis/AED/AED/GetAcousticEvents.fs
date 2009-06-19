3504ba92e440072eb8cb9b173d3ab8c0711c6458
Bradly Clow <bradly.clow@qut.edu.au>
2009-06-19 15:41:48 +1000

    tidy up

--- AudioAnalysis/AED/AED/GetAcousticEvents.fs
+++ AudioAnalysis/AED/AED/GetAcousticEvents.fs
@@ -7,4 +7,4 @@ open Set
 let rec spider (m:matrix) p (v:(int * int) Set) =
     let (i,j) = p
     if i >= m.NumRows or j < 0 or j >= m.NumCols or m.[i,j] = 0.0 or v.Contains(p) then v
-    else spider m (i,j-1) (add p v) |> spider m (i+1,j-1) |> spider m (i+1,j) |> spider m (i+1,j+1) |> spider m (i,j+1) 
+    else spider m (i+1,j-1) (add p v) |> spider m (i+1,j) |> spider m (i+1,j+1) |> spider m (i,j+1) 
