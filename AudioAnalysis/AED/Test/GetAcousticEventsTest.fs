349d02e717b89295e0b3d87c43475358d3503f8e
Bradly Clow <bradly.clow@qut.edu.au>
2009-06-19 17:00:15 +1000

    time to go home Original hash: 5263f3186e4b71a897d866c58fd36b20a0071e9e

--- AudioAnalysis/AED/Test/GetAcousticEventsTest.fs
+++ AudioAnalysis/AED/Test/GetAcousticEventsTest.fs
@@ -1,5 +1,6 @@
 ﻿#light
 
+open Common
 open Math.Matrix
 open QutSensors.AudioAnalysis.AED.GetAcousticEvents
 open Xunit
@@ -13,3 +14,22 @@ let spiderTest () =
     m.[1,1] <- 1.0
     m.[1,2] <- 1.0
     Assert.Equal(Set.of_list[(0,1);(1,1);(1,2)], spider m (0,1) Set.empty)
+ 
+[<Fact>]   
+let getAcousticEventsTestQuick () =
+    let m = Array2.create 4 3 0.0 |> of_array2
+    Assert.Equal([], getAcousticEvents m)
+    m.[0,1] <- 1.0
+    Assert.Equal([(1,0)], (getAcousticEvents m))
+    m.[1,1] <- 1.0
+    m.[1,2] <- 1.0
+    Assert.Equal([(1,0)], (getAcousticEvents m))
+    m.[3,0] <- 1.0
+    Assert.Equal(2, List.length (getAcousticEvents m))
+
+[<Fact(Skip="broken and slow")>]
+let getAcousticEventsTest () =
+    let i6bm = loadTestFile "I6b.txt" |> of_array2
+    let ae = getAcousticEvents i6bm
+    let aem = loadTestFile2 "AE.txt" 6 1229
+    Assert.Equal(1229, List.length ae)    
