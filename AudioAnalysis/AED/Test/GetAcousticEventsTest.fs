be62a1a69f52988a44f58d6e03c16ebf1bcb6d88
Bradly Clow <bradly.clow@qut.edu.au>
2009-06-19 15:03:09 +1000

    first steps in Get Acoustic Events Original hash: de4d06fec2b080a4f7ea3ab231cecfae3cfc57ba

---
+++ AudioAnalysis/AED/Test/GetAcousticEventsTest.fs
@@ -0,0 +1,15 @@
+﻿#light
+
+open Math.Matrix
+open QutSensors.AudioAnalysis.AED.GetAcousticEvents
+open Xunit
+
+[<Fact>]
+let spiderTest () = 
+    let m = Array2.create 2 3 0.0 |> of_array2
+    Assert.Equal(Set.empty, spider m (0,0) Set.empty)
+    m.[0,1] <- 1.0
+    Assert.Equal(Set.of_list[(0,1)], spider m (0,1) Set.empty)
+    m.[1,1] <- 1.0
+    m.[1,2] <- 1.0
+    Assert.Equal(Set.of_list[(0,1);(1,1);(1,2)], spider m (0,1) Set.empty)
