54abd5a298de0f9471a76b0afe250373069e4773
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-06 16:23:39 +1000

    matlab hist function for large event separation

---
+++ AudioAnalysis/AED/AED/LargeEvents.fs
@@ -0,0 +1,14 @@
+﻿#light
+module QutSensors.AudioAnalysis.AED.LargeEvents
+    
+let hist xs cs =
+    let ub = Seq.append (Seq.pairwise cs |> Seq.map (fun (x,y) -> x + ((y - x)/2))) [999999999] |> Seq.to_array // TODO what is MAX_INT?
+    let a = Array.create (Seq.length ub) 0
+    // TODO nasty bit of imperative code
+    let f x = 
+        let i = Array.find_index (fun b -> x <= b) ub
+        a.[i] <- a.[i] + 1
+    Seq.iter f xs
+    a
+    
+// seq {for i in 0..10 -> i * 1000}
