d69b9230bfc7c2b95711967c653e2f5a7a83117a
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-06 12:09:46 +1000

    improve get acoustic events tests

--- AudioAnalysis/AED/Test/GetAcousticEventsTest.fs
+++ AudioAnalysis/AED/Test/GetAcousticEventsTest.fs
@@ -21,10 +21,10 @@ let getAcousticEventsTestQuick () =
     let m = Array2.create 4 3 0.0 |> of_array2
     Assert.Equal([], getAcousticEvents m)
     m.[0,1] <- 1.0
-    Assert.Equal([(1,0)], (getAcousticEvents m))
+    Assert.Equal([{Left=1;Top=0}], (getAcousticEvents m))
     m.[1,1] <- 1.0
     m.[1,2] <- 1.0
-    Assert.Equal([(1,0)], (getAcousticEvents m))
+    Assert.Equal([{Left=1;Top=0}], (getAcousticEvents m))
     m.[3,0] <- 1.0
     Assert.Equal(2, List.length (getAcousticEvents m))
 
@@ -32,5 +32,14 @@ let getAcousticEventsTestQuick () =
 let getAcousticEventsTest () =
     let i6bm = loadTestFile "I6b.txt" |> of_array2
     let ae = getAcousticEvents i6bm
-    let aem = loadTestFile2 "AE.txt" 6 1229
-    Assert.Equal(1229, List.length ae)    
+    let MATLAB_LENGTH = 1229
+    let aem = loadTestFile2 "AE.txt" 6 MATLAB_LENGTH
+    Assert.Equal(MATLAB_LENGTH, List.length ae)
+    
+    // matlab output sorted by top left column, top left row
+    let f x y = if x.Left = y.Left then compare x.Top y.Top else compare x.Left y.Left
+    let inc = (+) 1 // matlab matrix indicies are 1 based, F# is 0 based
+    let MATLAB_LEFT, MATLAB_TOP = 0, 1
+    let g i x = inc x.Left = (int) aem.[MATLAB_LEFT,i] && inc x.Top = (int) aem.[MATLAB_TOP,i]
+    // hack to show the index failed at in the xunit output
+    List.tryfind_index (fun x -> not x) (List.sort f ae |> List.mapi g) |> Option.iter (fun i -> Assert.Equal(-1, i))
