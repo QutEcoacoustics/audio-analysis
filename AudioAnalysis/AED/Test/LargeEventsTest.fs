9d393c642166e395557e21025af1185f01b632e9
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-06 16:23:39 +1000

    matlab hist function for large event separation Original hash: 54abd5a298de0f9471a76b0afe250373069e4773

---
+++ AudioAnalysis/AED/Test/LargeEventsTest.fs
@@ -0,0 +1,9 @@
+﻿#light
+
+open QutSensors.AudioAnalysis.AED.LargeEvents
+open Xunit
+
+[<Fact>]
+let testHist () =
+    let cs = seq {for i in 0..10 -> i * 1000}
+    Assert.Equal([|2; 1; 0; 0; 0; 0; 0; 0; 0; 0; 1|], hist [1;500;501;20000] cs)
