9f30e4b70f79aee2cde21a90c53ce9c476b329c2
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-08 12:43:11 +1000

    mode large area threshold Original hash: 5580e7d8303aa3f55cb6b0d9fc8a77d0211d2484

--- AudioAnalysis/AED/AED/LargeEvents.fs
+++ AudioAnalysis/AED/AED/LargeEvents.fs
@@ -1,6 +1,8 @@
 ﻿#light
 module QutSensors.AudioAnalysis.AED.LargeEvents
     
+open GetAcousticEvents
+    
 let hist xs cs =
     let ub = Seq.append (Seq.pairwise cs |> Seq.map (fun (x,y) -> x + ((y - x)/2))) [999999999] |> Seq.to_array // TODO what is MAX_INT?
     let a = Array.create (Seq.length ub) 0
@@ -11,4 +13,15 @@ let hist xs cs =
     Seq.iter f xs
     a
     
-// seq {for i in 0..10 -> i * 1000}
+let uncurry f (x,y) = f x y // TODO is this already written?
+
+let lastMin cs h = 
+    let s = Seq.pairwise h |> Seq.map (fun (x,y) -> y-x) |> Seq.zip ( Seq.skip 1 cs)
+    Seq.take (Seq.length s - 1) s |> Seq.fold (fun z (c,x) -> if x < 0 then c else z) (Seq.hd cs)
+    
+//let threshold rs =
+//    let cs = seq {for i in 0..10 -> i * 1000}
+//    let h = hist (Seq.map (fun r -> r.Width * r.Height) rs) cs
+//    
+//    let i = Seq.pairwise h |> Seq.map (uncurry (-)) |> List.of_seq |> List.rev |> List.tryfind_index (fun x -> x > 0)
+    //Option.fold_left (fun t n -> ) 3000 i
