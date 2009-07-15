404c662073629d6fc42cffc5272ea1d0c8882c1f
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-15 16:30:40 +1000

    solved stack overflow with F# upgrade Original hash: 4a50895f53c23fd363e0fd002717833a03e41b86

--- AudioAnalysis/AED/AED/GetAcousticEvents.fs
+++ AudioAnalysis/AED/AED/GetAcousticEvents.fs
@@ -4,12 +4,15 @@ open Math.Matrix
 open Set
 
 // TODO can we do p@(i,j) like Haskell?
-let rec spider (m:matrix) p (v:(int * int) Set) =
-    let (i,j) = p
-    if i >= m.NumRows or j < 0 or j >= m.NumCols or i < 0 or i >= m.NumRows or m.[i,j] = 0.0 or v.Contains(p) then v
-    // TODO this is an interesting computation, threading the state through a sequence of identical functions
-    // else spider m (i+1,j-1) (add p v) |> spider m (i+1,j) |> spider m (i+1,j+1) |> spider m (i,j+1) 
-    else List.fold_left (fun z p -> spider m p z) (add p v) [(i-1,j-1);(i-1,j);(i-1,j+1);(i,j-1);(i,j);(i,j+1);(i+1,j-1);(i+1,j);(i+1,j+1)]
+let rec spider (m:matrix) xs (v:(int * int) Set) =
+    match xs with // TODO move this up?
+    | []    -> v
+    | p::ps -> let (i,j) = p // TODO should this be part of the pattern?
+               let (v', ps') = if j < 0 or j >= m.NumCols or i < 0 or i >= m.NumRows or m.[i,j] = 0.0 or v.Contains(p) then (v, ps)
+                               // TODO this is an interesting computation, threading the state through a sequence of identical functions
+                               // else spider m (i+1,j-1) (add p v) |> spider m (i+1,j) |> spider m (i+1,j+1) |> spider m (i,j+1) 
+                               else (add p v, [(i-1,j-1);(i-1,j);(i-1,j+1);(i,j-1);(i,j);(i,j+1);(i+1,j-1);(i+1,j);(i+1,j+1)] @ ps) 
+               spider m ps' v'
     
 type Rectangle = {Left:int; Top:int; Width:int; Height:int;}
     
@@ -20,5 +23,5 @@ let getAcousticEvents m =
         let (rs, cs) = List.unzip (to_list xs) 
         let l,t = List.min cs, List.min rs
         {Left=l; Top=t; Width=List.max cs - l + 1; Height=List.max rs - t + 1}
-    let f i j a x = if x = 0.0 or m'.[i,j] = 0.0 then a else (g(spider m (i,j) Set.empty))::a
+    let f i j a x = if x = 0.0 or m'.[i,j] = 0.0 then a else (g(spider m [(i,j)] Set.empty))::a
     foldi f [] m
