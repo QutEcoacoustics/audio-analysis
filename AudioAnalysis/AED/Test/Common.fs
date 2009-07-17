1d1cb68d39c0c3e6ae50b29c756516e13937cccd
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-17 15:14:17 +1000

    fit in with Birgits current AED matlab code

--- AudioAnalysis/AED/Test/Common.fs
+++ AudioAnalysis/AED/Test/Common.fs
@@ -9,6 +9,6 @@ let loadTestFile f = loadTestFile2 f 256 5188
 
 let loadEventsFile f j =
     // matlab matrix indicies are 1 based, F# is 0 based
-    let aem = loadTestFile2 f 6 j
+    let aem = loadTestFile2 f 10 j
     let dec x = (int x) - 1
     seq {for i in 0..(j-1) -> {Left=dec aem.[0,i]; Top=dec aem.[1,i]; Width=(int) aem.[2,i]; Height=(int) aem.[3,i]}}
