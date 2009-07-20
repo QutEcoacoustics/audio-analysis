9cdcc48d39d8a1611d8e305b387e178c7c8558e0
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-20 10:36:44 +1000

    tidy up

--- AudioAnalysis/AED/Test/WienerTest.fs
+++ AudioAnalysis/AED/Test/WienerTest.fs
@@ -6,6 +6,6 @@ open Xunit
 [<Fact>]
 let testWiener () = 
     let i1 = loadTestFile "I1.txt"
-    let i2 = wiener2 i1 5
+    let i2 = wiener2 5 i1
     let i2m = loadTestFile "I2.txt"
     Assert.True (a2FloatEquals i2 i2m 0.00001)
