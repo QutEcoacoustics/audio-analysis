196259c9ef0c25434e824977938873c9a934b609
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-20 15:44:52 +1000

    tidy up

--- AudioAnalysis/AED/AED/Matlab.fs
+++ AudioAnalysis/AED/AED/Matlab.fs
@@ -1,6 +1,12 @@
 ﻿module QutSensors.AudioAnalysis.AED.Matlab
 
-// TODO amalgamate with hist in wiener2
+// TODO amalgamate with hist in subbandmode
+
+(* This is one particular variation of the Matlab hist function, from the help:
+    N = HIST(Y,X), where X is a vector, returns the distribution of Y among bins with centers
+    specified by X. The first bin includes data between -inf and the first center and the last
+    bin includes data between the last bin and inf.
+*)
 let hist xs cs =
     let ub = Seq.append (Seq.pairwise cs |> Seq.map (fun (x,y) -> x + ((y - x)/2))) [999999999] |> Seq.to_array // TODO what is MAX_INT?
     let a = Array.create (Seq.length ub) 0
