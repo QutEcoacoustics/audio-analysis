a45181d819866bf1e638df71a84518aa73c30835
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-22 12:01:14 +1000

    tidying up to use Matrix instead of Array2D consistently

--- AudioAnalysis/AED/AED/Matlab.fs
+++ AudioAnalysis/AED/AED/Matlab.fs
@@ -16,3 +16,29 @@ let hist xs cs =
         a.[i] <- a.[i] + 1
     Seq.iter f xs
     a
+    
+    
+let mean m n = (Math.Matrix.fold (+) 0.0 m) / n
+    
+let variance a n m = (Math.Matrix.fold (fun z x -> z + (x*x)) 0.0 a) / n - (m*m)
+       
+let localMeansVariances n m =
+    let a = Math.Matrix.to_array2 m // TODO change Util.Array2.neighborhoodBounds a n x y to accept a matrix instead of an array
+    let f x y _ = 
+        let nba = Util.Array2.neighborhoodBounds a n x y |> m.Region
+        let nbs = float (n*n)
+        let m = mean nba nbs
+        (m, variance nba nbs m)
+    Array2D.mapi f a
+    
+(* In wiener2.m the local means are calculated using a sum of smaller neighbourhoods around the edges but
+   are always divided by a constant neighbourhood size. Implemented the same here.
+*)
+let wiener2 n m =
+    let lmv = localMeansVariances n m
+    let a = Math.Matrix.to_array2 m
+    let nv = (Util.Array2.a2fold (fun z (_, x) -> z + x) 0.0 lmv) / (float (a.GetLength(0) * a.GetLength(1))) // TODO single general mean function?
+    let f x y e =
+        let (m,v) = lmv.[x,y]
+        m + ((max 0.0 (v - nv)) / max v nv) * ((float e) - m)
+    Array2D.mapi f a
