9b56899eb44f9cb913dc2a76f3cf1e02b84929c9
Bradly Clow <bradly.clow@qut.edu.au>
2009-07-22 15:48:09 +1000

    tidy up wiener2 Original hash: 93286d4826418fae59d11b7ff3dedfb128f15c00

--- AudioAnalysis/AED/AED/Util.fs
+++ AudioAnalysis/AED/AED/Util.fs
@@ -9,3 +9,23 @@ let (|?) o p = if Option.isSome o then o else p
    Copy of Scala Option.getOrElse
 *)
 let (|?|) o d = match o with | Some x -> x | _ -> d
+
+// Assume matricies m,n are exactly same dimensions
+let matrixMap2 f (m:matrix) (n:matrix) = Math.Matrix.init m.NumRows m.NumCols (fun i j -> f m.[i,j] n.[i,j])
+
+// Assume matricies m,n,o are exactly same dimensions
+let matrixMap3 f (m:matrix) (n:matrix) (o:matrix) = Math.Matrix.init m.NumRows m.NumCols (fun i j -> f m.[i,j] n.[i,j] o.[i,j])
+
+let matrixMapi2Unzip f (m:matrix) =
+    let r = Math.Matrix.zero m.NumRows m.NumCols
+    let s = Math.Matrix.zero m.NumRows m.NumCols
+    let mutable x = (0.0, 0.0)
+    for i=0 to (m.NumRows-1) do
+      for j=0 to (m.NumCols-1) do
+        x <- f i j m.[i,j]
+        let (y,z) = x
+        r.[i,j] <- y
+        s.[i,j] <- z
+      done
+    done
+    (r,s) 
