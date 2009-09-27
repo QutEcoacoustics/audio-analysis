069bb32dec467dbec34d6e1b5e08e5ceabd78f36
Bradly Clow <bradly.clow@qut.edu.au>
2009-09-28 08:45:02 +1000

    tidied up FsCheckXunit significantly

--- AudioAnalysis/AED/Test/FsCheckXunit.fs
+++ AudioAnalysis/AED/Test/FsCheckXunit.fs
@@ -10,18 +10,8 @@ let xUnitRunner =
         member x.OnShrink(_,_) = ()
         member x.OnFinished(name, result) = 
             match result with 
-                | True data ->
-                    Assert.True(true) 
-                    data.Stamps |> Seq.iter (fun x -> printfn "%d - %A" (fst x) (snd x)) 
-                | False (data,_,args,Exception e,_) ->
-                    Assert.True(false, sprintf "%s - Falsifiable after %i tests (%i shrinks): %A with exception %O"
-                        name data.NumberOfTests data.NumberOfShrinks args e) 
-                | False (data,_,args,Timeout i,_) ->
-                    Assert.True(false, sprintf "%s - Timeout of %i milliseconds exceeded after %i tests (%i shrinks): %A"
-                        name i data.NumberOfTests data.NumberOfShrinks args) 
-                | False (data,_,args,_,_) ->
-                    Assert.True(false, sprintf "%s - Falsifiable after %i tests (%i shrinks): %A"
-                        name data.NumberOfTests data.NumberOfShrinks args) 
-                | Exhausted data ->  Assert.True(false, sprintf "Exhausted after %d tests" (data.NumberOfTests) ) 
+                | True data -> Assert.True(true)
+                | _ -> Assert.True(false, testFinishedToString name result) 
         } 
+        
 let config = {quick with Runner = xUnitRunner}
