816119b711b6d4f38fc0af8c9aca27d00bf41690
Bradly Clow <bradly.clow@qut.edu.au>
2009-09-30 16:22:56 +1000

    EPR tests project

---
+++ AudioAnalysis/EPR/Test/EventPatternRecogTest.fs
@@ -0,0 +1,20 @@
+﻿open QutSensors.AudioAnalysis.AED.Util
+open QutSensors.AudioAnalysis.EPR.EventPatternRecog
+open Xunit
+
+
+// TODO copied and modified from AED test - generalise
+let loadTestFile2 f i j = fileToMatrix (@"matlab\" + f) i j |> Math.Matrix.of_array2
+
+// TODO copied and modified from AED test - generalise
+// Format: time start, time duration, freq start, freq end
+let loadEventsFile f m n =
+    let aem = loadTestFile2 f m n
+    seq {for i in 0..(m-1) -> {Left=aem.[i,0]; Right=aem.[i,0]+aem.[i,1]; Bottom=aem.[i,2]; Top=aem.[i,3]}}
+
+[<Fact>]
+let detectGroundParrotsTest () = 
+    let t = loadEventsFile "GroundParrotTemplate.txt" 11 4
+    let ae = loadEventsFile "GroundParrotAE.txt" 811 10
+    let r = loadEventsFile "GroundParrotResults.txt" 10 4
+    Assert.Equal(Seq.sort r, detectGroundParrots t ae |> Seq.sort)
