3894475ec24ecda0163eb27dca6a0f4268a3d3a6
Bradly Clow <bradly.clow@qut.edu.au>
2009-10-05 11:56:23 +1000

    EPR: fixed up normalising template centroids

--- AudioAnalysis/EPR/EPR/EventPatternRecog.fs
+++ AudioAnalysis/EPR/EPR/EventPatternRecog.fs
@@ -8,25 +8,22 @@ let right r = r.Right
 let bottom r = r.Bottom
 let top r = r.Top
 let bottomLeft r = (r.Left, r.Bottom)
+let absLeftAbsBottom rs = (minmap left rs, minmap bottom rs)
 
-let normaliseTimeFreq st sf nt nf (t,f) =
-    let g x s l = let x' = rnd ((x - s) / l) in if x' < 1.0 then 1.0 else if x' > l then l else x'
-    (g t st nt, g f sf nf)
-    
-let normaliseTimeFreqs st sf nt nf = Seq.map (normaliseTimeFreq st sf nt nf)
+// TODO review all the normalising code
+let normaliseTimeFreq st sf td fr nt nf (t,f) =
+    let g x s d l = let x' = rnd ((x - s) / d * l) in if x' < 1.0 then 1.0 else if x' > l then l else x'
+    (g t st td nt, g f sf fr nf)
     
 // assuming l <= r
 let centre l r = l + (r - l) / 2.0
 
-// TODO investigate comprehension notation
 let centroids rs = Seq.map (fun r -> (centre r.Left r.Right, centre r.Bottom r.Top)) rs
     
-let absLeftAbsBottom rs = (minmap left rs, minmap bottom rs)
-
 // TODO investigate performance optimisation by normalising individual points in tuple computations
-let centroidsBottomLefts st sf nt nf rs = 
-    let f = normaliseTimeFreqs st sf nt nf 
-    (centroids rs |> f, Seq.map (fun r -> (r.Left, r.Bottom)) rs |> f)
+let centroidsBottomLefts st sf td fr nt nf rs = 
+    let f = Seq.map (normaliseTimeFreq st sf td fr nt nf)
+    (centroids rs |> f, Seq.map bottomLeft rs |> f)
     
 let euclidianDist (x1, y1) (x2, y2) = (x1 - x2) ** 2.0 + (y1 - y2) ** 2.0 |> sqrt
 
@@ -64,16 +61,16 @@ let detectGroundParrots t aes =
     let yl = tfr / freqMax * (freqBins-1.0) |> rnd |> (+) 1.0
     
     // Template centroids and bottom left corners normalised
-    let tst = 1.0 // TODO
-    let (tcs, tbls) = centroidsBottomLefts tst tb xl yl t
+    let (tcs, tbls) = centroidsBottomLefts tl tb ttd tfr xl yl t
     let tcbls = Seq.zip tcs tbls
     
-    let score tc tbl rs =
-        let (st, sf) = absLeftAbsBottom rs // TODO broken assumption that the same event will have both bottom and left? Same as matlab?
-        let (cs, bls) = centroidsBottomLefts st sf xl yl rs // TODO don't need to compute all bottom lefts here
-        let i = closestCentroidIndex tc cs
-        overlap tbl tc (normaliseTimeFreq st sf xl yl (bottomLeft (Seq.nth i rs))) (Seq.nth i cs)
+//    let score tc tbl rs =
+//        let (st, sf) = absLeftAbsBottom rs // TODO broken assumption that the same event will have both bottom and left? Same as matlab?
+//        let (cs, bls) = centroidsBottomLefts st sf xl yl rs // TODO don't need to compute all bottom lefts here
+//        let i = closestCentroidIndex tc cs
+//        overlap tbl tc (normaliseTimeFreq st sf xl yl (bottomLeft (Seq.nth i rs))) (Seq.nth i cs)
         
     let (saes, cs) = candidates (boundedInterval tb 500.0 500.0 0.0 freqMax) ttd tfr aes
-    let scores = Seq.map (fun c -> Seq.map (fun (tc, tbl) -> score tc tbl c) tcbls |> Seq.sum) cs
-    seq {for (sae,score) in Seq.zip saes scores do if score >= 3.5 then yield sae}    
+//    let scores = Seq.map (fun c -> Seq.map (fun (tc, tbl) -> score tc tbl c) tcbls |> Seq.sum) cs
+//    seq {for (sae,score) in Seq.zip saes scores do if score >= 3.5 then yield sae}
+    cs
