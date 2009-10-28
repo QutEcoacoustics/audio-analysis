db26fd0e97c41bd245984f4c9b25af8135bcade5
Bradly Clow <bradly.clow@qut.edu.au>
2009-10-28 15:19:57 +1000

    Large event separation now uses a constant area threshold of 3000 Original hash: c0ad8660010478cb77ef67414c30f5d94f7884a6

--- AudioAnalysis/AED/AED/AcousticEventDetection.fs
+++ AudioAnalysis/AED/AED/AcousticEventDetection.fs
@@ -18,23 +18,13 @@ let toFillIn (m:matrix) i j t =
 let joinHorizontalLines m = Math.Matrix.mapi (fun i j x -> if x = 1.0 or (toFillIn m i j 3) then 1.0 else 0.0) m
     
 let joinVerticalLines = Math.Matrix.transpose << joinHorizontalLines << Math.Matrix.transpose
-
-let lastMin cs h = 
-    let s = Seq.pairwise h |> Seq.map (fun (x,y) -> y-x) |> Seq.zip ( Seq.skip 1 cs)
-    Seq.take (Seq.length s - 1) s |> Seq.fold (fun z (c,x) -> if x < 0 then c else z) (Seq.hd cs)
-    
-let threshold rs =
-    let cs = seq {for i in 0..10 -> i * 1000}
-    let t = hist (areas rs) cs |> lastMin cs
-    let d = 3000
-    if t < d then d else t
     
 let aeToMatrix ae =
     let r = ae.Bounds
     Math.Matrix.init r.Height r.Width (fun i j -> if ae.Elements.Contains (r.Top + i, r.Left + j) then 1.0 else 0.0)
 
 let separateLargeEvents aes =
-    let areat = bounds aes |> threshold
+    let areat = 3000
     let f ae =
         let freqt = 20.0
         let m = aeToMatrix ae
@@ -42,7 +32,6 @@ let separateLargeEvents aes =
         let m1 = Math.Matrix.mapi (fun i _ x -> if Seq.nth i s then 0.0 else x) m
         let rs = getAcousticEvents m1
                  |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in {Left=b1.Left+b2.Left; Top=b1.Top+b2.Top; Width=b2.Width; Height=b2.Height})
-                    
         let timet = 100.0 / 3.0
         let m2 = m - m1
         rs @ (getAcousticEvents m2
@@ -51,7 +40,7 @@ let separateLargeEvents aes =
     Seq.collect (fun ae -> if area ae.Bounds < areat then [ae.Bounds] else f ae) aes
 
 let smallFirstMin cs h t =
-    let s = Seq.pairwise h |> Seq.map (fun (x,y) -> x-y) |> Seq.zip cs // TODO almost a copy from LargeEvents.lastMin
+    let s = Seq.pairwise h |> Seq.map (fun (x,y) -> x-y) |> Seq.zip cs
     let tf g = Seq.tryFind (fun (_,x) -> g x) s
     tf ((>) 0) |? lazy tf ((=) 0) |> Option.map fst |?| t
 
