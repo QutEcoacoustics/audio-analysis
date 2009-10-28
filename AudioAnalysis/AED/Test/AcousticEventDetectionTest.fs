c0ad8660010478cb77ef67414c30f5d94f7884a6
Bradly Clow <bradly.clow@qut.edu.au>
2009-10-28 15:19:57 +1000

    Large event separation now uses a constant area threshold of 3000

--- AudioAnalysis/AED/Test/AcousticEventDetectionTest.fs
+++ AudioAnalysis/AED/Test/AcousticEventDetectionTest.fs
@@ -56,23 +56,6 @@ let testJoinVerticalLines () =
         let i6am = loadTestFile4 "I6a.txt" md
         matrixFloatEquals i6a i6am 0.001 |> Assert.True
     testAll f
-    
-[<Fact>]
-let testLastMin () =
-    Assert.Equal(1, lastMin [0..2] [2;1;2])
-    Assert.Equal(2, lastMin [0..3] [1;2;1;1])
-    Assert.Equal(0, lastMin [0..2] [1;1;2])
-    Assert.Equal(3, lastMin [0..4] [2;1;1;0;1])
-    Assert.Equal(1, lastMin [0..3] [2;1;2;1])
-    
-[<Fact>]
-let testThreshold () =
-    let f d l t =
-        let aem = loadEventsFile d "AE.txt" l
-        Assert.Equal(t, threshold aem)
-    // TODO duplicated with GetAcousticEventsTest
-    f "BAC2_20071015-045040" 1229 9000
-    f "GParrots_JB2_20090607-173000.wav_minute_3" 5229 3000
 
 [<Fact>]
 let aeToMatrixBounds () = chk (fun ae -> let m = aeToMatrix ae in m.NumRows = ae.Bounds.Height && m.NumCols = ae.Bounds.Width)
@@ -92,7 +75,7 @@ let separateLargeEventsTest () =
         Assert.Equal(Seq.length ae2m, Seq.length ae2)
         Assert.Equal(Seq.sort ae2m, Seq.sort ae2)
     // TODO duplicated with GetAcousticEventsTest
-    f "BAC2_20071015-045040" 256 5188 1249
+    f "BAC2_20071015-045040" 256 5188 1249 // TODO this had previously used a threshold of 9000
     f "GParrots_JB2_20090607-173000.wav_minute_3" 256 5166 5291    
     
 [<Fact>]
