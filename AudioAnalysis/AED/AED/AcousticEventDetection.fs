556304927eda0f6e25a1d4d7ef057360c0fe4585
Bradly Clow <bradly.clow@qut.edu.au>
2009-10-28 15:01:58 +1000

    merge large events code into AcousticEventDetection.fs Original hash: 3b04ff9d607b634da8aff728dfff90211954fbe8

--- AudioAnalysis/AED/AED/AcousticEventDetection.fs
+++ AudioAnalysis/AED/AED/AcousticEventDetection.fs
@@ -1,7 +1,6 @@
 ﻿module QutSensors.AudioAnalysis.AED.AcousticEventDetection
 
 open GetAcousticEvents
-open LargeEvents
 open Matlab
 open TowseyLib
 open Util
@@ -19,6 +18,37 @@ let toFillIn (m:matrix) i j t =
 let joinHorizontalLines m = Math.Matrix.mapi (fun i j x -> if x = 1.0 or (toFillIn m i j 3) then 1.0 else 0.0) m
     
 let joinVerticalLines = Math.Matrix.transpose << joinHorizontalLines << Math.Matrix.transpose
+
+let lastMin cs h = 
+    let s = Seq.pairwise h |> Seq.map (fun (x,y) -> y-x) |> Seq.zip ( Seq.skip 1 cs)
+    Seq.take (Seq.length s - 1) s |> Seq.fold (fun z (c,x) -> if x < 0 then c else z) (Seq.hd cs)
+    
+let threshold rs =
+    let cs = seq {for i in 0..10 -> i * 1000}
+    let t = hist (areas rs) cs |> lastMin cs
+    let d = 3000
+    if t < d then d else t
+    
+let aeToMatrix ae =
+    let r = ae.Bounds
+    Math.Matrix.init r.Height r.Width (fun i j -> if ae.Elements.Contains (r.Top + i, r.Left + j) then 1.0 else 0.0)
+
+let separateLargeEvents aes =
+    let areat = bounds aes |> threshold
+    let f ae =
+        let freqt = 20.0
+        let m = aeToMatrix ae
+        let s = sumRows m |> Seq.map (fun x -> x / (float) m.NumCols * 100.0 <= freqt) 
+        let m1 = Math.Matrix.mapi (fun i _ x -> if Seq.nth i s then 0.0 else x) m
+        let rs = getAcousticEvents m1
+                 |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in {Left=b1.Left+b2.Left; Top=b1.Top+b2.Top; Width=b2.Width; Height=b2.Height})
+                    
+        let timet = 100.0 / 3.0
+        let m2 = m - m1
+        rs @ (getAcousticEvents m2
+              |> List.filter (fun x -> (float) x.Bounds.Height * 100.0 / (float) m2.NumRows >= timet)
+              |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in {Left=b1.Left+b2.Left; Top=b1.Top; Width=b2.Width; Height=b1.Height}))
+    Seq.collect (fun ae -> if area ae.Bounds < areat then [ae.Bounds] else f ae) aes
 
 let smallFirstMin cs h t =
     let s = Seq.pairwise h |> Seq.map (fun (x,y) -> x-y) |> Seq.zip cs // TODO almost a copy from LargeEvents.lastMin
