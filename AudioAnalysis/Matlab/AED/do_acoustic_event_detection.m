75e52852aeb7479baa6c3523da8dcba0edbf64c5
Bradly Clow <bradly.clow@qut.edu.au>
2009-11-02 16:53:49 +1000

    writing out csv files for f# tests

--- AudioAnalysis/Matlab/AED/do_acoustic_event_detection.m
+++ AudioAnalysis/Matlab/AED/do_acoustic_event_detection.m
@@ -21,36 +21,45 @@ addpath('../Common')
 [y,fs,I1,F,T,] = wavToSpectrogram('../../AED/Test/matlab/BAC2_20071015-045040.wav');
 [M,N] = size(I1);
 showImage(c,I1,T,F,1);
+% csvout('I1.csv',I1)
 
 % STEP 2: WIENER FILTERING
 I2 = wiener2(I1, [w w]);
+% csvout('I2.csv',I2)
 
 % STEP 3: GET MODAL INTENSITIES OF SUBBBANDS and REMOVE FROM IMAGE
 I3 = withoutSubbandModeIntensities(I2);
+% csvout('I3.csv',I3)
 
 % STEP 4: CONVERTS IMAGE TO BLACK AND WHITE USING INTENSITY THRESHOLD
 I4 = image_thresh_bw(I3,int_thresh);
+% csvout('I4.csv',I4)
 
 % STEP 5: JOIN VERTICAL LINES IN IMAGE
 I5 = join_vertical_lines(I4);
+% csvout('I5.csv',I5)
 
 % STEP 6: JOIN HORIZONTAL LINES IN IMAGE
 I6 = join_horizontal_lines(I5);
+% csvout('I6.csv',I6)
 
 % STEP 7: GET ACOUSTIC EVENTS
 [AE,L] = get_acoustic_events(I6,I1,I2,I3);
+% csvout('AE1.csv',AE)
 
 % STEP 8: SEPARATE EVENTS THAT ARE TOO LARGE INTO SMALLER EVENTS
 AE2 = [];
 if (~isempty(AE)) % do this next step only if acoustic events have been detected
     AE2 = separate_large_AEs_areas(AE,L,big_area_thresh,I6,I1,I2,I3); % separate large area events into smaller events
 end
+% csvout('AE2.csv',AE2)
 
 % STEP 9: KEEP LARGE EVENTS ONLY
 AE3 = [];
 if (~isempty(AE2))
     AE3 = mode_small_area_threshold(AE2, small_area_thresh); % compute small area threshold for culling acoustic events & cull small events
 end
+% csvout('AE3.csv',AE3)
 
 % STEP 10: CONVERT ACOUSTIC EVENTS TO TIME AND FREQUENCY
 AE4 = [];
@@ -63,3 +72,8 @@ if (~isempty(AE3))
     AE4(:,4) = F([AE3(2,:) + AE3(4,:) - 1]);
 end
 showImage(c,I3,T,F,2,AE4);
+end
+
+function csvout(name, M)
+    dlmwrite(name, M, 'precision', 8)
+end
