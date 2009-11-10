9192f448c4a67f1678c8931e31714d4d8affebcd
Anthony Truskinger <a.truskinger@qut.edu.au>
2009-11-10 18:06:49 +1000

    Submit includes jire isssue (SENSOR-35)
    Also includes style changes to the Data.aspx pages
    As well as substantial amounts of javascript code commenting - after we establish its safe to remove it will be deleted.

---
+++ SharedCode/DesignTime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SharedCode
+{
+    public static class DesignTime
+    {
+
+        private static bool? _isInDesignMode;
+
+        /// <summary>
+        /// Gets a value indicating whether the control is in design mode (running in Blend
+        /// or Visual Studio).
+        /// </summary>
+        public static bool IsInDesignModeStatic
+        {
+            get
+            {
+                if (!_isInDesignMode.HasValue)
+                {
+#if SILVERLIGHT
+                    _isInDesignMode = DesignerProperties.IsInDesignTool;
+#else
+            var prop = DesignerProperties.IsInDesignModeProperty;
+            _isInDesignMode
+                = (bool)DependencyPropertyDescriptor
+                .FromProperty(prop, typeof(FrameworkElement))
+                .Metadata.DefaultValue;
+#endif
+                }
+
+                return _isInDesignMode.Value;
+            }
+        }
+    }
+}
