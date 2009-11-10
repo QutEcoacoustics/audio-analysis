3a82675c070e42d03e181def286f7b87d8f8761d
Anthony Truskinger <a.truskinger@qut.edu.au>
2009-11-10 13:59:09 +1000

    Original hash: cd64442441f9ac6c983cf7210aad3358e6c22507

---
+++ SharedCode/CanvasHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace System.Windows.Controls
+{
+    public static class SelectionHelpers
+    {
+        public static double Top(this FrameworkElement frameworkElement)
+        {
+            return Canvas.GetTop(frameworkElement);
+        }
+        public static double Left(this FrameworkElement frameworkElement)
+        {
+            return Canvas.GetLeft(frameworkElement);
+        }
+        public static double Bottom(this FrameworkElement frameworkElement)
+        {
+            return Top(frameworkElement) + frameworkElement.ActualHeight;
+        }
+        public static double Right(this FrameworkElement frameworkElement)
+        {
+            return Left(frameworkElement) + frameworkElement.ActualWidth;
+        }
+        public static Rect Dimensions(this FrameworkElement frameworkElement)
+        {
+            return new Rect(frameworkElement.Left(), frameworkElement.Top(), frameworkElement.ActualWidth, frameworkElement.ActualHeight);
+        }
+     
+    }
+}
