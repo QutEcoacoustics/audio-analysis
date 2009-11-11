bda5916ef4d8e86849c912125a432715e38541f4
Michael Towsey <m.towsey@qut.edu.au>
2009-11-11 16:43:40 +1000

    Original hash: 371d9911b3fb1719d4235f21b7edab17a672a373

--- AudioAnalysis/AudioAnalysis/AcousticEvent.cs
+++ AudioAnalysis/AudioAnalysis/AcousticEvent.cs
@@ -8,27 +8,32 @@ namespace AudioAnalysis
 {
     public class AcousticEvent
     {
-        public static int    FreqBinCount;
-        public static double FreqBinWidth;
-        public static double FrameDuration;
-        public static double FramesPerSecond;
 
-
-        //'Start Time (s)' 'Duration (s)' 'Lowest Freq' 'Highest Freq' 'I1 Mean dB' 'I1 Var dB' 'I2 Mean dB' 'I2 Var dB' 'I3 Mean dB' 'I3 Var dB'
-
+        //DIMENSIONS OF THE EVENT
         public double StartTime { get; set; } // (s),
-        public double Duration; // (s),
-        public int    MinFreq;  //,
-        public int    MaxFreq;  //,
+        public double Duration; // in secondss
+        public int    MinFreq;  //
+        public int    MaxFreq;  //
+        public int    FreqRange { get { return(MaxFreq - MinFreq + 1); } }
+        public bool   IsMelscale { get; set; } 
+        public Oblong oblong { get; private set;}
+
+        public int FreqBinCount { get; private set;}     //required for conversions to & from MEL scale
+        public double FreqBinWidth { get; private set; } //required for freq-binID conversions
+        public double FrameDuration;    //frame duration in seconds
+        public double FrameOffset;      //time between frame starts in seconds
+        public double FramesPerSecond;  //inverse of the frame offset
+
+
+        //PROPERTIES OF THE EVENTS I.E. SCORE ETC
+        public double Score { get; set; }
         //double I1MeandB; //mean intensity of pixels in the event prior to noise subtraction 
-        //double I1VardB;  //,
+        //double I1Var;  //,
         //double I2MeandB; //mean intensity of pixels in the event after Wiener filter, prior to noise subtraction 
-        //double I2VardB;  //,
+        //double I2Var;  //,
         double I3Mean;   //mean intensity of pixels in the event AFTER noise reduciton 
         double I3Var;    //variance of intensity of pixels in the event.
-        public int FreqRange { get { return(MaxFreq - MinFreq + 1); } }
 
-        public Oblong oblong { get; private set;} 
 
                 /// <summary>
         /// CONSTRUCTOR
@@ -39,87 +44,98 @@ namespace AudioAnalysis
             this.Duration = duration;
             this.MinFreq = (int)minFreq;
             this.MaxFreq = (int)maxFreq;
+            this.IsMelscale = false;
             oblong = null;// have no info to convert time/Hz values to coordinates
         }
 
+        ///// <summary>
+        ///// CONSTRUCTOR
+        ///// </summary>
+        //public AcousticEvent(double startTime, double duration, double minFreq, double maxFreq, bool doMelscale)
+        //{
+        //    this.StartTime = startTime;
+        //    this.Duration = duration;
+        //    this.MinFreq = (int)minFreq;
+        //    this.MaxFreq = (int)maxFreq;
+        //    this.IsMelscale = doMelscale;
+
+        //    //first check that the static variables required to initialise an oblong object have been initialised.
+        //    CheckIfStaticsAreInitialised();
+        //    this.oblong   = ConvertEvent2Oblong();
+        //}
+
         /// <summary>
-        /// CONSTRUCTOR
+        /// This constructor works ONLY for linear Herz scale events
         /// </summary>
-        public AcousticEvent(double startTime, double duration, double minFreq, double maxFreq, bool doMelscale)
+        /// <param name="o"></param>
+        /// <param name="binWidth"></param>
+        public AcousticEvent(Oblong o, double frameOffset, double binWidth)
         {
-            this.StartTime = startTime;
-            this.Duration = duration;
-            this.MinFreq = (int)minFreq;
-            this.MaxFreq = (int)maxFreq;
-
-            //first check that the static variables required to initialise an oblong object have been initialised.
-            CheckIfStaticsAreInitialised();
-
-
-            //translate time/freq dimensions to coordinates in a matrix.
-            //columns of matrix are the freq bins. Origin is top left - as per matrix in the sonogram class.
-            //First translate time dimension = frames = matrix rows.
-            int topRow; int bottomRow;
-            Time2RowIDs(startTime, duration, out topRow, out bottomRow);
-
-            //Second translate freq dimension = freq bins = matrix columns.
-            int leftCol; int rightCol;
-            Freq2BinIDs(doMelscale, this.MinFreq, this.MaxFreq, out leftCol, out rightCol);
-
-            this.oblong   = new Oblong(topRow, leftCol, bottomRow, rightCol);
-        }
-
-        public AcousticEvent(Oblong o)
-        {
-            //first check that the static variables required to initialise an oblong object have been initialised.
-            CheckIfStaticsAreInitialised();
-
-            this.oblong = o;
+            this.oblong       = o;
+            this.FreqBinWidth = binWidth;
+            this.FrameOffset  = frameOffset;
+            this.IsMelscale   = false;
 
             double startTime; double duration;
-            RowIDs2Time(o.r1, o.r2, out startTime, out duration);
+            RowIDs2Time(o.r1, o.r2, frameOffset, out startTime, out duration);
             this.StartTime = startTime;
             this.Duration = duration;
             int minF; int maxF;
-            BinIDs2Freq(o.c1, o.c2, out minF, out maxF);
+            HerzBinIDs2Freq(o.c1, o.c2, binWidth, out minF, out maxF);
             this.MinFreq = minF;
             this.MaxFreq = maxF;
         }
 
-
-        private void CheckIfStaticsAreInitialised()
-        {            
-            if (AcousticEvent.FreqBinCount == 0)
-            {
-                Console.WriteLine("WARNING!! ######## Frequency bin count has not been set");
-                throw new Exception("FATAL ERROR - TERMINATE");
-            }
-            if (AcousticEvent.FreqBinWidth == 0.0)
-            {
-                Console.WriteLine("WARNING!! ######## Frequency bin width (Herz) has not been set");
-                throw new Exception("FATAL ERROR - TERMINATE");
-            }
-            if (AcousticEvent.FrameDuration == 0.0)
-            { 
-                Console.WriteLine("WARNING!! ######## Frame duration (seconds)   has not been set");
-                throw new Exception("FATAL ERROR - TERMINATE");
-            }
-        }
-    
-        /// <summary>
-        /// sets static variables
-        /// WARNING!! ASSUMES THAT FRAMES ARE OVERLAPPED 50% ##############################################################################
-        ///           NEED TO REWRITE IF THIS IS NOT THE CASE.
-        /// </summary>
-        /// <param name="samplingRate"></param>
-        /// <param name="windowSize"></param>
-        public static void SetStaticVariables(int samplingRate, int windowSize)
+        public void DoMelScale(bool doMelscale, int freqBinCount)
         {
-            AcousticEvent.FrameDuration   = windowSize / (double)samplingRate;
-            AcousticEvent.FramesPerSecond = 2 * samplingRate / (double)windowSize; //twice the expected because frames overlap 50%
-            AcousticEvent.FreqBinCount    = windowSize / 2;
-            AcousticEvent.FreqBinWidth    = samplingRate / (double)windowSize;
+            this.IsMelscale   = doMelscale;
+            this.FreqBinCount = freqBinCount;
         }
+
+        public void SetTimeAndFreqScales(int samplingRate, int windowSize, int windowOffset)
+        {
+            double frameDuration, frameOffset, framesPerSecond;
+            CalculateTimeScale(samplingRate, windowSize, windowOffset,
+                                         out frameDuration, out frameOffset, out framesPerSecond);
+            this.FrameDuration  = frameDuration;    //frame duration in seconds
+            this.FrameOffset    = frameOffset;
+            this.FramesPerSecond= framesPerSecond;  //inverse of the frame offset
+
+            int binCount;
+            double binWidth;
+            CalculateFreqScale(samplingRate, windowSize, out binCount, out binWidth);
+            this.FreqBinCount = binCount; //required for conversions to & from MEL scale
+            this.FreqBinWidth = binWidth; //required for freq-binID conversions
+
+            if (this.oblong == null) this.oblong = ConvertEvent2Oblong();
+
+        }
+
+
+        public Oblong ConvertEvent2Oblong()
+        {
+            //translate time/freq dimensions to coordinates in a matrix.
+            //columns of matrix are the freq bins. Origin is top left - as per matrix in the sonogram class.
+            //Translate time dimension = frames = matrix rows.
+            int topRow; int bottomRow;
+            Time2RowIDs(this.StartTime, this.Duration, this.FrameOffset, out topRow, out bottomRow);
+
+            //Translate freq dimension = freq bins = matrix columns.
+            int leftCol; int rightCol;
+            Freq2BinIDs(this.IsMelscale, this.MinFreq, this.MaxFreq, this.FreqBinCount, this.FreqBinWidth, out leftCol, out rightCol);
+
+            return new Oblong(topRow, leftCol, bottomRow, rightCol);
+        }
+
+        public string WriteProperties()
+        {
+            return " min-max=" + MinFreq + "-" + MaxFreq + ",  " + oblong.c1 + "-" + oblong.c2;
+        }
+
+
+
+        //#################################################################################################################
+        //METHODS TO CONVERT BETWEEN FREQ BIN AND HERZ OR MELS 
 
         /// <summary>
         /// converts frequency bounds of an event to left and right columns of object in sonogram matrix
@@ -128,57 +144,62 @@ namespace AudioAnalysis
         /// <param name="maxF"></param>
         /// <param name="leftCol"></param>
         /// <param name="rightCol"></param>
-        public static void Freq2BinIDs(bool doMelscale, int minFreq, int maxFreq, out int leftCol, out int rightCol)
+        public static void Freq2BinIDs(bool doMelscale, int minFreq, int maxFreq, int binCount, double binWidth,
+                                                                                              out int leftCol, out int rightCol)
         {
-            Freq2BinIDs(doMelscale, minFreq, maxFreq, AcousticEvent.FreqBinCount, AcousticEvent.FreqBinWidth, out leftCol, out rightCol);
+            if(doMelscale)
+                Freq2MelsBinIDs(minFreq, maxFreq, binWidth, binCount, out leftCol, out rightCol);
+            else
+                Freq2HerzBinIDs(minFreq, maxFreq, binWidth, out leftCol, out rightCol);
         }
-        public static void Freq2BinIDs(bool doMelscale, int minFreq, int maxFreq, int binCount, double binWidth, 
-            out int leftCol, out int rightCol)
+        public static void Freq2HerzBinIDs(int minFreq, int maxFreq, double binWidth, out int leftCol, out int rightCol)
         {
             leftCol  = (int)Math.Round(minFreq / binWidth);
             rightCol = (int)Math.Round(maxFreq / binWidth);
-            if (rightCol >= binCount) rightCol = binCount - 1;
-
-            if (doMelscale) //convert min max Hz to mel scale
-            {
-                double nyquistFrequency = binCount * binWidth;
-                double maxMel = Speech.Mel(binCount * binWidth); // the Nyquist Frequency
-                int melRange  = (int)(maxMel - 0 + 1);
-                double pixelPerMel = binCount / (double)melRange;
-                leftCol  = (int)Math.Round((double)Speech.Mel(minFreq) * pixelPerMel);
-                rightCol = (int)Math.Round((double)Speech.Mel(maxFreq) * pixelPerMel);
-            }
-
         }
+        public static void Freq2MelsBinIDs(int minFreq, int maxFreq, double binWidth, int binCount, out int leftCol, out int rightCol)
+        {
+                double nyquistFrequency = binCount * binWidth;
+                double maxMel = Speech.Mel(nyquistFrequency); 
+                int melRange = (int)(maxMel - 0 + 1);
+                double binsPerMel = binCount / (double)melRange;
+                leftCol  = (int)Math.Round((double)Speech.Mel(minFreq) * binsPerMel);
+                rightCol = (int)Math.Round((double)Speech.Mel(maxFreq) * binsPerMel);
+        }
+
         /// <summary>
         /// converts left and right column IDs to min and max frequency bounds of an event
-        /// WARNING!!! ONLY WORKS FOR LINEAR FREQ SCALE> NEED TO REWRITE FOR MEL SCALE ##############################################
+        /// WARNING!!! ONLY WORKS FOR LINEAR HERZ SCALE. NEED TO WRITE ANOTHER METHOD FOR MEL SCALE ############################
         /// </summary>
         /// <param name="leftCol"></param>
         /// <param name="rightCol"></param>
         /// <param name="minFreq"></param>
         /// <param name="maxFreq"></param>
-        public static void BinIDs2Freq(int leftCol, int rightCol, out int minFreq, out int maxFreq)
+        public static void HerzBinIDs2Freq(int leftCol, int rightCol, double binWidth, out int minFreq, out int maxFreq)
         {
-            minFreq = (int)Math.Round(leftCol  * FreqBinWidth);
-            maxFreq = (int)Math.Round(rightCol * FreqBinWidth);
+            minFreq = (int)Math.Round(leftCol * binWidth);
+            maxFreq = (int)Math.Round(rightCol * binWidth);
             //if (doMelscale) //convert min max Hz to mel scale
             //{
             //}
         }
 
-        public static void RowIDs2Time(int topRow, int bottomRow, out double startTime, out double duration)
+
+        
+        
+        //#################################################################################################################
+        //METHODS TO CONVERT BETWEEN TIME BIN AND SECONDS 
+        public static void RowIDs2Time(int topRow, int bottomRow, double frameOffset, out double startTime, out double duration)
         {
-            startTime  = topRow        * AcousticEvent.FrameDuration;
-            double end = (bottomRow+1) * AcousticEvent.FrameDuration;
+            startTime  = topRow * frameOffset;
+            double end = (bottomRow + 1) * frameOffset;
             duration = end - startTime;
         }
 
-        public static void Time2RowIDs(double startTime, double duration, out int topRow, out int bottomRow)
+        public static void Time2RowIDs(double startTime, double duration, double frameOffset, out int topRow, out int bottomRow)
         {
-            topRow    = (int)Math.Round(startTime / AcousticEvent.FrameDuration);
-            bottomRow = (int)Math.Round((startTime + duration) / AcousticEvent.FrameDuration);
-            //if (topRow < 0) topRow = 0;
+            topRow = (int)Math.Round(startTime / frameOffset);
+            bottomRow = (int)Math.Round((startTime + duration) / frameOffset);
         }
 
         public void SetNetIntensityAfterNoiseReduction(double mean, double var)
@@ -187,10 +208,28 @@ namespace AudioAnalysis
             this.I3Var  = var;  //
         }
 
-        public string WriteProperties()
+        /// <summary>
+        /// returns the frame duration and offset duration in seconds
+        /// </summary>
+        /// <param name="samplingRate"></param>
+        /// <param name="windowSize"></param>
+        /// <param name="windowOffset"></param>
+        /// <param name="frameDuration">units = seconds</param>
+        /// <param name="frameOffset">units = seconds</param>
+        /// <param name="framesPerSecond"></param>
+        public static void CalculateTimeScale(int samplingRate, int windowSize, int windowOffset,
+                                                        out double frameDuration, out double frameOffset, out double framesPerSecond)
         {
-            return " min-max=" + MinFreq + "-" + MaxFreq + ",  " + oblong.c1 + "-" + oblong.c2;
+            frameDuration = windowSize / (double)samplingRate;
+            frameOffset = windowOffset / (double)samplingRate;
+            framesPerSecond = 1 / frameOffset;
         }
+        public static void CalculateFreqScale(int samplingRate, int windowSize, out int binCount, out double binWidth)
+        {
+            binCount = windowSize / 2;
+            binWidth = samplingRate / (double)windowSize; //= Nyquist / binCount
+        }
+
 
     }
 }
