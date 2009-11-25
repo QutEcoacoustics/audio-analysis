6a27cb116a5d481bbe23eba61e7cda6a506b57b6
Bradly Clow <bradly.clow@qut.edu.au>
2009-11-25 16:44:32 +1000

    SPT Original hash: b50b3bb9af940cf17d26d31aa5c8a80ca9960484

---
+++ AudioAnalysis/AED/AED/SpectralPeakTrack.fs
@@ -0,0 +1,28 @@
+﻿module QutSensors.AudioAnalysis.AED.SpectralPeakTrack    
+
+open Matlab
+
+(* TODO idea for generalisation
+let byCol f (m:matrix) = 
+    let m' = Math.Matrix.zero m.NumRows m.NumCols
+    let g j i e = m'.[i,j] <- e
+    for j=0 to m.NumCols do
+        m.Column j |> f (g j)
+    done
+    m'
+    
+let verticalPeaks' m t =
+    let f g v =
+        let a = Math.Vector.toArray v
+        smooth a 3 |> findPeaks |> List.iter (fun i -> if a.[i] > t then g i a.[i])
+    byCol f m
+*)
+
+let verticalPeaks (m:matrix) t =
+    let m' = Math.Matrix.zero m.NumRows m.NumCols
+    for j=0 to m.NumCols do
+        let a = m.Column j |> Math.Vector.toArray
+        smooth a 3 |> findPeaks |> List.iter (fun i -> if a.[i] > t then m'.[i,j] <- a.[i])
+    done
+    m'
+
