b71975ef7553b9909780645dfdc55f211025dc65
Bradly Clow <bradly.clow@qut.edu.au>
2009-12-02 16:42:15 +1000

    SPT Original hash: 5be6be78c8207db6644f5a513d73c4bd16a0e42d

--- AudioAnalysis/AED/AED/SpectralPeakTrack.fs
+++ AudioAnalysis/AED/AED/SpectralPeakTrack.fs
@@ -2,27 +2,38 @@
 
 open Matlab
 
-(* TODO idea for generalisation
-let byCol f (m:matrix) = 
-    let m' = Math.Matrix.zero m.NumRows m.NumCols
-    let g j i e = m'.[i,j] <- e
-    for j=0 to m.NumCols do
-        m.Column j |> f (g j)
-    done
-    m'
-    
-let verticalPeaks' m t =
-    let f g v =
-        let a = Math.Vector.toArray v
-        smooth a 3 |> findPeaks |> List.iter (fun i -> if a.[i] > t then g i a.[i])
-    byCol f m
-*)
+// TODO move to Util
+let mapByCol f (m:matrix) = seq{m.NumCols-1..-1..0} |> Seq.fold (fun z j -> f (m.Column j) ::z) [] |> Math.Vector.Generic.ofList
 
 let verticalPeaks t (m:matrix) =
-    let m' = Math.Matrix.zero m.NumRows m.NumCols
-    for j=0 to (m.NumCols-1) do
-        let s = m.Column j |> Math.Vector.toArray |> smooth 3
-        findPeaks s |> List.iter (fun i -> if s.[i] > t then m'.[i,j] <- s.[i])
-    done
-    m'
+    let f c =
+        let s = Math.Vector.toArray c |> smooth 3
+        (s, findPeaks s |> List.filter (fun i -> s.[i] > t) |> Set.ofList)
+    mapByCol f m
+   
+let indexMaxNeighbour i ni (m:matrix) nj =
+    if Set.isEmpty ni then None else 
+        let s = Set.intersect ni (Set.ofSeq (seq{i-2..i+2}))
+        if Set.isEmpty s then None else
+            Set.map (fun e -> (e, m.[e,nj])) s |> Set.fold (fun (zi,zm) (i,m) -> if m > zm then (i,m) else (zi,zm)) (0,0.0) |> fst |> Some
+            // TODO check if this pattern of finding the max of one side of a tuple is already implemented
 
+// TODO modify this to be: verticalTracks ps:float [] * int Set Vector
+let verticalTracks m (ps:int Set List) =
+    let f (nj, prevRes, r) (this, next) =
+        let g (thisRes, nextRes) i = indexMaxNeighbour i next m nj
+                                        |> Option.fold (fun (tr,nr) j -> (Set.add i tr, Set.add j nr)) (thisRes, nextRes)
+        let (tr,nr) = Set.fold g (prevRes, Set.empty) this
+        (nj+1, nr, tr::r)
+    let (_, last, r) = Seq.fold f (1, Set.empty, []) (Seq.pairwise ps)
+    last::r |> List.rev
+
+let colIndicesToMatrix m n (ps:int Set seq) =
+        let m = Math.Matrix.zero m n
+        Seq.iteri (fun j -> Set.iter (fun i -> m.[i,j] <- 1.0)) ps
+        m
+        
+let peakTracks m =
+    let vp = verticalPeaks 9.0 m
+    //let vt = verticalTracks vp
+    ()
--- AudioAnalysis/AED/Test/SpectralPeakTrackTest.fs
+++ AudioAnalysis/AED/Test/SpectralPeakTrackTest.fs
@@ -6,6 +6,28 @@ open Xunit
 
 [<Fact>]
 let testVerticalPeaks () = 
-    let i3c = loadTestFile2 "SPT" "I3.csv" |> verticalPeaks 9.0
+    let v = loadTestFile2 "SPT" "I3.csv" |> verticalPeaks 9.0
+    let f i j = let (s, is) = v.[j] in if Set.contains i is then s.[i] else 0.0
+    let i3c = Math.Matrix.init (Array.length (fst v.[0])) v.Length f
     let i3cm = loadTestFile2 "SPT" "I3c.csv" 
     Assert.True (matrixFloatEquals i3cm i3c 0.0001)
+    
+[<Fact>]
+let testIndexMaxNeighbour () =
+    let m = Math.Matrix.ofList [[9.0; 10.0; 0.0]; [0.0; 11.0; 3.0]]
+    Assert.Equal(None, indexMaxNeighbour 0 Set.empty m 1)
+    Assert.Equal(Some 1, indexMaxNeighbour 0 (Set.ofList [0; 1]) m 1)
+    
+(* TODO Try FsCheck. The following properties should hold
+0 <= i < m.NumRows
+1 <= nj < m.numCols
+Set.isEmpty ni ==> indexMaxNeighbour i ni m nj == None
+indexMaxNeighbour i ni m nj == None ==> Set.isEmpty ni || forall x in ni |i-x| > 2
+*)  
+
+[<Fact>]
+let testVerticalTracks () =
+    let m = Math.Matrix.ofList [[9.0; 10.0; 0.0]; [0.0; 11.0; 3.0]]
+    Assert.Equal(List.replicate 3 Set.empty, verticalTracks m (List.replicate 3 Set.empty))
+    Assert.Equal([Set.ofList [0]; Set.ofList [1]; Set.empty], verticalTracks m [Set.ofList [0]; Set.ofList [0; 1]; Set.empty])
+    
