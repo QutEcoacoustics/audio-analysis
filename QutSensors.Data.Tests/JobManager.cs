fdb9b224e14868c947d42688118bc745e2618957
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-01-19 11:08:54 +1000

    Audio file uploads using intermediate file. Some code refactoring.

--- QutSensors.Data.Tests/JobManager.cs
+++ QutSensors.Data.Tests/JobManager.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AudioAnalysis;
 
 namespace QutSensors.Data.Tests
 {
