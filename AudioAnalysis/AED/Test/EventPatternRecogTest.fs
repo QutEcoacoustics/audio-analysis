e39aaba5b4cb578a971ba59c08e1475b547020d3
Bradly Clow <bradly.clow@qut.edu.au>
2010-01-29 11:29:52 +1000

    ground parrot template was updated and added some debugging output in the test

--- AudioAnalysis/AED/Test/EventPatternRecogTest.fs
+++ AudioAnalysis/AED/Test/EventPatternRecogTest.fs
@@ -1,23 +1,28 @@
 ﻿module EventPatternRecogTest
 
+open Common
 open QutSensors.AudioAnalysis.AED.EventPatternRecog
 open QutSensors.AudioAnalysis.AED.Util
 open Xunit
 
 
 // TODO copied and modified from AED test - generalise
-let loadTestFile2 f i j = fileToMatrix (@"matlab\" + f) i j |> Math.Matrix.ofArray2D
-
-// TODO copied and modified from AED test - generalise
 // Format: time start, time duration, freq start, freq end
-let loadEventsFile f m n =
-    let aem = loadTestFile2 f m n
-    seq {for i in 0..(m-1) -> {Left=aem.[i,0]; Right=aem.[i,0]+aem.[i,1]; Bottom=aem.[i,2]; Top=aem.[i,3]}}
+let loadEventsFile f md =
+    let aem = loadTestFile2 md.Dir f 
+    seq {for i in 0..(aem.NumCols-1) -> {Left=aem.[0,i]; Right=aem.[0,i]+aem.[1,i]; Bottom=aem.[2,i]; Top=aem.[3,i]}}
 
 [<Fact>]
 let detectGroundParrotsTest () = 
-    let ae = loadEventsFile "GroundParrotAE.txt" 811 10
-    let m = loadEventsFile "GroundParrotResults.txt" 10 4 |> Seq.sort
-    Assert.Equal(m, detectGroundParrots ae |> Seq.sort)
-    //let r = detectGroundParrots t ae |> Seq.sort
-    //Assert.True(false, sprintf "\nmatlab: %A\n\n F#: %A" (Seq.nth 9 m) (Seq.nth 9 r))
+    let md = GParrots_JB2_20090607_173000_wav_minute_3
+    let ae = loadEventsFile "EPRAE.csv" md
+    let m = loadEventsFile "EPRresults.csv" md |> Seq.sort
+    //Assert.Equal(m, detectGroundParrots ae |> Seq.sort)
+    let r = detectGroundParrots ae |> Seq.sort
+    
+    let toString r = sprintf "%f, %f, %f, %f" r.Left r.Right r.Bottom r.Top
+    let s = sprintf "\r\nmatlab %i, F# %i\r\n" (Seq.length m) (Seq.length r)
+    let f (m,r) = if m = r then "match" else sprintf "\r\nmatlab:\t %s \r\nF#:\t %s" (toString m) (toString r)
+    let l = sprintf "\r\n\r\nF# (9):\t %s\r\n" (toString (Seq.nth 8 r))
+    
+    Assert.True(false, s + (Seq.zip m r |> Seq.map f |> String.concat "\r\n") + l)
