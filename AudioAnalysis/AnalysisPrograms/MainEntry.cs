3e4ac858e29f4e8c914f0cff8622635af6d0f642
Bradly Clow <bradly.clow@qut.edu.au>
2010-02-18 10:56:57 +1000

    Introducing single project Main class, that switches based on the first argument Original hash: 07ef6c1778bc52fd18664751836225899a19da56

---
+++ AudioAnalysis/AnalysisPrograms/MainEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnalysisPrograms
+{
+    class MainEntry
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("usage");
+            }
+            else
+            {
+                string[] r = args.Skip(1).ToArray();
+                switch (args[0])
+                {
+                    case "groundparrot":
+                        Main_EPR.Main(r);
+                        break;
+                    case "snr":
+                        Main_SNR.Main(r);
+                        break;
+                    default:
+                        Console.WriteLine("unrecognised");
+                        break;
+                }
+            }
+        }
+    }
+}
