847900efd5894d3b0e2022691bfc30c4581d1aaf
Bradly Clow <bradly.clow@qut.edu.au>
2010-02-19 14:03:26 +1000

    added timestamp prefix to methods that writeline

--- AudioAnalysis/TowseyLib/Log.cs
+++ AudioAnalysis/TowseyLib/Log.cs
@@ -4,12 +4,10 @@ using System.Text;
 
 namespace TowseyLib
 {
-
-
-
 	public static class Log
 	{
 		public static int Verbosity = 0;
+        public static string mesgFormat = "{0:yyyy-MM-dd HH:mm:ss.fff}  {1}";
 
 		public static void Write(string format, params object[] args)
 		{
@@ -20,23 +18,23 @@ namespace TowseyLib
 		public static void WriteLine(string format, params object[] args)
 		{
 #if LOGTOCONSOLE
-			Console.WriteLine(format, args);
+            Console.WriteLine(mesgFormat, DateTime.Now, String.Format(format, args));
 #endif
 		}
 
 		public static void WriteLine(object o)
 		{
 #if LOGTOCONSOLE
-			Console.WriteLine(o);
+            Console.WriteLine(mesgFormat, DateTime.Now, o);
 #endif
 		}
 
 		public static void WriteIfVerbose(string format, params object[] args)
 		{
 #if LOGTOCONSOLE
-			if (Verbosity > 0)
-				Console.WriteLine(format, args);
+            if (Verbosity > 0)
+                Console.WriteLine(mesgFormat, DateTime.Now, String.Format(format, args));
 #endif
 		}
-	} //end class Log()
+	}
 }
