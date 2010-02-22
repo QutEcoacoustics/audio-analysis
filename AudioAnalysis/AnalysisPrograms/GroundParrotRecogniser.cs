2ce042eae001ad70c3c4b0c5e6ac9c5809cab21b
Bradly Clow <bradly.clow@qut.edu.au>
2010-02-22 16:35:21 +1000

    GroundParrotRecogniser now calls AED instead of duplicating Original hash: a5e643f0e5ba4942d60a559cd552743cb33dde90

--- AudioAnalysis/AnalysisPrograms/GroundParrotRecogniser.cs
+++ AudioAnalysis/AnalysisPrograms/GroundParrotRecogniser.cs
@@ -48,43 +48,28 @@ namespace AnalysisPrograms
         
         public static System.Tuple<AudioRecording, BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath)
         {
-            AudioRecording recording = new AudioRecording(wavFilePath);
-            if (recording.SampleRate != 22050) recording.ConvertSampleRate22kHz(); // TODO this will be common
-
-            SonogramConfig config = SonogramConfig.Load(appConfigPath);
-            config.NoiseReductionType = ConfigKeys.NoiseReductionType.NONE;
-            BaseSonogram sonogram = new SpectralSonogram(config, recording.GetWavReader());
-            // TODO the whole section to here will be common with other analysis
-
-            Log.WriteLine("AED start");
-            IEnumerable<Oblong> oblongs = AcousticEventDetection.detectEvents(3.0, 100, sonogram.Data);
-            Log.WriteLine("AED finished");
-
-            //get the time and freq scales
-            double freqBinWidth = config.FftConfig.NyquistFreq / (double)config.FreqBinCount;
-            double frameOffset = config.GetFrameOffset();
+            var aed = AED.detect(appConfigPath, wavFilePath, 3.0, 100);
 
             var events = new List<Util.Rectangle<double>>();
-            foreach (Oblong o in oblongs)
-            {
-                var e = new AcousticEvent(o, frameOffset, freqBinWidth); //this constructor assumes linear Herz scale events 
-                events.Add(Util.fcornersToRect(e.StartTime, e.EndTime, e.MaxFreq, e.MinFreq));
-                //Console.WriteLine(e.StartTime + "," + e.Duration + "," + e.MinFreq + "," + e.MaxFreq);
-            }
-            Log.WriteIfVerbose("AED # events: " + events.Count);
+            foreach (AcousticEvent ae in aed.Item3)
+                events.Add(Util.fcornersToRect(ae.StartTime, ae.EndTime, ae.MaxFreq, ae.MinFreq));
 
             Log.WriteLine("EPR start");
             IEnumerable<Util.Rectangle<double>> eprRects = EventPatternRecog.detectGroundParrots(events);
             Log.WriteLine("EPR finished");
 
+            var config = aed.Item2.Configuration;
+            var framesPerSec = 1 / config.GetFrameOffset(); // Surely this should go somewhere else
+            double freqBinWidth = config.FftConfig.NyquistFreq / (double)config.FreqBinCount; // TODO this is common with AED
+
             var eprEvents = new List<AcousticEvent>();
             foreach (Util.Rectangle<double> r in eprRects)
             {
                 var ae = new AcousticEvent(r.Left, r.Width, r.Bottom, r.Top);
-                ae.SetTimeAndFreqScales(1 / frameOffset, freqBinWidth);
+                ae.SetTimeAndFreqScales(framesPerSec, freqBinWidth);
                 eprEvents.Add(ae);
             }
-            return System.Tuple.Create(recording, sonogram, eprEvents);
+            return System.Tuple.Create(aed.Item1, aed.Item2, eprEvents);
         }
     }
 }
