4d3a57b5a167ea380a464dcf957502fd71e1c304
Bradly Clow <bradly.clow@qut.edu.au>
2010-02-23 16:38:26 +1000

    tidy up

--- AudioAnalysis/AnalysisPrograms/AED.cs
+++ AudioAnalysis/AnalysisPrograms/AED.cs
@@ -21,7 +21,7 @@ namespace AnalysisPrograms
             {
                 intensityThreshold = Default.intensityThreshold;
                 smallAreaThreshold = Default.smallAreaThreshold;
-                Log.WriteLine("Using AED defaults, intentsityThreshold={0}, smallAreaThreshold={1}", intensityThreshold, smallAreaThreshold);
+                Log.WriteIfVerbose("Using AED defaults");
             }
             else if (args.Length == 3)
             {
@@ -37,14 +37,14 @@ namespace AnalysisPrograms
                 Console.WriteLine("                    eg: \"trunk\\AudioAnalysis\\AED\\Test\\matlab\\BAC2_20071015-045040.wav\"");
                 Console.WriteLine("intensityThreshold: mandatory if smallAreaThreshold specified, otherwise default used");
                 Console.WriteLine("smallAreaThreshold: mandatory if intensityThreshold specified, otherwise default used");
-                System.Environment.Exit(1);
+                Environment.Exit(1);
             }
-                
+
+            string wavFilePath = args[0];
             string appConfigPath = ""; // TODO what is this for?
-            var result = detect(appConfigPath, args[0], Default.intensityThreshold, Default.smallAreaThreshold);
-            var recording = result.Item1;
-            var sonogram = result.Item2;
-            var events = result.Item3;
+            var result = detect(appConfigPath, wavFilePath, Default.intensityThreshold, Default.smallAreaThreshold);
+            var sonogram = result.Item1;
+            var events = result.Item2;
 
             Console.WriteLine();
             foreach (AcousticEvent ae in events)
@@ -52,27 +52,29 @@ namespace AnalysisPrograms
             Console.WriteLine();
 
             string outputFolder = @"C:\SensorNetworks\Output\";
-            Log.WriteIfVerbose("output folder =" + outputFolder);
-            string imagePath = Path.Combine(outputFolder, "RESULTS_" + Path.GetFileNameWithoutExtension(recording.FileName) + ".png");
+            string imagePath = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(wavFilePath) + ".png");
+            Log.WriteIfVerbose("imagePath = " + imagePath);
             var image = new Image_MultiTrack(sonogram.GetImage(false, true));
             //image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration));
             //image.AddTrack(Image_Track.GetWavEnvelopeTrack(recording, image.Image.Width));
             //image.AddTrack(Image_Track.GetSegmentationTrack(sonogram));
-            image.AddEvents(events); // Acoustic events fail to render on image as no Oblong field set
-            image.Save(outputFolder + recording.FileName + ".png");
+            image.AddEvents(events);
+            image.Save(imagePath);
             Log.WriteLine("Finished");
         }
 
-        public static System.Tuple<AudioRecording, BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath,
+        public static System.Tuple<BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath,
             double intensityThreshold, int smallAreaThreshold)
         {
+            Log.WriteLine("intensityThreshold = " + intensityThreshold);
+            Log.WriteLine("smallAreaThreshold = " + smallAreaThreshold);
+
             AudioRecording recording = new AudioRecording(wavFilePath);
             if (recording.SampleRate != 22050) recording.ConvertSampleRate22kHz(); // TODO this will be common
-
             SonogramConfig config = SonogramConfig.Load(appConfigPath);
             config.NoiseReductionType = ConfigKeys.NoiseReductionType.NONE;
             BaseSonogram sonogram = new SpectralSonogram(config, recording.GetWavReader());
-            // TODO the whole section to here will be common with other analysis
+            // TODO this whole section will be common with other analysis
 
             Log.WriteLine("AED start");
             IEnumerable<Oblong> oblongs = AcousticEventDetection.detectEvents(intensityThreshold, smallAreaThreshold, sonogram.Data);
@@ -84,7 +86,7 @@ namespace AnalysisPrograms
             foreach (Oblong o in oblongs)
                 events.Add(new AcousticEvent(o, config.GetFrameOffset(), freqBinWidth));
             Log.WriteIfVerbose("AED # events: " + events.Count);
-            return System.Tuple.Create(recording, sonogram, events);
+            return Tuple.Create(sonogram, events);
         }
     }
 }
--- AudioAnalysis/AnalysisPrograms/GroundParrotRecogniser.cs
+++ AudioAnalysis/AnalysisPrograms/GroundParrotRecogniser.cs
@@ -18,15 +18,16 @@ namespace AnalysisPrograms
             {
                 Console.WriteLine("Please supply a .wav recording as a command line argument.");
                 Console.WriteLine("Example: \"trunk\\AudioAnalysis\\Matlab\\EPR\\Ground Parrot\\GParrots_JB2_20090607-173000.wav_minute_3.wav\"");
+                Environment.Exit(1);
             }
             else
             {
                 Log.Verbosity = 1;
                 string appConfigPath = ""; // TODO what is this for?
-                var result = detect(appConfigPath, args[0]);
-                var recording = result.Item1;
-                var sonogram = result.Item2;
-                var eprEvents = result.Item3;
+                var wavFilePath = args[0];
+                var result = detect(appConfigPath, wavFilePath);
+                var sonogram = result.Item1;
+                var eprEvents = result.Item2;
 
                 Console.WriteLine();
                 foreach (AcousticEvent ae in eprEvents)
@@ -34,31 +35,32 @@ namespace AnalysisPrograms
                 Console.WriteLine();
 
                 string outputFolder = @"C:\SensorNetworks\Output\";
-                Log.WriteIfVerbose("output folder =" + outputFolder);
-                string imagePath = Path.Combine(outputFolder, "RESULTS_" + Path.GetFileNameWithoutExtension(recording.FileName) + ".png");
+                string imagePath = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(wavFilePath) + ".png");
+                Log.WriteIfVerbose("imagePath = " + imagePath);
                 var image = new Image_MultiTrack(sonogram.GetImage(false, true));
                 //image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration));
                 //image.AddTrack(Image_Track.GetWavEnvelopeTrack(recording, image.Image.Width));
                 //image.AddTrack(Image_Track.GetSegmentationTrack(sonogram));
-                image.AddEvents(eprEvents); // Acoustic events fail to render on image as no Oblong field set
-                image.Save(outputFolder + recording.FileName + ".png");
+                image.AddEvents(eprEvents);
+                image.Save(imagePath);
+
                 Log.WriteLine("Finished");                
             }
         }
         
-        public static System.Tuple<AudioRecording, BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath)
+        public static System.Tuple<BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath)
         {
             var aed = AED.detect(appConfigPath, wavFilePath, 3.0, 100);
 
             var events = new List<Util.Rectangle<double>>();
-            foreach (AcousticEvent ae in aed.Item3)
+            foreach (AcousticEvent ae in aed.Item2)
                 events.Add(Util.fcornersToRect(ae.StartTime, ae.EndTime, ae.MaxFreq, ae.MinFreq));
 
             Log.WriteLine("EPR start");
             IEnumerable<Util.Rectangle<double>> eprRects = EventPatternRecog.detectGroundParrots(events);
             Log.WriteLine("EPR finished");
 
-            var config = aed.Item2.Configuration;
+            var config = aed.Item1.Configuration;
             var framesPerSec = 1 / config.GetFrameOffset(); // Surely this should go somewhere else
             double freqBinWidth = config.FftConfig.NyquistFreq / (double)config.FreqBinCount; // TODO this is common with AED
 
@@ -69,7 +71,7 @@ namespace AnalysisPrograms
                 ae.SetTimeAndFreqScales(framesPerSec, freqBinWidth);
                 eprEvents.Add(ae);
             }
-            return System.Tuple.Create(aed.Item1, aed.Item2, eprEvents);
+            return Tuple.Create(aed.Item1, eprEvents);
         }
     }
 }
