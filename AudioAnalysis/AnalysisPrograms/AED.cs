773515520da42335de86d99c595a0cff01dbb33e
Bradly Clow <bradly.clow@qut.edu.au>
2010-02-23 10:31:20 +1000

    exposed AED parameters as command line arguments Original hash: b1b09dedeeddf3c030635285150b38b47ec58d25

--- AudioAnalysis/AnalysisPrograms/AED.cs
+++ AudioAnalysis/AnalysisPrograms/AED.cs
@@ -13,37 +13,54 @@ namespace AnalysisPrograms
     {
         public static void dev(string[] args)
         {
-            // TODO read intensityThreshold, smallAreaThreshold from args
-            if (args.Length == 0)
+            Log.Verbosity = 1;
+            double intensityThreshold;
+            int smallAreaThreshold;
+
+            if (args.Length == 1)
             {
-                Console.WriteLine("Please supply a .wav recording as a command line argument.");
-                Console.WriteLine("Example: \"trunk\\AudioAnalysis\\AED\\Test\\matlab\\BAC2_20071015-045040.wav\"");
+                intensityThreshold = Default.intensityThreshold;
+                smallAreaThreshold = Default.smallAreaThreshold;
+                Log.WriteLine("Using AED defaults, intentsityThreshold={0}, smallAreaThreshold={1}", intensityThreshold, smallAreaThreshold);
+            }
+            else if (args.Length == 3)
+            {
+                intensityThreshold = System.Convert.ToDouble(args[1]);
+                smallAreaThreshold = System.Convert.ToInt32(args[2]);
             }
             else
             {
-                Log.Verbosity = 1;
-                string appConfigPath = ""; // TODO what is this for?
-                var result = detect(appConfigPath, args[0], Default.intensityThreshold, Default.smallAreaThreshold);
-                var recording = result.Item1;
-                var sonogram = result.Item2;
-                var events = result.Item3;
-
-                Console.WriteLine();
-                foreach (AcousticEvent ae in events)
-                    Console.WriteLine(ae.StartTime + "," + ae.Duration + "," + ae.MinFreq + "," + ae.MaxFreq);
+                Console.WriteLine("The arguments for AED are: wavFile [intensityThreshold smallAreaThreshold]");
                 Console.WriteLine();
 
-                string outputFolder = @"C:\SensorNetworks\Output\";
-                Log.WriteIfVerbose("output folder =" + outputFolder);
-                string imagePath = Path.Combine(outputFolder, "RESULTS_" + Path.GetFileNameWithoutExtension(recording.FileName) + ".png");
-                var image = new Image_MultiTrack(sonogram.GetImage(false, true));
-                //image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration));
-                //image.AddTrack(Image_Track.GetWavEnvelopeTrack(recording, image.Image.Width));
-                //image.AddTrack(Image_Track.GetSegmentationTrack(sonogram));
-                image.AddEvents(events); // Acoustic events fail to render on image as no Oblong field set
-                image.Save(outputFolder + recording.FileName + ".png");
-                Log.WriteLine("Finished");
+                Console.WriteLine("wavFile:            path to .wav recording.");
+                Console.WriteLine("                    eg: \"trunk\\AudioAnalysis\\AED\\Test\\matlab\\BAC2_20071015-045040.wav\"");
+                Console.WriteLine("intensityThreshold: mandatory if smallAreaThreshold specified, otherwise default used");
+                Console.WriteLine("smallAreaThreshold: mandatory if intensityThreshold specified, otherwise default used");
+                System.Environment.Exit(1);
             }
+                
+            string appConfigPath = ""; // TODO what is this for?
+            var result = detect(appConfigPath, args[0], Default.intensityThreshold, Default.smallAreaThreshold);
+            var recording = result.Item1;
+            var sonogram = result.Item2;
+            var events = result.Item3;
+
+            Console.WriteLine();
+            foreach (AcousticEvent ae in events)
+                Console.WriteLine(ae.StartTime + "," + ae.Duration + "," + ae.MinFreq + "," + ae.MaxFreq);
+            Console.WriteLine();
+
+            string outputFolder = @"C:\SensorNetworks\Output\";
+            Log.WriteIfVerbose("output folder =" + outputFolder);
+            string imagePath = Path.Combine(outputFolder, "RESULTS_" + Path.GetFileNameWithoutExtension(recording.FileName) + ".png");
+            var image = new Image_MultiTrack(sonogram.GetImage(false, true));
+            //image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration));
+            //image.AddTrack(Image_Track.GetWavEnvelopeTrack(recording, image.Image.Width));
+            //image.AddTrack(Image_Track.GetSegmentationTrack(sonogram));
+            image.AddEvents(events); // Acoustic events fail to render on image as no Oblong field set
+            image.Save(outputFolder + recording.FileName + ".png");
+            Log.WriteLine("Finished");
         }
 
         public static System.Tuple<AudioRecording, BaseSonogram, List<AcousticEvent>> detect(string appConfigPath, string wavFilePath,
