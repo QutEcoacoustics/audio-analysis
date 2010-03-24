29b4ca93301fa9f4362a2a2fca838ec2dbdaf1da
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-03-24 16:07:40 +1000

    Direct Show improvements - should now be able to cope with most popular formats (when ffdshow is installed). Original hash: 2b439aaa5750f61bd7b65311c1cea483edb9048e

---
+++ AudioAnalysis/AnalysisPrograms/Processing/AudioConversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AudioTools;
+using System.IO;
+
+namespace AnalysisPrograms.Processing
+{
+    internal class AudioConversion
+    {
+        public static void Convert(string[] args)
+        {
+
+
+            string dir = args[0];
+            foreach (var path in Directory.GetFiles(dir))
+            {
+                Console.WriteLine("File: " + path);
+                
+                using (var reader = new WavReader(path))
+                {
+                    Console.WriteLine(reader.BitsPerSample);
+                    Console.WriteLine(reader.BytesPerSample);
+                    Console.WriteLine(reader.Channels);
+                    Console.WriteLine(reader.Epsilon);
+                    Console.WriteLine(reader.SampleRate);
+                    Console.WriteLine(reader.Samples.Length);
+                    Console.WriteLine(reader.Time);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
