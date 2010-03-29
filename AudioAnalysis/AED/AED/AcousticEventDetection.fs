1c5adfe4c5aa42b75f2dbd9ceb249a6e6e1525d3
Bradly Clow <bradly.clow@qut.edu.au>
2010-03-29 14:21:07 +1000

    speed up Original hash: 270671926a62df8982bd6f6d2d69a2b8d076d0e5

--- AudioAnalysis/AED/AED/AcousticEventDetection.fs
+++ AudioAnalysis/AED/AED/AcousticEventDetection.fs
@@ -34,22 +34,24 @@ let joinVerticalLines = mTranspose << joinHorizontalLines << mTranspose
     
 let aeToMatrix ae =
     let r = ae.Bounds
-    Math.Matrix.init r.Height r.Width (fun i j -> if ae.Elements.Contains (r.Top + i, r.Left + j) then 1.0 else 0.0)
+    let m = Math.Matrix.zero (height r) (width r)
+    Set.iter (fun (i,j) -> m.[i-(top r), j-(left r)] <- 1.0) ae.Elements
+    m
 
 let separateLargeEvents aes =
     let areat = 3000
+    let freqt = 20.0
+    let timet = 100.0 / 3.0
     let f ae =
-        let freqt = 20.0
         let m = aeToMatrix ae
-        let s = sumRows m |> Seq.map (fun x -> x / (float) m.NumCols * 100.0 <= freqt) 
-        let m1 = Math.Matrix.mapi (fun i _ x -> if Seq.nth i s then 0.0 else x) m
+        let s = sumRows m |> Math.Vector.toArray |> Array.map (fun x -> x / (float) m.NumCols * 100.0 <= freqt) 
+        let m1 = Math.Matrix.mapi (fun i _ x -> if s.[i] then 0.0 else x) m
         let rs = getAcousticEvents m1
                  |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in lengthsToRect (left b1 + left b2) (top b1 + top b2) (width b2) (height b2))
-        let timet = 100.0 / 3.0
         let m2 = m - m1
         rs @ (getAcousticEvents m2
               |> List.filter (fun x -> (float) x.Bounds.Height * 100.0 / (float) m2.NumRows >= timet)
-              |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in lengthsToRect (left b1 + left b2) (top b1) (width b2) (height b1)))
+              |> List.map (fun x -> let b1, b2 = ae.Bounds, x.Bounds in lengthsToRect (left b1 + left b2) (top b1) (width b2) (height b1)))         
     Seq.collect (fun ae -> if area ae.Bounds < areat then [ae.Bounds] else f ae) aes
 
 let smallFirstMin cs h t =
