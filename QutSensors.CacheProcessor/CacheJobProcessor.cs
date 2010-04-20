4fa9811abd828cc67ca9a6eb9ffd6f404c2c96b6
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-04-20 13:18:42 +1000

    More web UI pages. Some organisation of Business and Data projects.

--- QutSensors.CacheProcessor/CacheJobProcessor.cs
+++ QutSensors.CacheProcessor/CacheJobProcessor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using QutSensors.Data.Cache;
+
 using QutSensors.Data.Linq;
 using AudioTools;
 using AudioTools.DirectShow;
