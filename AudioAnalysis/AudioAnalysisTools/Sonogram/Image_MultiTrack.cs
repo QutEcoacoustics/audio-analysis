e859c950300557dbcfae5aa3cacf2f02c8ea4c11
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-06-18 17:15:39 +1000

    SegmentedDraw factored out. Fixes to spectrogram generation. Original hash: 5cbe8865e77000d8c24395f46ce3d2a53d4322c8

--- AudioAnalysis/AudioAnalysisTools/Sonogram/Image_MultiTrack.cs
+++ AudioAnalysis/AudioAnalysisTools/Sonogram/Image_MultiTrack.cs
@@ -8,17 +8,19 @@ using TowseyLib;
 
 namespace AudioAnalysisTools
 {
-	public class Image_MultiTrack : IDisposable
-	{
+    using QutSensors.Shared;
 
-		#region Properties
-		public Image SonoImage { get; private set; }
-		List<Image_Track> tracks = new List<Image_Track>();
-		public IEnumerable<Image_Track> Tracks { get { return tracks; } }
-        public  List<AcousticEvent> EventList { get; set; }
-        double[,] SuperimposedMatrix {get; set;}
+    public class Image_MultiTrack : IDisposable
+    {
+
+        #region Properties
+        public Image SonoImage { get; private set; }
+        List<Image_Track> tracks = new List<Image_Track>();
+        public IEnumerable<Image_Track> Tracks { get { return tracks; } }
+        public List<AcousticEvent> EventList { get; set; }
+        double[,] SuperimposedMatrix { get; set; }
         private double superImposedMaxScore;
-		#endregion
+        #endregion
 
         /// <summary>
         /// CONSTRUCTOR
@@ -28,12 +30,12 @@ namespace AudioAnalysisTools
         {
             SonoImage = image;
         }
-        
-        
+
+
         public void AddTrack(Image_Track track)
-		{
-			tracks.Add(track);
-		}
+        {
+            tracks.Add(track);
+        }
 
         public void AddEvents(List<AcousticEvent> list)
         {
@@ -53,14 +55,14 @@ namespace AudioAnalysisTools
         /// </summary>
         /// <param name="path"></param>
         public void Save(string path)
-		{
+        {
             Image image = GetImage();
             if (image == null)
             {
                 Log.WriteLine("MultiTrackImage.Save() - WARNING: NULL IMAGE.Cannot save to: " + path);
                 return;
             }
-			image.Save(path, ImageFormat.Png);
+            image.Save(path, ImageFormat.Png);
             //image.Save(path, ImageFormat.Jpeg);
         }
 
@@ -71,77 +73,52 @@ namespace AudioAnalysisTools
         /// Therefore call a recursive method to draw the image.
         /// </summary>
         /// <returns></returns>
-		public Image GetImage()
-		{
-			// Calculate total height of the bmp
-			var height = CalculateImageHeight();
+        public Image GetImage()
+        {
+            // Calculate total height of the bmp
+            var height = CalculateImageHeight();
 
             //set up a new image having the correct dimensions
-			var image2return = new Bitmap(SonoImage.Width, height, PixelFormat.Format24bppRgb);
+            var image2return = new Bitmap(SonoImage.Width, height, PixelFormat.Format24bppRgb);
 
             //create new graphics canvas and add in the sonogram image
             using (var g = Graphics.FromImage(image2return))
             {
-                //g.DrawImage(this.SonoImage, 0, 0);            // WARNING ### THIS CALL DID NOT WORK
-                int accumulatedX = 0;                           // THEREFORE
-                SegmentedDraw(g, this.SonoImage, accumulatedX); // USE THIS CALL INSTEAD.
+                ////g.DrawImage(this.SonoImage, 0, 0); // WARNING ### THIS CALL DID NOT WORK THEREFORE
+                GraphicsSegmented.Draw(g, this.SonoImage); // USE THIS CALL INSTEAD.
 
                 if (this.SuperimposedMatrix != null) Superimpose(g);
                 if (this.EventList != null) DrawEvents(g);
             }
 
             //now add tracks to the image
-			int offset = SonoImage.Height;
-			foreach (var track in Tracks)
-			{
-				track.topOffset = offset;
-                track.bottomOffset = offset + track.Height-1;
-                track.DrawTrack(image2return);
-				offset += track.Height;
-			}
-			return image2return;
-		}
-
-        /// <summary>
-        /// graphics.DrawImage() or GDI cannot draw an image that is too big, typically greater than 40000 pixels.
-        /// So call this method which calls itself recursively and draws image in small rectangles until the entire image is drawn.
-        /// This method was copied from WebFrontEnd solution:-  C:\SensorNetworks\Software\WebFrontend\App_Code\Spectrogram.cs
-        /// </summary>
-        /// <param name="graphics">the graphics object to be drawn to</param>
-        /// <param name="sourceImage">the image to be drawn</param>
-        /// <param name="accumulatedX">the left most location of current drawing rectangle.</param>
-        private void SegmentedDraw(Graphics graphics, Image sourceImage, int accumulatedX)
-        {
-            const int gdiLimit = 20000;
-
-            // how much do we have to draw? Is it narrower than the gdiLimit?
-            int writeWidth = Math.Min(gdiLimit, sourceImage.Width - accumulatedX);
-
-            Rectangle writeRectangle = new Rectangle(accumulatedX, 0, writeWidth, sourceImage.Height);
-            graphics.DrawImage(sourceImage, writeRectangle, accumulatedX, 0, writeWidth, sourceImage.Height, GraphicsUnit.Pixel);
-
-            accumulatedX += writeWidth;
-
-            if (accumulatedX < sourceImage.Width)
+            int offset = SonoImage.Height;
+            foreach (var track in Tracks)
             {
-                SegmentedDraw(graphics, sourceImage, accumulatedX);
+                track.topOffset = offset;
+                track.bottomOffset = offset + track.Height - 1;
+                track.DrawTrack(image2return);
+                offset += track.Height;
             }
+            return image2return;
         }
 
 
+
+
         private int CalculateImageHeight()
-		{
-			int totalHeight = SonoImage.Height;
+        {
+            int totalHeight = SonoImage.Height;
             foreach (Image_Track track in tracks)
                 totalHeight += track.Height;
-			return totalHeight;
-		}
-        
+            return totalHeight;
+        }
+
         void DrawEvents(Graphics g)
         {
             Pen p1 = new Pen(Color.Red);
             Pen p2 = new Pen(Color.Yellow);
-            foreach(AcousticEvent e in this.EventList)
+            foreach (AcousticEvent e in this.EventList)
             {
                 //double start = e.StartTime;
                 //double duration = e.Duration;
@@ -149,13 +126,13 @@ namespace AudioAnalysisTools
                 //int maxF = e.MaxFreq;
                 int x = e.oblong.r1;
                 int y = 256 - e.oblong.c2;
-                int width  = e.oblong.r2 - x + 1;
+                int width = e.oblong.r2 - x + 1;
                 int height = e.oblong.c2 - e.oblong.c1 + 1;
                 g.DrawRectangle(p1, x, y, width, height);
                 int scoreHt = (int)Math.Round(height * e.NormalisedScore);
                 int y1 = y + height;
                 int y2 = y1 - scoreHt;
-                g.DrawLine(p2, x,     y1, x,     y2);
+                g.DrawLine(p2, x, y1, x, y2);
                 g.DrawLine(p2, x + 1, y1, x + 1, y2);
                 g.DrawLine(p2, x + 2, y1, x + 2, y2);
                 g.DrawLine(p2, x + 3, y1, x + 3, y2);
@@ -171,7 +148,7 @@ namespace AudioAnalysisTools
             Pen p5 = new Pen(Color.Blue);
             Pen p6 = new Pen(Color.Indigo);
             Pen p7 = new Pen(Color.Violet);
-            var pens = new List<Pen>(); 
+            var pens = new List<Pen>();
             pens.Add(p1);
             pens.Add(p2);
             pens.Add(p3);
@@ -183,7 +160,7 @@ namespace AudioAnalysisTools
 
             int rows = this.SuperimposedMatrix.GetLength(0);
             int cols = this.SuperimposedMatrix.GetLength(1);
-            int imageHt = this.SonoImage.Height-1; //subtract 1 because indices start at zero
+            int imageHt = this.SonoImage.Height - 1; //subtract 1 because indices start at zero
             double[] bounds = { 0.0, 0.14, 0.28, 0.42, 0.56, 0.70, 0.85, 1.0 }; //for normalised score
 
             for (int c = 1; c < cols; c++)//traverse columns - skip DC column
@@ -195,22 +172,22 @@ namespace AudioAnalysisTools
                     double normScore = this.SuperimposedMatrix[r, c] / this.superImposedMaxScore;
                     if ((normScore > bounds[0]) && (normScore <= bounds[1])) pen = pens[0]; //red
                     else
-                    if ((normScore > bounds[1]) && (normScore <= bounds[2])) pen = pens[1]; //orange
-                    else
-                    if ((normScore > bounds[2]) && (normScore <= bounds[3])) pen = pens[2]; //yellow
-                    else
-                    if ((normScore > bounds[3]) && (normScore <= bounds[4])) pen = pens[3]; //green
-                    else
-                    if ((normScore > bounds[4]) && (normScore <= bounds[5])) pen = pens[4]; //blue
-                    else
-                    if ((normScore > bounds[5]) && (normScore <= bounds[6])) pen = pens[5]; //indigo
-                    else
-                    if ((normScore > bounds[6]) && (normScore <= bounds[7])) pen = pens[6]; //violet
-                    else pen = new Pen(Color.Brown);                                        //brown
+                        if ((normScore > bounds[1]) && (normScore <= bounds[2])) pen = pens[1]; //orange
+                        else
+                            if ((normScore > bounds[2]) && (normScore <= bounds[3])) pen = pens[2]; //yellow
+                            else
+                                if ((normScore > bounds[3]) && (normScore <= bounds[4])) pen = pens[3]; //green
+                                else
+                                    if ((normScore > bounds[4]) && (normScore <= bounds[5])) pen = pens[4]; //blue
+                                    else
+                                        if ((normScore > bounds[5]) && (normScore <= bounds[6])) pen = pens[5]; //indigo
+                                        else
+                                            if ((normScore > bounds[6]) && (normScore <= bounds[7])) pen = pens[6]; //violet
+                                            else pen = new Pen(Color.Brown);                                        //brown
                     g.DrawLine(pen, r, imageHt - c, r, imageHt - c + 1);
-                    r +=1;
+                    r += 1;
                 }
-                c +=1;
+                c += 1;
             }
 
 
