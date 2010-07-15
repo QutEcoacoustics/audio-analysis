9a17664256468cc9c30cc68d67225eba3ae76d25
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-07-15 15:25:19 +1000

    Fixes to speed up linq to sql queries that used entityset, and therefore IEnumerable.

--- ProcessorUI/Processor/ProcessorServiceWrapper.cs
+++ ProcessorUI/Processor/ProcessorServiceWrapper.cs
@@ -1,33 +1,52 @@
-﻿using System;
-using QutSensors.Processor.ProcessorService;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProcessorServiceWrapper.cs" company="MQUTeR">
+//   -
+// </copyright>
+// <summary>
+//   ProcessorService Wrapper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace QutSensors.Processor
 {
+    using System;
+
+    using QutSensors.Processor.ProcessorService;
+
     /// <summary>
     /// Wraps the WCF Service to ensure Abort or Close is called as appropriate
     /// Close should be called in normal circumstances, Abort if there's an error.
     /// Easiest way to use is:
     /// using (var ws = new ServiceWrapper()) {
-    ///		ws.Proxy.Call();
-    ///		ws.Close();
-    ///	}
-    ///	That way, if Call() fails then an exception is thrown and Dispose is called without Close
-    ///	being called beforehand. In that case the wrapper will call Abort.
+    ///     ws.Proxy.Call();
+    ///     ws.Close();
+    /// }
+    /// That way, if Call() fails then an exception is thrown and Dispose is called without Close
+    /// being called beforehand. In that case the wrapper will call Abort.
     /// </summary>
     public class ProcessorServiceWrapper : IDisposable
     {
-        ProcessorServiceClient proxy;
+        private ProcessorServiceClient proxy;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProcessorServiceWrapper"/> class.
+        /// </summary>
         public ProcessorServiceWrapper()
         {
             proxy = new ProcessorServiceClient("basicBinding_ProcessorService");
         }
 
+        /// <summary>
+        /// Gets Proxy.
+        /// </summary>
         public ProcessorServiceClient Proxy
         {
             get { return proxy; }
         }
 
+        /// <summary>
+        /// Close the proxy connection.
+        /// </summary>
         public void Close()
         {
             proxy.Close();
@@ -35,10 +54,16 @@ namespace QutSensors.Processor
         }
 
         #region IDisposable
+
+        /// <summary>
+        /// Clean up proxy connections.
+        /// </summary>
         public void Dispose()
         {
             if (proxy != null)
+            {
                 proxy.Abort();
+            }
         }
 
         ~ProcessorServiceWrapper()
