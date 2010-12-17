ae1f1a29f341da452dc6a62a91bfd615a2689e19
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2010-12-17 14:19:05 +1000

    - mediaopenedeventargs added  - improved links to player - can now load and jump to time.  - all loading and modification on player page now done by js.  - added filters for error logs.  - fileupload SL: changed text.

--- QutSensors.UI/Display/Managers/MiscDisplayManager.cs
+++ QutSensors.UI/Display/Managers/MiscDisplayManager.cs
@@ -23,8 +23,21 @@ namespace QutSensors.UI.Display.Managers
     /// </summary>
     public static class MiscDisplayManager
     {
-        private const string FitKgb = "http://fitkgbl07.fit.qut.edu.au";
-        private const string DateFormat = "yyyy-MM-dd HH:mm:ss";
+        private const string FitKgbString = "http://fitkgbl07.fit.qut.edu.au";
+        private const string DateFormatString = "yyyy-MM-dd HH:mm:ss";
+
+        private const string AudioReadingHandlerString = "/AudioReading.ashx";
+
+        private const string SpectrogramHandlerString = "/Spectrogram.ashx";
+
+        private const string DoesNotExistString = "does not exist";
+
+        private const string AudioPlayerXap = "/ClientBin/AudioPlayerControl.xap";
+
+        private const string AxdFiles = ".axd?";
+
+        private const string SqlServerTimeout =
+            "The timeout period elapsed prior to completion of the operation or the server is not responding.";
 
         /// <summary>
         /// Get error log items.
@@ -38,14 +51,17 @@ namespace QutSensors.UI.Display.Managers
         /// <param name="sortExpression">
         /// The sort expression.
         /// </param>
+        /// <param name="displayFilter">
+        /// The display Filter.
+        /// </param>
         /// <returns>
         /// List of error log items.
         /// </returns>
-        public static IEnumerable<ErrorLogItem> GetErrorLogItems(int maxItems, int startIndex, string sortExpression)
+        public static IEnumerable<ErrorLogItem> GetErrorLogItems(int maxItems, int startIndex, string sortExpression, string displayFilter)
         {
             using (var db = new QutSensorsDb())
             {
-                var results = db.ErrorLogs.Where(e => !e.Details.Contains(FitKgb));
+                var results = db.ErrorLogs.AsQueryable();
 
                 switch (sortExpression)
                 {
@@ -78,6 +94,8 @@ namespace QutSensors.UI.Display.Managers
                         break;
                 }
 
+                results = ApplyDisplayFilter(results, displayFilter);
+
                 return results.Skip(startIndex).Take(maxItems).Select(e => new ErrorLogItem
                             {
                                 ErrorId = e.ErrorID,
@@ -88,18 +106,69 @@ namespace QutSensors.UI.Display.Managers
             }
         }
 
+        /*
+        -- access issues
+select 
+substring(Error,40,CHARINDEX(' ',Error, 40)-40) as AccessType, 
+substring(Error,CHARINDEX(':',Error, 100)+2,100) as AccessUrl, 
+MAX([time]) as LastRecordedDate,
+COUNT(*) as AccessCount
+from ErrorLogs
+where 
+ Error like '%AuthenticationHelper%'
+ and Error not like '%http://fitkgbl07.fit.qut.edu.au/%'
+group by 
+substring(Error,40,CHARINDEX(' ',Error, 40)-40), 
+substring(Error,CHARINDEX(':',Error, 100)+2,100),
+datepart(year,[Time])
+order by COUNT(*) desc
+    */
+
         /// <summary>
         /// Get number of error log items.
         /// </summary>
         /// <returns>
         /// Number of error log items.
         /// </returns>
-        public static int GetErrorLogItemsCount()
+        public static int GetErrorLogItemsCount(string displayFilter)
         {
             using (var db = new QutSensorsDb())
             {
-                return db.ErrorLogs.Where(e => !e.Details.Contains(FitKgb)).Count();
+                return ApplyDisplayFilter(db.ErrorLogs.AsQueryable(), displayFilter).Count();
             }
+        }
+
+        private static IQueryable<ErrorLog> ApplyDisplayFilter(IQueryable<ErrorLog> results, string displayFilter)
+        {
+            // always filter out fitkgbl07 urls.
+            results = results.Where(e => !e.Details.Contains(FitKgbString));
+
+            if (!string.IsNullOrEmpty(displayFilter))
+            {
+                switch (displayFilter)
+                {
+                    case "AudioReadingHandler":
+                        results = results.Where(e => e.Error.Contains(AudioReadingHandlerString));
+                        break;
+                    case "SpectrogramHandler":
+                        results = results.Where(e => e.Error.Contains(SpectrogramHandlerString));
+                        break;
+                    case "MissingFiles":
+                        results = results.Where(e => e.Error.Contains(DoesNotExistString));
+                        break;
+                    case "AudioPlayer":
+                        results = results.Where(e => e.Error.Contains(AudioPlayerXap));
+                        break;
+                    case "ScriptFiles":
+                        results = results.Where(e => e.Error.Contains(AxdFiles));
+                        break;
+                    case "Timeout":
+                        results = results.Where(e => e.Error.Contains(SqlServerTimeout));
+                        break;
+                }
+            }
+
+            return results;
         }
 
         /// <summary>
@@ -236,12 +305,12 @@ namespace QutSensors.UI.Display.Managers
                 Type = s.Type.ToString(),
                 Status = s.Status,
                 MimeType = s.MimeType,
-                ProcessingStartTime = s.ProcessingStartTime.HasValue ? s.ProcessingStartTime.Value.ToString(DateFormat) : "not started",
-                LastAccessed = s.LastAccessed.HasValue ? s.LastAccessed.Value.ToString(DateFormat) : "unknown",
+                ProcessingStartTime = s.ProcessingStartTime.HasValue ? s.ProcessingStartTime.Value.ToString(DateFormatString) : "not started",
+                LastAccessed = s.LastAccessed.HasValue ? s.LastAccessed.Value.ToString(DateFormatString) : "unknown",
                 TimeSpentGenerating = s.TimeSpentGeneratingMs != 0 ? new TimeSpan((long)s.TimeSpentGeneratingMs * TimeSpan.TicksPerMillisecond).ToReadableString() : "unknown",
-                CreatedTime = s.CreatedTime.HasValue ? s.CreatedTime.Value.ToString(DateFormat) : "unknown",
-                End = s.Time.AddMilliseconds(s.End).ToString(DateFormat),
-                Start = s.Time.AddMilliseconds(s.Start).ToString(DateFormat),
+                CreatedTime = s.CreatedTime.HasValue ? s.CreatedTime.Value.ToString(DateFormatString) : "unknown",
+                End = s.Time.AddMilliseconds(s.End).ToString(DateFormatString),
+                Start = s.Time.AddMilliseconds(s.Start).ToString(DateFormatString),
                 Duration = (s.Time.AddMilliseconds(s.Start) - s.Time.AddMilliseconds(s.End)).ToReadableString(),
                 AudioReadingIdQs = new ReadingsFilter { CommaSeparatedAudioReadingIds = s.AudioReadingId.ToString() }.ToQueryString(true)
             });
