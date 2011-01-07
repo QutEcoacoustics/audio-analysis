7b7b364238d5fd45d8435d184f9b59fcb0799527
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2011-01-07 16:50:05 +1000

    - added playersettings audio Properties  - added console app for SL IS audio segments  - placeholder SL app web page

--- QutSensors.UI/PlayerSettings.cs
+++ QutSensors.UI/PlayerSettings.cs
@@ -9,6 +9,7 @@ namespace QutSensors.UI
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.Drawing;
     using System.Runtime.Serialization;
@@ -20,6 +21,26 @@ namespace QutSensors.UI
     [DataContract]
     public class PlayerSettings
     {
+        private const bool DefaultAutomaticallySkipToNextTrack = false;
+        private const string DefaultColorsLocked = "#30FF0800"; ////Color.FromArgb(48, 255, 8, 0).ToHexString(true);
+        private const string DefaultColorsSelected = "#30FCFF00"; ////Color.FromArgb(48, 252, 255, 0).ToHexString(true);
+        private const string DefaultColorsTag = "#3051FF00"; ////Color.FromArgb(48, 81, 255, 0).ToHexString(true);
+        private const string DefaultColorsProcessorTag = "#30003EFF"; ////Color.FromArgb(48, 0, 62, 255).ToHexString(true);
+        private const string DefaultColorsTagWithProcessorResultId = "#3000FF87"; /////Color.FromArgb(48, 0, 255, 135).ToHexString(true);
+        private const string DefaultColorsUnsavedTag = "#30FFFFFF"; ////Color.FromArgb(48, 255, 255, 255).ToHexString(true);
+        private const int DefaultDownloadsNumberForwardChunks = 2;
+        private const int DefaultDownloadsNumberPreviousChunks = 1;
+        private const int DefaultDownloadsRetryCount = 0;
+        private const bool DefaultIsMute = false;
+        private const int DefaultProfileSaveTimeMilliSeconds = 40 * 1000;
+        private const bool DefaultTurnOnLooping = true;
+        private const string DefaultUomFrequencyScale = "Hz";
+        private const bool DefaultUomMelScale = false;
+        private const string DefaultUomTimeScale = "ms";
+        private const bool DefaultUseLocalAudio = false;
+        private const bool DefaultUseRemoteAudio = true;
+        private const double DefaultVolume = 0.9;
+
         #region Constructors and Destructors
 
         /// <summary>
@@ -27,23 +48,25 @@ namespace QutSensors.UI
         /// </summary>
         public PlayerSettings()
         {
-            this.AutomaticallySkipToNextTrack = false;
-            this.ColorsLocked = Color.FromArgb(48, 255, 8, 0).ToHexString(true);
-            this.ColorsSelected = Color.FromArgb(48, 252, 255, 0).ToHexString(true);
-            this.ColorsTag = Color.FromArgb(48, 81, 255, 0).ToHexString(true);
-            this.ColorsProcessorTag = Color.FromArgb(48, 0, 62, 255).ToHexString(true);
-            this.ColorsTagWithProcessorResultId = Color.FromArgb(48, 0, 255, 135).ToHexString(true);
-            this.ColorsUnsavedTag = Color.FromArgb(48, 255, 255, 255).ToHexString(true);
-            this.DownloadsNumberForwardChunks = 2;
-            this.DownloadsNumberPreviousChunks = 1;
-            this.DownloadsRetryCount = 0;
-            this.IsMute = false;
-            this.ProfileSaveTimeMilliSeconds = 40 * 1000;
-            this.TurnOnLooping = true;
-            this.UomFrequencyScale = "Hz";
-            this.UomMelScale = false;
-            this.UomTimeScale = "ms";
-            this.Volume = 0.9;
+            this.AutomaticallySkipToNextTrack = DefaultAutomaticallySkipToNextTrack;
+            this.ColorsLocked = DefaultColorsLocked;
+            this.ColorsProcessorTag = DefaultColorsProcessorTag;
+            this.ColorsSelected = DefaultColorsSelected;
+            this.ColorsTag = DefaultColorsTag;
+            this.ColorsTagWithProcessorResultId = DefaultColorsTagWithProcessorResultId;
+            this.ColorsUnsavedTag = DefaultColorsUnsavedTag;
+            this.DownloadsNumberForwardChunks = DefaultDownloadsNumberForwardChunks;
+            this.DownloadsNumberPreviousChunks = DefaultDownloadsNumberPreviousChunks;
+            this.DownloadsRetryCount = DefaultDownloadsRetryCount;
+            this.IsMute = DefaultIsMute;
+            this.ProfileSaveTimeMilliSeconds = DefaultProfileSaveTimeMilliSeconds;
+            this.TurnOnLooping = DefaultTurnOnLooping;
+            this.UomFrequencyScale = DefaultUomFrequencyScale;
+            this.UomMelScale = DefaultUomMelScale;
+            this.UomTimeScale = DefaultUomTimeScale;
+            this.UseLocalAudio = DefaultUseLocalAudio;
+            this.UseRemoteAudio = DefaultUseRemoteAudio;
+            this.Volume = DefaultVolume;
         }
 
         #endregion
@@ -54,6 +77,7 @@ namespace QutSensors.UI
         /// Gets or sets a value indicating whether AutomaticallySkipToNextTrack.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultAutomaticallySkipToNextTrack)]
         public bool AutomaticallySkipToNextTrack { get; set; }
 
         /// <summary>
@@ -61,6 +85,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsLocked)]
         public string ColorsLocked { get; set; }
 
         /// <summary>
@@ -68,6 +93,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsProcessorTag)]
         public string ColorsProcessorTag { get; set; }
 
         /// <summary>
@@ -75,6 +101,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsSelected)]
         public string ColorsSelected { get; set; }
 
         /// <summary>
@@ -82,6 +109,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsTag)]
         public string ColorsTag { get; set; }
 
         /// <summary>
@@ -89,6 +117,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsTagWithProcessorResultId)]
         public string ColorsTagWithProcessorResultId { get; set; }
 
         /// <summary>
@@ -96,6 +125,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [RegularExpression(ImageExtensions.RegExHexColor, ErrorMessage = ImageExtensions.ColorRegExError)]
+        [DefaultValue(DefaultColorsUnsavedTag)]
         public string ColorsUnsavedTag { get; set; }
 
         /// <summary>
@@ -103,6 +133,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [Range(0, 10)]
+        [DefaultValue(DefaultDownloadsNumberForwardChunks)]
         public int DownloadsNumberForwardChunks { get; set; }
 
         /// <summary>
@@ -110,6 +141,7 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [Range(0, 10)]
+        [DefaultValue(DefaultDownloadsNumberPreviousChunks)]
         public int DownloadsNumberPreviousChunks { get; set; }
 
         /// <summary>
@@ -117,24 +149,28 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [Range(0, 100)]
+        [DefaultValue(DefaultDownloadsRetryCount)]
         public int DownloadsRetryCount { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether IsMute.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultIsMute)]
         public bool IsMute { get; set; }
 
         /// <summary>
         /// Gets or sets ProfileSaveTime_ms.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultProfileSaveTimeMilliSeconds)]
         public int ProfileSaveTimeMilliSeconds { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether TurnOnLooping.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultTurnOnLooping)]
         public bool TurnOnLooping { get; set; }
 
         /// <summary>
@@ -142,12 +178,14 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [StringLength(20, ErrorMessage = "The value of UomFrequencyScale must be less than 20 chars")]
+        [DefaultValue(DefaultUomFrequencyScale)]
         public string UomFrequencyScale { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether UomMelScale.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultUomMelScale)]
         public bool UomMelScale { get; set; }
 
         /// <summary>
@@ -155,12 +193,28 @@ namespace QutSensors.UI
         /// </summary>
         [DataMember]
         [StringLength(20, ErrorMessage = "The value of UomTimeScale must be less than 20 chars")]
+        [DefaultValue(DefaultUomTimeScale)]
         public string UomTimeScale { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use local audio files in Silverlight Isolated Storage.
+        /// </summary>
+        [DataMember]
+        [DefaultValue(DefaultUseLocalAudio)]
+        public bool UseLocalAudio { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to download audio files from web server.
+        /// </summary>
+        [DataMember]
+        [DefaultValue(DefaultUseRemoteAudio)]
+        public bool UseRemoteAudio { get; set; }
 
         /// <summary>
         /// Gets or sets Volume.
         /// </summary>
         [DataMember]
+        [DefaultValue(DefaultVolume)]
         public double Volume { get; set; }
 
         #endregion
@@ -189,6 +243,8 @@ namespace QutSensors.UI
             yield return "UomFrequencyScale: " + this.UomFrequencyScale;
             yield return "UomMelScale: " + this.UomMelScale;
             yield return "UomTimeScale: " + this.UomTimeScale;
+            yield return "UseLocalAudio: " + this.UseLocalAudio;
+            yield return "UseRemoteAudio: " + this.UseRemoteAudio;
             yield return "Volume: " + this.Volume;
         }
     }
