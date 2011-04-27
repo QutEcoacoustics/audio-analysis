3b685928714f634cc2c720f36b708d4523aa341a
Michael Towsey <m.towsey@qut.edu.au>
2011-04-27 16:09:47 +1000

    Original hash: f5ce2c53463befd05d28b980f90c9527ab259d15

---
+++ AudioAnalysis/AnalysisPrograms/FrogRibit.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using TowseyLib;
+using AudioAnalysisTools;
+
+
+namespace AnalysisPrograms
+{
+    class FrogRibit
+    {
+
+        public static void Dev(string[] args)
+        {
+            string title = "# DETECT FROG RIBBIT.";
+            string date  = "# DATE AND TIME: " + DateTime.Now;
+            Log.WriteLine(title);
+            Log.WriteLine(date);
+
+            //SET VERBOSITY
+            Log.Verbosity = 1;
+
+            //string recordingPath   = args[0];
+            //string iniPath        = args[0];
+            //string targetName     = args[2];   //prefix of name of created files 
+
+            string recordingPath = @"C:\SensorNetworks\WavFiles\Frogs\frog_Rheobatrachus_silus_MONO.wav";
+
+
+            //i: Set up the file names
+            //string outputDir = Path.GetDirectoryName(iniPath) + "\\";
+
+            //ii: READ PARAMETER VALUES FROM INI FILE
+            //var config = new Configuration(iniPath);
+            //Dictionary<string, string> dict = config.GetTable();
+            //string sourceFile = dict[FeltTemplate_Create.key_SOURCE_RECORDING];
+            //string sourceDir = dict[FeltTemplate_Create.key_SOURCE_DIRECTORY];
+            //double dB_Threshold = Double.Parse(dict[FeltTemplate_Create.key_DECIBEL_THRESHOLD]);
+            //double maxTemplateIntensity = Double.Parse(dict[FeltTemplate_Create.key_TEMPLATE_MAX_INTENSITY]);
+            //int neighbourhood = Int32.Parse(dict[FeltTemplate_Create.key_DONT_CARE_NH]);   //the do not care neighbourhood
+            //int lineLength = Int32.Parse(dict[FeltTemplate_Create.key_LINE_LENGTH]);
+            //double templateThreshold = dB_Threshold / maxTemplateIntensity;
+            //int bitmapThreshold = (int)(255 - (templateThreshold * 255));
+
+            //i: GET RECORDING
+            AudioRecording recording = new AudioRecording(recordingPath);
+            if (recording.SampleRate != 22050) recording.ConvertSampleRate22kHz();
+            int sr = recording.SampleRate;
+
+
+            string filterName = "Chebyshev_Lowpass_3000";
+            System.Console.WriteLine("\nApply filter: " + filterName);
+            var filteredRecording = recording.Filter_IIR(filterName); //retrun new filtered audio recording.
+
+            //ii: MAKE SONOGRAM
+            Log.WriteLine("Start sonogram.");
+            SonogramConfig sonoConfig = new SonogramConfig(); //default values config
+            sonoConfig.SourceFName = recording.FileName;
+            sonoConfig.WindowOverlap = 0.5;      // set default value
+            sonoConfig.DoMelScale = false;
+            sonoConfig.NoiseReductionType = NoiseReductionType.STANDARD;
+            AmplitudeSonogram basegram = new AmplitudeSonogram(sonoConfig, filteredRecording.GetWavReader());
+            SpectralSonogram sonogram = new SpectralSonogram(basegram);  //spectrogram has dim[N,257]
+            recording.Dispose(); //DO NOT DISPOSE BECAUSE REQUIRE AGAIN
+
+            Log.WriteLine("Signal: Duration={0}, Sample Rate={1}", sonogram.Duration, sr);
+            Log.WriteLine("Frames: Size={0}, Count={1}, Duration={2:f1}ms, Overlap={5:f0}%, Offset={3:f1}ms, Frames/s={4:f1}",
+                                       sonogram.Configuration.WindowSize, sonogram.FrameCount, (sonogram.FrameDuration * 1000),
+                                      (sonogram.FrameOffset * 1000), sonogram.FramesPerSecond, sonoConfig.WindowOverlap * 100);
+
+            string imagePath = recordingPath + ".png";
+            DrawSonogram(sonogram, imagePath);
+
+            Log.WriteLine("# Finished everything!");
+            Console.ReadLine();
+        } //DEV()
+
+        public static void DrawSonogram(BaseSonogram sonogram, string path)
+        {
+            Log.WriteLine("# Start to draw image of sonogram.");
+            bool doHighlightSubband = false; bool add1kHzLines = true;
+
+            using (System.Drawing.Image img = sonogram.GetImage(doHighlightSubband, add1kHzLines))
+            using (Image_MultiTrack image = new Image_MultiTrack(img))
+            {
+                //img.Save(@"C:\SensorNetworks\WavFiles\temp1\testimage1.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration, sonogram.FramesPerSecond));
+
+                image.Save(path);
+            } // using
+        } // DrawSonogram()
+
+    }
+}
