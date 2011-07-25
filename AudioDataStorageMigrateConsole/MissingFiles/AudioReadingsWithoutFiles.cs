55013956d827e64262d6372ee5e4ddc1f907ec85
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2011-07-25 21:44:24 +1000

    - migration: find audioreadings without files.

--- AudioDataStorageMigrateConsole/MissingFiles/AudioReadingsWithoutFiles.cs
+++ AudioDataStorageMigrateConsole/MissingFiles/AudioReadingsWithoutFiles.cs
@@ -1,87 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
 using QutSensors.Shared.LogProviders;
 using System.Diagnostics;
 using QutSensors.Business.Storage;
-using AudioDataStorageMigrateConsole.Classes;
 using QutSensors.Data.Linq;
 
 namespace AudioDataStorageMigrateConsole.MissingFiles
 {
+    using System.IO;
+
     public class AudioReadingsWithoutFiles
     {
+        private readonly FileSystemAudioDataStorage fileSystemAudioDataStorage;
+
         private readonly ILogProvider logProvider;
 
         private readonly Stopwatch singleItemWatch;
 
-        private readonly Stopwatch exportWatch;
-
-        public AudioReadingsWithoutFiles(
-            string logFileDir,
+        public AudioReadingsWithoutFiles(string logFileDir,
             FileSystemAudioDataStorage fileSystemAudioDataStorage)
         {
-            this.AudioFileInfo = new AudioFileInfo(fileSystemAudioDataStorage);
+            this.fileSystemAudioDataStorage = fileSystemAudioDataStorage;
 
             this.singleItemWatch = new Stopwatch();
-            this.exportWatch = new Stopwatch();
 
             var csvHeaders = new List<string>
                 {
                     "AudioReadingId",
-                    "FileSystemFileExistsBefore",
-                    "FileSystemFileLengthBefore",
-                    "FileSystemFileLengthFormattedBefore",
-                    "FileSystemFileExistsAfter",
-                    "FileSystemFileLengthAfter",
-                    "FileSystemFileLengthFormattedAfter",
-                    "DatabaseDataExistsBefore",
-                    "DatabaseDataLengthBefore",
-                    "DatabaseDataLengthFormattedBefore",
-                    "DatabaseDataExistsAfter",
-                    "DatabaseDataLengthAfter",
-                    "DatabaseDataLengthFormattedAfter",
+                    "FileSystemFileExists",
+                    "FileSystemFileLength",
+                    "FileSystemFileLengthFormatted",
+                    "DatabaseDataIsNull",
+                    "DatabaseDataLength",
+                    "DatabaseDataLengthFormatted",
                     "ReadWriteDuration",
                     "ReadWriteDurationFormatted",
                     "ReadWriteDurationMs",
                     "SingleAudioReadingProcessDuration",
                     "SingleAudioReadingProcessDurationFormatted",
-                    "SingleAudioReadingProcessDurationMs",
-                    "OverallRunningCount",
-                    "OverallRunningDuration",
-                    "OverallRunningDurationFormatted",
-                    "OverallRunningDurationMs",
+                    "SingleAudioReadingProcessDurationMs"
                 };
 
             this.logProvider = new MultiLogProvider(new CsvTextFileLogProvider(logFileDir, csvHeaders), new ConsoleLogProvider());
+
         }
 
-        /// <summary>
-        /// Gets DbDataInfo.
-        /// </summary>
-        public DbDataInfo DbDataInfo { get; private set; }
-
-        /// <summary>
-        /// Gets AudioFileInfo.
-        /// </summary>
-        public AudioFileInfo AudioFileInfo { get; private set; }
-
-        /// <summary>
-        /// Run checks.
-        /// </summary>
         public void Run()
         {
             using (var db = new QutSensorsDb())
             {
-                // exclude all with state = uploading. these may need to be fixed manually.
-                // !(ar.State == AudioReadingState.Uploading && ar.UploadStartUTC.HasValue)
-                var audioreadings =  db.AudioReadings.OrderByDescending(  ar => ar.Length).ThenByDescending(ar => ar.Time);
+                var audioreadings = db.AudioReadings.OrderByDescending(ar => ar.Length).ThenByDescending(ar => ar.Time);
 
                 foreach (var reading in audioreadings)
                 {
-                    //infoHolder = Process(db, reading, infoHolder);
+                    Check(reading);
                 }
+            }
+        }
+
+        private void Check(AudioReading reading)
+        {
+            var msg = string.Empty;
+            LogType logType = LogType.Information;
+
+            long fileByteCount = 0;
+            bool fileExists = false;
+
+            long dbDataByteCount = 0;
+            bool dbDataExists = false;
+
+            TimeSpan exportTime = TimeSpan.Zero;
+
+            this.singleItemWatch.Restart();
+
+            try
+            {
+                FileInfo file = this.fileSystemAudioDataStorage.GetDataFile(reading);
+
+                // see if file exists
+                fileExists = file != null && File.Exists(file.FullName);
+
+                // if a file does exist, get it's byte count
+                if (fileExists)
+                {
+                    fileByteCount = file.Length;
+                }
+
+                // see if there is data in the database
+                dbDataExists = MigrationUtils.AudioReadingSqlFileStreamDataExists(reading);
+
+                // if there is, get it's byte count
+                if (dbDataExists)
+                {
+                    dbDataByteCount = MigrationUtils.AudioReadingSqlFileStreamDataLength(reading);
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Exception: " + ex;
+                logType = LogType.Error;
+            }
+
+            this.singleItemWatch.Stop();
+
+            bool bothExistByteCountMismatch = fileExists && dbDataExists && fileByteCount != dbDataByteCount;
+
+            // only log if file does not exist, or both exist but byte sizes don't match
+            if (!fileExists || bothExistByteCountMismatch)
+            {
+                string extraMsg = !fileExists
+                                      ? "File does not exist."
+                                      : bothExistByteCountMismatch ? "Both exist but byte sizes don't match." : "Other.";
+
+                this.logProvider.WriteEntry(
+                    logType,
+                    msg + " -- " + extraMsg,
+                    reading.AudioReadingID,
+                    fileExists,
+                    fileByteCount,
+                    fileByteCount.ToByteDisplay(),
+                    dbDataExists,
+                    dbDataByteCount,
+                    dbDataByteCount.ToByteDisplay(),
+                    exportTime,
+                    exportTime.ToReadableString(),
+                    exportTime.TotalMilliseconds,
+                    this.singleItemWatch.Elapsed,
+                    this.singleItemWatch.Elapsed.ToReadableString(),
+                    this.singleItemWatch.Elapsed.TotalMilliseconds);
             }
         }
     }
