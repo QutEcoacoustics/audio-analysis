b069b92c8acb2ad46f7e1d5296c7ddad90f67fd6
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2011-10-23 23:45:30 +1000

    - clean up preparation for HPC integration Original hash: 13b025bcf5fa030bd4849ef526541828899710fb

--- QutSensors.AnalysisProcessor/ServiceInstall.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-echo Installing QutSensors Analysis Processor...
-sc create QutSensorsAnalysisLive binpath= "D:\Projects\Sensors\AnlaysisRoot\Live\Service\QutSensors.AnalysisProcessor.exe" start= auto obj= "nt authority\LocalService" DisplayName= QutSensorsAnalysisLive
-sc start QutSensorsAnalysisLive
-echo Done
-pause
-rem use 'nt authority\localservice' with no password to install
--- QutSensors.CacheProcessor.Service/ServiceInstall.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-echo Installing QutSensors.CacheProcessor...
-sc create QutCacheProcessor binpath= "D:\SensorsCacheProcessor\QutSensors.CacheProcessor.exe" start= auto obj= ".\sensorcacheprocessor" password= qscp84^#ihascb DisplayName= QutCacheProcessor
-sc start QutCacheProcessor
-echo Done
-pause
