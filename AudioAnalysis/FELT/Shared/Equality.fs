bd056549b9fd036eaf82987cdda0360abfecec4b
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-01-25 01:16:57 +1000

    Original hash: c7f678000c97dc70d6365f8f2c32dc83adf0c693

--- AudioAnalysis/FELT/Shared/Equality.fs
+++ AudioAnalysis/FELT/Shared/Equality.fs
@@ -1,4 +1,7 @@
 ﻿module  MQUTeR.FSharp.Shared.Equality
+    open Microsoft.FSharp.Quotations
+    open Microsoft.FSharp.Quotations.Patterns
+
     let equalsOn f x (yobj:obj) =
         match yobj with
         | :? 'T as y -> (f x = f y)
@@ -10,3 +13,49 @@
         match yobj with
         | :? 'T as y -> compare (f x) (f y)
         | _ -> invalidArg "yobj" "cannot compare values of different types"
+
+
+    type tt() = class
+        member this.A with get() = 3
+        member this.B with get() = 4
+        end
+
+    let getPropertyValue<'T when 'T : equality> p o=  
+        match p with
+            | PropertyGet(_,pi,_) -> pi.GetValue(o, null) :?> 'T
+            | _ -> invalidOp "Expecting property getter expression"
+
+    let equals (l: Expr<'T> list) x y = 
+        
+        let test t = 
+            (getPropertyValue t x) = (getPropertyValue t y)
+    
+        List.forall test l
+
+
+
+    let n =
+        let e = fun x -> x.GetType() 
+        //let v = e(3)
+        let n (v: 'a when 'a : equality) = <@ v = v @>
+        e
+    (*
+
+     override x.Equals(yobj) =
+            let t = x.Value
+            match yobj with
+            | :? BaseValue<'T> as y -> Unchecked.equals x.Value y.Value
+            | _ -> false
+    
+        override x.GetHashCode() = Unchecked.hash x.Value
+ 
+        interface System.IComparable with
+            member x.CompareTo yobj =
+                match yobj with
+                | :? BaseValue<'T> as y -> Unchecked.compare x.Value y.Value
+                | _ -> invalidArg "yobj" "cannot compare values of different types"
+   
+            end
+
+
+            *)
--- AudioAnalysis/FELT/Shared/Script.fsx
+++ AudioAnalysis/FELT/Shared/Script.fsx
@@ -3,9 +3,40 @@
 // Note that script files will not be part of the project build.
 
 #load "Operators.fs"
-#load "CSV.fs"
 
-open Felt.Shared.Utilities
-open Felt.Shared.CSV
 
-rowToList "Hello, this is  a,test,\"hell,lo\", to see,, what happen, s" ',';;
+//open Felt.Shared.Utilities
+//open Felt.Shared.CSV
+
+//rowToList "Hello, this is  a,test,\"hell,lo\", to see,, what happen, s" ',';;
+
+
+type aClass(valA:int) =
+    //let aVal:int = valA
+//    static member ci = ComparisonIdentity.Structural<aClass>
+//    static member hi = HashIdentity.Structural<aClass>
+
+    member this.Value
+            with get() = valA //aVal
+
+//    override x.Equals(yobj) =
+//        match yobj with
+//        | :? aClass as y ->  aClass.hi.Equals(x, y)
+//        | _ -> false
+//    
+//    override x.GetHashCode() =    aClass.hi.GetHashCode(x)
+// 
+//    interface System.IComparable with
+//        member x.CompareTo yobj =
+//            match yobj with
+//            | :? aClass as y -> Unchecked.compare x.Value y.Value
+//            | _ -> invalidArg "yobj" "cannot compare values of different types"
+//   
+//        end
+
+
+
+
+let a1 = new aClass(3)
+let a2 = new aClass(3)
+let aEquals = a1 = a2
