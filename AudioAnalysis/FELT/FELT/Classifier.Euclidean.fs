82307e7c0d737a9b3488b686b78d5fa71e27e272
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-01-31 06:53:19 +1000

    Original hash: 488b18c7a2726e452d88dfa43077d2c91fb208cb

--- AudioAnalysis/FELT/FELT/Classifier.Euclidean.fs
+++ AudioAnalysis/FELT/FELT/Classifier.Euclidean.fs
@@ -1,10 +1,41 @@
 ﻿namespace FELT.Classifiers
     open FELT.Classifiers
+    open MQUTeR.FSharp.Shared
+    open Microsoft.FSharp.Collections
+    open MQUTeR.FSharp.Shared
 
     type EuclideanClassifier() = class
         inherit ClassifierBase()
         
-        override this.Classify (dataA, dataB) =
+        let deMap (m: Map<ColumnHeader, Value[]>) = 
+            let colsWithNames = Map.toArray m
+            let cols = Array.map snd colsWithNames
+            // reversing array (i.e. row will be first index)
+            Array2D.init cols.[0].Length m.Count (fun rowIndex colIndex -> cols.[colIndex].[rowIndex])
+
+        let distance (a: Value[]) (b: Value[]) = 
+            match (a, b) with
+                | (IsNumbers nsa, IsNumbers nsb) -> Maths.euclideanDist (unwrap nsa) (unwrap nsb)
+                | _ -> failwith "Not implemented for types other than number" 
+            
+
+        override this.Classify (trainingData, testData) =
+
+            let colCount = trainingData.Instances.Count
+            if colCount = testData.Instances.Count then
+                failwith "Input data sets must have same number of columns"
+
+            // for each vector (t) in test data (td)
+                // measure distance to each vector (s) in training data (sd)
+
+            // we dont need column names so much for this
+            let trd = deMap trainingData.Instances
+            let ted = deMap testData.Instances
+              
+            let distances = Array2D.init (Array2D.length1 trd) (Array2D.length1 ted) (fun tedIdx trdIdx -> distance (Array2D.getRow tedIdx ted) (Array2D.getRow trdIdx trd))
+
+
+
             new obj()
 
         end
