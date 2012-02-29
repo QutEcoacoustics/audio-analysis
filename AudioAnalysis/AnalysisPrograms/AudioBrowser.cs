4d2ff3673e6ee77f9c68e79dcec7b257f5dbf9fa
Michael Towsey <m.towsey@qut.edu.au>
2012-03-01 06:30:00 +1000

    Original hash: c8650ab84773bdd83a1e3e60a195226d51b5e18d

--- AudioAnalysis/AnalysisPrograms/AudioBrowser.cs
+++ AudioAnalysis/AnalysisPrograms/AudioBrowser.cs
@@ -4,25 +4,67 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+
 //using AudioTools.AudioUtlity;
 using AudioAnalysisTools;
+using TowseyLib;
 
 namespace AnalysisPrograms
 {
+
     class AudioBrowser : Form 
     {
+
+
+        /// <summary>
+        /// a set of parameters derived from ini file
+        /// </summary>
+        public struct Parameters
+        {
+            public int frameLength, resampleRate, segmentDuration;
+            public double frameOverlap;
+            public int trackHeight , DRAW_SONOGRAMS;
+            public string csvPath, visualIndexPath, recordingPath, outputDir, AudacityPath;
+
+            public Parameters(string dummy)
+            {
+                segmentDuration = 1;
+                resampleRate = 17640;
+                frameLength = 512;
+                frameOverlap = 0.0;
+                //string workingDir = null;
+
+                csvPath         = @"C:\SensorNetworks\WavFiles\SunshineCoast\AcousticIndices_DM420062_WeightedIndices.csv";
+                visualIndexPath = @"C:\SensorNetworks\WavFiles\SunshineCoast\AcousticIndices_DM420062_WeightedIndices.png";
+                recordingPath   = @"Z:\Site 4\DM420062.mp3";
+                outputDir       = @"C:\SensorNetworks\WavFiles\SunshineCoast\";
+                AudacityPath    = @"C:\Program Files (x86)\Audacity 1.3 Beta (Unicode)\audacity.exe";
+
+                DRAW_SONOGRAMS = 0;
+                trackHeight = 20;
+            } //Parameters
+        } //struct Parameters
+        Parameters parameters;
+
+
+
 
         private Panel leftPanel = new Panel();
         private Panel rightPanel = new Panel();
         private Panel indexPanel = new Panel();
+        private Panel barTrackPanel = new Panel();
         private Panel sonogramPanel = new Panel();
         private HScrollBar sonogramPanel_hScrollBar = new HScrollBar();
+        PictureBox indexPicture;
         PictureBox sonogramPicture;
+        string recordingSegmentPath;
 
 
-        internal Button Button1;
-        internal TextBox timePosition;
-        internal TextBox segmentFileName;
+        internal Button loadIndicesButton;
+        internal Button audacityButton;
+        internal TextBox time_TextBox;
+        internal TextBox segmentName_TextBox;
 
 
 
@@ -36,20 +78,25 @@ namespace AnalysisPrograms
         private RadioButton radSpanish = new RadioButton();
 
 
-        int resampleRate = 0;
-        string workingDir = null;
-        int frameLength = 256;
-        double frameOverlap = 0.0;
-
-
 
         public static void Main() 
         {
-            Application.Run(new AudioBrowser());
+            string exeDir = System.Environment.CurrentDirectory;
+            string[] args = System.Environment.GetCommandLineArgs();
+            string iniPath = exeDir+"\\AudioBrowser.ini";
+            if(args.Length == 1) iniPath = args[0];
+
+            Application.Run(new AudioBrowser(iniPath));
         }
 
-        public AudioBrowser() //constructor
+        public AudioBrowser(string iniPath) //constructor
         {
+            int verbosity = 0;
+            //AcousticIndices.Parameters parameters = AcousticIndices.ReadIniFile(iniPath, verbosity);
+
+            string dummy = "";
+            parameters = new Parameters(dummy);
+
             initaliseButtons();
             setUpPanels();
             //setUpRadioButtons();
@@ -74,14 +121,21 @@ namespace AnalysisPrograms
            // this.FlowLayoutPanel1.SuspendLayout();
            // this.SuspendLayout();
 
+            int visualIndex_TrackCount  = 17;
+            int visualIndex_TrackHeight = 20;
+            int visualIndex_ImageHeight = visualIndex_TrackCount * visualIndex_TrackHeight;
+
             int windowWidth = 1760;
             int windowHeight = 750;
             int leftPanelWidth = 200;
             int leftPanelHeight = windowHeight;
             int rightPanelWidth = windowWidth - leftPanelWidth;
             int rightPanelHeight = windowHeight;
+
             int indexPanelWidth = rightPanelWidth;
-            int indexPanelHeight = windowHeight/2;
+            int indexPanelHeight = visualIndex_ImageHeight;
+            int barTrackPanelWidth  = rightPanelWidth;
+            int barTrackPanelHeight = visualIndex_TrackHeight;
             int sonogramPanelWidth = rightPanelWidth;
             int sonogramPanelHeight = windowHeight/2;
 
@@ -111,9 +165,15 @@ namespace AnalysisPrograms
             indexPanel.Location = new Point(0, 0);
             indexPanel.Size = new Size(rightPanelWidth, indexPanelHeight);
 
+            // Set color, location and size of the barTrackPanel panel
+            barTrackPanel.BackColor = Color.BlanchedAlmond;
+            barTrackPanel.Location = new Point(0, indexPanelHeight);
+            barTrackPanel.Size = new Size(barTrackPanelWidth, barTrackPanelHeight);
+
+
             // Set color, location and size of the sonogramPanel panel
             sonogramPanel.BackColor = Color.DarkGray;
-            sonogramPanel.Location = new Point(0, indexPanelHeight);
+            sonogramPanel.Location = new Point(0, indexPanelHeight + barTrackPanelHeight);
             sonogramPanel.Size = new Size(rightPanelWidth, sonogramPanelHeight);
             sonogramPanel.Controls.Add(this.sonogramPanel_hScrollBar);
             this.sonogramPanel_hScrollBar.LargeChange = 240;
@@ -128,6 +188,7 @@ namespace AnalysisPrograms
             this.Controls.Add(leftPanel);
             this.Controls.Add(rightPanel);
             rightPanel.Controls.Add(indexPanel);
+            //rightPanel.Controls.Add(barTrackPanel);
             rightPanel.Controls.Add(sonogramPanel);
 
             this.leftPanel.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
@@ -135,38 +196,51 @@ namespace AnalysisPrograms
                 | System.Windows.Forms.AnchorStyles.Right);
             this.leftPanel.AutoScroll = true;
             this.leftPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.leftPanel.Controls.AddRange(new System.Windows.Forms.Control[] { this.Button1, this.timePosition, this.segmentFileName});
+            this.leftPanel.Controls.AddRange(new System.Windows.Forms.Control[] { this.loadIndicesButton, this.audacityButton, this.time_TextBox, this.segmentName_TextBox});
 
 
             //this.visualIndex.MouseHover += new System.EventHandler(this.image_MouseHover);
             this.visualIndex.MouseMove += new MouseEventHandler(this.image_MouseMove);
             this.visualIndex.MouseClick += new MouseEventHandler(this.image_MouseClick);
+            this.visualIndex.MouseHover += new System.EventHandler(this.image_MouseHover);
+            
         }
 
 
 
         public void initaliseButtons()
         {
-            this.Button1 = new System.Windows.Forms.Button();
-            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.Button1.Location = new System.Drawing.Point(16, 20);
-            this.Button1.Name = "Load Index File";
-            this.Button1.Size = new System.Drawing.Size(120, 24);
-            this.Button1.TabIndex = 0;
-            this.Button1.Text = "Load Index File";
-            this.Button1.Click += new System.EventHandler(this.button1_Click);
+            this.loadIndicesButton = new System.Windows.Forms.Button();
+            this.loadIndicesButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.loadIndicesButton.Location = new System.Drawing.Point(16, 20);
+            this.loadIndicesButton.Name = "Load Index File";
+            this.loadIndicesButton.Size = new System.Drawing.Size(120, 24);
+            this.loadIndicesButton.TabIndex = 0;
+            this.loadIndicesButton.Text = "Load Index File";
+            this.loadIndicesButton.Click += new System.EventHandler(this.loadIndicesButton_Click);
 
-            this.timePosition = new TextBox();
-            timePosition.Top = 100;
-            timePosition.Left = 10;
 
-            this.segmentFileName = new TextBox();
-            segmentFileName.Top = 200;
-            segmentFileName.Left = 10;
+            this.audacityButton = new System.Windows.Forms.Button();
+            this.audacityButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.audacityButton.Location = new System.Drawing.Point(16, 300);
+            this.audacityButton.Name = "Audacity";
+            this.audacityButton.Size = new System.Drawing.Size(120, 24);
+            this.audacityButton.TabIndex = 0;
+            this.audacityButton.Text = "Load in Audacity";
+            this.audacityButton.Click += new System.EventHandler(this.audacityButton_Click);
+
+
+            this.time_TextBox = new TextBox();
+            time_TextBox.Top = 100;
+            time_TextBox.Left = 10;
+
+            this.segmentName_TextBox = new TextBox();
+            segmentName_TextBox.Top = 200;
+            segmentName_TextBox.Left = 10;
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void loadIndicesButton_Click(object sender, EventArgs e)
         {
             // Wrap the creation of the OpenFileDialog instance in a using statement to ensure proper disposal
             //using (OpenFileDialog dlg = new OpenFileDialog())
@@ -186,26 +260,83 @@ namespace AnalysisPrograms
             //    }
             //}
 
-            string fileName = @"C:\SensorNetworks\WavFiles\SunshineCoast\AcousticIndices_DM420062_WeightedIndices.png";
-            visualIndex.Image = new Bitmap(fileName);
+            //USE FOLLOWING THREE LINES TO LOAD A PNG IMAGE
+            //visualIndex.Image = new Bitmap(parameters.visualIndexPath);
+            //visualIndex.Dock = DockStyle.Fill;
+            //indexPanel.Controls.Add(visualIndex);
+
+            //USE FOLLOWING LINES TO LOAD A CSV FILE
+            var tuple = FileTools.ReadCSVFile(parameters.csvPath);
+            var headers = tuple.Item1;  //List<string>
+            var values  = tuple.Item2;  //List<double[]>> 
+
+            visualIndex.Image = ConstructIndexImage(headers, values, indexPanel.Width, parameters.trackHeight);
             visualIndex.Dock = DockStyle.Fill;
             indexPanel.Controls.Add(visualIndex);
-        }//button1_Click
+            
+        }//loadIndicesButton_Click
 
 
-        //private void image_MouseHover(object sender, EventArgs e)
-       // {
-        //    int myX = Form.MousePosition.X;
-        //    //int myX = e.X;
-        //    //int myY = e.Y;
-        //    //this.visualIndex.
-        //    this.timePosition.Text = myX.ToString(); // pixel position = minutes
-        //}
+        public static Bitmap ConstructIndexImage(List<string> headers, List<double[]> values, int imageWidth, int trackHeight)
+        {
+            int headerCount = headers.Count;
+            bool[] displayColumn = { false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true};
+            double threshold = 0.5;
+
+            int trackCount = DataTools.CountTrues(displayColumn) +3; //+2 for top and bottom time tracks
+            int imageHt = trackHeight * trackCount;
+            Bitmap compositeBmp = new Bitmap(imageWidth, imageHt);
+            Graphics gr = Graphics.FromImage(compositeBmp);
+            int duration = 1436; //time in minutes
+            int scale = 60;
+            int offset = 0;
+            Bitmap timeBmp = Image_Track.DrawTimeTrack(duration, scale, imageWidth, trackHeight, "Time (hours)");
+            gr.DrawImage(timeBmp, 0, offset);
+
+
+            offset += trackHeight;
+            for (int i = 0; i < displayColumn.Length; i++) //for pixels in the line
+            {
+                if (!displayColumn[i]) continue;
+                if(i >= headerCount) break;
+                Bitmap bmp = Image_Track.DrawBarScoreTrack(values[i], trackHeight, threshold, headers[i]);
+                gr.DrawImage(bmp, 0, offset);
+                gr.DrawString(headers[i], new Font("Arial", 9), Brushes.Black, new PointF(duration + 5, offset));
+                offset += trackHeight;
+            }
+            gr.DrawImage(timeBmp, 0, offset);
+            return compositeBmp;
+        }
+
+
+
+
+        private void audacityButton_Click(object sender, EventArgs e)
+        {
+            OpenAudacity(this.recordingSegmentPath);
+        }//audacityButton_Click
+
+
+
+
+
+        private void image_MouseHover(object sender, EventArgs e)
+        {
+            visualIndex.Cursor = Cursors.Cross;
+        }
 
         private void image_MouseMove(object sender, EventArgs e)
         {
             int myX = Form.MousePosition.X - rightPanel.Left - this.Left - 8; //8 = border width - must be better way to do this!!!!!
-            this.timePosition.Text = myX.ToString(); // pixel position = minutes
+            string text = (myX / 60) + "hr:" + (myX % 60) + "min (" + myX + ")";
+            this.time_TextBox.Text = text; // pixel position = minutes
+
+            //show mouse position in barGraph
+            //Graphics g = barTrackPanel.CreateGraphics();
+            //Point pt1 = new Point(this.barTrackPanel.Left + myX, this.barTrackPanel.Top);
+            //Point pt2 = new Point(this.barTrackPanel.Left + myX + 100, this.barTrackPanel.Bottom);
+            //g.DrawLine(new Pen(Color.Red, 2.0F), pt1, pt2);
+
         }
 
         private void image_MouseClick(object sender, MouseEventArgs e)
@@ -214,36 +345,42 @@ namespace AnalysisPrograms
             int myY = e.Y;
 
 
-            Point pt1 = new Point(this.visualIndex.Left + myX, this.visualIndex.Bottom-34);
-            Point pt2 = new Point(this.visualIndex.Left + myX, this.visualIndex.Bottom -2);
+            Point pt1 = new Point(this.visualIndex.Left + myX, this.rightPanel.Top);
+            Point pt2 = new Point(this.visualIndex.Left + myX, this.rightPanel.Bottom - 2);
+            Graphics g = rightPanel.CreateGraphics();
 
-            Graphics g = visualIndex.CreateGraphics();
+            //Point pt1 = new Point(this.visualIndex.Left + myX, this.rightPanel.Top);
+            //Point pt2 = new Point(this.visualIndex.Left + myX, this.rightPanel.Bottom);
+            //Graphics g = barTrackPanel.CreateGraphics();
+
             g.DrawLine(new Pen(Color.Red, 2.0F), pt1, pt2);
 
 
             //double startHr = 2.0;
             //double endHr   = 2.2;
-            int startMilliseconds = (myX-1) * 60000;
-            if (startMilliseconds < 0) startMilliseconds = 0;
+            int startMilliseconds = (myX) * 60000;
             int endMilliseconds   = (myX + 1) * 60000;
+            if (parameters.segmentDuration == 3)
+            {
+                startMilliseconds = (myX - 1) * 60000;
+                endMilliseconds   = (myX + 2) * 60000;
+            }
+            if (startMilliseconds < 0) startMilliseconds = 0;
             //Console.WriteLine("\nWAIT - extracting segment!");
 
 
             //string recordingPath = @"C:\SensorNetworks\WavFiles\SunshineCoast\AcousticIndices_browserTemp.wav";
-            string recordingPath = @"Z:\Site 4\DM420062.mp3";
-            this.resampleRate = 17640;
-            this.frameLength = 512;
-            this.frameOverlap = 0.5;
-            string outputDir = @"C:\SensorNetworks\WavFiles\SunshineCoast\";
             //this.segmentFileName.Text = "WAIT!";
-            AudioRecording recording = AudioRecording.GetSegmentFromAudioRecording(recordingPath, startMilliseconds, endMilliseconds, this.resampleRate, outputDir);
-            this.segmentFileName.Text = recording.FileName;
+            AudioRecording recording = AudioRecording.GetSegmentFromAudioRecording(parameters.recordingPath, startMilliseconds, endMilliseconds, parameters.resampleRate, parameters.outputDir);
+            this.segmentName_TextBox.Text = recording.FileName;
+
+            this.recordingSegmentPath = recording.FilePath;
 
             //make the sonogram
             SonogramConfig sonoConfig = new SonogramConfig(); //default values config
-            sonoConfig.SourceFName = recording.FileName;
-            sonoConfig.WindowSize = frameLength;
-            sonoConfig.WindowOverlap = frameOverlap;
+            sonoConfig.SourceFName   = recording.FileName;
+            sonoConfig.WindowSize    = parameters.frameLength;
+            sonoConfig.WindowOverlap = parameters.frameOverlap;
             BaseSonogram sonogram = new SpectralSonogram(sonoConfig, recording.GetWavReader());
 
             //prepare the image
@@ -252,6 +389,7 @@ namespace AnalysisPrograms
             using (System.Drawing.Image img = sonogram.GetImage(doHighlightSubband, add1kHzLines))
             using (Image_MultiTrack image = new Image_MultiTrack(img))
             {
+                if (sonogramPicture != null) sonogramPicture.Dispose(); //get rid of previous sonogram
                 //add time scale
                 image.AddTrack(Image_Track.GetTimeTrack(sonogram.Duration, sonogram.FramesPerSecond));
                 sonogramPicture = new PictureBox();
@@ -262,10 +400,25 @@ namespace AnalysisPrograms
                 this.sonogramPanel_hScrollBar.Maximum = img.Width + this.ClientSize.Width;
                 this.sonogramPanel_hScrollBar.Value = 0;
                 this.sonogramPanel_hScrollBar.Visible = true;
-
             }
-
         }
+
+
+        /// <summary>
+        /// Opens Audacity but NOT in separate thread.
+        /// </summary>
+        /// <param name="recordingPath"></param>
+        private void OpenAudacity(string recordingPath)
+        {
+            string audacityDir = Path.GetDirectoryName(parameters.AudacityPath);
+            DirectoryInfo dirInfo = new DirectoryInfo(audacityDir); 
+            string appName = Path.GetFileName(parameters.AudacityPath);
+            ProcessRunner process = new ProcessRunner(dirInfo, appName, recordingPath);
+            process.Start();
+            var consoleOutput = process.OutputData;
+            var errorData     = process.ErrorData;
+        }
+
 
         private void PaintLineOnPanel(object sender, PaintEventArgs e)
         {
@@ -297,7 +450,7 @@ namespace AnalysisPrograms
 
             // Display the current values in the title bar.
             //this.segmentFileName.Text = "x = " + this.panel1.Location.X + ", y = " + this.panel1.Location.Y;
-            this.segmentFileName.Text = "x = " + this.sonogramPanel_hScrollBar.Value;
+            this.segmentName_TextBox.Text = "x = " + this.sonogramPanel_hScrollBar.Value;
         }
 
 
