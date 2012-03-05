c21702f829d20e7f250181066fac16b25f074b5a
Michael Towsey <m.towsey@qut.edu.au>
2012-03-05 15:30:47 +1000

    Original hash: 8632339543ad034196ba844e44a603629456aab3

---
+++ AudioAnalysis/TowseyLib/TextBoxStreamWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TowseyLib
+{
+    public class TextBoxStreamWriter : TextWriter
+    {
+        TextBox _output = null;
+
+        public TextBoxStreamWriter(TextBox output)
+        {
+            _output = output;
+        } //TextBoxStreamWriter()
+
+        /// <summary>
+        /// Override the Write(char value) method, which is called when character data is written to the stream.
+        /// </summary>
+        /// <param name="value"></param>
+        public override void Write(char value)
+        {
+            base.Write(value);
+            _output.AppendText(value.ToString()); // When character data is written, append it to the text box.
+        }
+
+        /// <summary>
+        /// Override the abstract “Encoding” property. 
+        /// </summary>
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+
+
+    } //class TextBoxStreamWriter
+}
