289c22a1672e369ebf20f8e13d66ee639b12b2ba
Michael Towsey <m.towsey@qut.edu.au>
2012-03-08 11:34:36 +1000

    Added AudioBrowser project

---
+++ AudioAnalysis/AudioBrowser/AudioBrowser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AudioBrowser
+{
+    static class AudioBrowser
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string[] args = System.Environment.GetCommandLineArgs();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AudioBrowser1(args));
+            //Application.Run(new AudioBrowser2(args));
+        }
+
+
+    }
+}
