f5128ae15e7cae1008fdf86b9a3901fedcbd83b7
Michael Towsey <m.towsey@qut.edu.au>
2012-03-15 16:45:37 +1000

    Original hash: d8dc0a20c4c420c86e2531d2dee83c8dbb3d973f

---
+++ AudioAnalysis/AnalysisPrograms/AcousticIndicesAudioProcessor.cs
@@ -0,0 +1,31 @@
+﻿namespace AnalysisPrograms
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    using AudioTools.AudioUtlity;
+
+    using QutSensors.Shared;
+    using QutSensors.Shared.Tools;
+
+
+    class AcousticIndicesAudioProcessor : AudioProcessors.AbstractAudioProcessor
+    {
+
+
+
+        protected abstract DataTable Analysis(AnalysisPrograms.AudioProcessors.AudioProcessorConfig config, FileInfo segmentAudioFile)
+        {
+            var data = new DataTable();
+            return data;
+        }
+
+
+    }
+}
---
+++ AudioAnalysis/AnalysisPrograms/AudioProcessorConfig.cs
@@ -0,0 +1,36 @@
+﻿namespace AnalysisPrograms.AudioProcessors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AudioProcessorConfig : Dictionary<string, object>
+    {
+        public IEnumerable<double> GetDoubles(string keyString)
+        {
+            if (string.IsNullOrEmpty(keyString))
+            {
+                return new List<double>();
+            }
+
+            var key = this[keyString];
+
+            var doublearray = key as double[];
+
+            if (doublearray != null)
+            {
+                return doublearray;
+            }
+
+            var doublelist = key as List<double>;
+
+            if (doublelist != null)
+            {
+                return doublelist;
+            }
+
+            var result = keyString.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(i => double.Parse(i));
+            return result;
+        }
+    }
+}
