309202f64035ed791524eca091f0c0a0ecf6ab6f
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-04-21 16:36:26 +1000

    Original hash: 6a36349df46b6732b899764121e4791b658b36fc

---
+++ AudioAnalysis/FELT/Shared/Microsoft.FSharp.Collections.Set.fs
@@ -0,0 +1,27 @@
+﻿namespace Microsoft.FSharp.Collections
+    
+    open System
+    
+    module Set = 
+
+        let countSubsets s =
+            Set.count s |> pown 2 |> decrement
+
+        let countSubsetsIncludeEmpty s =
+            pown 2 (Set.count s)
+
+        let powerset s = 
+            let rec loop n l =
+                seq {
+                      match n, l with
+                      | 0, _  -> yield []
+                      | _, [] -> ()
+                      | n, x::xs -> yield! Seq.map (fun l -> x::l) (loop (n-1) xs)
+                                    yield! loop n xs
+                }   
+            let xs = s |> Set.toList     
+            seq {
+                for i = 0 to List.length xs do
+                    for x in loop i xs -> set x
+            }
+            
