729c80a3223fdc93a8af82fdf59db2764da95ceb
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2012-05-11 16:57:37 +1000

    Original hash: 6905c77b97ed15c3eb40bd81b8ef319e5f867339

---
+++ AudioAnalysis/AnalysisPrograms/Process/AcousticIndicesAnalysis.cs
@@ -0,0 +1,37 @@
+﻿namespace AnalysisPrograms
+{
+    using System.Data;
+    using System.IO;
+
+    using AnalysisBase;
+
+    /// <summary>
+    /// </summary>
+    public class AcousticIndicesAudioProcessor : IAnalysis
+    {
+        /// <summary>
+        /// Gets the initial settings for the analysis. These are the default settings.
+        /// </summary>
+        public AnalysisSettings InitialSettings
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Run analysis usign the given analysis settings.
+        /// </summary>
+        /// <param name="analysisSettings">
+        /// The analysis Settings.
+        /// </param>
+        /// <returns>
+        /// The results of the analysis.
+        /// </returns>
+        public AnalysisResult Analyse(AnalysisSettings analysisSettings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- AudioAnalysis/AnalysisPrograms/Process/AnalysisRunner.cs
+++ AudioAnalysis/AnalysisPrograms/Process/AnalysisRunner.cs
@@ -5,11 +5,21 @@
     using System.Linq;
     using System.Text;
 
+    using AnalysisBase;
+
     /// <summary>
     /// Runs analyses that implement IAnalysis.
     /// </summary>
     public class AnalysisRunner
     {
+        public AnalysisRunner()
+        {
+            
+        }
 
+        public void RunSingle(IAnalysis analysis)
+        {
+            
+        }
     }
 }
