02e4dda3e04b203fe582ace8f1a6aef950e067d0
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-06-03 22:35:36 +1000

    Original hash: 63431c00c3fd3b9c8f074df67ce0fb861518b836

--- AudioAnalysis/FELT/FELT/AssemblyInfo.fs
+++ AudioAnalysis/FELT/FELT/AssemblyInfo.fs
@@ -33,6 +33,6 @@
     // You can specify all the values or you can default the Build and Revision Numbers
     // by using the ‘*’ as shown below:
     // [assembly: AssemblyVersion("1.0.*")]
-    [<assembly: AssemblyVersion("1.0.993.3139")>]
-    [<assembly: AssemblyFileVersion("1.0.993.3139")>]
+    [<assembly: AssemblyVersion("1.0.1005.3141")>]
+    [<assembly: AssemblyFileVersion("1.0.1005.3141")>]
     ()
