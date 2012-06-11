be86c0af8129fef295b92afdf91a31ba7774281e
Michael Towsey <m.towsey@qut.edu.au>
2012-06-12 08:05:46 +1000

    Original hash: 409fc1c3889b73f4d4b989b744cb2da401a44815

---
+++ AudioAnalysis/AudioAnalysisTools/Keys.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioAnalysisTools
+{
+    public static class Keys
+    {
+
+
+        //KEYS TO PARAMETERS IN CONFIG FILE
+        public static string ANNOTATE_SONOGRAM  = "ANNOTATE_SONOGRAM";
+        public static string ANALYSIS_NAME = "ANALYSIS_NAME";
+        public static string CALL_DURATION = "CALL_DURATION";
+        public static string DECIBEL_THRESHOLD = "DECIBEL_THRESHOLD";
+        public static string DRAW_SONOGRAMS = "DRAW_SONOGRAMS";
+        public static string DISPLAY_COLUMNS = "DISPLAY_COLUMNS";
+        public static string EVENT_THRESHOLD = "EVENT_THRESHOLD";
+        public static string FRAME_LENGTH = "FRAME_LENGTH";
+        public static string FRAME_OVERLAP = "FRAME_OVERLAP";
+        public static string INTENSITY_THRESHOLD = "INTENSITY_THRESHOLD";
+        public static string MIN_HZ = "MIN_HZ";
+        public static string MAX_HZ = "MAX_HZ";
+        public static string MIN_GAP = "MIN_GAP";
+        public static string MAX_GAP = "MAX_GAP";
+        public static string MIN_AMPLITUDE = "MIN_AMPLITUDE";
+        public static string MIN_DURATION = "MIN_DURATION";
+        public static string MAX_DURATION = "MAX_DURATION";
+        public static string NOISE_REDUCTION_TYPE = "NOISE_REDUCTION_TYPE";
+        public static string NOISE_DO_REDUCTION = "DO_NOISE_REDUCTION";
+        public static string NOISE_BG_REDUCTION = "BG_NOISE_REDUCTION";
+        public static string RESAMPLE_RATE = "RESAMPLE_RATE";
+        public static string SEGMENT_DURATION = "SEGMENT_DURATION";
+        public static string SEGMENT_OVERLAP = "SEGMENT_OVERLAP";
+
+
+
+
+
+        //KEYS TO OUTPUT INDICES
+        public static string COUNT        = "count";
+        public static string SEGMENT_TIMESPAN = "SegTimeSpan";
+        public static string AV_AMPLITUDE = "avAmp-dB";
+        public static string START_MIN    = "start-min";
+        public static string CALL_DENSITY = "CallDensity";
+        public static string CALL_SCORE   = "CallScore";
+
+        //KEYS TO OUTPUT EVENTS
+        public static string EVENT_START_ABS = "EvStartAbs";
+        public static string EVENT_START_SEC = "EvStartSec";
+        public static string EVENT_START_MIN = "EvStartMin";
+        public static string EVENT_TOTAL     = "# events";
+    }
+}
