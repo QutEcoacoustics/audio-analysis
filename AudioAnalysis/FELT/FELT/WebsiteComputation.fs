117cef466c3788429e816adab5230ea22b820bbb
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-06-14 22:13:56 +1000

    Original hash: 724fc6acd8bf9966aab0a21f60443c675b89e208

---
+++ AudioAnalysis/FELT/FELT/WebsiteComputation.fs
@@ -0,0 +1,28 @@
+﻿namespace FELT.Results
+
+    open System.Reflection
+    open OfficeOpenXml
+    open System
+    open MQUTeR.FSharp.Shared
+    open MQUTeR.FSharp.Shared.Maths
+    open Microsoft.FSharp.Collections
+    open Microsoft.FSharp.Core
+    open System.IO
+    open FELT.Classifiers
+    open MQUTeR.FSharp.Shared.IO
+
+
+    type ResultsComputation(config:ReportConfig) = class
+        let OnePlace:Place = 1
+        
+
+
+        
+
+        /// warning this class by default involves a lot of mutation and intrinsically causes side-affects
+        member this.Calculate (trainingData:Data) (testData:Data) (classificationResults:ClassifierResult) (opList: (string * string * string) list) =
+
+            3.0
+
+
+        end
