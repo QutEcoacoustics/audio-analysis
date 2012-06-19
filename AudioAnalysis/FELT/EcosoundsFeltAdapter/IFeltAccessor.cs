59fbbdb63a2b34a88fa741393bdf8225c0e08f8e
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-06-19 22:11:50 +1000

    - adjusted the REST API so that proper responses are sent back for HTTP StatusCodes - started debugging my felt stuff

---
+++ AudioAnalysis/FELT/EcosoundsFeltAdapter/IFeltAccessor.cs
@@ -0,0 +1,21 @@
+namespace EcosoundsFeltAdapter
+{
+    using System.Collections.Generic;
+
+    using MQUTeR.FSharp.Shared;
+
+    public interface IFeltAccessor
+    {
+        /// <summary>
+        /// The search.
+        /// </summary>
+        /// <returns>
+        /// The search.
+        /// </returns>
+        Dictionary<string, int> Search(Dictionary<string, Value> values, int limit);
+
+        bool IsSearchAvailable { get; }
+
+        string[] SearchUnavilabilityMessages { get; }
+    }
+}
---
+++ AudioAnalysis/FELT/EcosoundsFeltAdapter/UnavailableFeltAccessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EcosoundsFeltAdapter
+{
+    using MQUTeR.FSharp.Shared;
+
+    public class UnavailableFeltAccessor : IFeltAccessor
+    {
+        private string[] messages;
+
+        public UnavailableFeltAccessor(params string[] messages)
+        {
+            this.messages = messages;
+        }
+
+        public Dictionary<string, int> Search(Dictionary<string, Value> values, int limit)
+        {
+            throw new InvalidOperationException("No search is possible at this time.");
+        }
+
+        public bool IsSearchAvailable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string[] SearchUnavilabilityMessages
+        {
+            get
+            {
+                return this.messages;
+            }
+        }
+    }
+}
