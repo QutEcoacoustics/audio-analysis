dc5a02e9a786324af97e964031ac04d6cf256e1d
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2012-07-23 16:13:31 +1000

    Original hash: d4588e7a6fa5be1d0341024d57994bab381bcc8c

--- AudioAnalysis/AnalysisRunner/Program.cs
+++ AudioAnalysis/AnalysisRunner/Program.cs
@@ -153,16 +153,16 @@
 
         private static void PageEventsToCsv()
         {
-            var propNames =
-                File.ReadAllLines(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_propnames.txt");
-            var propValues =
-                File.ReadAllLines(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_propvalues.txt");
+            //var propNames =
+            //    File.ReadAllLines(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_propnames.txt");
+            //var propValues =
+            //    File.ReadAllLines(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_propvalues.txt");
 
-            var propsFile = @"C:\Work\Masters\Papers\eScience experiment documents\user12002_props.txt";
+            //var propsFile = @"C:\Work\Masters\Papers\eScience experiment documents\user12002_props.txt";
 
             StringKeyValueStore.SaveToCsv(
-                new FileInfo(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_all.csv"),
-                new FileInfo(@"C:\Work\Masters\Papers\eScience experiment documents\user12002_output.csv"));
+                new FileInfo(@"C:\Users\n5414628\Dropbox\Sensors\Mark\user12003_all.csv"),
+                new FileInfo(@"C:\Users\n5414628\Dropbox\Sensors\Mark\user12003_output.csv"));
 
         }
     }
