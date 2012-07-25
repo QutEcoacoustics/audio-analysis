e326865439e4d6fb412887fd1d13bb333c1618fd
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-07-25 21:25:17 +1000

    updated drawabox to include showing boxes only - not fully tested

--- AudioAnalysis/FELT/FELT/AssemblyInfo.fs
+++ AudioAnalysis/FELT/FELT/AssemblyInfo.fs
@@ -33,6 +33,6 @@
     // You can specify all the values or you can default the Build and Revision Numbers
     // by using the ‘*’ as shown below:
     // [assembly: AssemblyVersion("1.0.*")]
-    [<assembly: AssemblyVersion("1.1.1196.3252")>]
-    [<assembly: AssemblyFileVersion("1.1.1196.3252")>]
+    [<assembly: AssemblyVersion("1.1.1198.3280")>]
+    [<assembly: AssemblyFileVersion("1.1.1198.3280")>]
     ()
