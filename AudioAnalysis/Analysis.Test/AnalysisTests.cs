ee652762f04ca31df62703e0e352b37552df0142
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2012-07-29 16:59:10 +1000

    Original hash: 8fa83188ce45d93754983ffe932a25daac55ea7f

---
+++ AudioAnalysis/Analysis.Test/AnalysisTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ecosounds.Test.AnalysisPrograms
+{
+    using System.IO;
+
+    using EcoSounds.Mvc.Tests;
+
+    using TowseyLib;
+
+    using global::AnalysisPrograms;
+
+    [TestClass]
+    public class AnalysisTests
+    {
+        [TestMethod]
+        public void Canetoad()
+        {
+            var canetoad = new Canetoad();
+            var settings = canetoad.DefaultSettings;
+
+            var inputFile = TestHelper.GetTestAudioFile("cane toad.wav");
+            var configFile = TestHelper.GetAnalysisConfigFile(canetoad.Identifier);
+
+            settings.ConfigFile = configFile;
+            settings.AudioFile = inputFile;
+            settings.EventsFile = TestHelper.GetTempFile("csv");
+            settings.ImageFile = TestHelper.GetTempFile("csv");
+            settings.IndicesFile = TestHelper.GetTempFile("csv");
+
+            var configuration = new ConfigDictionary(settings.ConfigFile.FullName);
+            settings.ConfigDict = configuration.GetTable();
+
+            var results = canetoad.Analyse(settings);
+
+
+        }
+
+        
+    }
+}
