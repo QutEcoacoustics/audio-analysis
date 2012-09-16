56ad467a825a9e559fa57cbd65771edbb157b8b9
Anthony Truskinger <a.truskinger@qut.edu.au>
2012-09-16 22:30:07 +1000

    Original hash: 1d8e06fb0642e9eea271d627dfee5f99ffdc7798

--- AudioAnalysis/FELT/FELT/Classifier.ZScore.fs
+++ AudioAnalysis/FELT/FELT/Classifier.ZScore.fs
@@ -34,7 +34,7 @@
             d
         
         
-        override this.Classify (trainingData, testData) =
+        override this.Classify trainingData testData =
 
             let colCount = trainingData.Instances.Count
             if colCount <> testData.Instances.Count then
