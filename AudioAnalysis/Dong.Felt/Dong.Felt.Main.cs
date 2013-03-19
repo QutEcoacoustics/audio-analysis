69ae44a99055e4266a992fb7c9c29160d0f93f7e
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-03-19 17:09:07 +1000

    Original hash: fd7488c2b411dfab5f814ad31c989b2c5a771314

--- AudioAnalysis/Dong.Felt/Dong.Felt.Main.cs
+++ AudioAnalysis/Dong.Felt/Dong.Felt.Main.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +11,7 @@ using YamlDotNet.RepresentationModel;
 using YamlDotNet;
 using YamlDotNet.RepresentationModel.Serialization;
 using AudioAnalysisTools;
-using TowseyLib;
+
 
 namespace Dong.Felt
 {
@@ -68,92 +66,11 @@ namespace Dong.Felt
             //}
 
             // Writing my code here
-
             // get wav.file path
             string wavFilePath = analysisSettings.SourceFile.FullName;
             //"C:\\Test recordings\\ctest.wav";
             // Read the .wav file
-            var recording = new AudioRecording(wavFilePath);
-            //if (recording.SampleRate != 22050)
-            //{
-            //    recording.ConvertSampleRate22kHz();
-            //}
-
-            // make random acoustic events
-            // TODO: make real acoustic events
-            //var events = new List<AcousticEvent>() { 
-            //    new AcousticEvent(5.0,2.0,500,1000),   
-            //    new AcousticEvent(8.0,2.0,500,1000),
-            //    new AcousticEvent(11.0,2.0,500,1000),
-            //    new AcousticEvent(14.0,2.0,500,1000),
-            //    new AcousticEvent(17.0,2.0,500,1000),
-            //};
-
-            //foreach (var e in events)
-            //{
-            //    e.BorderColour = AcousticEvent.DEFAULT_BORDER_COLOR;
-            //}
-           
-            // generate spectrogram
-            var config = new SonogramConfig { NoiseReductionType = NoiseReductionType.NONE };
-            //var spectrogram = new SpectralSonogram(config, recording.GetWavReader());
-
-            //var image = new Image_MultiTrack(spectrogram.GetImage(false, true));
-            //image.AddTrack(Image_Track.GetTimeTrack(spectrogram.Duration, spectrogram.FramesPerSecond));
-            ////image.AddTrack(Image_Track.GetWavEnvelopeTrack(sonogram, image.sonogramImage.Width));
-            //image.AddTrack(Image_Track.GetSegmentationTrack(spectrogram));
-            //image.AddEvents(events, spectrogram.NyquistFrequency, spectrogram.Configuration.FreqBinCount, spectrogram.FramesPerSecond);
-            //image.Save("C:\\Test recordings\\Test1.png");
-            
-            // Draw a line in the spectrogram
-            //var amplitudeSpectrogram = new SpectralSonogram(config, recording.GetWavReader());
-            //var spectrogramAmplitudeMatrix = amplitudeSpectrogram.Data;
-
-            //for (int i = 0; i < 2000; i++)
-            //{
-            //    for (int j = 30; j < 50; j++)
-            //    {
-            //        spectrogramAmplitudeMatrix[i, j] = 1;
-            //    }
-            //}
-            //var imageResult = new Image_MultiTrack(amplitudeSpectrogram.GetImage(false, true));
-            //imageResult.Save("C:\\Test recordings\\Test2.png");
-            
-            // Draw a box on a customerized frequency and time range
-            var amplitudeSpectrogram = new SpectralSonogram(config, recording.GetWavReader());
-            var spectrogramAmplitudeMatrix = amplitudeSpectrogram.Data;
-            int MinFreq = 2000;
-            int MaxFreq = 3500;
-            int minFreqBin = (int)Math.Round(MinFreq / amplitudeSpectrogram.FBinWidth);
-            int maxFreqBin = (int)Math.Round(MaxFreq / amplitudeSpectrogram.FBinWidth);
-
-            int StartTime = 16;
-            int EndTime = 22;
-            int minFrameNum = (int)Math.Round(StartTime * amplitudeSpectrogram.FramesPerSecond);
-            int maxFrameNum = (int)Math.Round(EndTime * amplitudeSpectrogram.FramesPerSecond);
-
-            for (int i = minFrameNum; i < maxFrameNum; i++)
-            {
-                spectrogramAmplitudeMatrix[i, minFreqBin] = 1;
-            }
-            for (int i = minFrameNum; i < maxFrameNum; i++)
-            {
-                spectrogramAmplitudeMatrix[i, maxFreqBin] = 1;
-            }
-            for (int j = minFreqBin; j < maxFreqBin; j++)
-            {
-                spectrogramAmplitudeMatrix[minFrameNum, j] = 1;
-            }
-            for (int j = minFreqBin; j < maxFreqBin; j++)
-            {
-                spectrogramAmplitudeMatrix[maxFrameNum, j] = 1;
-            }
-            var imageResult = new Image_MultiTrack(amplitudeSpectrogram.GetImage(false, true));
-            imageResult.Save("C:\\Test recordings\\Test3.png");
-
-            // print configure dictionary
-            string printMessage = analysisSettings.ConfigDict["my_custom_setting"];
-            Log.Info(printMessage);
+            //var recording = new AudioRecording(wavFilePath);
 
             //throw new NotImplementedException();
             var result = new AnalysisResult();
@@ -183,12 +100,10 @@ namespace Dong.Felt
             //{
             //    var testDirectory = @"C:\XUEYAN\targetDirectory";
             //    string testConfig = @"C:\XUEYAN\config.yml";
-
             //    arguments = new[] { testConfig, testDirectory };
-
             //}
             string date = "# Date and Time:" + DateTime.Now;
-            // Log.Info("Read the wav. file and save it as a Spectrogram");  //
+            // Log.Info("Read the wav. file and save it as a Spectrogram"); 
             // Log.Info("Read the wav. file path"); // 14/March/2013
             Log.Info("Read the wav. file path");
             Log.Info(date);
