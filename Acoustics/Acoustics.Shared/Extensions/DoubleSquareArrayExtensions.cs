17d151d8589dfd622abda2ff29b4fbe2e0a9d8d9
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-03-27 09:04:46 +1000

    Check the bounds of points inside matrix and rectangle.

---
+++ Acoustics/Acoustics.Shared/Extensions/DoubleSquareArrayExtensions.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DoubleSquareArrayExtensions.cs" company="MQUTeR">
+//   -
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Acoustics.Shared.Extensions
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Drawing;
+
+    /// <summary>
+    /// The double square array extensions.
+    /// </summary>
+    public static class DoubleSquareArrayExtensions
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The point intersect.
+        /// </summary>
+        /// <param name="array">
+        /// The array.
+        /// </param>
+        /// <param name="point">
+        /// The point.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool PointIntersect(this double[,] array, Point point)
+        {
+            Contract.Requires(array.Rank == 2);
+
+            int dimX = array.GetLength(0);
+            int dimY = array.GetLength(1);
+
+            return point.X >= array.GetLowerBound(0) && point.X < dimX && point.Y >= array.GetLowerBound(1) && point.Y < dimY;
+        }
+
+        /// <summary>
+        /// The point intersect.
+        /// </summary>
+        /// <param name="array">
+        /// The array.
+        /// </param>
+        /// <param name="x">
+        /// The x.
+        /// </param>
+        /// <param name="y">
+        /// The y.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool PointIntersect(this double[,] array, int x, int y)
+        {
+            Contract.Requires(array.Rank == 2);
+
+            int dimX = array.GetLength(0);
+            int dimY = array.GetLength(1);
+
+
+            return x >= array.GetLowerBound(0) && x < dimX && y >= array.GetLowerBound(1) && y < dimY;
+        }
+
+        #endregion
+    }
+}
