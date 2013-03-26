933931a5bb69991278adbffbb78d4f864a9e7226
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-03-27 09:04:46 +1000

    Check the bounds of points inside matrix and rectangle. Original hash: 17d151d8589dfd622abda2ff29b4fbe2e0a9d8d9

---
+++ Acoustics/Acoustics.Shared/Extensions/RectangleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Acoustics.Shared.Extensions
+{
+    using System.Drawing;
+
+    public static class RectangleExtensions
+    {
+        public static bool PointIntersect(this Rectangle rectangle, Point point)
+        {
+            return point.X >= rectangle.Left && point.X < rectangle.Right && point.Y >= rectangle.Bottom
+                   && point.Y < rectangle.Top;
+        }
+        
+        public static bool PointIntersect(this Rectangle rectangle, int x, int y)
+        {
+            return x >= rectangle.Left && x < rectangle.Right && y >= rectangle.Bottom
+                   && y < rectangle.Top;
+        }
+    }
+}
