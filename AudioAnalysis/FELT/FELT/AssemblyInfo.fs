cb42effd21100672324ae8ba07f646b2747e4c12
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-05-09 18:26:58 +1000

    Original hash: d2d2867c067f791427aeb6f80052a59cc9470300

--- AudioAnalysis/FELT/FELT/AssemblyInfo.fs
+++ AudioAnalysis/FELT/FELT/AssemblyInfo.fs
@@ -33,7 +33,7 @@
     // You can specify all the values or you can default the Build and Revision Numbers
     // by using the ‘*’ as shown below:
     // [assembly: AssemblyVersion("1.0.*")]
-    [<assembly: AssemblyVersion("1.2.2387.3603")>]
-    [<assembly: AssemblyFileVersion("1.2.2387.3603")>]
+    [<assembly: AssemblyVersion("1.2.2422.3605")>]
+    [<assembly: AssemblyFileVersion("1.2.2422.3605")>]
 
     ()
