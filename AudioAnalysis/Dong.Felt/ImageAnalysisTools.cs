264a6283811e798daa16a0fbffb9a155f8e8e359
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-05-17 12:51:56 +1000

    Original hash: 2cab7abced2a5d58aa28a1d0834d1773ae2a931e

--- AudioAnalysis/Dong.Felt/ImageAnalysisTools.cs
+++ AudioAnalysis/Dong.Felt/ImageAnalysisTools.cs
@@ -4,8 +4,11 @@ namespace Dong.Felt
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.IO;
     using System.Text;
-    using TowseyLib; 
+    using TowseyLib;
+    using System.Data;   
+    using System.Drawing;
 
     class ImageAnalysisTools
     {
@@ -36,223 +39,197 @@ namespace Dong.Felt
         /// Canny detector for edge detection in a noisy image 
         /// it involves five steps here, first, it needs to do the Gaussian convolution, then a simple derivative operator(like Roberts Cross or Sobel operator) is 
         /// applied to the smoothed image to highlight regions of the image. Finally, it will track .....
-        /// 
-        public static void CannyDetector(double[,] m)
+        ///
+
+        // Generate the gaussian kernel
+        public static double[,] GenerateGaussianKernel(int sizeOfKernel, double sigma)
         {
-            // Do Gaussian smooth
-            var smoothedMatrix = TowseyLib.ImageTools.Convolve(m, Kernal.gaussianBlur5);
-
-            // Sobel operator
-            int MaximumXIndex = m.GetLength(0);
-            int MaximumYIndex = m.GetLength(1);
-
-            var offset = (int)(SobelX.GetLength(0) / 2);
-            //var numberOfVetex = MaximumXIndex * MaximumYIndex;
-
-            var partialDifferenceX = new double[MaximumXIndex, MaximumYIndex];
-            var partialDifferenceY = new double[MaximumXIndex, MaximumYIndex];
-
-            for (int row = 0; row < MaximumXIndex; row++)
-            {
-                for (int col = 0; col < MaximumYIndex; col++)
-                {
-                    var TempDifferenceX = 0.0;
-                    var TempDifferenceY = 0.0;
-                    for (int indexX = -offset; indexX < offset; indexX++)
-                    {
-                        for (int indexY = -offset; indexY < offset; indexY++)
-                        {
-                            // Todo : fix the out of range
-                            if ((row + indexY) >= 0 && (row + indexY) < MaximumXIndex && (col + indexX) >= 0 && (col + indexX) < MaximumYIndex)
-                            {
-                                //TempDifferenceX = m[row + indexY, col + indexX] * CannyDifferenceX[indexX + offset, indexY + offset];
-                                //TempDifferenceY = m[row + indexY, col + indexX] * CannyDifferenceY[indexX + offset, indexY + offset];
-                            }
-                        }
-                    }
-                    partialDifferenceX[row, col] = TempDifferenceX;
-                    partialDifferenceY[row, col] = TempDifferenceY;
-                }
-            }
-            //PointF
-        }
-
-        // 1. Smoothing: Blurring of the image to remove noise.
-        private void GenerateGaussianKernel(int sizeOfKernel, double sigma, out int weight)
-        {
-            double pi = (float)Math.PI;
+            double pi = (double)Math.PI;
             var kernel = new double[sizeOfKernel, sizeOfKernel];
-            var GaussianKernel = new int[sizeOfKernel, sizeOfKernel];
-            float[,] OP = new float[sizeOfKernel, sizeOfKernel];
-            double coeffients1 = 1 / (2 * pi * sigma * sigma);
+            double coeffients1 = 2 * pi * sigma * sigma;
             double coeffients2 = 2 * sigma * sigma;
 
-            int offset = sizeOfKernel / 2; 
-            double min = 1000;
-
-            for (int i = -offset; i <= offset; i++)
+            int kernelRadius = sizeOfKernel / 2; 
+ 
+            double sum = 0.0;
+            for (int i = -kernelRadius; i <= kernelRadius; i++)
             {
-                for (int j = -offset; j <= offset; j++)
+                for (int j = -kernelRadius; j <= kernelRadius; j++)
                 {
-                    kernel[offset + i, offset + j] = ((1 / coeffients1) * (float)Math.Exp(-(Math.Pow(i, 2) + Math.Pow(j, 2)) / coeffients2));
-                    if (kernel[offset + i, offset + j] < min)
-                        min = kernel[offset + i, offset + j];
+                    kernel[kernelRadius + i, kernelRadius + j] = (1 / coeffients1) * (double)Math.Exp(-(Math.Pow(i, 2) + Math.Pow(j, 2)) / coeffients2);
+                    sum += kernel[kernelRadius + i, kernelRadius + j];
+                }
+            }
+            //the process of normalizing the kernel elements            
+            for (int i = 0; i < sizeOfKernel; i++)
+            {
+                for (int j = 0; j < sizeOfKernel; j++)
+                {
+                    kernel[i, j] /= sum;
                 }
             }
 
-            //the process of normalizing the weights
-            int reciprocal = (int)(1 / min);
-            int sum = 0;
-            if ((min > 0) && (min < 1))
-            {
-                for (int i = -offset; i <= offset; i++)
-                {
-                    for (int j = -offset; j <= offset; j++)
-                    {
-                        kernel[offset + i, offset + j] = (double)Math.Round(kernel[offset + i, offset + j] * reciprocal, 0);
-                        GaussianKernel[offset + i, offset + j] = (int)kernel[offset + i, offset + j];
-                        sum = sum + GaussianKernel[offset + i, offset + j];
-                    }
-                }
-            }
-            else
-            {
-                sum = 0;
-                for (int i = -reciprocal; i <= reciprocal; i++)
-                {
-                    for (int j = -reciprocal; j <= reciprocal; j++)
-                    {
-                        kernel[reciprocal + i, reciprocal + j] = (float)Math.Round(kernel[reciprocal + i, reciprocal + j], 0);
-                        GaussianKernel[reciprocal + i, reciprocal + j] = (int)kernel[reciprocal + i, reciprocal + j];
-                        sum = sum + GaussianKernel[reciprocal + i, reciprocal + j];
-                    }
-                }
-            }
-            //Normalizing kernel Weight
-            weight = sum;
-            return;
+            return kernel;
         }
 
-        // Following subroutine removes noise by Gaussian Filtering
-        private double[,] GaussianFilter(double[,] m, int kernalSize, int kernelWeight)
+        // Gaussian Filtering, smoothing the image
+        public static double[,] GaussianFilter(double[,] m, double[,] gaussianKernel)
         {
             int MaximumXindex = m.GetLength(0);
             int MaximumYindex = m.GetLength(1);
             var result = new double [MaximumXindex, MaximumYindex];
 
-            int offset = kernalSize / 2;
-            double sum = 0.0;
-            result = m; // Removes Unwanted Data Omission due to kernel bias while convolution
+            int gaussianKernelRadius = gaussianKernel.GetLength(0) / 2;
 
-            for (int row = offset; row <= ((MaximumXindex - 1) - offset); row++)
+            for (int row = gaussianKernelRadius; row < (MaximumXindex - gaussianKernelRadius); row++)
             {
-                for (int col = offset; col <= ((MaximumYindex - 1) - offset); col++)
+                for (int col = gaussianKernelRadius; col <(MaximumYindex  - gaussianKernelRadius); col++)
                 {
-                    sum = 0;
-                    for (int i = -offset; i <= offset; i++)
+                    double sum = 0;
+                    for (int i = -gaussianKernelRadius; i <= gaussianKernelRadius; i++)
                     {
-                        for (int j = -offset; j <= offset; j++)
+                        for (int j = -gaussianKernelRadius; j <= gaussianKernelRadius; j++)
                         {
-                            // Todo: fix this statement 
-                            //sum = sum + ((float)m[i + k, j + l] * GaussianKernel[offset + k, offset + l]);
+                            sum = sum + m[row + i, col + j] * gaussianKernel[i + gaussianKernelRadius, j + gaussianKernelRadius];
                         }
                     }
-                    result[row, col] = (int)(Math.Round(sum / (float)kernelWeight));
+                    result[row, col] = sum;
                 }
             }
 
             return result;
         }
-        // 2. Finding gradients: The edges should be marked where the gradients of the image haslarge magnitudes.
-        // Sobel X and Y Masks are used to generate X & Y Gradients of Image; next function implements differentiation using sobel Filter Mask
-        //public static float[,] Differentiate(int[,] m, int[,] Filter)
-        //{
-        //    int i, j, k, l, Fh, Fw;
-        //    Fw = Filter.GetLength(0);
-        //    Fh = Filter.GetLength(1);
-        //    float sum = 0;
-        //    float[,] Output = new float[Width, Height];
 
-        //    for (i = Fw / 2; i <= (Width - Fw / 2) - 1; i++)
-        //    {
-        //        for (j = Fh / 2; j <= (Height - Fh / 2) - 1; j++)
-        //        {
-        //            sum = 0;
-        //            for (k = -Fw / 2; k <= Fw / 2; k++)
-        //            {
-        //                for (l = -Fh / 2; l <= Fh / 2; l++)
-        //                {
-        //                    sum = sum + Data[i + k, j + l] * Filter[Fw / 2 + k, Fh / 2 + l];
-        //                }
-        //            }
-        //            Output[i, j] = sum;
-        //        }
-        //    }
+        // Finding gradients(also known as the edge strenghs): The edges should be marked where the gradients of the image has large magnitudes.
+        // Sobel X and Y Masks are used to generate X & Y Gradients of Image; 
+        // here the magnitude = |GX| + |GY| , OR it can be equal to euclidean distance measure 
+        public static double[,] FindGradient(double[,] m, double[,] edgeMaskX, double[,] edgeMaskY)
+        {
+            int MaximumXindex = m.GetLength(0);
+            int MaximumYindex = m.GetLength(1);
 
-        //    return Output;
-        //}
+            double[,] result = new double[MaximumXindex, MaximumYindex];
+            int edgeMaskRadius = edgeMaskX.GetLength(0) / 2;
 
-        // 3. Non-maximum suppression: Only local maxima should be marked as edges.
-        // We find gradient direction and using these direction we perform non maxima suppression (Read “Digital Image Processing- by R Gonzales-Pearson Education)
-        // Perform Non maximum suppression:
-           // NonMax = Gradient;
-            //for (i = 0; i <= (Width - 1); i++)
-            //{
-            //    for (j = 0; j <= (Height - 1); j++)
-            //    {
-            //        NonMax[i, j] = Gradient[i, j];
-            //    }
-            //}
-         
-            //int Limit = KernelSize / 2;
-            //int r, c;
-            //float Tangent;
-    
+            for (int row = edgeMaskRadius; row < (MaximumXindex - edgeMaskRadius); row++)
+            {
+                for (int col = edgeMaskRadius; col < (MaximumYindex - edgeMaskRadius); col++)
+                {
+                    var sumX = 0.0;
+                    var sumY = 0.0;
+                    for (int i = -edgeMaskRadius; i <= edgeMaskRadius; i++)
+                    {
+                        for (int j = -edgeMaskRadius; j <= edgeMaskRadius; j++)
+                        {
+                            sumX = sumX + m[row + i, col + j] * edgeMaskX[i + edgeMaskRadius, j + edgeMaskRadius];
+                            sumY = sumY + m[row + i, col + j] * edgeMaskY[i + edgeMaskRadius, j + edgeMaskRadius];
+                        }
+                    }
+                    result[row, col] = Math.Abs(sumX) + Math.Abs(sumY);
+                }
+            }
 
-            //for (i = Limit; i <= (Width - Limit) - 1; i++)
-            //{
-            //    for (j = Limit; j <= (Height - Limit) - 1; j++)
-            //    {
+            return result;
+        }
 
-            //        if (DerivativeX[i, j] == 0)
-            //            Tangent = 90F;
-            //        else
-            //            Tangent = (float)(Math.Atan(DerivativeY[i, j] / DerivativeX[i, j]) * 180 / Math.PI); //rad to degree
+        // Finding the edge direction 
+        public static double[,] FindGradientDirection(double[,] gradientX, double[,] gradientY)
+        {
+            int MaximumXindex = gradientX.GetLength(0);
+            int MaximumYindex = gradientX.GetLength(1);
 
+            double[,] result = new double[MaximumXindex, MaximumYindex];
 
+            for (int row = 0; row < MaximumXindex; row++)
+            {
+                for (int col = 0; col < MaximumYindex; col++)
+                {
+                    if (gradientX[row, col] == 0)
+                    {
+                        if (gradientY[row, col] == 0)
+                        {
+                            result[row, col] = 0; 
+                        }
+                        else
+                        {
+                            result[row, col] = 90; 
+                        }
+                    }
+                    else
+                    {
+                        result[row, col] = (Math.Atan(gradientY[row, col] / gradientX[row, col])) * 180 / Math.PI;
+                    }
+                }
+            }
 
-            //        //Horizontal Edge
-            //        if (((-22.5 < Tangent) && (Tangent <= 22.5)) || ((157.5 < Tangent) && (Tangent <= -157.5)))
-            //        {
-            //            if ((Gradient[i, j] < Gradient[i, j + 1]) || (Gradient[i, j] < Gradient[i, j - 1]))
-            //                NonMax[i, j] = 0;
-            //        }
+            return result;
+        }
 
+        // Non-maximum suppression: Only local maxima should be marked as edges.
 
-            //        //Vertical Edge
-            //        if (((-112.5 < Tangent) && (Tangent <= -67.5)) || ((67.5 < Tangent) && (Tangent <= 112.5)))
-            //        {
-            //            if ((Gradient[i, j] < Gradient[i + 1, j]) || (Gradient[i, j] < Gradient[i - 1, j]))
-            //                NonMax[i, j] = 0;
-            //        }
+        public static double[,] NonMaximumSuppression(double[,] gradient, double[,] direction, int kernelSize)
+        {
+            int MaximumXindex = gradient.GetLength(0);
+            int MaximumYindex = gradient.GetLength(1);
+            int kernelRadius = kernelSize / 2; 
 
-            //        //+45 Degree Edge
-            //        if (((-67.5 < Tangent) && (Tangent <= -22.5)) || ((112.5 < Tangent) && (Tangent <= 157.5)))
-            //        {
-            //            if ((Gradient[i, j] < Gradient[i + 1, j - 1]) || (Gradient[i, j] < Gradient[i - 1, j + 1]))
-            //                NonMax[i, j] = 0;
-            //        }
+            var result = new double[MaximumXindex, MaximumYindex];
+            
+            for (int row = 0; row < MaximumXindex; row++)
+            {
+                for (int col = 0; col < MaximumYindex; col++)
+                {
+                    result[row, col] = gradient[row, col];
+                }
+            }
 
-            //        //-45 Degree Edge
-            //        if (((-157.5 < Tangent) && (Tangent <= -112.5)) || ((67.5 < Tangent) && (Tangent <= 22.5)))
-            //        {
-            //            if ((Gradient[i, j] < Gradient[i + 1, j + 1]) || (Gradient[i, j] < Gradient[i - 1, j - 1]))
-            //                NonMax[i, j] = 0;
-            //        }
+            // check the direction 
+            for (int i = kernelRadius; i < MaximumXindex - kernelRadius; i++)
+            {
+                for (int j = kernelRadius; j < MaximumYindex - kernelRadius; j++)
+                {
+                    // Horizontal edge
+                    if ((-22.5 < direction[i,j] && direction[i,j] <= 22.5) || (157.5 < direction[i,j] && direction[i,j] <= 157.5))
+                    {
+                        if ((gradient[i, j] < gradient[i, j + 1]) || (gradient[i, j] < gradient[i, j - 1])) 
+                        {
+                            result[i, j] = 0;
+                        }                   
+                    }
+                    
+                    // Vertical Edge
+                    if ((-112.5 < direction[i,j] && direction[i,j] <= -67.5) || (67.5 < direction[i,j] && direction[i,j] <= 112.5))
+                    {
+                        if ((gradient[i, j] < gradient[i + 1, j]) || (gradient[i, j] < gradient[i - 1, j])) 
+                        {
+                            result[i, j] = 0;
+                        }                   
+                    }
 
-            //    }
-            //}
+                    // +45 Degree Edge
+                    if ((-67.5 < direction[i,j] && direction[i,j] <= -22.5) || (112.5 < direction[i,j] && direction[i,j] <= 157.5))
+                    {
+                        if ((gradient[i, j] < gradient[i + 1, j - 1]) || (gradient[i, j] < gradient[i - 1, j + 1])) 
+                        {
+                            result[i, j] = 0;
+                        }                   
+                    }
 
+                    //-45 Degree Edge
+                    if ((-157.5 < direction[i,j] && direction[i,j] <= -112.5) || (67.5 < direction[i,j] && direction[i,j] <= 22.5))
+                    {
+                        if ((gradient[i, j] < gradient[i + 1, j + 1]) || (gradient[i, j] < gradient[i - 1, j - 1])) 
+                        {
+                            result[i, j] = 0;
+                        }                   
+                    }
+
+                }
+            }
+
+            return result;
+        }
+  
       //4.Double thresholding: Potential edges are determined by thresholding.
 
       //5.Edge tracking by hysteresis: Final edges are determined by suppressing all edges that are not connected to a very certain (strong) edge.
--- AudioAnalysis/FELT/FELT/AssemblyInfo.fs
+++ AudioAnalysis/FELT/FELT/AssemblyInfo.fs
@@ -33,7 +33,7 @@
     // You can specify all the values or you can default the Build and Revision Numbers
     // by using the ‘*’ as shown below:
     // [assembly: AssemblyVersion("1.0.*")]
-    [<assembly: AssemblyVersion("1.2.2496.3626")>]
-    [<assembly: AssemblyFileVersion("1.2.2496.3626")>]
+    [<assembly: AssemblyVersion("1.2.2501.3627")>]
+    [<assembly: AssemblyFileVersion("1.2.2501.3627")>]
 
     ()
