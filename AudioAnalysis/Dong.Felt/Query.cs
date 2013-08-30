d7991da3a8e03c38ba59a129a7ecd27b0ea3fc0b
Xueyan Dong <xueyan.dong@student.qut.edu.au>
2013-08-30 17:05:14 +1000

    Original hash: 91b68f507b015f5e595fccefbe4af7d801c0874d

---
+++ AudioAnalysis/Dong.Felt/Query.cs
@@ -0,0 +1,51 @@
+﻿namespace Dong.Felt
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Query
+    {
+        #region Private Properties
+
+        private double maxFrequency { get; set; }
+
+        private double minFrequency { get; set; }
+
+        private double startTime { get; set; }
+
+        private double endTime { get; set; }
+
+        #endregion
+
+        #region Public Properties
+
+        public double duration { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Query()
+        {
+            maxFrequency = 0.0;
+            minFrequency = 0.0;
+            startTime = 0.0;
+            endTime = 0.0;
+            duration = endTime - startTime; 
+        }
+
+        public Query(double maximumFrequency, double minimumFrequency, double starttime, double endtime)
+        {
+            maxFrequency = maximumFrequency;
+            minFrequency = minimumFrequency;
+            startTime = starttime;
+            endTime = endtime;
+            duration = endTime - startTime; 
+        }
+        
+        #endregion
+
+    }
+}
