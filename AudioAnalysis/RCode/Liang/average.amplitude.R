31b61554669fbb51149d92ff6369d89980c4ade6
Liang Zhang <l68.zhang@student.edu.au>
2013-10-16 11:34:35 +1000

    noise-removed spectrogram has been changed to decibel, corresponding changes in average.amplitude.R

--- AudioAnalysis/RCode/Liang/average.amplitude.R
+++ AudioAnalysis/RCode/Liang/average.amplitude.R
@@ -1,6 +1,7 @@
 # calculate the average amplitude
-copy.noise.removed <- noise.removed
-copy.noise.removed[which(copy.noise.removed == 0)] <- 1
-average.spectrum <- rowMeans(log2(copy.noise.removed ^ 2))
+average.spectrum <- rowMeans(noise.removed)
+# copy.noise.removed <- noise.removed
+# copy.noise.removed[which(copy.noise.removed == 0)] <- 1
+# average.spectrum <- rowMeans(log2(copy.noise.removed ^ 2))
 
-rm(copy.noise.removed)
+# rm(copy.noise.removed)
