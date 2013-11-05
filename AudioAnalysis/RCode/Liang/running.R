bf66e3ec3661d684e54b66a4a63dbd5032ff875b
Liang Zhang <l68.zhang@student.edu.au>
2013-11-05 13:35:36 +1000

    small modifications for running.R (not finish)

--- AudioAnalysis/RCode/Liang/running.R
+++ AudioAnalysis/RCode/Liang/running.R
@@ -1,17 +1,28 @@
 
 filename <- dir(path = "c:/work/myfile/output/Towsey.Acoustic", pattern = "wav")
 file.len <- length(filename)
-for(i in 1:file.len){
-  file.path <- paste("c:/work/myfile/output/Towsey.Acoustic/", filename[i], 
+for(cnt in 1:file.len){
+  file.path <- paste("c:/work/myfile/output/Towsey.Acoustic/", filename[cnt], 
                      sep = "")
   source("C:/Work/Source/AudioAnalysis/RCode/Liang/spectrogram.R")
-  source("C:/Work/Source/AudioAnalysis/RCode/Liang/acoustic.complexity.index.R")
-  if(i != 1)
-    ACI <- rbind(ACI,ACI.profile)
+#   source("C:/Work/Source/AudioAnalysis/RCode/Liang/acoustic.complexity.index.R")
+#   source("c:/Work/Source/AudioAnalysis/RCode/Liang/spectro.entropy.R")
+#   if(cnt != 1)
+#     entropy <- rbind(entropy,spectro.entropy)
+#   else
+#     entropy <- spectro.entropy
+  source("C:/Work/Source/AudioAnalysis/RCode/Liang/normalisation.R")
+  source("C:/Work/Source/AudioAnalysis/RCode/Liang/noise.removal.R")
+  source("C:/Work/Source/AudioAnalysis/RCode/Liang/coverage.R")
+  source("C:/Work/Source/AudioAnalysis/RCode/Liang/average.amplitude.R")
+  if(cnt != 1)
+    cover <- rbind(cover, cover.spectrum)
   else
-    ACI <- ACI.profile
-#   source("C:/Work/Source/AudioAnalysis/RCode/Liang/normalization.R")
-#   source("C:/Work/Source/AudioAnalysis/RCode/Liang/noise.removal.R")
+    cover <- cover.spectrum
+  if(cnt != 1)
+    avg <- rbind(avg, average.spectrum)
+  else
+    avg <- average.spectrum
 }
 
 
--- AudioAnalysis/RCode/Liang/spectro.entropy.R
+++ AudioAnalysis/RCode/Liang/spectro.entropy.R
@@ -3,6 +3,6 @@ power.spectro <- amp^2
 f.bin.total <- rowSums(power.spectro)
 pmf <- power.spectro / f.bin.total
 pmf[which(pmf == 0)] <- 1
-spectro.entropy <- (-1) * rowSums(log2(pmf)) / log2(ncol(pmf))
+spectro.entropy <- (-1) * rowSums(pmf * log2(pmf)) / log2(ncol(pmf))
 
 rm(power.spectro)
