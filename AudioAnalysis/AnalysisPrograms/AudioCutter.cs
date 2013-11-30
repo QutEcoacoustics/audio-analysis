d123cfc5f6ddd707948eb5bcc00d66611af8c649
Mark Cottman-Fields <mark.cottmanfields@qut.edu.au>
2013-11-30 14:06:48 +1000

    Added test files and missing .cs files

---
+++ AudioAnalysis/AnalysisPrograms/AudioCutter.cs
@@ -0,0 +1,44 @@
+﻿namespace AnalysisPrograms
+{
+    using AnalysisPrograms.Production;
+    using PowerArgs;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class AudioCutter
+    {
+        public class Arguments : SourceConfigOutputDirArguments, IArgClassValidator
+        {
+            
+            [ArgDescription("The top-level directory to begin looking for audio files.")]
+            [Production.ArgExistingDirectory]
+            public DirectoryInfo TopDirectory { get; set; }
+
+            [ArgDescription("Whether to recurse into subdirectories.")]
+            public bool Recurse { get; set; }
+
+            [ArgDescription("The end offset to stop analysing (in seconds)")]
+            [ArgRange(0, double.MaxValue)]
+            public double? EndOffset { get; set; }
+
+            public void Validate()
+            {
+                Directory.EnumerateFiles("", "", SearchOption.TopDirectoryOnly);
+                /*
+                if (this.StartOffset.HasValue ^ this.EndOffset.HasValue)
+                {
+                    throw new InvalidStartOrEndException("If StartOffset or EndOffset is specifified, then both must be specified");
+                }
+
+                if (this.StartOffset.HasValue && this.EndOffset.Value <= this.StartOffset.Value)
+                {
+                    throw new InvalidStartOrEndException("Start offset must be less than end offset.");
+                }
+                */
+            }
+        }
+    }
+}
