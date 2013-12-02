e17d9a2cf405535f35c1c04dbc37eb25dbc5690f
Anthony Truskinger <a.truskinger@qut.edu.au>
2013-12-02 14:58:30 +1000

    Fixed erroneous assertion bug in AnalysisCoordinator

---
+++ Acoustics/Acoustics.Shared/Extensions/ExceptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace System
+{
+    public static class ExceptionsExtensions
+    {
+        public static void PreserveStackTrace(this Exception exception)
+        {
+            MethodInfo preserveStackTrace = typeof(Exception).GetMethod("InternalPreserveStackTrace",
+              BindingFlags.Instance | BindingFlags.NonPublic);
+            preserveStackTrace.Invoke(exception, null);
+
+        }
+    }
+}
