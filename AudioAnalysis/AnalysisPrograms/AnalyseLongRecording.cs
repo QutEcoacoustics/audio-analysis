ea3e5adc526c0a3dfc24ffdc642e95b885fb2483
Michael Towsey <m.towsey@qut.edu.au>
2013-12-27 10:18:33 +1000

    More work on fixing the situation where the signal is flat i.e. all its envelope values are less than 0.001. Now get spectrograms. THis has been tested on a real signal with artificial inserts. Original hash: d4d50c7134d2577fccbbcdf11f1bf844de5c2695

--- AudioAnalysis/AnalysisPrograms/AnalyseLongRecording.cs
+++ AudioAnalysis/AnalysisPrograms/AnalyseLongRecording.cs
@@ -100,7 +100,8 @@ namespace AnalysisPrograms
             //string recordingPath = @"C:\SensorNetworks\WavFiles\KoalaMale\SmallTestSet\HoneymoonBay_StBees_20080905-001000.wav"; //2 min recording
             //string recordingPath = @"C:\SensorNetworks\WavFiles\KoalaMale\SmallTestSet\DaguilarGoldCreek1_DM420157_0000m_00s__0059m_47s_49h.mp3";
             //string recordingPath = @"C:\SensorNetworks\WavFiles\Kiwi\TUITCE_20091215_220004.wav";
-            string recordingPath = @"Y:\Eclipise 2012\Eclipse\Site 4 - Farmstay\ECLIPSE3_20121115_040001.wav";
+            string recordingPath = @"C:\SensorNetworks\WavFiles\TestRecordings\TEST_TUITCE_20091215_220004.wav";
+            //string recordingPath = @"Y:\Eclipise 2012\Eclipse\Site 4 - Farmstay\ECLIPSE3_20121115_040001.wav";
 
             // DEV CONFIG OPTIONS
             string configPath = @"C:\SensorNetworks\Software\AudioAnalysis\AnalysisConfigFiles\Towsey.Acoustic.cfg";
@@ -114,7 +115,7 @@ namespace AnalysisPrograms
                 Source = recordingPath.ToFileInfo(),
                 Config = configPath.ToFileInfo(),
                 //Output = @"C:\SensorNetworks\Output\LSKiwi3\Test_Dec2013".ToDirectoryInfo()
-                Output = @"C:\SensorNetworks\Output\Eclipse\Test1".ToDirectoryInfo()
+                Output = @"C:\SensorNetworks\Output\Test1".ToDirectoryInfo()
             };
 
             // ACOUSTIC_INDICES_LSK_TUITCE_20091215_220004
