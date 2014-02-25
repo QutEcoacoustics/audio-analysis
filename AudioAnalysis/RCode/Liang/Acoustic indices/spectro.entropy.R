44fdacd73079660664e6e5851eda52d9da7aa197
Liang Zhang <l68.zhang@student.edu.au>
2014-02-25 12:00:07 +1000

    fix a small bug

--- AudioAnalysis/RCode/Liang/Acoustic indices/spectro.entropy.R
+++ AudioAnalysis/RCode/Liang/Acoustic indices/spectro.entropy.R
@@ -4,7 +4,7 @@ temporalEntropy <- function(amp){
   # calculate the entropy of each frequency bin from spectrogram
   powerSpectro <- amp ^ 2
   frequencyBin <- rowSums(powerSpectro)
-  pmf <- powerDpectro / frequencyBin
+  pmf <- powerSpectro / frequencyBin
   pmf[which(pmf == 0)] <- 1
   entropyProfile <- (-1) * rowSums(pmf * log2(pmf)) / log2(ncol(pmf))
 
