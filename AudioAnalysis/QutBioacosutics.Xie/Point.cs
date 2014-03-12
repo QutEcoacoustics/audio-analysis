567286cd6f69e78eb16f04eb5bd824686392d04c
Anthony Truskinger <a.truskinger@qut.edu.au>
2014-03-12 19:14:30 +1000

    Setup shell for Jie's work.
    Also added support for parsing dynamic YAML documents.
    
    Also updated nuget packages.
    
    Also reorganised the solution a little bit.

---
+++ AudioAnalysis/QutBioacosutics.Xie/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QutBioacosutics.Xie
+{
+    public class Point
+    {
+        public int StartFrame { get; set; }
+
+        public double Amplitude { get; set; }
+
+        public int LowBin { get; set; }
+    }
+
+}
---
+++ Extra Assemblies/YamlDotNet/README.md
@@ -0,0 +1,3 @@
+These dlls were only added because of a bug in the source library.
+
+When https://github.com/aaubry/YamlDotNet/pull/92 is merged, these packages should be added back in via nuget.
