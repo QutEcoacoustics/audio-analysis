be23b6c1c0d3821f541e8d447799993071dc53a0
Jie Xie <j3.xie@student.qut.edu.au>
2014-03-13 18:44:24 +1000

    Original hash: 15af9c36634e20c0d2cc051f3db5498301353805

---
+++ AudioAnalysis/QutBioacosutics.Xie/ExtractTracks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics;
+
+namespace QutBioacosutics.Xie
+{
+    class ExtractTracks
+    {
+        public double[,] GetTracks(double[,] matrix)
+        {
+
+            var row = matrix.GetLength(1);
+            var column = matrix.GetLength(0);
+
+            // save local peaks to an array of list
+
+            var Points = new List<Peak>();
+
+            
+
+            //for (int i = 0; i < rowMaximum; i++)
+            //{
+            //    for (int j = 0; j < colMaximum; i++)
+            //    {
+            //        var point = new Peak();
+            //        point.X = i;
+            //        point.Y = j;
+            //        point.Amplitude = matrix[i, j];
+            //        Points.Add(point);
+            //    }
+
+            //}
+            return null;
+                
+        
+        
+        }
+
+    }
+}
