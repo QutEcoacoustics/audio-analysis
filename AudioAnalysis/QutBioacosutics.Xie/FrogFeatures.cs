63757d275f4df3141fb5142cf4b761216c93a3e0
Jie Xie <j3.xie@student.qut.edu.au>
2014-04-01 12:01:25 +1000

    Four features for frog recognition Original hash: 87addfc48ffa8485348b640e42e40df092a7e851

---
+++ AudioAnalysis/QutBioacosutics.Xie/FrogFeatures.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QutBioacosutics.Xie
+{
+    public class FrogFeatures
+    {
+        public double Track { get; set; }
+
+        public double LongTrack { get; set; }
+
+        public double Oscillation { get; set; }
+
+        public double Harmonic { get; set; }
+
+    }
+}
