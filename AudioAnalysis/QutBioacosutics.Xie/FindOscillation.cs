0a931388d9e6043ef83fae6fc8e7731c5ff16b7a
J3xie <j3.xie@student.qut.edu.au>
2014-04-17 18:47:53 +1000

    Added function to calculate oscillation rate and remove two method for detecting oscillation Original hash: cd79efbb33f6d095a1515724108448e5f2ea7b3c

--- AudioAnalysis/QutBioacosutics.Xie/FindOscillation.cs
+++ AudioAnalysis/QutBioacosutics.Xie/FindOscillation.cs
@@ -13,58 +13,24 @@ namespace QutBioacosutics.Xie
 {
     class FindOscillation
     {
-        public double[] getOscillation(double[,] matrix, int zeroBinIndex)
+        public double[] DetectOscillation(double[,] matrix, int zeroBinIndex)
         {
-
             matrix = MatrixTools.MatrixRotate90Anticlockwise(matrix);
 
-            // get oscillation of the whole duration of one recording
-
+            // Get oscillation of the whole duration of one recording
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
-
-            //int numSubRows = rows / 5;
-            //int subRows = rows % 5;
-
-            //var tempMatrix = new double[rows, cols];
-
-            //for (int j = 0; j < cols; j++)
-            //{
-            //    for (int r = 0; r < numSubRows; r++)
-            //    {
-            //        double temp = 0;
-            //        for (int i = r * 5; i < (r + 1) * 5; i++)
-            //        {
-            //            temp = matrix[i, j] + temp;
-            //        }
-
-            //        for (int i = r * 5; i < (r + 1) * 5; i++)
-            //        {
-            //            tempMatrix[i, j] = temp;                    
-            //        }
-            //    }
-            //}
-
-            //matrix = tempMatrix;
-       
+      
             int numSubCols = cols / 128;
             int subCols = cols % 128;
 
-            //var intensityArray = new double[rows];
             var score = new double[rows];
             var result = new double[rows,cols];
 
-            // find peaks in the frequency direction
-
-            //matrix = XieFunction.MedianFilter(matrix, 6);
-        
+            // Find peaks in the frequency direction
             var peakMatrix = new double[rows, cols];
             var freqlocalPeaks = new FindLocalPeaks();
             peakMatrix = freqlocalPeaks.LocalPeaksOscillation(matrix, 6, 4, 3);
-            //peakMatrix = freqlocalPeaks.LocalPeaksOscillationNew(matrix);
-
-
-            //peakMatrix = XieFunction.MedianFilter(peakMatrix, 3);
 
             for (int i = 0; i < rows; i++)
             {
@@ -91,17 +57,12 @@ namespace QutBioacosutics.Xie
                 }            
             }
 
-            //peakMatrix = MatrixTools.MatrixRotate90Anticlockwise(peakMatrix);
-            //var image = ImageTools.DrawMatrix(peakMatrix);
-            //image.Save(@"C:\Jie\output\3.png");
-
             cols = cols - subCols;
             int windoezSize = 128;
             var numberPeriodicity = new double[numSubCols];
 
             for (int r = zeroBinIndex; r < rows; r++)
             {
-                //var periodicity = new double[numSubCols];
         
                 for (int i = 0; i < numSubCols; i++)
                 {
@@ -136,27 +97,19 @@ namespace QutBioacosutics.Xie
                                         {
                                             result[r, t] = 1;
                                         }
-                                        //for (int t = i * windoezSize; t < (i + 1) * windoezSize; t++)
-                                        //{
-
-                                        //}
                                         score[r]++;
                                         break;
                                     }
-
                                 }
                         }                        
                     }                          
                 }
             }
 
-            // calculate the last part
+            // Calculate the last part
             for (int r = zeroBinIndex; r < rows; r++)
             {
-                //var periodicity = new double[numSubCols];
-
                 var listLow = new List<int>();
-
                 var arrayLow = new int[windoezSize];
                 for (int c = (cols + 1); c < cols + subCols; c++)
                 {
@@ -181,12 +134,10 @@ namespace QutBioacosutics.Xie
                             double diffB = tempC - tempB;
                             if (Math.Abs(diffA) < 4 & Math.Abs(diffB) < 4)
                             {
-
                                 for (int t = arrayLow[0]; t < arrayLow[arrayLow.Length - 1]; t++)
                                 {
                                     result[r, t] = 1;
                                 }
-
                                 score[r]++;
                                 break;
                             }
@@ -197,319 +148,26 @@ namespace QutBioacosutics.Xie
 
             }
 
-            // normalize the score
+            // Normalize the score
             var norArray = new double[score.Length];
             for (int i = 0; i < score.Length; i++)
             {
                 norArray[i] = score[i] / (numSubCols + 1);
             }
-
             return norArray;
         }
 
-        // A method for extracting oscillation structure
-        public double[,] BardeliFindOscillation(double[,] matrix)
+        public static double[,] CalculateOscillationRate(SpectrogramStandard sonogram, Configuration.CanetoadConfiguration canetoadConfig)
         {
-            // 5 bins are used to calculate the energy
+            int minHz = canetoadConfig.MaximumFrequencyCanetoad;
+            int maxHz = canetoadConfig.MaximumFrequencyCanetoad;
 
-            matrix = MatrixTools.MatrixRotate90Anticlockwise(matrix);
+            double dctDuration = canetoadConfig.Dct_DurationCanetoad;
+            double dctThreshold = canetoadConfig.Dct_ThresholdCanetoad;
 
-            matrix = XieFunction.MedianFilter(matrix, 5);
+            int minOscilFreq = canetoadConfig.MinimumOscillationNumberCanetoad;
+            int maxOscilFreq = canetoadConfig.MaximumOscillationNumberCanetoad;
 
-            // get oscillation of the whole duration of one recording
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            var rowsIndex = rows / 5;
-            var rowsResidual = rows % 5;
-
-            var colsIndex = cols / 128;
-            var colsResidual = cols % 128;
-
-            //var diffArray = new double[cols - 1];
-            //var diffMatrix = new double[rows, (cols - 1)];
-
-            for (int i = 0; i < rowsIndex; i++)
-            {
-                for (int j = 0; j < colsIndex; j++)
-                {
-                    for (int c = j * 128; c < (j + 1) * 128; c++)
-                    {
-                        var tempArray = new double[128];
-                        for (int r = i * 5; r < (i + 1) * 5; r++)
-                        {
-                            tempArray[c] = matrix[r, c] + tempArray[c];
-                                                   
-                        }
-                        // find loacal peaks
-
-                        //double[] signal2 = { 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1 };
-                        //tempArray = signal2;
-                        //var crossArray = XieFunction.CrossCorrelation(tempArray, tempArray);
-                        //var xiecorrelation =  MathNet.Numerics.Statistics.Correlation[tempArray];
-                        //var result = (tempArray.Length, tempArray.Length);
-
-
-
-
-
-
-
-
-
-                    }
-                
-                }         
-            }
-
-            return null;
-        }
-
-        public double[,] DctFindOscillation(double[,] matrix, int zeroBinIndex)
-        {
-            //matrix = MatrixTools.MatrixRotate90Anticlockwise(matrix);
-
-            // get oscillation of the whole duration of one recording
-
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            var colsIndex = cols / 128;
-            var colsResidual = cols % 128;
-
-            var score = new double[rows];
-            var result = new double[rows, cols];
-
-            // find peaks in the frequency direction
-
-            matrix = XieFunction.MedianFilter(matrix, 5);
-
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int n = 0; n < (colsIndex - 1); n++)
-                {
-                    var tempList = new List<double>();
-                    var tempArray = new double[128];
-                    var tempDct = new double[128];
-                    var arrayF = new double[128]; 
-                    for (int k = n * 128; k < (n + 1) * 128; k++)
-                    {
-                        tempList.Add(matrix[i, k]);
-
-                    }
-
-                    var array = tempList.ToArray();
-                    //DataTools.writeBarGraph(array);
-
-                    
-                    var A = DataTools.AutoCorrelation(array, 0, (array.Length - 1));
-
-                    int L = A.Length;
-                    double[] smooth = DataTools.filterMovingAverage(A, 3);
-
-
-                    var peakLocation = new List<int>();
-                    for (int s = 2; s < (smooth.Length - 2); s++)
-                    {
-                        if (smooth[s] - smooth[s - 1] > 0 & smooth[s] - smooth[s + 1] > 0
-                            & smooth[s] - smooth[s - 2] > 0 & smooth[s] - smooth[s - 2] > 0)
-                        {
-                            peakLocation.Add(s);
-                            s = s + 2;
-                        }
-                    }
-
-                    //find the interval
-                    var peakArray = peakLocation.ToArray();
-                    var intervalList = new List<int>();
-                    for (int s = 1; s < peakArray.Length; s++)
-                    {
-                        intervalList.Add(peakArray[s] - peakArray[s - 1]);
-                    }
-
-                    var intervalArray = intervalList.ToArray();
-
-                    var doubleArray = new double[intervalArray.Length];
-                    for (int s = 0; s < intervalArray.Length; s++)
-                    {
-                        doubleArray[s] = (double)intervalArray[s];
-                    }
-
-
-
-                    if (doubleArray.Length > 3)
-                    {
-                        for (int s = 0; s < (doubleArray.Length - 3); s++)
-                        {
-                            var arrayList = new List<double>();
-                            for (int t = s; t < s + 2; t++)
-                            {
-                                arrayList.Add(doubleArray[t]);                            
-                            }
-                            double average;
-                            double sd;
-                            NormalDist.AverageAndSD(arrayList.ToArray(), out average, out sd);
-                            if (sd * 3 < average)
-                            {
-                                for (int c1 = n * 128; c1 < (n + 1) * 128; c1++)
-                                {
-                                    result[i, c1] = 1;
-
-                                }
-                            
-                            }
-                            
-                        }
-
-                    }
-
-                }
-
-            }
-            return result;
-        }
-
-        public double[,] CrossCorrelationFindOscillation(double[,] matrix, int zeroBinIndex)
-        {
-
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            var colsIndex = cols / 128;
-            var colsResidual = cols % 128;
-
-            int numSubCols = cols / 128;
-            int subCols = cols % 128;
-
-            var score = new double[rows];
-            var result = new double[rows, cols];
-
-
-            matrix = XieFunction.MedianFilter(matrix, 6);
-
-            var peakMatrix = new double[rows, cols];
-
-            var freqlocalPeaks = new FindLocalPeaks();
-
-            peakMatrix = freqlocalPeaks.LocalPeaksOscillation(matrix, 6, 4, 3);
-        
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    if (peakMatrix[i, j] < 0)
-                    {
-                        peakMatrix[i, j] = 0;
-                    }
-                    if (peakMatrix[i, j] >= 1)
-                    {
-                        peakMatrix[i, j] = 1;
-                    }
-
-                }
-            }
-
-            for (int i = 1; i < (rows - 1); i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    if ((peakMatrix[i, j] > 0) & (peakMatrix[(i + 1), j] == 0) & (peakMatrix[(i - 1), j] == 0))
-                    {
-                        peakMatrix[i, j] = 0;
-                        peakMatrix[(i + 1), j] = 0;
-                        peakMatrix[(i - 1), j] = 0;
-                    }
-                }
-            }
-
-            // get oscillation of the whole duration of one recording
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int n = 0; n < (colsIndex - 1); n++)
-                {
-                    var tempList = new List<double>();
-                    var tempArray = new double[128];
-                    var tempDct = new double[128];
-                    var arrayF = new double[128];
-                    for (int k = n * 128; k < (n + 1) * 128; k++)
-                    {
-                        tempList.Add(peakMatrix[i, k]);
-
-                    }
-
-                    tempArray = tempList.ToArray();
-
-                    
-                    //double[] signal2 = { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-                    //double[] signal2 = { 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 };
-
-                    var crossArray = tempArray;
-
-                    //var crossArray = signal2;
-                    //var crossArray = XieFunction.CrossCorrelation(signal2, signal2);
-
-                    // find position
-                    var position = new List<int>();
-                    var interval = new List<int>();
-                    for (int n1 = 0; n1 < crossArray.Length; n1++)
-                    {
-                        if (crossArray[n1] > 0)
-                            position.Add(n1);                    
-                    }
-
-                    // find interval
-                    if (interval.Count >= 3)
-                    {
-                        for (int p = 1; p < position.Count; p++)
-                        {
-                            var temp = 2 * (position[p] - position[p - 1]);
-                            interval.Add(temp);
-                        }
-
-                        var intervalArray = interval.ToArray();
-                        var newArray = new double[intervalArray.Length];
-                        for (int a = 0; a < intervalArray.Length; a++)
-                        {
-                            newArray[a] = (double)intervalArray[a];
-                        }
-
-                        var sd = XieFunction.StandarDeviation(newArray);
-                        if (sd < 3)
-                        {
-                            for (int k = (n * 128); k < (n * 128); k++)
-                            {
-                                result[i, k] = 1;
-                            }
-
-                        }
-                    
-                    
-                    }
-
-
-                }
-            }
-
-            return result;
-        }
-
-
-        // Frog species:Mixophyes fasciolatus, Litoria caerulea, Litoria fallax, Litoria gracilenta, Litoria nasuta, 
-        // Litoria verreauxii, Litoria rothii, Litoria latopalmata, Cane_Toad.
-        // Calculate the oscillation rate for 9 frog species.
-        // Parameters for different frog species: 1. Frequency Band, 2. Dct duration, 3.Minimum OscFreq, 4. Maximum OscFreq, 5. Min amplitude, 6. Min duration, 7. Max duration.
-        // Step.1: divide the frequency band into several bands for 9 frog species properly
-        // Step.2: for each frequency band, If there is only one frog species,just find the maximum to form tracks. 
-        // otherwise find the local maximum to form tracks
-        // Step.3: According to tracks, calculate oscillation rate in different frequency bands.
-
-        // CANE_TOAD
-
-        public double[,] OscillationRate(SpectrogramStandard sonogram, int minHz, int maxHz,
-                                                   double dctDuration, int minOscilFreq, int maxOscilFreq, double minAmplitude)
-        {
             int minBin = (int)(minHz / sonogram.FBinWidth);
             int maxBin = (int)(maxHz / sonogram.FBinWidth);
 
@@ -525,14 +183,6 @@ namespace QutBioacosutics.Xie
             Double[,] matrix = sonogram.Data;
 
             double[,] cosines = MFCCStuff.Cosines(dctLength, dctLength); //set up the cosine coefficients
-            //following two lines write matrix of cos values for checking.
-            //string txtPath = @"C:\SensorNetworks\Output\cosines.txt";
-            //FileTools.WriteMatrix2File_Formatted(cosines, txtPath, "F3");
-
-            //following two lines write bmp image of cos values for checking.
-            //string bmpPath = @"C:\SensorNetworks\Output\cosines.png";
-            //ImageTools.DrawMatrix(cosines, bmpPath, true);
-
             for (int c = minBin; c <= maxBin; c++)//traverse columns - skip DC column
             {
                 for (int r = 0; r < rows - dctLength; r++)
@@ -557,7 +207,7 @@ namespace QutBioacosutics.Xie
                     //      DataTools.writeBarGraph(dct);
 
                     //mark DCT location with oscillation freq, only if oscillation freq is in correct range and amplitude
-                    if ((indexOfMaxValue >= minIndex) && (indexOfMaxValue <= maxIndex) && (dct[indexOfMaxValue] > minAmplitude))
+                    if ((indexOfMaxValue >= minIndex) && (indexOfMaxValue <= maxIndex) && (dct[indexOfMaxValue] > dctThreshold))
                     {
                         for (int i = 0; i < dctLength; i++) hits[r + i, c] = oscilFreq;
                     }
@@ -565,12 +215,8 @@ namespace QutBioacosutics.Xie
                 }
                 c++; //do alternate columns
             }
+
             return hits;
         }
-
-
-
-
-   
     }
 }
