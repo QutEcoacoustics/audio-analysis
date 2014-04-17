cf626565a4a9296dde58d317239111c0bf012a7e
J3xie <j3.xie@student.qut.edu.au>
2014-04-17 18:48:03 +1000

    Added

---
+++ AudioAnalysis/QutBioacosutics.Xie/Configuration/TracksConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QutBioacosutics.Xie.Configuration
+{
+    public class TracksConfiguration
+    {
+        public int FrequencyLowCanetoad { get; set; }
+        public int FrequencyHighCanetoad { get; set; }
+
+        public int FrequencyLowCaerulea { get; set; }
+        public int FrequencyHighCaerulea { get; set; }
+
+        public int FrequencyLowNasuta { get; set; }
+        public int FrequencyHighNasuta { get; set; }
+
+        public int FrequencyLowGracillenta { get; set; }
+        public int FrequencyHighGracillenta { get; set; }
+
+        public int FrequencyLowFallax { get; set; }
+        public int FrequencyHighFallax { get; set; }
+
+        public int FrequencyLowLatopalmata { get; set; }
+        public int FrequencyHighLatopalmata { get; set; }
+
+
+    }
+}
