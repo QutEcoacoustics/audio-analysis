de1ed556eef96a9f230dc2148e4bfd1c0cf45435
J3xie <j3.xie@student.qut.edu.au>
2014-04-17 13:48:38 +1000

    Added configuration file for canetoad

---
+++ AudioAnalysis/QutBioacosutics.Xie/Configuration/CanetoadConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QutBioacosutics.Xie.Configuration
+{
+    public class CanetoadConfiguration
+    {
+        public int MinimumOscillationNumberCanetoad { get; set; }
+
+        public int MaximumOscillationNumberCanetoad { get; set; }
+
+        public int MinimumFrequencyCanetoad { get; set; }
+
+        public int MaximumFrequencyCanetoad { get; set; }
+
+        public double Dct_DurationCanetoad { get; set; }
+
+        public double Dct_ThresholdCanetoad { get; set; }
+
+    }
+}
