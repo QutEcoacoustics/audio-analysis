8d53bc9cefedf34e51985d9c32900826841b18a7
J3xie <j3.xie@student.qut.edu.au>
2014-04-19 17:57:50 +1000

    Added configuration for litoria nasuta Original hash: 082188672aeeaa806e086196ac19d4e299497e19

---
+++ AudioAnalysis/QutBioacosutics.Xie/Configuration/NasutaConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QutBioacosutics.Xie.Configuration
+{
+    public class NasutaConfiguration
+    {
+        public double AmplitudeThresholdNasuta { get; set; }
+        public int RangeNasuta { get; set; }
+        public int DistanceNasuta { get; set; }
+
+        public double BinToreanceNasuta { get; set; }
+        public int FrameThresholdNasuta { get; set; }
+        public int TrackDurationThresholdNasuta { get; set; }
+        public double TrackThresholdNasuta { get; set; }
+        public int MaximumTrackDurationNasuta { get; set; }
+        public int MinimumTrackDurationNasuta { get; set; }
+        public double BinDifferencNasuta { get; set; }
+
+        public int FrequencyLowNasuta { get; set; }
+        public int FrequencyHighNasuta { get; set; }
+
+        public int MinimumOscillationNumberNasuta { get; set; }
+        public int MaximumOscillationNumberNasuta { get; set; }
+        public int MinimumFrequencyNasuta { get; set; }
+        public int MaximumFrequencyNasuta { get; set; }
+        public double Dct_DurationNasuta { get; set; }
+        public double Dct_ThresholdNasuta { get; set; }
+
+        public bool DoSlopeNasuta { get; set; }
+
+        public int HarmonicComponentNasuta { get; set; }
+        public int HarmonicSensityNasuta { get; set; }
+    }
+}
