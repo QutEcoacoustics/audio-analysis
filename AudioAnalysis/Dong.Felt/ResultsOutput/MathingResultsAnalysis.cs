92a749592e1f1959a697aa184f55e32b0f6170ce
Cathy Dong <xueyan.dong@student.qut.edu.au>
2014-05-01 09:34:40 +1000

    Work on ResultsOutput class
    Tried to put all the results into one folder for each configuration
    setting.
    Improved the matchingResultsAnalysis method to do it.

--- AudioAnalysis/Dong.Felt/ResultsOutput/MathingResultsAnalysis.cs
+++ AudioAnalysis/Dong.Felt/ResultsOutput/MathingResultsAnalysis.cs
@@ -8,17 +8,57 @@ namespace Dong.Felt.ResultsOutput
     public class MathingResultsAnalysis
     {
         /// <summary>
+        /// To get or set the species name of query. 
+        /// </summary>
+        public string QuerySpeciesName { get; set; }
+
+        /// <summary>
+        /// To get or set the species name of query. 
+        /// </summary>
+        public string HitSpeciesName { get; set; }
+
+        /// <summary>
         /// To get or set the ranking position of the hit in the ranking list. 
         /// </summary>
-        public int HitPosition { get; set; }
+        public int FirstHitPosition { get; set; }
+
+        /// <summary>
+        /// To get or set the ranking position of the hit in the ranking list. 
+        /// </summary>
+        public int SecondHitPosition { get; set; }
 
         /// <summary>
         /// To get or set whether hit is correct. 
         /// </summary>
-        public bool Hit { get; set; }
+        public int FirstHit { get; set; }
+
+        /// <summary>
+        /// To get or set whether hit is correct. 
+        /// </summary>
+        public int SecondHit { get; set; }
+
+        /// <summary>
+        /// To get or set whether hit is correct. 
+        /// </summary>
+        public int ThirdHit { get; set; }
+
+        /// <summary>
+        /// To get or set whether hit is correct. 
+        /// </summary>
+        public int FourthHit { get; set; }
+
+        /// <summary>
+        /// To get or set whether hit is correct. 
+        /// </summary>
+        public int FifthHit { get; set; }
 
         /// <summary>
         /// To get or set the query path.  
+        /// </summary>
+        public string QueryAudioName { get; set; }
+        
+        /// <summary>
+        /// To get or set the matched audio path.
         /// </summary>
         public string MatchedAudioName { get; set; }        
 
