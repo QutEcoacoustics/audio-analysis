92721a239ff3a978f100fc4712885a154063866d
towsey <m.towsey@qut.edu.au>
2014-07-31 16:45:29 +1000

    Work on Create 4 spectral images
    Work on getting cepstral coefficients for MFCC production

--- AudioAnalysis/AudioAnalysisTools/StandardSpectrograms/BaseSonogram.cs
+++ AudioAnalysis/AudioAnalysisTools/StandardSpectrograms/BaseSonogram.cs
@@ -983,6 +983,14 @@
             : base(config, wav, false)
         { }
 
+        public AmplitudeSonogram(SonogramConfig config, double[,] amplitudeData)
+            : base(config, amplitudeData)
+        {
+            var frames = new double[4,4];
+            this.SnrFullband = new SNR(frames);
+            this.SnrFullband.Decibels = new double[amplitudeData.GetLength(0)];
+        }
+
         /// <summary>
         /// This method does nothing because do not want to change the amplitude sonogram in any way.
         /// Actually the constructor of this class calls the BaseSonogram constructor that does NOT include a call to make().
