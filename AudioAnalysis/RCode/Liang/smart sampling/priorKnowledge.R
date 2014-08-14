a082c8e679e1b68302b8dc50c575847d2316d734
liang4182 <l68.zhang@student.qut.edu.au>
2014-08-14 21:27:18 +1000

    fix small bugs

--- AudioAnalysis/RCode/Liang/smart sampling/priorKnowledge.R
+++ AudioAnalysis/RCode/Liang/smart sampling/priorKnowledge.R
@@ -1,7 +1,8 @@
 priorKnowledge <- function(filepath){
-  oct <- read.csv(filepath)
+  source('C:/Work/GitHub/audio-analysis/AudioAnalysis/RCode/Liang/smart sampling/Accumulative Curve.R')
+  calls <- read.csv(callFile)
   
-  speciesNum <- oct$speciesNum
+  speciesNum <- calls$callCount
   index <- 1:1440
   priorKnow <- cbind(index, speciesNum)
   rm(index,speciesNum)
@@ -9,7 +10,7 @@ priorKnowledge <- function(filepath){
   priorKnowIndex <- sort(priorKnow[,2],decreasing=TRUE,index.return=TRUE)
   priorKnowRanked <- priorKnow[priorKnowIndex[[2]], ]
   priorCalls<-calls[priorKnowRanked[ ,1], ]
-  Acurve <- drawAC(priorCalls[1:100, ], nrow(priorCalls[1:100, ]))
+  Acurve <- drawAC(priorCalls[1:100, ], nrow(priorCalls[1:100, ]), total)
   
   return(Acurve)
 }
