504ad3754ae85ce80ca6d40fdd6ab2a0a0ec6d0f
towsey <m.towsey@qut.edu.au>
2014-08-17 18:19:49 +1000

    Contrast normalisation and contrast stretching
    1) Completed method that does local contrast normalisation. This is part
    of a noise removal technique that is used by LeCun. It is motivated by
    what happens in visual cortex.
    2) Wrote a method for contrast stretching. This is useful when
    displaying amplitude spectrograms that do not display well using
    standard normalisation.
    3) Added a GaussianTools class to the TowseyLibrary. It is useful for
    doing Gaussian filters on images.

---
+++ AudioAnalysis/TowseyLibrary/GaussianTools.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowseyLibrary
+{
+    public static class GaussianTools
+    {
+
+
+        /// <summary>
+        /// returns a 2-D Gaussian filter with side corresponding to 2 sigma.
+        /// This formula derived from code of LeCun.
+        /// See python code at the bottom of the class LocalContrastNormalisation.
+        /// </summary>
+        /// <param name="side"></param>
+        /// <returns></returns>
+        public static double[,] Gaussian_filter(int side)
+        {
+            double sigma = 2.0;
+            int halfSide = side / 2;
+            double[,] x = new double[side, side];
+            double sum = 0.0;
+
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    x[i, j] = Gauss(i - halfSide, j - halfSide, sigma);
+                    sum += x[i, j];
+                }
+            }
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    x[i, j] /= sum;
+                }
+            }
+            return x;
+        }
+
+
+        /// <summary>
+        /// returns a gaussian coefficient for point x,y distance from the centre of the distribution.
+        /// This formula derived from code of LeCun.
+        /// See python code at the bottom of the class LocalContrastNormalisation.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="sigma"></param>
+        /// <returns></returns>
+        public static double Gauss(int x, int y, double sigma)
+        {
+            double Z = 2 * Math.PI * sigma * sigma;
+            double G = (1 / Z) * Math.Pow(Math.E, (-((x * x) + (y * y)) / (2 * sigma * sigma)));
+            return G;
+        }
+
+
+
+    }
+}
