3d25fa9969ed8294e90753d5251f3c82a92138dc
towsey <m.towsey@qut.edu.au>
2014-08-21 16:36:11 +1000

    Check the accuracy of the wavelets class Original hash: 51bd20d104287066736b379277eecaa96830985f

--- AudioAnalysis/TowseyLibrary/Wavelets.cs
+++ AudioAnalysis/TowseyLibrary/Wavelets.cs
@@ -1,6 +1,7 @@
 ﻿using MathNet.Numerics.LinearAlgebra.Generic;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -13,13 +14,89 @@ namespace TowseyLibrary
     /// read "WAVELETS FOR KIDS, A Tutorial Introduction", by Brani Vidakovic and Peter Mueller, Duke University.
     /// WARNING: This article on the Haar wavelet is NOT for kids!
     /// </summary>
-    public static class Wavelets
+    public class Wavelets
     {
         public const double SQRT2 = 1.4142135623730950488016887242097;
 
+        public int NumberOfLevels { private set; get; }
+        private List<BinVector> listOfBinVectors;
+
+        /// <summary>
+        /// Assume the signal is power of 2 in length
+        /// </summary>
+        /// <param name="signal"></param>
+        public Wavelets(double[] signal)
+        {
+            if(! DataTools.IsPowerOfTwo((ulong)signal.Length))
+            {
+                throw new Exception("Wavelets CONSTUCTOR FATAL ERROR: Length of signal is not power of 2.");
+            }
+            this.NumberOfLevels = DataTools.PowerOf2Exponent(signal.Length);
+
+            this.listOfBinVectors = Wavelets.GetTreeOfBinVectors(signal);
+        }
+
+
+        /// <summary>
+        /// assume tree is full decomposed WPD tree.
+        /// Assume original signal is power of 2 in length
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public double[,] GetWPDSignalTree()
+        {
+            int nodeCount = this.listOfBinVectors.Count;
+            int signalLength = (nodeCount + 1) / 2;
+
+            double[,] wpdTree = new double[this.NumberOfLevels + 1, signalLength];
+
+            foreach (BinVector bv in this.listOfBinVectors)
+            {
+                int level = bv.levelNumber;
+                int bin = bv.binNumber;
+                int start = (bin - 1) * bv.binLength;
+                double[] signal = bv.signal;
+                // normalise each row
+                //signal = DataTools.normalise(signal);
+                for (int i = 0; i < signal.Length; i++)
+                {
+                    wpdTree[level - 1, start + i] = bv.signal[i];
+                }
+            }
+            return wpdTree;
+        }
+
+        /// <summary>
+        /// assume tree is full decomposed WPD tree.
+        /// Assume original signal is power of 2 in length
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public double[] GetWPDEnergyVector()
+        {
+            int nodeCount = this.listOfBinVectors.Count;
+            double[] wpdEnergyVector = new double[nodeCount];
+
+            foreach (BinVector bv in this.listOfBinVectors)
+            {
+                wpdEnergyVector[bv.sequenceNumber - 1] = bv.energy;
+            }
+            return wpdEnergyVector;
+        }
+
+        public double[] GetWPDEnergySpectrumWithoutDC()
+        {
+            var wpdEnergyVector = this.GetWPDEnergyVector();
+            int signalLength = (wpdEnergyVector.Length + 1) / 2;
+            int startID = signalLength - 1 + 1; // to avoid DC
+            int vectorLength = (signalLength / 2); // half the signal length
+            wpdEnergyVector = DataTools.Subarray(wpdEnergyVector, startID, vectorLength);
+            return wpdEnergyVector;
+        }
+
         
         /// <summary>
-        /// Represents a node in the WPD tree.
+        /// Represents a single node in the WPD tree.
         /// THe nodes are usually called "bin vectors".
         /// At the bottom of the WPD tree each bin vector contains only one element.
         /// </summary>
@@ -70,7 +147,67 @@ namespace TowseyLibrary
         } // END of class BinVector each of which is a node in the WPD tree.
 
 
+        // ############################### NEXT TWO METHODS CREATE THE TREE. SECOND METHOD IS RECURSIVE ###########################
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="signal"></param>
+        /// <returns></returns>
+        public static List<BinVector> GetTreeOfBinVectors(double[] signal)
+        {
+            var list = new List<BinVector>();
+            BinVector sigBin = new BinVector(1, 1, signal);
+            sigBin.childApprox = null;
+            sigBin.childDetail = null;
+
+            list.Add(sigBin);
+            // call recursive method to construct tree
+            Wavelets.GetTreeOfBinVectors(list, sigBin);
+            return list;
+        }
+
+
+        /// <summary>
+        /// NOTE: THIS METHOD IS RECURSIVE.
+        /// It performs a depth first calculation of the wavelet coefficients.
+        /// Depth first search terminates when the bin vector contains only one element.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="bv"></param>
+        /// <returns></returns>
+        public static List<BinVector> GetTreeOfBinVectors(List<BinVector> list, BinVector bv)
+        {
+            int level = bv.levelNumber;
+            int bin = bv.binNumber;
+
+            // display info about nodes
+            // Console.WriteLine("nodeCount={0}   level={1}   bin={2}  seqNum={3}  sigLength={4}", list.Count, level, bin, bv.sequenceNumber, bv.signal.Length);
+
+            double[] approxVector = LowPassAndDecimate(bv.signal);
+            double[] detailVector = HiPassAndDecimate(bv.signal);
+
+            if ((approxVector == null) || (approxVector == null))
+            {
+                //list.Add(null);
+                return list;
+            }
+
+            BinVector approxBin = new BinVector((level + 1), (2 * bin) - 1, approxVector);
+            approxBin.parent = bv;
+            bv.childApprox = approxBin;
+            BinVector detailBin = new BinVector((level + 1), (2 * bin), detailVector);
+            detailBin.parent = bv;
+            bv.childDetail = detailBin;
+
+            list.Add(approxBin);
+            Wavelets.GetTreeOfBinVectors(list, approxBin);
+            list.Add(detailBin);
+            Wavelets.GetTreeOfBinVectors(list, detailBin);
+            return list;
+        }
+
+        // ####################### ABOVE TWO METHODS CREATE THE TREE.###########################################################################
 
 
         /// <summary>
@@ -133,71 +270,6 @@ namespace TowseyLibrary
             return factor * sdOfCoefficients;
         }
 
-
-
-        public static List<BinVector> GetTreeOfBinVectors(double[] signal)
-        {
-            var list = new List<BinVector>();
-            BinVector sigBin = new BinVector(1, 1, signal);
-            sigBin.childApprox = null;
-            sigBin.childDetail = null;
-
-            list.Add(sigBin);
-            GetTreeOfBinVectors(list, sigBin);
-
-            double[,] wpdTree = GetWPDSignalTree(list);
-
-            return list;
-        }
-
-        /// <summary>
-        /// assume tree is full decomposed WPD tree.
-        /// Assume original signal is power of 2 in length
-        /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public static double[,] GetWPDSignalTree(List<BinVector> list)
-        {
-            int nodeCount = list.Count;
-            int signalLength = (nodeCount + 1) / 2;
-            int levelCount = DataTools.PowerOf2Exponent(signalLength);
-
-            double[,] wpdTree = new double[levelCount+1, signalLength];
-
-            foreach (BinVector bv in list)
-            {
-                int level = bv.levelNumber;
-                int bin = bv.binNumber;
-                int start = (bin - 1) * bv.binLength;
-                double[] signal = bv.signal;
-                // normalise each row
-                //signal = DataTools.normalise(signal);
-                for (int i = 0; i < signal.Length; i++)
-                {
-                    wpdTree[level - 1, start + i] = bv.signal[i];
-                }
-            }
-            return wpdTree;
-        }
-
-
-        /// <summary>
-        /// assume tree is full decomposed WPD tree.
-        /// Assume original signal is power of 2 in length
-        /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public static double[] GetWPDEnergyVector(List<BinVector> list)
-        {
-            int nodeCount = list.Count;
-            double[] wpdEnergyVector = new double[list.Count];
-
-            foreach (BinVector bv in list)
-            {
-                wpdEnergyVector[bv.sequenceNumber - 1] = bv.energy;
-            }
-            return wpdEnergyVector;
-        }
 
         /// <summary>
         /// UNFINISHED #########################################################################################
@@ -326,8 +398,9 @@ namespace TowseyLibrary
             {
                 int start = s * windowWidth;
                 double[] subArray = DataTools.Subarray(signal, start, windowWidth);
-                List<Wavelets.BinVector> list = Wavelets.GetTreeOfBinVectors(subArray);
-                double[,] treeMatrix = Wavelets.GetWPDSignalTree(list);
+                var wpd = new Wavelets(subArray);
+                double[] energyVector = wpd.GetWPDEnergyVector();
+                double[,] treeMatrix = wpd.GetWPDSignalTree();
 
                 // get bottom row of the tree matrix i.e. the WPD spectrum
                 double[] wpdSpectrum = MatrixTools.GetRow(treeMatrix, levelNumber);
@@ -366,8 +439,8 @@ namespace TowseyLibrary
             {
                 int start = s * windowWidth;
                 double[] subArray = DataTools.Subarray(signal, start, windowWidth);
-                List<Wavelets.BinVector> list = Wavelets.GetTreeOfBinVectors(subArray);
-                double[] energyVector = Wavelets.GetWPDEnergyVector(list);
+                var wpd = new Wavelets(subArray);
+                double[] energyVector = wpd.GetWPDEnergyVector();
 
                 // reverse the energy vector so that low resolution coefficients are at the bottom. 
                 energyVector = DataTools.reverseArray(energyVector);
@@ -377,45 +450,6 @@ namespace TowseyLibrary
             return wpdByTime;
         }
             
-
-        /// <summary>
-        /// NOTE: THIS METHOD IS RECURSIVE.
-        /// It performs a depth first calculation of the wavelet coefficients.
-        /// Depth first search terminates when the bin vector contains only one element.
-        /// </summary>
-        /// <param name="list"></param>
-        /// <param name="bv"></param>
-        /// <returns></returns>
-        public static List<BinVector> GetTreeOfBinVectors(List<BinVector>list, BinVector bv)
-        {
-            int level = bv.levelNumber;
-            int bin   = bv.binNumber;
-
-            // display info about nodes
-            // Console.WriteLine("nodeCount={0}   level={1}   bin={2}  seqNum={3}  sigLength={4}", list.Count, level, bin, bv.sequenceNumber, bv.signal.Length);
-            
-            double[] approxVector = LowPassAndDecimate(bv.signal);
-            double[] detailVector = HiPassAndDecimate(bv.signal);
-
-            if ((approxVector == null)||(approxVector == null))
-            {
-                //list.Add(null);
-                return list;
-            }
-
-            BinVector approxBin = new BinVector((level + 1), (2 * bin) - 1, approxVector);
-            approxBin.parent = bv;
-            bv.childApprox = approxBin;
-            BinVector detailBin = new BinVector((level + 1), (2 * bin),     detailVector);
-            detailBin.parent = bv;
-            bv.childDetail = detailBin;
-
-            list.Add(approxBin);
-            Wavelets.GetTreeOfBinVectors(list, approxBin);
-            list.Add(detailBin);
-            Wavelets.GetTreeOfBinVectors(list, detailBin);
-            return list;
-        }
 
         /// <summary>
         /// implements the Haar low pass filter
@@ -456,6 +490,71 @@ namespace TowseyLibrary
                 hiPass[i] = (signal[index] - signal[index + 1]) / (double)SQRT2;
             }
             return hiPass;
+        }
+
+        /// <summary>
+        /// These examples are used to do Wavelet Packet Decomposition on test signals to which noise can be added.
+        /// </summary>
+        public static void ExampleOfWavelets_1()
+        {
+            //double[] signal = {1,0,0,0,0,0,0,0};
+            //double[] signal = { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+            //double[] signal = { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+
+            //this signal contains four cycles
+            //double[] signal = { 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 };
+
+            //this signal contains eight cycles
+            //double[] signal = { 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            //                    1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 };
+
+            //this signal contains 16 cycles
+            //double[] signal = { 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+            //                    1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 };
+
+            //this signal contains four step cycles
+            //double[] signal = { 1, 1, 0.5, 0, -0.5, -1.0, -1.0, -0.5, 0, 0.5, 1.0, 1.0, 0.5, 0.0, -0.5, -1, -1, -0.5, 0, 0.5, 1.0, 1.0, 0.5, 0.0, -0.5, -1.0, -1.0,  -0.5, 0, 0.5, 1.0, 1.0,
+            //                    1, 1, 0.5, 0, -0.5, -1.0, -1.0, -0.5, 0, 0.5, 1.0, 1.0, 0.5, 0.0, -0.5, -1, -1, -0.5, 0, 0.5, 1.0, 1.0, 0.5, 0.0, -0.5, -1.0, -1.0,  -0.5, 0, 0.5, 1.0, 1.0 };
+
+            //this 128 sample signal contains 32 cycles
+            //double[] signal = { 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+            //                        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+            //                        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+            //                        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 };
+
+            //this 128 sample signal contains 64 cycles
+            //The output bin vector tree and image will show strong energy at level level 8, bin zero and bin 64.
+            //i.e. bin 64 implies 64 cycles within the length of the WPD window of 128. 
+            double[] signal = { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+                                1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+                                1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+                                1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+
+            // add noise to signal
+            //RandomNumber rn = new RandomNumber();
+            //double[] rv = RandomNumber.GetRandomVector(128, rn);
+
+            // note that even when noise is twice amplitude of signal the first column of UMatrix is excellent reproduction of 
+            // first column when signal has no added noise.
+            // relative noise amplitude
+            //double noiseAmplitude = 2.0;
+            //DataTools.Normalise(rv, 0.0, noiseAmplitude);
+            //// signal plus noise
+            //signal = DataTools.AddVectors(signal, rv);
+            //// normalising seems to make little difference to the result
+            //signal = DataTools.normalise(signal);
+
+            //int levelNumber = 7;
+
+            Wavelets wpd = new Wavelets(signal);
+            double[,] M = wpd.GetWPDSignalTree();
+
+            string path = @"C:\SensorNetworks\Output\Test\testwavelet.png";
+            ImageTools.DrawReversedMatrix(M, path);
+            // MatrixTools.writeMatrix(M);
+
+            double[] energySpectrumWithoutDC = wpd.GetWPDEnergySpectrumWithoutDC();
+
         }
 
     
