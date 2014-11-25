81a14eb7729de8a1ffcb50d1624f2d9a0247093b
liang4182 <l68.zhang@student.qut.edu.au>
2014-11-25 17:06:07 +1000

    add a Butterworth high-pass filter
    low frequency components bias the matching pursuit algorithm

--- AudioAnalysis/Matlab/Liang/MP_features.m
+++ AudioAnalysis/Matlab/Liang/MP_features.m
@@ -9,10 +9,16 @@ MP_freq_mean = zeros(len - 2, 1);
 MP_freq_std = zeros(len - 2, 1);
 MP_amp_mean = zeros(len - 2 , 1);
 MP_amp_std = zeros(len - 2, 1);
+
+dict = dictread('C:\\Program Files (x86)\\MPTK\\mptk\\reference\\dictionary\\dic_gabor_one_scale.xml');
+
+%2nd-order butterworth highpass filter
+[B,A] = butter(2, 1000/8820, 'high');
+
 for i = 3:len
 % for i = 3
     [signal, sampleRate] = sigread(['C:\Work\myfile\MP-feature\raw audio\' category '\' filename(i).name]);
-    dict = dictread('C:\\Program Files (x86)\\MPTK\\mptk\\reference\\dictionary\\dic_gabor_one_scale.xml');
+    signal = filter(B, A, signal);
     book = mpdecomp(signal(:,1),sampleRate,dict,iteration);
     MP_freq_mean(i - 2) = mean(book.atom.params.freq);
     MP_freq_std(i - 2) = std(book.atom.params.freq);
