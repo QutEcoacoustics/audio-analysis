cfad0c23ce1ca1b0131033712df778288eb594c9
Anthony Truskinger <anthony.truskinger@gmail.com>
2014-12-16 21:38:04 +1000

    Implemented at stronger random temp file generator In theory, this may be causing the parallelism bug. It is a giant guess Original hash: fe9090005d363ce35d78960a533c55dee46d8c3a

--- Acoustics/Acoustics.Shared/TempFileHelper.cs
+++ Acoustics/Acoustics.Shared/TempFileHelper.cs
@@ -1,8 +1,12 @@
 ﻿namespace Acoustics.Shared
 {
+    using System;
     using System.Configuration;
+    using System.Diagnostics.Contracts;
     using System.IO;
     using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
     public static class TempFileHelper
     {
@@ -42,7 +46,7 @@
         /// </summary>
         public static FileInfo NewTempFile()
         {
-            return new FileInfo(Path.Combine(TempDir().FullName, Path.GetRandomFileName()));
+            return new FileInfo(Path.Combine(TempDir().FullName, GetStrongerRandomFileName()));
         }
 
         /// <summary>
@@ -54,7 +58,7 @@
         /// <returns>Temp file that does not exist.</returns>
         public static FileInfo NewTempFile(DirectoryInfo tempDir)
         {
-            return new FileInfo(Path.Combine(tempDir.FullName, Path.GetRandomFileName()));
+            return new FileInfo(Path.Combine(tempDir.FullName, GetStrongerRandomFileName()));
         }
 
         /// <summary>
@@ -90,8 +94,7 @@
                 extension = "." + extension;
             }
 
-            var fileName = Path.GetRandomFileName();
-            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            var fileNameWithoutExtension = GetStrongerRandomFileName(false);
             return new FileInfo(Path.Combine(tempDir.FullName, fileNameWithoutExtension+extension));
         }
 
@@ -118,6 +121,100 @@
             {
                 source.CopyToStream(target);
             }
+        }
+
+        /// <summary>
+        /// We suspect the original implementation of GetRandomFileName of
+        /// returning colliding filenames because *we* trimmed the extension off the file.
+        /// <para>
+        /// This is a modification of the original method that can be found here: 
+        /// http://referencesource.microsoft.com/#mscorlib/system/io/path.cs,efb113f637a6bb47
+        /// </para>
+        /// </summary>
+        /// <returns></returns>
+        private static string GetStrongerRandomFileName(bool extension = true)
+        {
+            lock (new object())
+            {
+                // 10 bytes == 80 bits == 80/5 == 16 chars in our (base32) encoding
+                // This gives us exactly 8 chars. We want to avoid the 8.3 short name issue
+                byte[] key = new byte[20];
+
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    rng.GetBytes(key);
+
+                    // rndCharArray is expected to be 32 chars
+                    return ToBase32StringSuitableForDirName(key) + (extension ? ".tmp" : string.Empty);
+                }
+            }
+        }
+
+        private static readonly char[] SBase32Char =
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '0', '1', '2', '3', '4', '5'
+            };
+
+        /// <summary>
+        /// Modified form of http://referencesource.microsoft.com/#mscorlib/system/io/path.cs,78811508e2f49ab8
+        /// </summary>
+        /// <param name="buff"></param>
+        /// <returns></returns>
+        private static string ToBase32StringSuitableForDirName(byte[] buff)
+        {
+            // This routine is optimised to be used with buffs of length 20
+            Contract.Assert((buff.Length % 5) == 0, "Unexpected hash length");
+
+            StringBuilder sb = new StringBuilder(buff.Length * 8 / 5);
+            byte b0, b1, b2, b3, b4;
+            int l, i;
+
+            l = buff.Length;
+            i = 0;
+
+            // Create l chars using the last 5 bits of each byte.  
+            // Consume 3 MSB bits 5 bytes at a time.
+            do
+            {
+                b0 = (i < l) ? buff[i++] : (byte)0;
+                b1 = (i < l) ? buff[i++] : (byte)0;
+                b2 = (i < l) ? buff[i++] : (byte)0;
+                b3 = (i < l) ? buff[i++] : (byte)0;
+                b4 = (i < l) ? buff[i++] : (byte)0;
+
+                // Consume the 5 Least significant bits of each byte
+                sb.Append(SBase32Char[b0 & 0x1F]);
+                sb.Append(SBase32Char[b1 & 0x1F]);
+                sb.Append(SBase32Char[b2 & 0x1F]);
+                sb.Append(SBase32Char[b3 & 0x1F]);
+                sb.Append(SBase32Char[b4 & 0x1F]);
+
+                // Consume 3 MSB of b0, b1, MSB bits 6, 7 of b3, b4
+                sb.Append(SBase32Char[(((b0 & 0xE0) >> 5) | ((b3 & 0x60) >> 2))]);
+
+                sb.Append(SBase32Char[(((b1 & 0xE0) >> 5) | ((b4 & 0x60) >> 2))]);
+
+                // Consume 3 MSB bits of b2, 1 MSB bit of b3, b4
+                b2 >>= 5;
+
+                Contract.Assert((b2 & 0xF8) == 0, "Unexpected set bits");
+
+                if ((b3 & 0x80) != 0)
+                {
+                    b2 |= 0x08;
+                }
+                if ((b4 & 0x80) != 0)
+                {
+                    b2 |= 0x10;
+                }
+
+                sb.Append(SBase32Char[b2]);
+            }
+            while (i < l);
+
+            return sb.ToString();
         }
     }
 }
