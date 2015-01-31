e4a10a2b07c53b927ced485b8b1a1b5aa2f87ccc
Anthony Truskinger <anthony.truskinger@gmail.com>
2015-02-01 00:02:57 +1000

    Completed refactor for AED - Unit tests fail
    - refactored AED configuration options
    - Separate large events can now be tuned and configured
    - Specifying a bandpass filter is now optional
    - Added an extra separate large event call for splitting large events
    along the y (vertical) axis
    - And all the appropriate refactoring for that to work

--- AudioAnalysis/AED/AED/Default.fs
+++ AudioAnalysis/AED/AED/Default.fs
@@ -1,13 +1,58 @@
-﻿module QutSensors.AudioAnalysis.AED.Default
+﻿namespace QutSensors.AudioAnalysis.AED
 
-let intensityThreshold = 9.0
+module Default =
+    open Util
 
-let smallAreaThreshold = 200
+    let intensityThreshold = 9.0
 
-let freqMax = 11025.0
+    let smallAreaThreshold = 200
 
-let bandPassMaxDefault = freqMax
+    //let freqMax = 11025.0
 
-let bandPassMinDefault = 0.0
+    //let bandPassMaxDefault = freqMax
 
-let eprNormalisedMinScore = 4.0/15.0
+    //let bandPassMinDefault = 0.0
+
+    let bandPassFilter : (float * float) option = None
+
+    let doNoiseRemoval = true
+
+    type SeparateParameters = {
+        AreaThreshold : int<px^2>
+        MainThreshold : Percent
+        OrthogonalThreshold : Percent
+        ExtrapolateBridgeEvents : bool 
+    }
+
+    type SeparateStyle =
+        | Horizontal of SeparateParameters
+        | Vertical of SeparateParameters
+        | Skip
+
+    let largeAreaHorizontal = Horizontal {
+            AreaThreshold = 3000<px * px>;
+            MainThreshold = 20.0.percent;
+            OrthogonalThreshold = 100.0.percent / 3.0;
+            ExtrapolateBridgeEvents = true
+        }
+
+    let largeAreaVeritical = Skip
+
+    let eprNormalisedMinScore = 4.0/15.0
+
+open System.Runtime.InteropServices
+type AedOptions (nyquistFrequency : float,
+                 ?intensityThreshold,
+                 ?smallAreaThreshold ,
+                 ?bandPassFilter,
+                 ?doNoiseRemoval,
+                 ?largeAreaHorizontal,
+                 ?largeAreaVeritical) =
+    member val NyquistFrequency = nyquistFrequency        
+    member val IntensityThreshold  = defaultArg intensityThreshold   Default.intensityThreshold   with get, set   
+    member val SmallAreaThreshold  = defaultArg smallAreaThreshold   Default.smallAreaThreshold   with get, set   
+    member val BandPassFilter      = defaultArg bandPassFilter       Default.bandPassFilter       with get, set   
+    member val DoNoiseRemoval      = defaultArg doNoiseRemoval       Default.doNoiseRemoval       with get, set   
+    member val LargeAreaHorizontal = defaultArg largeAreaHorizontal  Default.largeAreaHorizontal  with get, set
+    member val LargeAreaVeritical = defaultArg largeAreaVeritical    Default.largeAreaVeritical   with get, set
+    new(nyquistFrequency : float) = AedOptions(nyquistFrequency)
