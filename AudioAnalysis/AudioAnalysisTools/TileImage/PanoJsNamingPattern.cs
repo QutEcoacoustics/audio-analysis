2630bc69dd3090e7a8ee0908887d7fefb311dc68
Anthony Truskinger <anthony.truskinger@gmail.com>
2015-02-18 14:21:35 +1000

    More work on the tiler - non functional Original hash: ac89192fa728203072e5691ec0a04a8caa8d0b33

---
+++ AudioAnalysis/AudioAnalysisTools/TileImage/PanoJsNamingPattern.cs
@@ -0,0 +1,45 @@
+﻿namespace AudioAnalysisTools.TileImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+
+    public class PanoJsTilingProfile : TilingProfile
+    {
+        public PanoJsTilingProfile()
+        {
+            if (!this.IsSquare)
+            {
+                throw new NotSupportedException("The PanoJS tiler requires square tiles");
+            }
+        }
+
+        public override int TileWidth
+        {
+            get
+            {
+                return 300; // pixels
+            }
+        }
+
+        public override int TileHeight
+        {
+            get
+            {
+                return 300; // pixels
+            }
+        }
+
+        public override int GetZoomIndex(Layer selectedLayer)
+        {
+            return selectedLayer.ScaleIndex;
+        }
+
+        public override Point GetTileIndexes(Layer selectedLayer, Point offsets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AudioAnalysis/AudioAnalysisTools/TileImage/TilingProfile.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TileNamingPattern.cs" company="QutBioacoustics">
+//   All code in this file and all associated files are the copyright of the QUT Bioacoustics Research Group (formally MQUTeR).
+// </copyright>
+// <summary>
+//   Defines the TileNamingPattern type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AudioAnalysisTools.TileImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+
+    public class Tile
+    {
+        
+    }
+
+    public abstract class TilingProfile
+    {
+        public abstract int TileWidth { get; }
+
+        public abstract int TileHeight { get; }
+
+        public bool IsSquare
+        {
+            get
+            {
+                return this.TileHeight == this.TileWidth;
+            }
+        }
+
+        public abstract int GetZoomIndex(Layer selectedLayer);
+
+        public abstract Point GetTileIndexes(Layer selectedLayer, Point offsets);
+
+        public virtual string GetFileBaseName()
+        {
+
+            //var coordinates = this.GetTileIndexes();
+            //return string.Format("{0}-{1}_{2}_{3}", "stub", this.GetZoomIndex(), coordinates.X, coordinates.Y);
+            throw new NotImplementedException();
+        }
+    }
+}
