14f6020f1cc5dfe70e64e5635ebeb960fc29394b
Cathy Dong <xueyan.dong@student.qut.edu.au>
2015-02-26 16:12:05 +1000

    Fixed the problem with pattern overlap
    small pattern can be overlap with large pattern, do the score
    calculation twice to make it work

--- AudioAnalysis/Dong.Felt/Representations/RegionRrepresentation.cs
+++ AudioAnalysis/Dong.Felt/Representations/RegionRrepresentation.cs
@@ -369,7 +369,8 @@ namespace Dong.Felt.Representations
         {
             var result = new List<RegionRepresentation>();
 
-            var anchorCentroid = queryRepresentations.MajorEvent.Centroid;
+            var bottomCentroid = queryRepresentations.MajorEvent.Bottom + queryRepresentations.MajorEvent.Width / 2;
+            //var anchorCentroid = queryRepresentations.MajorEvent.Centroid;
             var orientationType = queryRepresentations.MajorEvent.InsideRidgeOrientation;
             var maxFreq = spectrogram.Configuration.FreqBinCount;
             var maxFrame = spectrogram.FrameCount;
@@ -377,7 +378,8 @@ namespace Dong.Felt.Representations
             var potentialCandidatesStart = new List<EventBasedRepresentation>();
             foreach (var c in candidateEventList[orientationType])
             {
-                if (Math.Abs(c.Centroid.Y - anchorCentroid.Y) < centroidFreqOffset)
+                var cBottomCentroi = c.Bottom + c.Width / 2;
+                if (Math.Abs(cBottomCentroi - bottomCentroid) < centroidFreqOffset)
                 {
                     potentialCandidatesStart.Add(c);
                 }
