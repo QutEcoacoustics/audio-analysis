f864c4f95d82f184479f4f24452b6fe48890e362
Anthony Truskinger <anthony.truskinger@gmail.com>
2015-04-01 16:59:44 +1000

    Integrated tiling code into michael's code
    Tiler ready to be run and debugged.
    
    Also added unit tests for the images of the 1.0s/px image.
    
    Also cleaned up code a bit.

--- Acoustics/Acoustics.Test/AudioAnalysisTools/TilerTests.cs
+++ Acoustics/Acoustics.Test/AudioAnalysisTools/TilerTests.cs
@@ -138,6 +138,25 @@ namespace Acoustics.Test.AudioAnalysisTools
 
             Assert.AreEqual(12, producedFiles.Length);
 
+            var expectedImages = new[]
+                                     {
+                                         "panojstile_000_012_000.png", "panojstile_000_013_000.png",
+                                         "panojstile_000_014_000.png", "panojstile_000_015_000.png",
+                                         "panojstile_000_016_000.png", "panojstile_000_017_000.png",
+                                         "panojstile_000_018_000.png", "panojstile_000_019_000.png",
+                                         "panojstile_000_020_000.png", "panojstile_000_021_000.png",
+                                         "panojstile_000_022_000.png", "panojstile_000_023_000.png"
+                                     }.OrderBy(x => x).ToArray();
+
+            var loadedImages = expectedImages.Select(Image.FromFile).ToArray();
+
+            for (int i = 0; i < loadedImages.Length; i++)
+            {
+
+                var producedImage = Image.FromFile(producedFiles[i].FullName);
+                var areEqual = BitmapEquals((Bitmap)loadedImages[i], (Bitmap)producedImage);
+                Assert.IsTrue(areEqual, "Bitmaps were not equal {0}, {1}", expectedImages[i], producedFiles[i].Name);
+            }
         }
 
         private static bool BitmapEquals(Bitmap bmp1, Bitmap bmp2)
@@ -234,31 +253,31 @@ namespace Acoustics.Test.AudioAnalysisTools
                     Rectangle.FromLTRB(50, 50, 250, 250) // 1A.d 1B.cd 1C.c 2A.ad 2B.abcd 2C.bc 3A.a 3B.ab 3C.b
                 };
 
-            global::AudioAnalysisTools.TileImage.ImageComponent f1A_d = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(50, 50, 100, 100), -1, -1), 
-                                 f1C_c = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(200, 50, 250, 100), 1, -1), 
-                                 f1B_cd = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 50, 200, 100), 0, -1), 
-                                 f2A_a = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(50, 100, 100, 150), -1, 0), 
-                                 f2B_ab = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 100, 200, 150), 0, 0), 
-                                 f2C_b = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(200, 100, 250, 150), 1, 0), 
-                                 f2A_d = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(50, 150, 100, 200), -1, 0), 
-                                 f2B_cd = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 150, 200, 200), 0, 0), 
-                                 f3B_ab = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 200, 200, 250), 0, 1), 
-                                 f2C_c = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(200, 150, 250, 200), 1, 0), 
-                                 f3A_a = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(50, 200, 100, 250), -1, 1), 
-                                 f3C_b = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(200, 200, 250, 250), 1, 1), 
-                                 f1B_c = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 50, 150, 100), 0, -1), 
-                                 f2A_ad = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(50, 100, 100, 200), -1, 0), 
-                                 f2B_bc = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 100, 150, 200), 0, 0), 
-                                 f2B_ad = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(150, 100, 200, 200), 0, 0), 
-                                 f2C_bc = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(200, 100, 250, 200), 1, 0), 
-                                 f2B_b = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 100, 150, 150), 0, 0), 
-                                 f2B_d = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(150, 150, 200, 200), 0, 0), 
-                                 f3B_a = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(150, 200, 200, 250), 0, 1), 
-                                 f2B_c = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 150, 150, 200), 0, 0), 
-                                 f3B_b = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 200, 150, 250), 0, 1), 
-                                 f1B_d = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(150, 50, 200, 100), 0, -1), 
-                                 f2B_a = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(150, 100, 200, 150), 0, 0), 
-                                 f2B_abcd = new global::AudioAnalysisTools.TileImage.ImageComponent(Rectangle.FromLTRB(100, 100, 200, 200), 0, 0);
+            ImageComponent f1A_d = new ImageComponent(Rectangle.FromLTRB(50, 50, 100, 100), -1, -1),
+                           f1C_c = new ImageComponent(Rectangle.FromLTRB(200, 50, 250, 100), 1, -1),
+                           f1B_cd = new ImageComponent(Rectangle.FromLTRB(100, 50, 200, 100), 0, -1),
+                           f2A_a = new ImageComponent(Rectangle.FromLTRB(50, 100, 100, 150), -1, 0),
+                           f2B_ab = new ImageComponent(Rectangle.FromLTRB(100, 100, 200, 150), 0, 0),
+                           f2C_b = new ImageComponent(Rectangle.FromLTRB(200, 100, 250, 150), 1, 0),
+                           f2A_d = new ImageComponent(Rectangle.FromLTRB(50, 150, 100, 200), -1, 0),
+                           f2B_cd = new ImageComponent(Rectangle.FromLTRB(100, 150, 200, 200), 0, 0),
+                           f3B_ab = new ImageComponent(Rectangle.FromLTRB(100, 200, 200, 250), 0, 1),
+                           f2C_c = new ImageComponent(Rectangle.FromLTRB(200, 150, 250, 200), 1, 0),
+                           f3A_a = new ImageComponent(Rectangle.FromLTRB(50, 200, 100, 250), -1, 1),
+                           f3C_b = new ImageComponent(Rectangle.FromLTRB(200, 200, 250, 250), 1, 1),
+                           f1B_c = new ImageComponent(Rectangle.FromLTRB(100, 50, 150, 100), 0, -1),
+                           f2A_ad = new ImageComponent(Rectangle.FromLTRB(50, 100, 100, 200), -1, 0),
+                           f2B_bc = new ImageComponent(Rectangle.FromLTRB(100, 100, 150, 200), 0, 0),
+                           f2B_ad = new ImageComponent(Rectangle.FromLTRB(150, 100, 200, 200), 0, 0),
+                           f2C_bc = new ImageComponent(Rectangle.FromLTRB(200, 100, 250, 200), 1, 0),
+                           f2B_b = new ImageComponent(Rectangle.FromLTRB(100, 100, 150, 150), 0, 0),
+                           f2B_d = new ImageComponent(Rectangle.FromLTRB(150, 150, 200, 200), 0, 0),
+                           f3B_a = new ImageComponent(Rectangle.FromLTRB(150, 200, 200, 250), 0, 1),
+                           f2B_c = new ImageComponent(Rectangle.FromLTRB(100, 150, 150, 200), 0, 0),
+                           f3B_b = new ImageComponent(Rectangle.FromLTRB(100, 200, 150, 250), 0, 1),
+                           f1B_d = new ImageComponent(Rectangle.FromLTRB(150, 50, 200, 100), 0, -1),
+                           f2B_a = new ImageComponent(Rectangle.FromLTRB(150, 100, 200, 150), 0, 0),
+                           f2B_abcd = new ImageComponent(Rectangle.FromLTRB(100, 100, 200, 200), 0, 0);
 
             this.answers = new[]
                           {
