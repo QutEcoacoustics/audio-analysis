6215a7578387ee0899289f60b9bb1ab375480faa
Cathy Dong <xueyan.dong@student.qut.edu.au>
2015-04-04 13:38:02 +1000

    More analysis on Song scope results

--- AudioAnalysis/Dong.Felt/Dong.Sandpit.cs
+++ AudioAnalysis/Dong.Felt/Dong.Sandpit.cs
@@ -158,14 +158,12 @@
                     //var goundTruthFile = @"C:\XUEYAN\PHD research work\First experiment datasets-six species\GroundTruth\GroundTruth-trainingData.csv";
                     //OutputResults.AutomatedMatchingAnalysis(inputDirectory, goundTruthFile);
                     //OutputResults.CSVMatchingAnalysisOfSongScope(inputDirectory, goundTruthFile);
-                    OutputResults.SplitFiles(inputDirectory, inputDirectory);
+                    //OutputResults.SplitFiles(inputDirectory, inputDirectory);
                     //var rate = OutputResults.ClassificationStatistics(inputDirectory, 5);
-                    //var outputFile = @"C:\XUEYAN\PHD research work\Fourth experiment-Gaussian Masks\Output\MatchingResult.csv";
-                    //OutputResults.MatchingSummary(inputDirectory, outputFile);
-
-                    /// Split one files into many subfiles based on file name
-                    
-
+                    var outputFile = @"C:\XUEYAN\PHD research work\Fourth experiment-Gaussian Masks\Output\MatchingResult.csv";
+                    OutputResults.SCMatchingSummary(inputDirectory, outputFile, 7
+                        );
+                 
                     //GaussianBlurAmplitudeSpectro(inputDirectory.FullName, config, ridgeConfig, 1.0, 3);
 
                     ///GaussianBlur
