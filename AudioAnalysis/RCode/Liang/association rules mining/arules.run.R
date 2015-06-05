fbe01c63c765877188497cca139da24d80538b07
liang4182 <l68.zhang@student.qut.edu.au>
2015-06-05 13:47:49 +1000

    test association rules

--- AudioAnalysis/RCode/Liang/association rules mining/arules.run.R
+++ AudioAnalysis/RCode/Liang/association rules mining/arules.run.R
@@ -1,8 +1,14 @@
 library(arules)
-library(arulesViz)
+# library(arulesViz)
 
 #search for all relevant files
 folder <- "C:\\Work\\myfile\\SERF_callCount_20sites"
 pattern <- "hitmaps*"
 filename <- list.files(folder, pattern, no..=TRUE)
 
+source("C:\\Work\\GitHub\\audio-analysis\\AudioAnalysis\\RCode\\Liang\\association rules mining\\species.count.perMinute.R")
+species.count <- numeric()
+for(i in 1:length(filename)){
+  count <- species.count.perMinute(paste(folder, "\\", filename[i], sep=""))
+  species.count <- cbind(species.count, count)
+}
