2635a2bc4a376c957889f4111faabec6bdd7a968
Yvonne Phillips <y.phillips@hdr.qut.edu.au>
2015-06-10 17:05:42 +1000

    More efficient file to plot quartiles

---
+++ AudioAnalysis/RCode/Yvonne/Quartile plotsEE practice.R
@@ -0,0 +1,100 @@
+setwd("C:\\Work\\CSV files\\Eastern Eucalypt")
+
+myFiles <- list.files(full.names=TRUE,pattern="*.csv")
+length<-length(myFiles)
+
+for(i in 1:length) {
+  assign(paste("propertiesEE",i, sep=""),read.csv(myFiles[i]))
+}
+
+########### PLOTTING ##############
+positions<-c(8,23,38,53,68,83,98,113)
+timeLabels1<-c("1:45 pm","2:00","2:15","2:30","2:45","3:00","3:15","3:30")
+timeLabels2<-c("3:45 pm","4:00","4:15","4:30","4:45","5:00","5:15","5:30")
+timeLabels3<-c("5:45 pm","6:00","6:15","6:30","6:45","7:00","7:15","7:30")
+timeLabels4<-c("7:45 pm","8:00","8:15","8:30","8:45","9:00","9:15","9:30")
+timeLabels5<-c("9:45 pm","10:00","10:15","10:30","10:45","11:00","11:15","11:30")
+timeLabels6<-c("11:45 pm","00:00","00:15","00:30","00:45","1:00","1:15","1:30")
+timeLabels7<-c("1:45 am","2:00","2:15","2:30","2:45","3:00","3:15","3:30")
+timeLabels8<-c("3:45 am","4:00","4:15","4:30","4:45","5:00","5:15","5:30")
+timeLabels9<-c("5:45 am","6:00","6:15","6:30","6:45","7:00","7:15","7:30")
+timeLabels10<-c("7:45 am","8:00","8:15","8:30","8:45","9:00","9:15","9:30")
+timeLabels11<-c("9:45 am","10:00","10:15","10:30","10:45","11:00","11:15","11:30")
+timeLabels12<-c("11:45 am","12:00","12:15","12:30","12:45","1:00","1:15","1:30")
+
+###### Q25 GRAPH SET 1#################################
+par(mfrow=c(3,1)) # set layout
+par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
+pos1 <- 0.85
+pos2 <- 0.515
+pos3 <- 0.185
+
+plotQuartile <- function (data1, data2, data3, data4, data5, data6, data7, 
+                          data8, pos, fileName, timeLab)
+{
+plot(c(0:119),c(data1,data2), 
+     ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
+     xlab="",type="l",
+     mgp = c(1.5, 0.5, 0),cex=0.9)
+par(new=TRUE)
+plot(c(0:119),c(data3, data4), 
+     ylab="",ylim=c(0,7500),
+     xlab="",type="l",xaxt='n',yaxt='n',col="red")
+par(new=TRUE)
+plot(c(0:119),c(data5,data6), 
+     ylab="",ylim=c(0,7500),
+     xlab="",type="l",xaxt='n',yaxt='n',col="blue")
+par(new=TRUE)
+plot(c(0:119),c(data7,data8), 
+     ylab="",ylim=c(0,7500),
+     xlab="",type="l",xaxt='n',yaxt='n',col="pink")
+legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),          
+       col=c(1,2,4),lty = 1,cex=0.9,bty="n",horiz=TRUE,
+       x.intersp=0.8,text.width=c(4.2, 4.2))
+axis(side=1,at=positions,labels=timeLab, mgp=c(1.8,0.5,0))
+mtext(side=4, fileName ,line=-1.80, font=1, cex=0.7, 
+      outer=TRUE,at=pos)
+abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2); 
+abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
+}
+
+pdf("EE1.pdf", width=11.69, height=8.27)
+par(mfrow=c(3,1)) # set layout
+par(mar=c(4.3, 4.6, 0.7, 2.1)) # set margins
+plotQuartile (propertiesEE3$Q25,propertiesEE4$Q25,
+              propertiesEE3$median,propertiesEE4$median,
+              propertiesEE3$Q75,propertiesEE4$Q75,
+              propertiesEE3$cent,propertiesEE4$cent,pos1, myFiles[3],
+              timeLabels1)
+
+plotQuartile (propertiesEE5$Q25,propertiesEE6$Q25,
+              propertiesEE5$median,propertiesEE6$median,
+              propertiesEE5$Q75,propertiesEE6$Q75,
+              propertiesEE5$cent,propertiesEE6$cent,pos2, myFiles[5],
+              timeLabels2)
+plotQuartile (propertiesEE7$Q25,propertiesEE8$Q25,
+              propertiesEE7$median,propertiesEE8$median,
+              propertiesEE7$Q75,propertiesEE8$Q75,
+              propertiesEE7$cent,propertiesEE8$cent,pos3, myFiles[7],
+              timeLabels3)
+dev.off()
+
+pdf("EE2.pdf", width=11.69, height=8.27)
+par(mfrow=c(3,1)) # set layout
+par(mar=c(4.3, 4.6, 0.7, 2.1)) # set margins
+plotQuartile (propertiesEE9$Q25,propertiesEE10$Q25,
+              propertiesEE9$median,propertiesEE10$median,
+              propertiesEE9$Q75,propertiesEE10$Q75,
+              propertiesEE9$cent,propertiesEE10$cent,pos1, myFiles[9],
+              timeLabels4)
+plotQuartile (propertiesEE11$Q25,propertiesEE12$Q25,
+              propertiesEE11$median,propertiesEE12$median,
+              propertiesEE11$Q75,propertiesEE12$Q75,
+              propertiesEE11$cent,propertiesEE12$cent,pos2, myFiles[11],
+              timeLabels5)
+plotQuartile (propertiesEE13$Q25,propertiesEE14$Q25,
+              propertiesEE13$median,propertiesEE14$median,
+              propertiesEE13$Q75,propertiesEE14$Q75,
+              propertiesEE13$cent,propertiesEE14$cent,pos3, myFiles[13],
+              timeLabels6)
+dev.off()
--- AudioAnalysis/RCode/Yvonne/Quartile plotsEE.R
+++ AudioAnalysis/RCode/Yvonne/Quartile plotsEE.R
@@ -101,10 +101,8 @@ mtext(side=4,myFiles[5],line=-1.80, font=1, cex=0.7,
       outer=TRUE,at=0.515)# at 0.185
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
+#########################################################
 
-###### Q25 GRAPH SET 2#################################
-#par(mfrow=c(3,1)) # set layout
-#par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE7$Q25,propertiesEE8$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="5:34 to 7:34 pm on 22 March 2015",type="l",
@@ -130,7 +128,10 @@ mtext(side=4,myFiles[7],line=-1.8, font=1, cex=0.7,
 abline(v=21)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-#############################
+
+###### Q25 GRAPH SET 2#################################
+par(mfrow=c(3,1)) # set layout
+par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE9$Q25,propertiesEE10$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="7:37 to 9:37 pm on 22 March 2015",type="l",
@@ -148,10 +149,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels4, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[9],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
+      outer=TRUE,at=0.85)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
+##############
 plot(c(0:119),c(propertiesEE11$Q25,propertiesEE12$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="9:34pm to 11:34  on 22 March 2015",type="l",
@@ -169,13 +170,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2 ,4.2))
 axis(side=1,at=positions,labels=timeLabels5, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[11],line=-1.8, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
-###### Q25 GRAPH SET 3#################################
-par(mfrow=c(3,1)) # set layout
-par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
+########################
 plot(c(0:119),c(propertiesEE13$Q25,propertiesEE14$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="11:37 to 1:37 am on 22-23 March 2015",type="l",
@@ -193,10 +191,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels6, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[13],line=-1.8, font=1, cex=0.7, 
-      outer=TRUE,at=0.85)
+      outer=TRUE,at=0.185)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
+###### Q25 GRAPH SET 3#################################
+par(mfrow=c(3,1)) # set layout
+par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE15$Q25,propertiesEE16$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="3:34 to 5:34 am on 23 March 2015",type="l",
@@ -214,10 +214,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels7, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[15],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
+      outer=TRUE,at=0.85)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
+##################
 plot(c(0:119),c(propertiesEE17$Q25,propertiesEE18$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="5:34 to 7:34 am on 23 March 2015",type="l",
@@ -236,13 +236,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels8, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[17],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
-###### Q25 GRAPH SET 4#################################
-par(mfrow=c(3,1)) # set layout
-par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
+###################
 plot(c(0:119),c(propertiesEE19$Q25,propertiesEE20$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="7:34 to 9:34 am on 23 March 2015",type="l",
@@ -260,10 +257,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels9, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[19],line=-1.8, font=1, cex=0.7, 
-      outer=TRUE,at=0.85)
+      outer=TRUE,at=0.185)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
+###### Q25 GRAPH SET 4#################################
+par(mfrow=c(3,1)) # set layout
+par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE21$Q25,propertiesEE22$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="9:34 to 11:34 am on 23 March 2015",type="l",
@@ -278,13 +277,13 @@ plot(c(0:119),c(propertiesEE21$Q75,propertiesEE22$Q75),
      xlab="",type="l",xaxt='n',yaxt='n',col="blue")
 axis(side=1,at=positions,labels=timeLabels10, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[21],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
+      outer=TRUE,at=0.85)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
 legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),          
        col=c(1,2,4),lty = 1,cex=0.9,bty="n",horiz=TRUE,
        x.intersp=0.8,text.width=c(4.2, 4.2))
-
+#################
 plot(c(0:119),c(propertiesEE23$Q25,propertiesEE24$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="11:34 to 1:34 pm on 23 March 2015",type="l",
@@ -299,14 +298,13 @@ plot(c(0:119),c(propertiesEE23$Q75,propertiesEE24$Q75),
      xlab="",type="l",xaxt='n',yaxt='n',col="blue")
 axis(side=1,at=positions,labels=timeLabels11, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[23],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
 legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),          
        col=c(1,2,4),lty = 1,cex=0.9,bty="n",horiz=TRUE,
        x.intersp=0.8,text.width=c(4.2, 4.2))
-
-###### Q25 GRAPH SET 5#################################
+#####################
 par(mfrow=c(3,1)) # set layout
 par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE25$Q25,propertiesEE26$Q25), 
@@ -326,11 +324,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels12, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[25],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.85)
+      outer=TRUE,at=0.185)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
-############################################
+###### Q25 GRAPH SET 5#################################
 plot(c(0:119),c(propertiesEE27$Q25,propertiesEE28$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="1:37 to 3:37 pm on 23 March 2015",type="l",
@@ -369,13 +366,10 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels2, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[29],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515) #0.185
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2); abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2); abline(h=6000,lwd=0.5,lty=2)
-
-###### Q25 GRAPH SET 6#################################
-#par(mfrow=c(3,1)) # set layout
-#par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
+############################################
 plot(c(0:119),c(propertiesEE31$Q25,propertiesEE32$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="5:34 to 7:34 pm on 23 March 2015",type="l",
@@ -393,13 +387,13 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels3, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[31],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)# 0.85
+      outer=TRUE,at=0.185)
 abline(v=21)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-#################
+###### Q25 GRAPH SET 6#################################
 plot(c(0:119),c(propertiesEE33$Q25,propertiesEE34$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="7:37 to 9:37 pm on 15 March 2015",type="l",
@@ -417,8 +411,8 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels4, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[33],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
-
+      outer=TRUE,at=0.85)
+#################
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
@@ -441,14 +435,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels5, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[35],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
-
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-
-###### Q25 GRAPH SET 7#################################
+#################
 par(mfrow=c(3,1)) # set layout
 par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE37$Q25,propertiesEE38$Q25), 
@@ -468,13 +460,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels6, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[37],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.85)
-
+      outer=TRUE,at=0.185)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-
+###### Q25 GRAPH SET 7#################################
 plot(c(0:119),c(propertiesEE39$Q25,propertiesEE40$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="3:34 to 5:34 am on 23 March 2015",type="l",
@@ -492,13 +483,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels7, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[39],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
-
+      outer=TRUE,at=0.85)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-
+############################
 plot(c(0:119),c(propertiesEE41$Q25,propertiesEE42$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="5:34 to 7:34 am on 24 March 2015",type="l",
@@ -517,13 +507,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels8, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[41],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
+      outer=TRUE,at=0.515)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-
-###### Q25 GRAPH SET 8#################################
+################################
 par(mfrow=c(3,1)) # set layout
 par(mar=c(1.6, 3.1, 0.4, 1.8)) # set margins
 plot(c(0:119),c(propertiesEE43$Q25,propertiesEE44$Q25), 
@@ -543,13 +532,12 @@ legend("topright",inset=c(-0.0,-0.02),legend = c("Q25","Median","Q75"),
        x.intersp=0.8,text.width=c(4.2, 4.2))
 axis(side=1,at=positions,labels=timeLabels9, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[43],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.85)
-
+      outer=TRUE,at=0.185)
 abline(h=0,lwd=0.1,lty=2)
 abline(h=2000,lwd=0.1,lty=2)
 abline(h=4000,lwd=0.1,lty=2)
 abline(h=6000,lwd=0.5,lty=2)
-
+###### Q25 GRAPH SET 8#################################
 plot(c(0:119),c(propertiesEE45$Q25,propertiesEE46$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="9:34 to 11:34 am on 24 March 2015",type="l",
@@ -564,9 +552,8 @@ plot(c(0:119),c(propertiesEE45$Q75,propertiesEE46$Q75),
      xlab="",type="l",xaxt='n',yaxt='n',col="blue")
 axis(side=1,at=positions,labels=timeLabels10, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[45],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.515)
-
-
+      outer=TRUE,at=0.85)
+#######################
 plot(c(0:119),c(propertiesEE47$Q25,propertiesEE48$Q25), 
      ylab="Frequency (Hz)",ylim=c(0,7500),xaxt='n',
      xlab="11:34 to 1:34 pm on 24 March 2015",type="l",
@@ -581,9 +568,8 @@ plot(c(0:119),c(propertiesEE47$Q75,propertiesEE48$Q75),
      xlab="",type="l",xaxt='n',yaxt='n',col="blue")
 axis(side=1,at=positions,labels=timeLabels11, mgp=c(1.8,0.5,0))
 mtext(side=4,myFiles[47],line=-1.80, font=1, cex=0.7, 
-      outer=TRUE,at=0.185)
-
-#########################################
+      outer=TRUE,at=0.515)
+######## #OTHER ################################
 plot(c(0:119),c(propertiesEE5$acousticCompIndex,propertiesEE6$acousticCompIndex), 
      ylab="Frequency (Hz)",ylim=c(148,162),xaxt='n',col=1,
      xlab="1:37 to 3:37 pm on 22 March 2015",type="l",
