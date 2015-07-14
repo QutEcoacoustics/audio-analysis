343de1f5ed3219cd4a15d37af82e31c2f654e246
Yvonne Phillips <y.phillips@hdr.qut.edu.au>
2015-07-14 17:05:24 +1000

    Tidying up the source folders

--- AudioAnalysis/RCode/shared/dateTime_function.R
+++ AudioAnalysis/RCode/shared/dateTime_function.R
@@ -4,12 +4,14 @@
 # by the SM2
 
 dateTime <- function(file){
-
-dates <- sub('.*([[:digit:]]{8})_([[:digit:]]{6}).*','\\1', myFiles)
-times <- sub('.*([[:digit:]]{8})_([[:digit:]]{6}).*','\\2', myFiles)
-datesAndTime <- cbind(dates, times)
-return(datesAndTime)
+  
+  dates <- sub('.*([[:digit:]]{8})_([[:digit:]]{6}).*','\\1', myFiles)
+  times <- sub('.*([[:digit:]]{8})_([[:digit:]]{6}).*','\\2', myFiles)
+  datesAndTime <- cbind(dates, times)
+  return(datesAndTime)
 }
+
+#datesTimes <- as.POSIXct(dates, "", "%Y%m%d %H%M%S")
 
 # The function is run with 
 # source("C:\\Work\\Github\\audio-analysis\\AudioAnalysis\\RCode\\shared\\dateTime_function.R")
