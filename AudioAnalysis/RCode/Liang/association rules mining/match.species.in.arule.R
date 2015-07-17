5504808b38b58d5970f050541950d7319faefaa0
liang4182 <l68.zhang@student.qut.edu.au>
2015-07-17 12:36:15 +1000

    sparse species names in rules

--- AudioAnalysis/RCode/Liang/association rules mining/match.species.in.arule.R
+++ AudioAnalysis/RCode/Liang/association rules mining/match.species.in.arule.R
@@ -1,6 +1,6 @@
 match.species.in.arule <- function(arule){
   antecedent <- sub(".*\\{(.*)=1\\} =>.*",'\\1',arule)
   consequent <- sub(".*\\{(.*)=1\\}",'\\1',arule)
-  species.names <- list(antecedent=antecedent, consequent=consequent)
+  species.names <- c(antecedent, consequent)
   return(species.names)
 }
