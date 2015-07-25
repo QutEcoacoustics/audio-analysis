7c4775c0335ac01fe0aceacd8dbfe9ee069d40b3
liang4182 <l68.zhang@student.qut.edu.au>
2015-07-25 22:30:56 +1000

    change input variable
    from multi-dimensional data to bird species names

--- AudioAnalysis/RCode/Liang/association rules mining/random.pair.R
+++ AudioAnalysis/RCode/Liang/association rules mining/random.pair.R
@@ -1,8 +1,7 @@
-random.pair <- function(species){
+random.pair <- function(birdNames){
 #   set.seed(as.numeric(Sys.time()) %% 1000)
   
   #generate a sequence with the same length as the number of all species
-  birdNames <- names(species)
   sequence <- 1:length(birdNames)
   
   #pick up two indexes from the sequence at random, refering to a pair of species
