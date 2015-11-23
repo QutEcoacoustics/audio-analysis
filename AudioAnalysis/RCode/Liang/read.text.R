417dac3a3e6552affad69d26961c57dbd2d0937b
liang4182 <l68.zhang@student.qut.edu.au>
2015-11-23 16:54:55 +1000

    read predictions of .arff from meka

---
+++ AudioAnalysis/RCode/Liang/read.text.R
@@ -0,0 +1,17 @@
+# create connection and read lines
+# read meka multi-label predictions
+con<-file("C:\\Users\\n8781699\\Downloads\\meka-release-1.7.7\\meka-1.7.7-SNAPSHOT\\prediction_oct15.arff",open="r")
+line<-readLines(con)
+
+# tidy up
+close(con)
+
+labels <- 4
+row.num <- as.numeric(sub('.*N=([[:digit:]]+).*', '\\1', line[1]))
+predicted.annotations <- matrix(0, nrow=row.num, ncol=labels)
+for(i in 2:(row.num + 1)){
+# for(i in 2:5){
+  predict <- sub('.*\\[.*\\] \\[(.*)]', '\\1', line[i])
+  presence <- as.numeric(unlist(strsplit(predict, split=', ')))
+  predicted.annotations[i - 1, presence + 1] <- 1 
+}
