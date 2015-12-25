4c52f5b6bc1b6d95a480bbaabae12e2ba9b66177
Yvonne Phillips <y.phillips@hdr.qut.edu.au>
2015-12-26 08:26:06 +1000

    Ecosounds link generation

--- AudioAnalysis/RCode/Yvonne/Ecosounds_Link_generation.R
+++ AudioAnalysis/RCode/Yvonne/Ecosounds_Link_generation.R
@@ -18,8 +18,15 @@ a <- NULL
 date_times <- NULL
 hour <- NULL
 minute <- NULL
+seconds <- NULL
 site <- NULL
+file.id <- mapping$id[1]
 file.ids <- NULL
+site.ids <- NULL
+site.id <- mapping$site_id[1]
+duration <- NULL 
+dur <- mapping$duration_seconds[1]
+  
 for (i in 1:length(random_9)) {
   # Find date
   day.ref <- floor(random_9[i]/1440) + 1
@@ -48,6 +55,9 @@ for (i in 1:length(random_9)) {
   }
   if(minute1==60) {
     hour1 <- as.integer(hour1) + 1
+    if (hour1<10) {
+      hour1 <- paste("0",as.integer(hour1),sep = "")
+    }
     minute1 <- "00"
   }
   date_time <- paste(substr(date1,1,4),substr(date1,6,7),
@@ -58,7 +68,7 @@ for (i in 1:length(random_9)) {
                      ==substr(date_time,1,8)) & 
                       (substr(mapping$original_file_name,10,15) < 
                          substr(date_time,10,15)))
- time.ref <- NULL
+  time.ref <- NULL
   if(length(file.ref)>1) {
     for(j in file.ref) {
       time.rf <- substr(mapping$original_file_name[j],10,13)
@@ -66,8 +76,10 @@ for (i in 1:length(random_9)) {
       ref <- which(time.ref==max(time.ref))
     }
     file.ref <- file.ref[ref]
-    file.id <- mapping$id[file.ref]
   }
+  file.id <- mapping$id[file.ref]
+  site.id <- mapping$site_id[file.ref]
+  dur <- mapping$duration_seconds[file.ref]
   a <- c(a,file.ref)
   file.ids <- c(file.ids, file.id)
   site.ids <- c(site.ids, site.id)
@@ -75,6 +87,7 @@ for (i in 1:length(random_9)) {
   hour <- c(hour, hour1)
   minute <- c(minute, minute1)
   site <- c(site,ste)
+  duration <- c(duration, dur)
 }
 
 list <- NULL
@@ -83,5 +96,27 @@ for (i in a) {
   list <- c(list,lst)
 }
 
-dataset <- cbind(random_9,a,file.ids, site,date_times,hour,minute,list)
+file.ref <- a
+orig.files <- list
+
+# determine the number of seconds since the start of the recording
+sec <- NULL
+for (i in 1:length(list_9)) {
+  rec.start.hour <- substr(mapping$original_file_name[file.ref[i]],10,11)
+  rec.start.min <- substr(mapping$original_file_name[file.ref[i]],12,13)
+  rec.start.sec <- substr(mapping$original_file_name[file.ref[i]],14,15)
+  total.rec.start.sec <- (as.integer(rec.start.hour)*3600 +
+                          as.integer(rec.start.min)*60 + 
+                          as.integer(rec.start.sec)) 
+  actual.sec.since.midnight <- as.integer(hour[i])*3600 + as.integer(minute[i])*60
+  diff.sec <- actual.sec.since.midnight-total.rec.start.sec
+  sec <- c(sec, diff.sec)
+}
+
+seconds.into.rec <- sec
+sec.remainder <- duration - seconds.into.rec 
+
+dataset <- cbind(list_9,file.ref,file.ids, site.ids,site,date_times,hour,
+                 minute,orig.files, seconds.into.rec, duration,sec.remainder)
 write.csv(dataset, "cluster9_dataset.csv", row.names=F)
+
