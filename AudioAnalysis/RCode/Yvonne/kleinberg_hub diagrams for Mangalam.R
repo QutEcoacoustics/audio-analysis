cc6c1f6cdcf2ba9953852bac631923398632e319
Yvonne Phillips <y.phillips@hdr.qut.edu.au>
2016-04-09 08:12:38 +1000

    Update of Network code

--- AudioAnalysis/RCode/Yvonne/kleinberg_hub diagrams for Mangalam.R
+++ AudioAnalysis/RCode/Yvonne/kleinberg_hub diagrams for Mangalam.R
@@ -6,11 +6,9 @@ data <- read.csv("Minute_cluster mapping - all.csv", header = TRUE)
 data <- t(data)
 library(igraph)
 require(igraph)
-####################
-# Set up layout
-####################
-
-for(i in 1:12) {
+####################################
+# Set up layout with first data set
+###################################
 site <- data[1,i]
 site
 A <- unname(data[2:(length(data[,1])-1),i])
@@ -21,8 +19,19 @@ g <- graph_from_data_frame(g1, directed=TRUE, vertices = NULL)
 #layout <- layout.fruchterman.reingold(g)
 #layout <- layout.circle(g)
 #layout <- layout.drl(g)
-#layout <- layout.star(g,21)
+layout <- layout.star(g,21)
 
+#####################################
+# Complete all with the same layout
+####################################
+for(i in 1:12) {
+site <- data[1,i]
+site
+A <- unname(data[2:(length(data[,1])-1),i])
+B <- unname(data[3:length(data[,1]), i])
+g1 <- data.frame(A=A,B=B)
+
+g <- graph_from_data_frame(g1, directed=TRUE, vertices = NULL)
 
 #plot(g, layout=layout, vertex.size=20, edge.arrow.size=.01)
 #plot(g, layout=layout, edge.arrow.size=.08,
@@ -57,42 +66,35 @@ require(qgraph)
 pdf(paste("plots_star_",site,".pdf",sep = ""))
 
 par(mar=c(20,20,20,20))
-qgraph(datafr,node.width=0.6,asize=1.6,  
+nodeNames <- c("day","day","day","day","day","day","night",
+               "night","night","day","night","night","night",
+               "night","day","day","day","day","night","night",
+               "day","day","day","day","night","day","day")
+colour <- c("lightyellow2", "lightyellow2","lightyellow2",
+        "lightyellow2","lightyellow2", "lightyellow2",
+        "grey70","grey70","grey70","lightyellow2",
+        "grey70","grey70","grey70","grey70","lightyellow2",
+        "lightyellow2","lightyellow2","lightyellow2",
+        "grey70","grey70","lightyellow2","lightyellow2",
+        "lightyellow2","lightyellow2","grey70","lightyellow2",
+        "lightyellow2")
+qgraph(datafr, node.width=0.6, asize=1.6,  
        bidirectional=FALSE, nNodes=27, #threshold=10, 
-       normalize=TRUE,cut=20, edge.label.cex=0.8, 
-       directed=TRUE, weighted=TRUE,minimum=0, 
+       normalize=TRUE, cut=20, edge.label.cex=0.8, 
+       directed=TRUE, weighted=TRUE, minimum=0, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T,legend=T,
-      nodeNames=nodeNames)
+       edge.color="black", maximum=800, color=colour, 
+       edge.labels=T,legend=T,
+       nodeNames=nodeNames)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
-nodeNames <- c("day","night")
 qgraph(datafr, node.width=0.6,asize=1.6,  
        bidirectional=FALSE, nNodes=27, #threshold=10, 
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE,minimum=1, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color=colour,"black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6, 
@@ -100,17 +102,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=2, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6,  
@@ -118,17 +111,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=3, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6, 
@@ -136,17 +120,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=4, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6,  
@@ -154,17 +129,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=5, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6, 
@@ -172,17 +138,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=6, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colours,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6,  
@@ -190,17 +147,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE, minimum=7, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6, 
@@ -208,17 +156,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE,minimum=10, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 qgraph(datafr, node.width=0.6,asize=1.6,  
@@ -226,17 +165,8 @@ qgraph(datafr, node.width=0.6,asize=1.6,
        normalize=TRUE,cut=20, edge.label.cex=0.8, 
        directed=TRUE, weighted=TRUE,minimum=20, 
        layout=layout, curve=1.7, fade=FALSE,
-       edge.color="black", maximum=800, color=c("lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "grey70","lightyellow2","grey70","grey70",
-      "grey70","grey70","lightyellow2",
-      "lightyellow2","lightyellow2",
-      "lightyellow2","grey70","grey70",
-      "lightyellow2","orange","lightyellow2",
-      "lightyellow2","grey70","lightyellow2",
-      "lightyellow2"), edge.labels=T)
+       edge.color="black", maximum=800, color=colour,
+       edge.labels=T)
 mtext(paste("                                                                                                                                                    ",
             site),side = 1, line = 18, cex = 0.8)
 dev.off()
@@ -264,24 +194,4 @@ closeness <- central[,2]
 #hub <- stat1[,2]
 stats <- cbind(betweenness,closeness)
 #write.csv(stats, paste("stats_",site,sep = "",".csv"))
-}
-
-
-####################
-# Day5 
-####################
-A <- unname(data[2:(length(data[,5])-1),2])
-B <- unname(data[3:length(data[,5]), 2])
-g1 <- data.frame(A=A,B=B)
-df <- NULL
-datafr <- NULL
-
-A <- as.numeric(A)
-B <- as.numeric(B)
-for (j in 1:max(A)) {
-  for(k in 1:max(B)) {
-    a <- which(as.integer(A)==j & as.integer(B)==k) 
-    df <- c(j, k, length(a))
-    datafr <- rbind(datafr, df)
-  }
 }
