e5ad7c496a095d5d6b6a3f4531342c828730e074
Michael Towsey <m.towsey@qut.edu.au>
2016-04-19 11:55:04 +1000

    Entropy calculations for EcoAcoustics
    Entropy calculations for paper for EcoAcoustics Congress 2016 at MSU.
    Calculating entropy of distribution of bird call counts for SERF
    recordings Oct 2010.

--- AudioAnalysis/FELT/FELT/AssemblyInfo.fs
+++ AudioAnalysis/FELT/FELT/AssemblyInfo.fs
@@ -33,7 +33,7 @@
     // You can specify all the values or you can default the Build and Revision Numbers
     // by using the ‘*’ as shown below:
     // [assembly: AssemblyVersion("1.0.*")]
-    [<assembly: AssemblyVersion("16.02.2700.3665")>]
-    [<assembly: AssemblyFileVersion("16.02.2700.3665")>]
+    [<assembly: AssemblyVersion("16.04.2700.3665")>]
+    [<assembly: AssemblyFileVersion("16.04.2700.3665")>]
 
     ()
