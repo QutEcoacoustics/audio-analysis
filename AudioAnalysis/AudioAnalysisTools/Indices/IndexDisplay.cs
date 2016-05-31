QutEcoacoustics/audio-analysis

commit 0d0daf63f9ec134f3f9b4f70ead54265b8bffda0

Zoomable spectrograms
Decrease the IndexCalculation Duration down to 0.25 seconds.
Works OK.
Start work on compression of normal spectrograms.

AudioAnalysis/AudioAnalysisTools/Indices/IndexDisplay.cs
@@ -92,8 +92,8 @@
                 scaleLength = array.Length;
                 Image bitmap = ip.GetPlotImage(array);
 
-
-                arrayOfBitmaps[ip.Order] = bitmap;
+                if (arrayOfBitmaps.Length > ip.Order) // THIS IF CONDITION IS A HACK. THERE IS A BUG SOMEWHERE.
+                        arrayOfBitmaps[ip.Order] = bitmap;
             }
 
             var listOfBitmaps = arrayOfBitmaps.Where(b => b != null).ToList();

AudioAnalysis/AnalysisConfigFiles/Towsey.Acoustic.yml
@@ -4,8 +4,8 @@ AnalysisName: Towsey.Acoustic
 SegmentDuration: 1
 SegmentOverlap: 0
 # IndexCalculationDuration: units=seconds (default=60 seconds);   BGNoiseNeighbourhood: units=seconds (default IndexCalculationDuration = 60 seconds);
-IndexCalculationDuration: 
-BGNoiseNeighbourhood: 
+IndexCalculationDuration: 0.25
+BGNoiseNeighbourhood: 5
 #FRAME LENGTH in samples and Frame Overlap as fraction 
 FrameLength: 512
 FrameOverlap: 0.0

AudioAnalysis/AnalysisPrograms/AcousticIndices.cs
@@ -39,6 +39,7 @@ namespace AnalysisPrograms
     using PowerArgs;
 
     using TowseyLibrary;
+    using AudioAnalysisTools.StandardSpectrograms;
 
     public class Acoustic : IAnalyser2
     {
@@ -246,94 +247,6 @@ namespace AnalysisPrograms
             return;
         }
 
-        /// <summary>
-        /// Directs task to the appropriate method based on the first argument in the command line string.
-        /// </summary>
-        /// <returns></returns>
-        //public static void Execute(Arguments arguments)
-        //{
-        //    Contract.Requires(arguments != null);
-  
-        //    // loads a csv file for visulisation
-        //    if (arguments.TaskIsLoadCsv)
-        //    {
-        //        string[] defaultColumns2Display = { "avAmp-dB", "snr-dB", "bg-dB", "activity", "segCount", "avSegDur", "hfCover", "mfCover", "lfCover", "H[ampl]", "H[avSpectrum]", "#clusters", "avClustDur" };
-        //        var fiCsvFile = arguments.InputCsv;
-        //        var fiConfigFile = arguments.Config;
-        //        //var fiImageFile  = new FileInfo(restOfArgs[2]); //path to which to save image file.
-        //        IAnalyser analyser = new Acoustic();
-        //        var dataTables = analyser.ProcessCsvFile(fiCsvFile, fiConfigFile);
-        //        //returns two datatables, the second of which is to be converted to an image (fiImageFile) for display
-        //    } 
-        //    else if (arguments.TaskIsAnalyse)
-        //    {
-        //        // perform the analysis task
-        //        ExecuteAnalysis(arguments);
-        //    }
-        //} // Execute()
-
-
-        /// <summary>
-        /// A WRAPPER AROUND THE analyser.Analyse(analysisSettings) METHOD
-        /// To be called as an executable with command line arguments.
-        /// </summary>
-        //public static void ExecuteAnalysis(Arguments args)
-        //{
-        //    // Check arguments and that paths are valid
-        //    AnalysisSettings analysisSettings = GetAndCheckAllArguments(args);
-        //    analysisSettings.SegmentStartOffset = new TimeSpan(0, 0, args.Start ?? 0);
-        //    analysisSettings.SegmentMaxDuration = new TimeSpan(0, 0, args.Duration ?? 0);
-
-        //    // EXTRACT THE REQUIRED RECORDING SEGMENT
-        //    FileInfo fiSource = analysisSettings.SourceFile;
-        //    FileInfo tempF = analysisSettings.AudioFile;
-        //    if (tempF.Exists) { tempF.Delete(); }
-
-        //    // GET INFO ABOUT THE SOURCE and the TARGET files - esp need the sampling rate
-        //    AudioUtilityModifiedInfo beforeAndAfterInfo;
-
-        //    if (analysisSettings.SegmentMaxDuration != null) // Process entire file
-        //    {
-        //        beforeAndAfterInfo = AudioFilePreparer.PrepareFile(fiSource, tempF, new AudioUtilityRequest { TargetSampleRate = IndexCalculate.RESAMPLE_RATE }, analysisSettings.AnalysisBaseTempDirectoryChecked);
-        //    }
-        //    else
-        //    {
-        //        beforeAndAfterInfo = AudioFilePreparer.PrepareFile(
-        //            fiSource,
-        //            tempF,
-        //            new AudioUtilityRequest
-        //            {
-        //                TargetSampleRate = IndexCalculate.RESAMPLE_RATE,
-        //                OffsetStart = analysisSettings.SegmentStartOffset,
-        //                OffsetEnd = analysisSettings.SegmentStartOffset.Value.Add(analysisSettings.SegmentMaxDuration.Value)
-        //            }, analysisSettings.AnalysisBaseTempDirectoryChecked);
-        //    }
-
-        //    // Store source sample rate - may need during the analysis if have upsampled the source.
-        //    analysisSettings.SampleRateOfOriginalAudioFile = beforeAndAfterInfo.SourceInfo.SampleRate;
-
-        //    // DO THE ANALYSIS
-        //    // #############################################################################################################################################
-        //    IAnalyser analyser = new Acoustic();
-        //    AnalysisResult result = analyser.Analyse(analysisSettings);
-        //    DataTable dt = result.Data;
-        //    if (dt == null) { throw new InvalidOperationException("Data table of results is null"); }
-        //    // #############################################################################################################################################
-
-        //    // ADD IN ADDITIONAL INFO TO RESULTS TABLE
-        //    int iter = 0; // dummy - iteration number would ordinarily be available at this point.
-        //    int startMinute = (int)(args.Start ?? 0);
-        //    foreach (DataRow row in dt.Rows)
-        //    {
-        //        row[IndexProperties.header_count] = iter;
-        //        row[IndexProperties.header_startMin] = startMinute;
-        //        row[IndexProperties.header_SecondsDuration] = result.AudioDuration.TotalSeconds;
-        //    }
-
-        //    CsvTools.DataTable2CSV(dt, analysisSettings.IndicesFile.FullName);
-        //    //DataTableTools.WriteTable2Console(dt);
-        //} // ExecuteAnalysis()
-
 
 
         public AnalysisResult2 Analyse(AnalysisSettings analysisSettings)
@@ -382,6 +295,24 @@ namespace AnalysisPrograms
                 this.WriteSpectrumIndicesFiles(analysisSettings.SpectrumIndicesDirectory, Path.GetFileNameWithoutExtension(analysisSettings.AudioFile.Name), analysisResults.SpectralIndices);
                 analysisResults.SpectraIndicesFiles = spectralIndexFiles;
             }
+
+            // write the one minute spectrogram to CSV
+            SonogramConfig sonoConfig = new SonogramConfig(); // default values config
+            sonoConfig.SourceFName = recording.FilePath;
+            sonoConfig.WindowSize = (int?)analysisSettings.Configuration[AnalysisKeys.FrameLength] ?? IndexCalculate.DefaultWindowSize;
+            sonoConfig.WindowOverlap = (double?)analysisSettings.Configuration[AnalysisKeys.FrameOverlap] ?? 0.0; // the default
+            //sonoConfig.NoiseReductionType = NoiseReductionType.NONE; // the default
+            //sonoConfig.NoiseReductionType = NoiseReductionType.STANDARD;
+            var sonogram = new SpectrogramStandard(sonoConfig, recording.WavReader);
+            // remove the DC row of the spectrogram
+            sonogram.Data = MatrixTools.Submatrix(sonogram.Data, 0, 1, sonogram.Data.GetLength(0) - 1, sonogram.Data.GetLength(1) - 1);
+            string csvPath = Path.Combine(outputDirectory.FullName, recording.FileName + ".csv");
+            Csv.WriteMatrixToCsv(csvPath.ToFileInfo(), sonogram.Data);
+
+
+
+
+
             //if ((indexCalculateResult.Sg != null) && (analysisSettings.ImageFile != null))
             //{
             //    string imagePath = Path.Combine(outputDirectory.FullName, analysisSettings.ImageFile.Name);

AudioAnalysis/AnalysisPrograms/AnalyseLongRecordings/AnalyseLongRecording.cs
@@ -162,7 +162,7 @@ Output  to  directory: {1}
             // set IndexCalculationDuration i.e. duration of a subsegment
             try
             {
-                int indexCalculationDuration = configuration[AnalysisKeys.IndexCalculationDuration];
+                double indexCalculationDuration = configuration[AnalysisKeys.IndexCalculationDuration];
                 analysisSettings.IndexCalculationDuration = TimeSpan.FromSeconds(indexCalculationDuration);
             }
             catch (Exception ex)

AudioAnalysis/AnalysisPrograms/DrawZoomingSpectrograms.cs
@@ -83,7 +83,8 @@ namespace AnalysisPrograms
             string ipFileName = "TEST_TUITCE_20091215_220004_Towsey.Acoustic"; //exclude the analysis type from file name i.e. "Indices"
             //string ipdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\Towsey.Acoustic";
             //string opdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\Towsey.Acoustic";
-            string ipdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\Towsey.Acoustic.OneSecondIndices";
+            //string ipdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\Towsey.Acoustic.OneSecondIndices";
+            string ipdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\Towsey.Acoustic.250msIndices";
             string opdir = @"C:\SensorNetworks\Output\FalseColourSpectrograms\SpectrogramZoom\ZoomImages";
 
             DirectoryInfo ipDir = new DirectoryInfo(ipdir);
@@ -123,9 +124,9 @@ namespace AnalysisPrograms
             }
 
             var config = LdSpectrogramConfig.ReadYamlToConfig(arguments.SpectrogramConfigPath);
-
-            config.IndexCalculationDuration = TimeSpan.FromSeconds(1.0);
-            config.XAxisTicInterval = TimeSpan.FromSeconds(60.0);
+            // need to set the data scale. THis info not available at present
+            config.IndexCalculationDuration = TimeSpan.FromSeconds(0.25);
+            //config.XAxisTicInterval = TimeSpan.FromSeconds(60.0);
 
             ZoomingSpectrograms.DrawSpectrogramsFromSpectralIndices(config, arguments.IndexPropertiesConfig);
         }

AudioAnalysis/AnalysisPrograms/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.01.0.0")]
-[assembly: AssemblyFileVersion("15.01.0.0")]
+[assembly: AssemblyVersion("15.02.0.0")]
+[assembly: AssemblyFileVersion("15.02.0.0")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]

AudioAnalysis/AudioAnalysisTools/ActivityAndCover.cs
@@ -107,9 +107,9 @@ namespace AudioAnalysisTools
 
             //bool[] events2 = {false, false, true, true, true, false, true, true, false, false, true, true, true}; //3 events; lengths = 3, 2, 3
             List<int> eventList = DataTools.GetEventLengths(events);
-            var filtered = eventList.Where(x => x >= minFrameLength);
-            int eventCount = filtered.Count();
-            int eventSum   = filtered.Sum();
+            var listOfFilteredEvents = eventList.Where(x => x >= minFrameLength);
+            int eventCount = listOfFilteredEvents.Count();
+            int eventSum   = listOfFilteredEvents.Sum();
 
             if (eventCount == 0)
                 return new SummaryActivity(activeFrames, activeFrameCount, activeAvDB, events, 0, TimeSpan.Zero);

AudioAnalysis/AudioAnalysisTools/Indices/IndexCalculate.cs
@@ -125,7 +125,7 @@ namespace AudioAnalysisTools.Indices
 
             // get TimeSpans and durations
             TimeSpan subsegmentTimeSpan = (TimeSpan)analysisSettings.IndexCalculationDuration;
-            double subsegmentDuration = subsegmentTimeSpan.TotalSeconds;
+            double subsegmentSecondsDuration = subsegmentTimeSpan.TotalSeconds;
             TimeSpan ts = (TimeSpan)analysisSettings.SubsegmentOffset;
             double subsegmentOffset = ts.TotalSeconds;
             ts = (TimeSpan)analysisSettings.SegmentStartOffset;
@@ -136,7 +136,7 @@ namespace AudioAnalysisTools.Indices
 
             // calculate start and end samples of the subsegment and noise segment
             int sampleStart = (int)(localOffsetInSeconds * sampleRate);
-            int subsegmentSampleCount = (int)(subsegmentDuration * sampleRate);
+            int subsegmentSampleCount = (int)(subsegmentSecondsDuration * sampleRate);
             int sampleEnd   = sampleStart + subsegmentSampleCount - 1;
 
             int noiseBuffer    = (int)(BGNoiseNeighbourhood * sampleRate);
@@ -188,7 +188,7 @@ namespace AudioAnalysisTools.Indices
             // average absolute value over the minute recording
             // double[] avAbsolute = dspOutput1.Average; 
             double[] signalEnvelope = dspOutput1.Envelope;
-            double avSignalEnvelope = signalEnvelope.Average();
+            double avgSignalEnvelope = signalEnvelope.Average();
 
 
             // set up DATA STORAGE struct and class in which to return all the indices and other data.
@@ -196,28 +196,25 @@ namespace AudioAnalysisTools.Indices
             SummaryIndexValues summaryIndexValues = result.SummaryIndexValues;
             
             // average high ampl rate per second
-            summaryIndexValues.HighAmplitudeIndex = dspOutput1.MaxAmplitudeCount / subsegmentDuration;
+            summaryIndexValues.HighAmplitudeIndex = dspOutput1.MaxAmplitudeCount / subsegmentSecondsDuration;
             // average clip rate per second
-            summaryIndexValues.ClippingIndex = dspOutput1.ClipCount / subsegmentDuration;
+            summaryIndexValues.ClippingIndex = dspOutput1.ClipCount / subsegmentSecondsDuration;
 
             // Following deals with case where the signal waveform is continuous flat with values < 0.001. Has happened!! 
             // Although signal appears zero, this condition is required
-            if (avSignalEnvelope < 0.001)
+            if (avgSignalEnvelope < 0.001)
             {
-                Logger.Debug("Segment skipped because avSignalEnvelope is too small!");
+                Logger.Debug("Segment skipped because avSignalEnvelope is < 0.001!");
 
                 return result;
             }
 
-            // i: FRAME ENERGIES - convert signal to decibels and subtract background noise. By default, number of noise SDs = ZERO
+            // i: FRAME ENERGIES - convert signal to decibels and subtract background noise.
             double[] dBSignal = SNR.Signal2Decibels(dspOutput1.Envelope);
             double[] dBArray  = SNR.SubtractAndTruncate2Zero(dBSignal, signalBGN);
 
-            // ii: ACTIVITY and EVENT STATISTICS for NOISE REDUCED ARRAY
-            var activity = ActivityAndCover.CalculateActivity(dBArray, frameTimeSpan);
-
-            // fraction of frames having acoustic activity 
-            summaryIndexValues.Activity = activity.fractionOfActiveFrames;
+            // 10 times log of amplitude squared     
+            summaryIndexValues.AvgSignalAmplitude = 20 * Math.Log10(avgSignalEnvelope);
 
             // bg noise in dB
             summaryIndexValues.BackgroundNoise = signalBGN;
@@ -225,21 +222,24 @@ namespace AudioAnalysisTools.Indices
             // SNR
             summaryIndexValues.Snr = dBArray.Max(); 
 
+            // ii: ACTIVITY and EVENT STATISTICS for NOISE REDUCED ARRAY
+            var activity = ActivityAndCover.CalculateActivity(dBArray, frameTimeSpan);
+
+            // fraction of frames having acoustic activity 
+            summaryIndexValues.Activity = activity.fractionOfActiveFrames;
+
             // snr calculated from active frames only
             summaryIndexValues.AvgSnrOfActiveFrames = activity.activeAvDB;
-
-            // 10 times log of amplitude squared     
-            summaryIndexValues.AvgSignalAmplitude = 20 * Math.Log10(avSignalEnvelope);
 
             // ENTROPY of ENERGY ENVELOPE -- 1-Ht because want measure of concentration of acoustic energy.
             double entropy = DataTools.Entropy_normalised(DataTools.SquareValues(signalEnvelope));
             summaryIndexValues.TemporalEntropy = 1 - entropy;
 
-            // number of segments whose duration > one frame
-            summaryIndexValues.EventsPerSecond = activity.eventCount / subsegmentDuration;
+            // average number of events per second whose duration > one frame
+            summaryIndexValues.EventsPerSecond = activity.eventCount / subsegmentSecondsDuration;
 
-            // av event duration in milliseconds
-            summaryIndexValues.AvgEventDuration = activity.avEventDuration;
+            // average event duration in milliseconds - no longer calculated
+            //summaryIndexValues.AvgEventDuration = activity.avEventDuration;
 
 
             // (B) ################################## EXTRACT INDICES FROM THE AMPLITUDE SPECTROGRAM ################################## 
@@ -356,12 +356,7 @@ namespace AudioAnalysisTools.Indices
             spectra.SPT = sptInfo.spSpectrum;
 
             summaryIndexValues.AvgSptDuration = sptInfo.avTrackDuration;
-            summaryIndexValues.SptPerSecond = sptInfo.trackCount / subsegmentDuration;
-
-
-            // TODO: calculate av track duration and total duration as fraction of recording duration
-            ////ImageTools.DrawMatrix(sptInfo.peaks, @"C:\SensorNetworks\Output\LSKiwi3\Test_00April2014\Towsey.Acoustic\peaks.png");
-
+            summaryIndexValues.SptPerSecond = sptInfo.trackCount / subsegmentSecondsDuration;
 
             // #V#####################################################################################################################################################
             // iv:  set up other info to return
@@ -408,15 +403,12 @@ namespace AudioAnalysisTools.Indices
             // return if activeFrameCount too small or segmentCount == 0  because no point doing clustering
             if ((activity.activeFrameCount <= 2) || (activity.eventCount == 0))
             {
-                summaryIndexValues.ClusterCount = 0;
-                summaryIndexValues.AvgClusterDuration = TimeSpan.Zero;
-                summaryIndexValues.ThreeGramCount = 0;
+                //summaryIndexValues.ClusterCount = 0;
+                //summaryIndexValues.AvgClusterDuration = TimeSpan.Zero;
+                //summaryIndexValues.ThreeGramCount = 0;
                 result.Sg = sonogram;
                 result.Hits = hits;
                 result.TrackScores = scores;
-                ////result.Tracks = trackInfo.listOfSPTracks;
-                //IndexCalculate.MarkClippedSpectra(spectra, summaryIndexValues.HighAmplitudeIndex, summaryIndexValues.ClippingIndex);
-
                 return result;
             }
 
@@ -444,17 +436,16 @@ namespace AudioAnalysisTools.Indices
             if (data.trainingData.Count <= 8)     
             {
                 clusterInfo.clusterHits2 = null;
-                summaryIndexValues.ClusterCount = 0;
-                summaryIndexValues.AvgClusterDuration = TimeSpan.Zero;
-                summaryIndexValues.ThreeGramCount = 0;
+                //summaryIndexValues.ClusterCount = 0;
+                //summaryIndexValues.AvgClusterDuration = TimeSpan.Zero;
+                //summaryIndexValues.ThreeGramCount = 0;
             }
             else
             {
                 clusterInfo = SpectralClustering.ClusterAnalysis(data.trainingData, WtThreshold, HitThreshold, data.selectedFrames);
-                ////Log.WriteLine("Cluster Count=" + clusterInfo.clusterCount);
-                summaryIndexValues.ClusterCount = clusterInfo.clusterCount;
-                summaryIndexValues.AvgClusterDuration = TimeSpan.FromSeconds(clusterInfo.av2 * frameTimeSpan.TotalSeconds); // av cluster duration
-                summaryIndexValues.ThreeGramCount = clusterInfo.triGramUniqueCount;
+                //summaryIndexValues.ClusterCount = clusterInfo.clusterCount;
+                //summaryIndexValues.AvgClusterDuration = TimeSpan.FromSeconds(clusterInfo.av2 * frameTimeSpan.TotalSeconds); // av cluster duration
+                //summaryIndexValues.ThreeGramCount = clusterInfo.triGramUniqueCount;
 
                 double[] clusterSpectrum = clusterInfo.clusterSpectrum;
                 spectra.CLS = SpectralClustering.RestoreFullLengthSpectrum(clusterSpectrum, freqBinCount, data.lowBinBound, data.reductionFactor);
@@ -480,9 +471,6 @@ namespace AudioAnalysisTools.Indices
             result.Hits = hits;
             result.TrackScores = scores;
             result.Tracks = sptInfo.listOfSPTracks;
-
-            //IndexCalculate.MarkClippedSpectra(spectra, summaryIndexValues.HighAmplitudeIndex, summaryIndexValues.ClippingIndex); 
-
             return result;
         } // end of method Analysis()
 

commit de87e8933da405128501a1134545c0e2db4ccb05

Fixed bug in index display class
Introduced in 0d0daf63f9ec134f3f9b4f70ead54265b8bffda0

AudioAnalysis/AudioAnalysisTools/Indices/IndexDisplay.cs
@@ -124,7 +124,7 @@
 
             const int TrackHeight = DrawSummaryIndices.DefaultTrackHeight;
             int scaleLength = 0;
-            var arrayOfBitmaps = new Image[dictionaryOfCsvFile.Keys.Count];
+            var bitmapList = new List<Tuple<IndexProperties, Image>>(dictionaryOfCsvFile.Keys.Count);
                 // accumulate the individual tracks in a List
 
             foreach (string key in dictionaryOfCsvFile.Keys)
@@ -160,11 +160,13 @@
                 scaleLength = array.Length;
                 Image bitmap = ip.GetPlotImage(array, errors);
 
-                if (arrayOfBitmaps.Length > ip.Order) // THIS IF CONDITION IS A HACK. THERE IS A BUG SOMEWHERE.
-                    arrayOfBitmaps[ip.Order] = bitmap;
+                bitmapList.Add(Tuple.Create(ip, bitmap));
             }
 
-            var listOfBitmaps = arrayOfBitmaps.Where(b => b != null).ToList();
+            var listOfBitmaps = bitmapList
+                .OrderBy(tuple => tuple.Item1.Order)
+                .Select(tuple => tuple.Item2)
+                .Where(b => b != null).ToList();
 
 
             //set up the composite image parameters