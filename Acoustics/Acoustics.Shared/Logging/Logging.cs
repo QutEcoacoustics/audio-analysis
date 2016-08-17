155768bc52ed6826efedd9fa84c92d787086ae12
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-08-18 00:45:03 +1000

    Enhanced logging - COLOR OUTPUT :joy: - More advanced appenders means Logging.cs got simplified - Added new custom level SUCCESS Original hash: 096535daf5c6c5c83ec3d3f3ad9bfe0a066964fa

--- Acoustics/Acoustics.Shared/Logging/Logging.cs
+++ Acoustics/Acoustics.Shared/Logging/Logging.cs
@@ -23,21 +23,18 @@ namespace System
     /// <summary>
     /// This class is designed to be an abstraction to the system console.
     /// Messages normally written to the System.Console Out and Error are additionally logged in this class.
-    /// Be sure the logging provider monitoring this class does not redunantly print these messages to the console.
+    /// The logging appenders filter out messages for this class and print them in a clean format.
     /// </summary>
     public static class LoggedConsole
     {
-        private static readonly ILog Log = LogManager.GetLogger("LoggedConsole");
-
-        private static readonly string NewLine = Environment.NewLine;
+        public static readonly ILog Log = LogManager.GetLogger("CleanLogger");
 
         public static void Write(string str)
         {
             Log.Info(str);
-            Console.Write(str);
         }
 
-        public static void Write(string format, params Object[] args)
+        public static void Write(string format, params object[] args)
         {
             var str = string.Format(format, args);
             Write(str);
@@ -46,7 +43,11 @@ namespace System
         public static void WriteLine(string str)
         {
             Log.Info(str);
-            Console.WriteLine(str);
+        }
+
+        public static void WriteSuccessLine(string str)
+        {
+            Log.Success(str);
         }
 
         public static void WriteLine()
@@ -54,54 +55,90 @@ namespace System
             WriteLine(null);
         }
 
-        public static void WriteLine(string format, params Object[] args)
+        public static void WriteLine(string format, params object[] args)
         {
             var str = string.Format(format, args);
             WriteLine(str);
         }
 
+        public static void WriteSuccessLine(string format, params object[] args)
+        {
+            var str = string.Format(format, args);
+            WriteSuccessLine(str);
+        }
+
         public static void WriteLine(object obj)
         {
             Log.Info(obj);
-            Console.WriteLine(obj);
         }
 
 
         public static void WriteError(string str)
         {
             Log.Error(str);
-            Console.Error.Write(str);
         }
 
         public static void WriteErrorLine(string format, params object[] args)
         {
             var str = string.Format(format, args);
             Log.Error(str);
-            Console.Error.WriteLine(str);
         }
 
         public static void WriteWarnLine(string format, params object[] args)
         {
             var str = string.Format(format, args);
             Log.Warn(str);
-            Console.WriteLine(str);
         }
 
         public static void WriteErrorLine(string str)
         {
             Log.Error(str);
-            Console.Error.WriteLine(str);
         }
 
         public static void WriteFatalLine(string str, Exception exception)
         {
             Log.Fatal(str, exception);
-            Console.Error.WriteLine(str + exception.Message);
         }
     }
 
     public static class LogExtensions
     {
+        // equivalent to NOTICE
+        private static readonly log4net.Core.Level SuccessLevel = new log4net.Core.Level(50000, "SUCCESS");
+
+        /// <summary>
+        /// Log a message object with the <see cref="F:LogExtensions.SuccessLevel"/> level -
+        /// equivalent to <see cref="F:log4net.Core.Level.Notice"/> 
+        /// </summary>
+        /// <param name="log">
+        /// The log.
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        public static void Success(this ILog log, object message)
+        {
+            log.Logger.Log(null, SuccessLevel, message, null);
+        }
+
+        /// <summary>
+        /// Log a message object with the <see cref="F:LogExtensions.SuccessLevel"/> level -
+        /// equivalent to <see cref="F:log4net.Core.Level.Notice"/> 
+        /// </summary>
+        /// <param name="log">
+        /// The log.
+        /// </param>
+        /// <param name="format">
+        /// The string format.
+        /// </param>
+        /// <param name="args">
+        /// The args to the format string.
+        /// </param>
+        public static void Success(this ILog log, string format, params object[] args)
+        {
+            log.Logger.Log(null, SuccessLevel, string.Format(format, args), null);
+        }
+
         /// <summary>
         /// Log a message object with the <see cref="F:log4net.Core.Level.Verbose"/> level.
         /// Verbose is the most detailed log level.
@@ -127,7 +164,7 @@ namespace System
         /// </remarks>
         public static void Verbose(this ILog log, string format, params object[] args)
         {
-            log.Logger.Log(null, log4net.Core.Level.Finest, String.Format(format, args), null);
+            log.Logger.Log(null, log4net.Core.Level.Finest, string.Format(format, args), null);
         }
 
         /// <summary>
@@ -148,7 +185,7 @@ namespace System
         }
 
         /// <summary>
-        /// Log a message object with the <see cref="F:log4net.Core.Level.Verbose"/> level including
+        /// Log a message object with the <see cref="F:log4net.Core.Level.Trace"/> level including
         ///             the stack trace of the <see cref="T:System.Exception"/> passed
         ///             as a parameter.
         /// Verbose is the most detailed log level.
@@ -161,7 +198,7 @@ namespace System
         /// </remarks>
         public static void Trace(this ILog log, object message)
         {
-            log.Logger.Log(null, log4net.Core.Level.Fine, message, null);
+            log.Logger.Log(null, log4net.Core.Level.Finer, message, null);
         }
 
         /// <summary>
@@ -175,7 +212,7 @@ namespace System
         /// </remarks>
         public static void Trace(this ILog log, string format, params object[] args)
         {
-            log.Logger.Log(null, log4net.Core.Level.Fine, String.Format(format, args), null);
+            log.Logger.Log(null, log4net.Core.Level.Finer, string.Format(format, args), null);
         }
 
         /// <summary>
@@ -193,7 +230,7 @@ namespace System
         /// <seealso cref="M:Trace(object)"/><seealso cref="P:log4net.ILog.IsTraceEnabled"/>   
         public static void Trace(this ILog log, object message, Exception exception)
         {
-            log.Logger.Log(null, log4net.Core.Level.Fine, message, exception);
+            log.Logger.Log(null, log4net.Core.Level.Finer, message, exception);
         }
     }
 }
