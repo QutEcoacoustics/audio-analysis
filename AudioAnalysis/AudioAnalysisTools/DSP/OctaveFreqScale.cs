bf6efa9694cc21dae2ab9649569fdfc6b5891355
towsey <m.towsey@qut.edu.au>
2017-02-28 16:57:04 +1000

    More work on Octave freq scale Original hash: 05c6a3813791325e8bbd478d4629d403f4ee5598

--- AudioAnalysis/AudioAnalysisTools/DSP/OctaveFreqScale.cs
+++ AudioAnalysis/AudioAnalysisTools/DSP/OctaveFreqScale.cs
@@ -1,37 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace AudioAnalysisTools.DSP
 {
     public class OctaveFreqScale
-    { 
+    {
 
-        public static double[] GetFractionalOctaveBands(double minFreq, double maxFreq, int subbandCount)
+        
+
+        public static int[,] LinearToSplitLinearOctaveScale(int sr, int frameSize, int finalBinCount, int lowerFreqBound, int upperFreqBound, int octaveDivisions)
         {
-            double[] FreqBandCentres = { 15.625, 31.25, 62.5, 125, 250, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000 };
-            double sqrt2 = 1.41421356;
+
+            var bandBounds = OctaveFreqScale.GetFractionalOctaveBands(lowerFreqBound, upperFreqBound, octaveDivisions);
+            var linearFreqScale = GetLinearFreqScale(sr, frameSize);
+
+            int nyquist = sr / 2;
+            var splitLinearOctaveIndexBounds = new int[finalBinCount, 2];
+
+            int binCount = frameSize / 2;
+            double freqStep = nyquist / (double)binCount;
+
+
+            int topLinearIndex = (int)Math.Round(lowerFreqBound/freqStep);
+            // fill in the linear part of the freq scale
+            for (int i = 0; i < topLinearIndex; i++)
+            {
+                splitLinearOctaveIndexBounds[i, 0] = i;
+                splitLinearOctaveIndexBounds[i, 1] = (int)Math.Round(linearFreqScale[i]);
+            }
+
+
+
+            for (int i = topLinearIndex; i < finalBinCount; i++)
+            {
+
+                for (int j = 0; j < linearFreqScale.Length; j++)
+                {
+                    if (linearFreqScale[j] > bandBounds[i])
+                    {
+                        splitLinearOctaveIndexBounds[i, 0] = j;
+                        splitLinearOctaveIndexBounds[i, 1] = (int)Math.Round(linearFreqScale[j]);
+                        break;
+                    }
+                }
+            }
+
+            return splitLinearOctaveIndexBounds;
+        }
+
+
+        public static int[,] LinearToFullOctaveScale(int sr, int frameSize, int finalBinCount, int lowerFreqBound, int upperFreqBound, int octaveDivisions)
+        {
+
+            var bandBounds = OctaveFreqScale.GetFractionalOctaveBands(lowerFreqBound, upperFreqBound, octaveDivisions);
+            var linearFreqScale = GetLinearFreqScale(sr, frameSize);
+
+            int nyquist = sr / 2;
+            var octaveIndexBounds = new int[finalBinCount, 2];
+
+            for (int i = 0; i < finalBinCount; i++)
+            {
+
+                for (int j = 0; j < linearFreqScale.Length; j++)
+                {
+                    if (linearFreqScale[j] > bandBounds[i])
+                    {
+                        octaveIndexBounds[i, 0] = j;
+                        octaveIndexBounds[i, 1] = (int)Math.Round(linearFreqScale[j]);
+                        break;
+                    }
+                }
+            }
+
+            return octaveIndexBounds;
+        }
+
+
+
+        public static double[] GetFractionalOctaveBands(double minFreq, double maxFreq, int octaveDivisions)
+        {
+            double[] freqBandCentres = { 15.625, 31.25, 62.5, 125, 250, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000 };
+            const double sqrt2 = 1.41421356;
 
             var list = new List<double>();
 
-            for (int i = 0; i < FreqBandCentres.Length; i++)
+            for (int i = 0; i < freqBandCentres.Length; i++)
             {
-                if (FreqBandCentres[i] < minFreq) continue;
-                if (FreqBandCentres[i] > maxFreq) break;
+                if (freqBandCentres[i] < minFreq) continue;
+                if (freqBandCentres[i] > maxFreq) break;
            
-                double[] fractionalOctaveBands = GetFractionalOctaveBands(FreqBandCentres[i], subbandCount);
+                double[] fractionalOctaveBands = GetFractionalOctaveBands(freqBandCentres[i], octaveDivisions);
 
-                for (int j = 0; j < subbandCount; j++)
+                for (int j = 0; j < octaveDivisions; j++)
                 {
                     // convert the centre to a floor by dividing by sqrt2
                     double floor = fractionalOctaveBands[j] / sqrt2;
                     //double ceiling = fractionalOctaveBands[j] * sqrt2;
                     if (floor < minFreq) continue;
-                    else
-                    {
-                        list.Add(floor);
-                    }
+
+                    list.Add(floor);
                 }
             }
             return list.ToArray();
@@ -52,6 +119,28 @@ namespace AudioAnalysisTools.DSP
 
             return fractionalOctaveBands;
         }
+
+        /// <summary>
+        /// THis method assumes that the frameSize will be power of 2 and that 
+        /// </summary>
+        /// <param name="sr"></param>
+        /// <param name="frameSize"></param>
+        /// <returns></returns>
+        public static double[] GetLinearFreqScale(int sr, int frameSize)
+        {
+            int nyquist = sr/2;
+            int binCount = frameSize/2;
+            double freqStep = nyquist / (double)binCount;
+            double[] linearFreqScale = new double[binCount];
+
+            for (int i = 0; i < binCount; i++)
+            {
+                linearFreqScale[i] = freqStep*i;
+            }
+
+            return linearFreqScale;            
+        }
+
 
     }
 }
