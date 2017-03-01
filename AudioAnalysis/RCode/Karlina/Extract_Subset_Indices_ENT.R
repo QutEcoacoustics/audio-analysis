e28ce316ba9045f8dcf5e8abc1e182a7aa4470f9
Karlina Indraswari <karlina.indraswari@hdr.qut.edu.au>
2017-03-01 15:12:19 +1000

    Added R script

---
+++ AudioAnalysis/RCode/Karlina/Extract_Subset_Indices_ENT.R
@@ -0,0 +1,38 @@
+DirName <- "C:\\Work\\RHome\\Groote_Projects\\Indices\\ENT"
+DirName2 <-"C:\\Work\\RHome\\Groote_Projects\\Indices"
+Filelist <- list.files(path = DirName)
+
+#In name, use specific species and type of index that wants to be extracted
+
+dir.create(paste(DirName2, "Lrothii_ENT", sep="\\"))
+
+#if transposed
+
+  #dir.create(paste(DirName, "transposed_LrothiiENT", sep="\\"))
+           
+for(indices_file in Filelist) { 
+  path <- paste(DirName, indices_file, sep="\\")
+  index_data <- read.csv(path)
+  
+#if transpose is desired
+
+  #transposed_index_data <- t(index_data)
+  
+#Index selection corresponds to the frequency bandwith of a frog. Specific for Groote Eyandt the bandwith are as follows
+#L_rothii bandwith = 23:68
+#Lbicolor = 49:145
+#Lwotjulumensis = 22:96
+#Uinundata = 33:80
+  
+  index_sub_selection <- index_data[,23:68]
+  
+#use the following if transposed
+  
+  #index_sub_selection <- transposed_index_data[23:68,]
+  
+
+  new_filename <- paste("ENT_", indices_file)
+  output_csv <- paste(DirName2, "\\Lrothii_ENT\\", new_filename, ".csv", sep = "")
+  write.csv(index_sub_selection, output_csv)
+}
+
