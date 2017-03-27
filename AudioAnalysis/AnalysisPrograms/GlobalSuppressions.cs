5724dd781bf4b4c7391a9ad350ef3ea3af169f8b
Michael Towsey <m.towsey@qut.edu.au>
2017-03-27 15:16:57 +1000

    Changes due to installation of VS2017

---
+++ AudioAnalysis/AnalysisPrograms/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1614:Element parameter documentation must have text", Justification = "<Pending>", Scope = "member", Target = "~M:AnalysisPrograms.Recognizers.ArdeaInsignis.Recognize(AudioAnalysisTools.WavTools.AudioRecording,,System.TimeSpan,System.Lazy{AudioAnalysisTools.Indices.IndexCalculateResult[]},System.IO.DirectoryInfo,System.Nullable{System.Int32})~AnalysisPrograms.Recognizers.Base.RecognizerResults")]
+
