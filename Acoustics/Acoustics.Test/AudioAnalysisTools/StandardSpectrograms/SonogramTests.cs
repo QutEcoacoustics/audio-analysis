55142089c8eb65d46e2f96f1d2f9a30d89b62710
Anthony Truskinger <anthony.truskinger@gmail.com>
2017-04-05 17:52:07 +1000

    Rewrote sonogram tests
    Also added a Binary serializer (with unit tests!) to help us efficiently dump and load blobs. Additionally added *.bin to git-lfs tracked filed

--- Acoustics/Acoustics.Test/AudioAnalysisTools/StandardSpectrograms/SonogramTests.cs
+++ Acoustics/Acoustics.Test/AudioAnalysisTools/StandardSpectrograms/SonogramTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="FrequencyScaleTests.cs" company="QutEcoacoustics">
+﻿// <copyright file="SonogramTests.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
 
@@ -64,19 +64,12 @@ namespace Acoustics.Test.AudioAnalysisTools.StandardSpectrograms
         /// Check it on standard one minute recording.
         /// </summary>
         [TestMethod]
-        public void Sonograms()
+        public void TestAmplitudeSonogram()
         {
-            var recordingPath = @"Recordings\BAC2_20071008-085040.wav";
-            var opFileStem = "BAC2_20071008";
-            var outputDir = this.outputDirectory;
-
-            var recording = new AudioRecording(recordingPath);
+            var recording = new AudioRecording(@"Recordings\BAC2_20071008-085040.wav");
 
             // specfied linear scale
-            int nyquist = 11025;
-            int frameSize = 1024;
-            int hertzInterval = 1000;
-            var freqScale = new FrequencyScale(nyquist, frameSize, hertzInterval);
+            var freqScale = new FrequencyScale(nyquist: 11025, frameSize: 1024, herzInterval: 1000);
 
             var sonoConfig = new SonogramConfig
             {
@@ -87,42 +80,80 @@ namespace Acoustics.Test.AudioAnalysisTools.StandardSpectrograms
                 NoiseReductionParameter = 0.0,
             };
 
-            // DO FILE EQUALITY TEST on the AMPLITUDE SONGOGRAM DATA
+            // DO EQUALITY TEST on the AMPLITUDE SONGOGRAM DATA
             // Do not bother with the image because this is only an amplitude spectrogram.
             var sonogram = new AmplitudeSonogram(sonoConfig, recording.WavReader);
-            var stemOfExpectedFile = opFileStem + "_AmplSonogramData.EXPECTED.json";
-            var stemOfActualFile = opFileStem + "_AmplSonogramData.ACTUAL.json";
-            var expectedFile1 = new FileInfo("StandardSonograms\\" + stemOfExpectedFile);
-            if (!expectedFile1.Exists)
-            {
-                LoggedConsole.WriteErrorLine("An EXPECTED results file does not exist. Test will fail!");
-                LoggedConsole.WriteErrorLine("If ACTUAL results file is correct, move it to dir <...\\TestResources\\FrequencyScale> and change its suffix to <.EXPECTED.json>");
-            }
+            var expectedFile = new FileInfo("StandardSonograms\\BAC2_20071008_AmplSonogramData.EXPECTED.bin");
 
-            var actualFile1 = new FileInfo(Path.Combine(outputDir.FullName, stemOfActualFile));
-            Json.Serialise(actualFile1, sonogram.Data);
-            TextFileEqualityTests.TextFileEqual(expectedFile1, actualFile1);
+            // run this once to generate expected test data (and remember to copy out of bin/debug!)
+            //Binary.Serialize(expectedFile, sonogram.Data);
+
+            var expected = Binary.Deserialize<double[,]>(expectedFile);
+
+            CollectionAssert.AreEqual(expected, sonogram.Data);
+
+        }
+
+        [TestMethod]
+        public void TestDecibelSpectrogram()
+        {
+            var recording = new AudioRecording(@"Recordings\BAC2_20071008-085040.wav");
+
+            // specfied linear scale
+            var freqScale = new FrequencyScale(nyquist: 11025, frameSize: 1024, herzInterval: 1000);
+
+            var sonoConfig = new SonogramConfig
+            {
+                WindowSize = freqScale.FinalBinCount * 2,
+                WindowOverlap = 0.2,
+                SourceFName = recording.BaseName,
+                NoiseReductionType = NoiseReductionType.None,
+                NoiseReductionParameter = 0.0,
+            };
+
+            // DO EQUALITY TEST on the AMPLITUDE SONGOGRAM DATA
+            // Do not bother with the image because this is only an amplitude spectrogram.
+            var sonogram = new AmplitudeSonogram(sonoConfig, recording.WavReader);
 
             // DO FILE EQUALITY TEST on the DECIBEL SONGOGRAM DATA
             // Do not bother with the image because this has been tested elsewhere.
             var decibelSonogram = MFCCStuff.DecibelSpectra(sonogram.Data, sonogram.Configuration.WindowPower, sonogram.SampleRate, sonogram.Configuration.epsilon);
 
-            stemOfExpectedFile = opFileStem + "_DecibelSonogramData.EXPECTED.json";
-            stemOfActualFile = opFileStem + "_DecibelSonogramData.ACTUAL.json";
-            var expectedFile2 = new FileInfo("StandardSonograms\\" + stemOfExpectedFile);
-            if (!expectedFile2.Exists)
+            var expectedFile = new FileInfo("StandardSonograms\\BAC2_20071008_DecibelSonogramData.EXPECTED.bin");
+
+            // run this once to generate expected test data (and remember to copy out of bin/debug!)
+            //Binary.Serialize(expectedFile, decibelSonogram);
+
+            var expected = Binary.Deserialize<double[,]>(expectedFile);
+
+            CollectionAssert.AreEqual(expected, decibelSonogram);
+        }
+
+        [TestMethod]
+        public void SonogramDecibelMethodsAreEquivalent()
+        {
+            var recording = new AudioRecording(@"Recordings\BAC2_20071008-085040.wav");
+
+            // specfied linear scale
+            var freqScale = new FrequencyScale(nyquist: 11025, frameSize: 1024, herzInterval: 1000);
+
+            var sonoConfig = new SonogramConfig
             {
-                LoggedConsole.WriteErrorLine("An EXPECTED results file does not exist. Test will fail!");
-                LoggedConsole.WriteErrorLine("If ACTUAL results file is correct, move it to dir <...\\TestResources\\FrequencyScale> and change its suffix to <.EXPECTED.json>");
-            }
+                WindowSize = freqScale.FinalBinCount * 2,
+                WindowOverlap = 0.2,
+                SourceFName = recording.BaseName,
+                NoiseReductionType = NoiseReductionType.None,
+                NoiseReductionParameter = 0.0,
+            };
 
-            var actualFile2 = new FileInfo(Path.Combine(outputDir.FullName, stemOfActualFile));
-            Json.Serialise(actualFile2, decibelSonogram);
-            TextFileEqualityTests.TextFileEqual(expectedFile2, actualFile2);
+            // Method 1
+            var sonogram = new AmplitudeSonogram(sonoConfig, recording.WavReader);
+            var expectedDecibelSonogram = MFCCStuff.DecibelSpectra(sonogram.Data, sonogram.Configuration.WindowPower, sonogram.SampleRate, sonogram.Configuration.epsilon);
 
-            // now make sure that the decibel spectrum is the same no matter which path we take to calculate it.
-            var sonogram2 = new SpectrogramStandard(sonoConfig, recording.WavReader);
-            CollectionAssert.AreEqual(decibelSonogram, sonogram2.Data);
+            // Method 2: make sure that the decibel spectrum is the same no matter which path we take to calculate it.
+            var actualDecibelSpectrogram = new SpectrogramStandard(sonoConfig, recording.WavReader);
+
+            CollectionAssert.AreEqual(expectedDecibelSonogram, actualDecibelSpectrogram.Data);
         }
     }
 }
---
+++ Acoustics/Acoustics.Test/Shared/BinaryTests.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="BinaryTests.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
+
+namespace Acoustics.Test.Shared
+{
+    using System;
+    using System.IO;
+    using Acoustics.Shared;
+    using EcoSounds.Mvc.Tests;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BinaryTests
+    {
+        private DirectoryInfo outputDirectory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.outputDirectory = TestHelper.GetTempDir();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TestHelper.DeleteTempDir(this.outputDirectory);
+        }
+
+        [TestMethod]
+        public void TestBinarySerializationRoundTrip()
+        {
+            var random = new Random();
+
+            var numbers = new double[100];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.NextDouble();
+            }
+
+            // serialize to temporay file
+            var tempFile = this.outputDirectory.CombineFile("dump.bin");
+            Binary.Serialize(tempFile, numbers);
+
+            // deserialize
+            var actual = Binary.Deserialize<double[]>(tempFile);
+
+            // test
+            CollectionAssert.AreEqual(numbers, actual);
+        }
+    }
+}
