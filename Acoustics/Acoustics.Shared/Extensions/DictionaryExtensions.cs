ed07e1e7c21421f322f2363ac86481e73f481ce2
Anthony Truskinger <anthony.truskinger@gmail.com>
2017-05-09 12:06:52 +1000

    Enhance ImageAssert with extra helper method

---
+++ Acoustics/Acoustics.Shared/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+
+        public static string ToDebugString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
+        }
+    }
+}
