75f351637c5ba735cee8fbad5f05d65d1547f268
Anthony Truskinger <anthony.truskinger@gmail.com>
2017-05-12 20:18:23 +1000

    Testing CSV serializer wrt to TimeSpans
    I changed the CSV Serializer so that b default it outputs better error
    messages for bad type conversions. I also added a unit test to make sure
    that it works. An important and unfortunate side effect is that we had
    to disable yield the CSV stream. It remains to be seen if this has a big
    impact on performance.
    
    Additionally I wrote a unit test to ensure that the CSV Serializer is
    round tripping TImeSpan values correctly. The test tests 30 orders of
    magnitude for randomly generated time spans. This is necessary because
    an earlier version of .NET changed the default formatting for TimeSpans
    which sometimes causes serialization errors. This test ensures our
    serializer is not at fault and that new data generated is not at fault
    either.

--- Acoustics/Acoustics.Shared/Extensions/DictionaryExtensions.cs
+++ Acoustics/Acoustics.Shared/Extensions/DictionaryExtensions.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿// <copyright file="DictionaryExtensions.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
 // ReSharper disable once CheckNamespace
 namespace System.Collections.Generic
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public static class DictionaryExtensions
     {
 
         public static string ToDebugString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
         {
             return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
+        }
+
+        public static string ToDictDebugString(this IDictionary dictionary)
+        {
+            var items = new List<string>(dictionary.Count);
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                items.Add(entry.Key.ToString() + "=" + entry.Value.ToString());
+            }
+
+            return "{" + string.Join(",",  items) + "}";
         }
     }
 }
