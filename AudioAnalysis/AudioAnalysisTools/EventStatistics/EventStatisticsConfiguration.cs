e00415a7d5fc220045b43c027f6b35f95ba3e875
Michael Towsey <m.towsey@qut.edu.au>
2017-05-31 15:20:25 +1000

    Create two new classes to assist with EventStatistics calculation Original hash: 9a824020816f2c6dac2d2b517e21531798c18240

---
+++ AudioAnalysis/AudioAnalysisTools/EventStatistics/EventStatisticsConfiguration.cs
@@ -0,0 +1,13 @@
+// <copyright file="EventStatisticsConfiguration.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
+
+namespace AudioAnalysisTools.EventStatistics
+{
+    public class EventStatisticsConfiguration
+    {
+        public int FrameSize { get; set; }
+
+        public int FrameStep { get; set; }
+    }
+}
---
+++ AudioAnalysis/AudioAnalysisTools/EventStatistics/EventStatsticsCalculate.cs
@@ -0,0 +1,167 @@
+﻿// <copyright file="EventStatsticsCalculate.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
+
+namespace AudioAnalysisTools.EventStatistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Acoustics.Tools.Wav;
+    using DSP;
+    using TowseyLibrary;
+    using WavTools;
+    using AudioAnalysisTools.StandardSpectrograms;
+
+    public static class EventStatsticsCalculate
+    {
+        /// <summary>
+        /// The acoustic statistics calculated in this method are based on methods outlined in
+        /// "Acoustic classification of multiple simultaneous bird species: A multi-instance multi-label approach",
+        /// by Forrest Briggs, Balaji Lakshminarayanan, Lawrence Neal, Xiaoli Z.Fern, Raviv Raich, Sarah J.K.Hadley, Adam S. Hadley, Matthew G. Betts, et al.
+        /// The Journal of the Acoustical Society of America v131, pp4640 (2012); doi: http://dx.doi.org/10.1121/1.4707424
+        /// ..
+        /// The Briggs feature are calculated from the column (freq bin) and row (frame) sums of the extracted spectrogram.
+        /// 1. Gini Index for frame and bin sums. A measure of dispersion. Problem with gini is that its value is dependent on the row or colukmn count.
+        ///    We use entropy instead because value not dependent on row or column count because it is normalised.
+        /// For the following meausres of k-central moments, the freq and time values are normalised in 0,1 to width of the event.
+        /// 2. freq-mean
+        /// 3. freq-variance
+        /// 4. freq-skew
+        /// 5. time-mean
+        /// 6. time-variance
+        /// 7. time-skew
+        /// 8. freq-max (normalised)
+        /// 9. time-max (normalised)
+        /// ..
+        /// Briggs et al also calculate a 16 value histogram of gradients for each event mask. We do not do that here although we could.
+        /// ..
+        /// NOTE: This method assumes that the passed event occurs totally within the passed recording,
+        /// AND that the passed recording is of sufficient duration to obtain reliable BGN noise profile
+        /// BUT not so long as to cause memory constipation.
+        /// </summary>
+        /// <param name="recording">as type AudioRecording which contains the event</param>
+        /// <param name="temporalTarget">both start and end bounds</param>
+        /// <param name="spectralTarget">both bottom and top bounds in Herz</param>
+        /// <param name="config">parameters that determine the outcome of the analysis</param>
+        /// <returns>an instance of EventStatistics</returns>
+        public static EventStatistics AnalyzeAudioEvent(
+            AudioRecording recording,
+            (TimeSpan start, TimeSpan end) temporalTarget,
+            (int start, int end) spectralTarget,
+            EventStatisticsConfiguration config)
+        {
+            var stats = new EventStatistics
+            {
+                EventStart = temporalTarget.start,
+                EventEnd = temporalTarget.end,
+                Duration = temporalTarget.end - temporalTarget.start,
+                FreqLow = spectralTarget.start,
+                FreqTop = spectralTarget.end,
+                BandWidth = spectralTarget.end - spectralTarget.start,
+            };
+
+            // convert recording to spectrogram
+            int sampleRate = recording.SampleRate;
+            double epsilon = recording.Epsilon;
+
+            // extract the spectrogram
+            var dspOutput1 = DSP_Frames.ExtractEnvelopeAndFfts(recording, config.FrameSize, config.FrameStep);
+            double herzPerBin = dspOutput1.FreqBinWidth;
+            int nyquist = dspOutput1.NyquistFreq;
+            var spectrogram = dspOutput1.AmplitudeSpectrogram;
+            var frameDuration = TimeSpan.FromSeconds(config.FrameSize / (double)sampleRate);
+            var stepDuration = TimeSpan.FromSeconds(config.FrameStep / (double)sampleRate);
+            var startFrame = (int)Math.Floor(temporalTarget.start.TotalSeconds / stepDuration.TotalSeconds);
+            var endFrame = (int)Math.Ceiling(temporalTarget.end.TotalSeconds / stepDuration.TotalSeconds);
+
+            var bottomBin = (int)Math.Floor(spectralTarget.start / herzPerBin);
+            var topBin = (int)Math.Ceiling(spectralTarget.end / herzPerBin);
+
+            // Convert amplitude spectrogram to deciBels and calculate the dB background noise profile
+            double[,] decibelSpectrogram = MFCCStuff.DecibelSpectra(dspOutput1.AmplitudeSpectrogram, dspOutput1.WindowPower, sampleRate, epsilon);
+            double[] spectralDecibelBgn = NoiseProfile.CalculateBackgroundNoise(decibelSpectrogram);
+
+            decibelSpectrogram = SNR.TruncateBgNoiseFromSpectrogram(decibelSpectrogram, spectralDecibelBgn);
+            decibelSpectrogram = SNR.RemoveNeighbourhoodBackgroundNoise(decibelSpectrogram, nhThreshold: 2.0);
+
+            // extract the required acoustic event
+            var eventMatrix = MatrixTools.Submatrix(decibelSpectrogram, startFrame, bottomBin, endFrame, topBin);
+
+            // Assume linear scale.
+            var freqScale = new FrequencyScale(nyquist: nyquist, frameSize: config.FrameSize, herzLinearGridInterval: 1000);
+
+            var columnSums = MatrixTools.GetColumnSums(eventMatrix);
+            int maxColumnId = DataTools.GetMaxIndex(columnSums);
+            var rowSums = MatrixTools.GetRowSums(eventMatrix);
+            int maxRowId = DataTools.GetMaxIndex(rowSums);
+
+            // TODO
+            NormalDist.AverageAndSD(spectrogram, out double av, out double sd);
+            stats.AverageAmplitude = av;
+            stats.DominantFrequency = (int)Math.Round(herzPerBin * maxColumnId) + spectralTarget.start;
+
+            return stats;
+        }
+
+        public static void TestCalculateEventStatistics()
+        {
+            int sampleRate = 22050;
+            double duration = 28; // signal duration in seconds = 7 minutes
+            int[] harmonics1 = { 500 };
+            int[] harmonics2 = { 500, 1000, 2000, 4000, 8000 };
+            var recording1 = DspFilters.GenerateTestSignal(sampleRate, duration, harmonics1);
+            var recording2 = DspFilters.GenerateTestSignal(sampleRate, 4, harmonics2);
+            var recording3 = DspFilters.GenerateTestSignal(sampleRate, duration, harmonics1);
+            var list = new List<double[]>();
+            list.Add(recording1.WavReader.Samples);
+            list.Add(recording2.WavReader.Samples);
+            list.Add(recording3.WavReader.Samples);
+            var signal = DataTools.ConcatenateVectors(list);
+            var wr = new WavReader(signal, 1, 16, sampleRate);
+            var recording = new AudioRecording(wr);
+
+            var start = TimeSpan.FromSeconds(28);
+            var end = TimeSpan.FromSeconds(32);
+            int lowFreq = 1500;
+            int topFreq = 8500;
+
+            var statsConfig = new EventStatisticsConfiguration()
+            {
+                FrameSize = 512,
+                FrameStep = 512,
+            };
+
+            EventStatistics stats = AnalyzeAudioEvent(recording, (start, end), (lowFreq, topFreq), statsConfig);
+
+            // Assume linear scale.
+            int nyquist = sampleRate / 2;
+            var freqScale = new FrequencyScale(nyquist: nyquist, frameSize: statsConfig.FrameSize, herzLinearGridInterval: 1000);
+
+            var sonoConfig = new SonogramConfig
+            {
+                WindowSize = statsConfig.FrameSize,
+                WindowStep = statsConfig.FrameSize,
+                WindowOverlap = 0.0,
+                SourceFName = "SineSignal3",
+                NoiseReductionType = NoiseReductionType.Standard,
+                NoiseReductionParameter = 0.12,
+            };
+            var sonogram = new AmplitudeSonogram(sonoConfig, recording.WavReader);
+            var image = sonogram.GetImage();
+            string title = $"Spectrogram of Harmonics: SR={sampleRate}  Window={freqScale.WindowSize}";
+            image = sonogram.GetImageFullyAnnotated(image, title, freqScale.GridLineLocations);
+            string path = @"C:\SensorNetworks\Output\Sonograms\UnitTestSonograms\SineSignal3.png";
+            image.Save(path);
+
+            // get spectrum from row 1300
+            var normalisedIndex = DataTools.normalise(MatrixTools.GetRow(sonogram.Data, 1300));
+            var image2 = GraphsAndCharts.DrawGraph("SPECTRUM", normalisedIndex, 100);
+            string path2 = @"C:\SensorNetworks\Output\Sonograms\UnitTestSonograms\Spectrum3.png";
+            image2.Save(path2);
+
+        }
+    }
+}
