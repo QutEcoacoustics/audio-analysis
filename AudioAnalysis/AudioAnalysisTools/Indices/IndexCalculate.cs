fe05e1f85eaf32c5bddcddc2b8e212236b1c82ed
Michael Towsey <m.towsey@qut.edu.au>
2017-08-29 12:07:17 +1000

    add some method comments Original hash: 2c3ac03dc0173a31400629b7b7568756b49ab0e0

--- AudioAnalysis/AudioAnalysisTools/Indices/IndexCalculate.cs
+++ AudioAnalysis/AudioAnalysisTools/Indices/IndexCalculate.cs
@@ -424,9 +424,7 @@ namespace AudioAnalysisTools.Indices
             }
 
             // YES WE WILL DO CLUSTERING! to determine cluster count (spectral diversity) and spectral persistence.
-            // Only use midband AMPLITDUE SPECTRUM. In June 2016, the mid-band (i.e. the bird-band) was set to lowerBound=1000Hz, upperBound=8000hz.
-            // NOTE: Clustering is performed only on the midBandAmplSpectrogram of the noise reduced decibelSpectrogram.
-            // NOTE: The amplitudeSpectrogram is already noise reduced at this stage.
+            // Only use midband decibel SPECTRUM. In June 2016, the mid-band (i.e. the bird-band) was set to lowerBound=1000Hz, upperBound=8000hz.
             // Actually do clustering of binary spectra. Must first threshold
             double binaryThreshold = SpectralClustering.DefaultBinaryThresholdInDecibels;
             var midBandSpectrogram = MatrixTools.Submatrix(deciBelSpectrogram, 0, lowerBinBound, deciBelSpectrogram.GetLength(0) - 1, middleBinBound);
