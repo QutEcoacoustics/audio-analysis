4bfb6a52267df42d996f9370da0d2b6736a602fe
aroelo <aniek93@live.nl>
2017-12-11 16:01:34 +1000

    Created class StandardizedFeatureExtraction
    -Given it an execute method
    -Takes arguments and print 'hello world'
    -Began implementation of AbstractStrongAnalyser

---
+++ AudioAnalysis/AnalysisPrograms/StandardizedFeatureExtraction/StandardizedFeatureExtraction.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="StandardizedFeatureExtraction.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
+
+namespace AnalysisPrograms.StandardizedFeatureExtraction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using AnalysisBase;
+    using AnalysisBase.ResultBases;
+    using PowerArgs;
+
+    public class StandardizedFeatureExtraction : AbstractStrongAnalyser
+    {
+        private const string Sentence = "Hello World";
+
+        // Input is a class object of Arguments, class is made below
+        public static void Execute(Arguments arguments)
+        {
+            for (int i = 0; i < arguments.Multiplication; i++)
+            {
+                Console.WriteLine(Sentence);
+            }
+        }
+
+        // Creates a class that constructs arguments, the description is shown in help StandardizedFeatureExtraction
+        // The variable Multiplication can later be used
+        public class Arguments
+        {
+            [ArgDescription("How many times hello world")]
+            public int Multiplication { get; set; }
+        }
+
+        // Implemented from AbstractStrongAnalyser
+        public override string DisplayName
+        {
+            get { return "Standardized Feature Extraction"; }
+        }
+
+        public override string Identifier
+        {
+            get { return "Standardized.Feature.Extraction"; }
+        }
+
+        public virtual string Description
+        {
+            get { return "Performs a standardized feature extraction."; }
+        }
+
+        public override AnalysisResult2 Analyze<T>(AnalysisSettings analysisSettings, SegmentSettings<T> segmentSettings)
+        {
+            FileInfo audioFile = segmentSettings.SegmentAudioFile;
+
+            Dictionary<string, string> configuration = analysisSettings.Configuration;
+
+
+            throw new NotImplementedException();
+        }
+
+        public override void WriteEventsFile(FileInfo destination, IEnumerable<EventBase> results)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteSummaryIndicesFile(FileInfo destination, IEnumerable<SummaryIndexBase> results)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<FileInfo> WriteSpectrumIndicesFiles(DirectoryInfo destination, string fileNameBase, IEnumerable<SpectralIndexBase> results)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SummariseResults(
+            AnalysisSettings settings,
+            FileSegment inputFileSegment,
+            EventBase[] events,
+            SummaryIndexBase[] indices,
+            SpectralIndexBase[] spectralIndices,
+            AnalysisResult2[] results)
+        {
+            // no op
+        }
+    }
+}
