c0e47a595b792b56d3831845fc25beb9aeaa8931
Mahnoosh Kholghi <kholghim@qut.edu.au>
2018-07-20 10:48:45 +1000

    added a test for PSD class

--- tests/Acoustics.Test/AudioAnalysisTools/DSP/UnsupervisedFeatureLearningTest.cs
+++ tests/Acoustics.Test/AudioAnalysisTools/DSP/UnsupervisedFeatureLearningTest.cs
@@ -471,5 +471,73 @@ namespace Acoustics.Test.AudioAnalysisTools.DSP
             reconstructedSpecImage.Save(outputReSpecImagePath, ImageFormat.Png);
             */
         }
+
+        [TestMethod]
+        public void PowerSpectrumDensityTest()
+        {
+            var inputPath = @"C:\Users\kholghim\Mahnoosh\Liz\TrainSet\";
+            var resultPath = @"C:\Users\kholghim\Mahnoosh\Liz\PowerSpectrumDensity\train_PSD.bmp"; // @"M:\Postdoc\PowerSpectrumDensity\train_PSD.bmp";//
+            //var inputPath =Path.Combine(inputDir, "TrainSet"); // directory of the one-min recordings of one day (21 and 23 Apr - Black Rail Data)
+
+            // check whether there is any file in the folder/subfolders
+            if (Directory.GetFiles(inputPath, "*", SearchOption.AllDirectories).Length == 0)
+            {
+                throw new ArgumentException("The folder of recordings is empty...");
+            }
+
+            // get the nyquist value from the first wav file in the folder of recordings
+            int nq = new AudioRecording(Directory.GetFiles(inputPath, "*.wav")[0]).Nyquist;
+            int nyquist = nq; // 11025;
+            int frameSize = 1024;
+            int finalBinCount = 128;
+            int hertzInterval = 1000;
+            FreqScaleType scaleType = FreqScaleType.Mel;
+            var freqScale = new FrequencyScale(scaleType, nyquist, frameSize, finalBinCount, hertzInterval);
+            var fst = freqScale.ScaleType;
+
+            var sonoConfig = new SonogramConfig
+            {
+                WindowSize = frameSize,
+                WindowOverlap = 0.1028,
+                DoMelScale = (scaleType == FreqScaleType.Mel) ? true : false,
+                MelBinCount = (scaleType == FreqScaleType.Mel) ? finalBinCount : frameSize / 2,
+                NoiseReductionType = NoiseReductionType.None,
+            };
+
+            List<double[]> psd = new List<double[]>();
+            foreach (string filePath in Directory.GetFiles(inputPath, "*.wav"))
+            {
+                FileInfo fileInfo = filePath.ToFileInfo();
+
+                // process the wav file if it is not empty
+                if (fileInfo.Length != 0)
+                {
+                    var recording = new AudioRecording(filePath);
+                    sonoConfig.SourceFName = recording.BaseName;
+
+                    //var sonogram = new SpectrogramStandard(sonoConfig, recording.WavReader);
+                    var amplitudeSpectrogram = new AmplitudeSonogram(sonoConfig, recording.WavReader);
+                    // save the matrix 
+                    // skip normalisation
+                    // skip mel
+
+                    var energySpectrogram = new EnergySpectrogram(sonoConfig, amplitudeSpectrogram.Data);
+
+                    // square the FFT coefficients to get an energy spectrogram
+                    //double[,] energySpectrogram = PowerSpectrumDensity.GetEnergyValues(sonogram.Data);
+
+                    // RMS NORMALIZATION
+                    double[,] normalizedValues = SNR.RmsNormalization(energySpectrogram.Data);
+
+                    // Median Noise Reduction
+                    double[,] noiseReducedValues = PcaWhitening.NoiseReduction(normalizedValues);
+
+                    psd.Add(PowerSpectrumDensity.GetPowerSpectrum(noiseReducedValues));
+                }
+            }
+
+            //ImageTools.DrawMatrix(psd.ToArray().ToMatrix(), resultPath);
+            ImageTools.DrawReversedMatrix(psd.ToArray().ToMatrix(), resultPath);
+        }
     }
 }
