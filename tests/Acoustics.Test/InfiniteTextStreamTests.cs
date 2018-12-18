27c7947814cfbbe48ebe1c1ae2a406d70ba80dbe
Anthony Truskinger <anthony.truskinger@gmail.com>
2018-12-18 12:08:34 +1000

    Increases timeout for infinite stream test
    This compute constrained test is failing on the CI server because of underpowered VMs. Upping the timout should allow the test to pass more often.

--- tests/Acoustics.Test/InfiniteTextStreamTests.cs
+++ tests/Acoustics.Test/InfiniteTextStreamTests.cs
@@ -17,7 +17,7 @@ namespace Acoustics.Test
     [DoNotParallelize]
     public class InfiniteTextStreamTests
     {
-        private readonly TimeSpan timeout = TimeSpan.FromMilliseconds(600);
+        private readonly TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
 
         [TestMethod]
         [Timeout(5_000)]
@@ -26,7 +26,7 @@ namespace Acoustics.Test
         {
             Debug.WriteLine("Generating output");
 
-            StringBuilder builder = new StringBuilder(1_000_000);
+            var builder = new StringBuilder(1_000_000);
             var source = new CancellationTokenSource();
             var token = source.Token;
 
@@ -52,7 +52,7 @@ namespace Acoustics.Test
                 }
             }
 
-            var work = Task.Run((Action)Generate, token);
+            var work = Task.Run(Generate, token);
 
             Assert.IsFalse(work.IsCompleted);
             source.CancelAfter(this.timeout);
@@ -60,9 +60,9 @@ namespace Acoustics.Test
             Assert.IsTrue(work.IsCompleted);
 
             string s = builder.ToString();
-            Debug.WriteLine($"Generation complete (length: {s.Length}:");
+            Debug.WriteLine($"Generation complete (length: {s.Length}):");
 
-            Assert.IsTrue(s.Length > 1_000);
+            Assert.IsTrue(s.Length > 1_000, $"Length {s.Length} was not greater than expected length of 1000");
 
             //Debug.WriteLine(s);
         }
