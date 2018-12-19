QutEcoacoustics/audio-analysis

commit ae5f67328984003a18e581ed4dc3f7005083bef2

Make all greyscale spectrograms available
Previously only drew 6 greyscale spectrograms - i.e. those used to make two maps image. Now draw all available greyscale spectrograms. This required considerable changes to code which had duplicated lists of indices etc. now making considerable use of reflection

src/AudioAnalysisTools/Indices/SpectralIndexValues.cs
@@ -8,7 +8,6 @@ namespace AudioAnalysisTools.Indices
     using System.Collections.Generic;
     using System.Drawing;
     using System.Linq;
-
     using Acoustics.Shared;
     using AnalysisBase.ResultBases;
     using Fasterflect;
@@ -117,79 +116,20 @@ namespace AudioAnalysisTools.Indices
             }
         }
 
-        public static Dictionary<string, Func<SpectralIndexBase, double[]>> CachedSelectors
-        {
-            get
-            {
-                return CachedSelectorsInternal;
-            }
-        }
+        public static Dictionary<string, Func<SpectralIndexBase, double[]>> CachedSelectors => CachedSelectorsInternal;
 
-        public static Dictionary<string, Action<SpectralIndexValues, double[]>> CachedSetters
-        {
-            get
-            {
-                return CachedSettersInternal;
-            }
-        }
+        public static Dictionary<string, Action<SpectralIndexValues, double[]>> CachedSetters => CachedSettersInternal;
 
-        public static string[] GetKeys()
-        {
-            return CachedSelectorsInternal.Keys.ToArray();
-        }
+        public static string[] Keys = CachedSelectors.Keys.ToArray();
 
         public static Image CreateImageOfSpectralIndices(SpectralIndexValues spectralIndices)
         {
-            string[] keys = { "ACI", "BGN", "CVR", "ENT", "EVN", "PMN", "POW", "RHZ", "RNG", "RPS", "RVT", "R3D", "SPT" };
-            var images = new List<Image>();
-            foreach (var key in keys)
-            {
-                double[] normalisedIndex = null;
 
-                switch (key)
-                {
-                    case "ACI":
-                        normalisedIndex = DataTools.normalise(spectralIndices.ACI);
-                        break;
-                    case "BGN":
-                        normalisedIndex = DataTools.normalise(spectralIndices.BGN);
-                        break;
-                    case "CVR":
-                        normalisedIndex = DataTools.normalise(spectralIndices.CVR);
-                        break;
-                    case "ENT":
-                        normalisedIndex = DataTools.normalise(spectralIndices.ENT);
-                        break;
-                    case "EVN":
-                        normalisedIndex = DataTools.normalise(spectralIndices.EVN);
-                        break;
-                    case "PMN":
-                        normalisedIndex = DataTools.normalise(spectralIndices.PMN);
-                        break;
-                    case "POW":
-                        normalisedIndex = DataTools.normalise(spectralIndices.POW);
-                        break;
-                    case "RHZ":
-                        normalisedIndex = DataTools.normalise(spectralIndices.RHZ);
-                        break;
-                    case "RNG":
-                        normalisedIndex = DataTools.normalise(spectralIndices.RNG);
-                        break;
-                    case "RPS":
-                        normalisedIndex = DataTools.normalise(spectralIndices.RPS);
-                        break;
-                    case "RVT":
-                        normalisedIndex = DataTools.normalise(spectralIndices.RVT);
-                        break;
-                    case "R3D":
-                        normalisedIndex = DataTools.normalise(spectralIndices.R3D);
-                        break;
-                    case "SPT":
-                        normalisedIndex = DataTools.normalise(spectralIndices.SPT);
-                        break;
-                    default:
-                        break;
-                }
+            var images = new List<Image>();
+            foreach (var key in Keys)
+            {
+                var spectrum = CachedSelectors[key](spectralIndices);
+                double[] normalisedIndex = DataTools.normalise(spectrum);
 
                 var image = GraphsAndCharts.DrawGraph(key, normalisedIndex, 100);
                 images.Add(image);
@@ -214,24 +154,26 @@ namespace AudioAnalysisTools.Indices
 
         public double[] CVR { get; set; }
 
+        // This property only calculated for ACI when zooming
         public double[] DIF { get; set; }
 
+        // Entropy
         public double[] ENT { get; set; }
 
         public double[] EVN { get; set; }
 
         /// <summary>
-        /// Gets or sets pMN = Power Minus Noise.
+        /// Gets or sets the OSC (oscillation) spectral index.
+        /// This index added in November 2018.
+        /// </summary>
+        public double[] OSC { get; set; }
+
+        /// <summary>
+        /// Gets or sets PMN = Power Minus Noise.
         /// PMN is measured in decibels but should replace POW as the average decibel spectrogram.
         /// PMN calculates the average decibel spectrogram correctly.
         /// </summary>
         public double[] PMN { get; set; }
-
-        /// <summary>
-        /// Gets or sets the POW spectral index should eventually be depracated.
-        /// It is derived from an incorrect way of averaging decibel values
-        /// </summary>
-        public double[] POW { get; set; }
 
         // Spectral Ridges Horizontal
         public double[] RHZ { get; set; }
@@ -251,6 +193,7 @@ namespace AudioAnalysisTools.Indices
         // Spectral Peak Tracks
         public double[] SPT { get; set; }
 
+        // This property only calculated for ACI when zooming
         public double[] SUM { get; set; }
 
         public override Dictionary<string, Func<SpectralIndexBase, double[]>> GetSelectors()

src/AudioAnalysisTools/LongDurationSpectrograms/LDSpectrogramRGB.cs
@@ -7,11 +7,10 @@
 //   Important properties are:
 //   1) the colour map which maps three acoutic indices to RGB.
 //   2) The scale of the x and y axes which are determined by the sample rate, frame size etc.
-//   In order to create false colour spectrograms, copy the method
-//   public static void DrawFalseColourSpectrograms(LDSpectrogramConfig configuration)
+//   In order to create false colour spectrograms, copy the following method:
+//                                           public static void DrawFalseColourSpectrograms(LDSpectrogramConfig configuration)
 //   All the arguments can be passed through a config file.
-//   Create the config file through an instance of the class LDSpectrogramConfig
-//   and then call config.WritConfigToYAML(FileInfo path).
+//   Create the config file through an instance of the class LDSpectrogramConfig and then call config.WritConfigToYAML(FileInfo path).
 //   Then pass that path to the above static method.
 //
 //  Activity Codes for other tasks to do with spectrograms and audio files:
@@ -64,9 +63,10 @@ namespace AudioAnalysisTools.LongDurationSpectrograms
     /// </summary>
     public class LDSpectrogramRGB
     {
+        // Below is some history about how indices were assigned to the RGB channels to make long-duration false-colour spectrograms
         // string[] keys = { "ACI", "TEN", "CVR", "BGN", "AVG", "VAR" }; // the OLDEST default i.e. used in 2014
         // string[] keys = { "ACI", "ENT", "EVN", "BGN", "POW", "EVN" }; // the OLD default i.e. since July 2015
-        // assign indices to RGB - the next two lines are for experimental purposes
+        // More recently (2018 onwards) other combinations have been used expecially for the blue channel index.
         // public static readonly string DefaultColorMap1 = "ACI, ENT, EVN";
         // public static readonly string DefaultColorMap2 = "BGN, PMN, R3D";
 
@@ -74,15 +74,14 @@ namespace AudioAnalysisTools.LongDurationSpectrograms
         public static readonly string DefaultColorMap1 = SpectrogramConstants.RGBMap_ACI_ENT_EVN;
         public static readonly string DefaultColorMap2 = SpectrogramConstants.RGBMap_BGN_PMN_R3D;
 
-        // Before May 2017, only the required six spectral indices were incorporated in a dicitonary of spectral matrices.
-        // Since May 2017, all the likely available matrices are incorporated into a dictionary. Note the new names for PMN and R3D, previously POW and HPN respectively.
-        // Note 1: This default array will be subsequently over-written by the indices in the IndexPropertiesConfig file if one is available.
-        // Note 1: RHZ, SPT and CVR are correlated with POW and do not add much. CLS is not particularly useful. Currently using R3D
-        private static readonly string[] DefaultKeys = { "ACI", "BGN", "CLS", "CVR", "ENT", "EVN", "PMN", "POW", "RHZ", "RVT", "RPS", "RNG", "R3D", "SPT" };
-
         public static string[] GetArrayOfAvailableKeys()
         {
-            return DefaultKeys;
+            // Before May 2017, only the required six spectral indices were incorporated in a dictionary of spectral matrices.
+            // Since May 2017, all the likely available matrices are incorporated into a dictionary. Note the new names for PMN and R3D, previously POW and HPN respectively.
+            // Note 1: This default array will be subsequently over-written by the indices in the IndexPropertiesConfig file if one is available.
+            // Note 1: RHZ, SPT and CVR are correlated with POW and do not add much. CLS is not particularly useful. Currently using R3D
+            // Decmeber 2018: Now all spectral indices are always used in drawing images
+            return SpectralIndexValues.Keys;
         }
 
         // used to save all spectrograms as dictionary of matrices
@@ -113,7 +112,7 @@ namespace AudioAnalysisTools.LongDurationSpectrograms
             this.FrameWidth = SpectrogramConstants.FRAME_LENGTH; // default value - from which spectrogram was derived
             this.XTicInterval = SpectrogramConstants.X_AXIS_TIC_INTERVAL; // default = one minute spectra and hourly time lines
             this.StartOffset = SpectrogramConstants.MINUTE_OFFSET;
-            this.SpectrogramKeys = DefaultKeys;
+            this.SpectrogramKeys = GetArrayOfAvailableKeys();
         }
 
         /// <summary>
@@ -139,7 +138,7 @@ namespace AudioAnalysisTools.LongDurationSpectrograms
             // IMPORTANT NOTE: If an IndexPropertiesConfig file is available, these default keys are later over-written in the method
             // SetSpectralIndexProperties(Dictionary < string, IndexProperties > dictionaryOfSpectralIndexProperties)
             // Consequently the INDEX names in DefaultKeys should match those in IndexPropertiesConfig file. If they do not, consequences are undefined!
-            this.SpectrogramKeys = DefaultKeys;
+            this.SpectrogramKeys = GetArrayOfAvailableKeys();
         }
 
         /// <summary>
@@ -505,7 +504,8 @@ namespace AudioAnalysisTools.LongDurationSpectrograms
         /// </summary>
         public void DrawGreyScaleSpectrograms(DirectoryInfo opdir, string opFileName)
         {
-            string[] keys = this.SpectrogramKeys;
+            var keys = SpectralIndexValues.Keys;
+            //string[] keys = this.SpectrogramKeys;
             this.DrawGreyScaleSpectrograms(opdir, opFileName, keys);
         }
 

commit 7117f58d84fbca4f84bf07ecf609a1644e5ac9b4

Partial unit test fixes for drawing all grey FCS
The changes in ae5f67328984003a18e581ed4dc3f7005083bef2 broke many tests.

This commit fixes some but not all tests. More work needed.

Work for #199

AudioAnalysis.sln.DotSettings
@@ -282,6 +282,7 @@
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=appenders/@EntryIndexedValue">True</s:Boolean>
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=Ecoacoustics/@EntryIndexedValue">True</s:Boolean>
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=filenames/@EntryIndexedValue">True</s:Boolean>
+	<s:Boolean x:Key="/Default/UserDictionary/Words/=normalised/@EntryIndexedValue">True</s:Boolean>
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=parallelized/@EntryIndexedValue">True</s:Boolean>
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=Resample/@EntryIndexedValue">True</s:Boolean>
 	<s:Boolean x:Key="/Default/UserDictionary/Words/=Resampling/@EntryIndexedValue">True</s:Boolean>

src/AudioAnalysisTools/Indices/SpectralIndexValues.cs
@@ -15,33 +15,32 @@ namespace AudioAnalysisTools.Indices
 
     public class SpectralIndexValues : SpectralIndexBase
     {
-        private static readonly Dictionary<string, Func<SpectralIndexBase, double[]>> CachedSelectorsInternal;
-        private static readonly Dictionary<string, Action<SpectralIndexValues, double[]>> CachedSettersInternal;
-
         static SpectralIndexValues()
         {
             var getters = ReflectionExtensions.GetGetters<SpectralIndexValues, double[]>();
 
-            CachedSelectorsInternal = new Dictionary<string, Func<SpectralIndexBase, double[]>>(getters.Count);
+            CachedSelectors = new Dictionary<string, Func<SpectralIndexBase, double[]>>(getters.Count);
             foreach (var keyValuePair in getters)
             {
                 // var key = keyValuePair.Key;
                 var selector = keyValuePair.Value;
 
-                CachedSelectorsInternal.Add(
+                CachedSelectors.Add(
                     keyValuePair.Key,
                     spectrumBase => selector((SpectralIndexValues)spectrumBase));
             }
 
+            Keys = CachedSelectors.Keys.ToArray();
+
             var setters = ReflectionExtensions.GetSetters<SpectralIndexValues, double[]>();
 
-            CachedSettersInternal = new Dictionary<string, Action<SpectralIndexValues, double[]>>(getters.Count);
+            CachedSetters = new Dictionary<string, Action<SpectralIndexValues, double[]>>(getters.Count);
             foreach (var keyValuePair in setters)
             {
                 // var key = keyValuePair.Key;
                 var setter = keyValuePair.Value;
 
-                CachedSettersInternal.Add(
+                CachedSetters.Add(
                     keyValuePair.Key,
                     (spectrumBase, value) => setter(spectrumBase, value));
             }
@@ -72,7 +71,9 @@ namespace AudioAnalysisTools.Indices
                 // WARNING: Potential throw site
                 // No need to give following warning because should call CheckExistenceOfSpectralIndexValues() method before entering loop.
                 // This prevents multiple warnings through loop.
-                this.SetPropertyValue(cachedSetter.Key, initArray);
+                //this.SetPropertyValue(cachedSetter.Key, initArray);
+
+                cachedSetter.Value(this, initArray);
             }
 
             this.Configuration = configuration;
@@ -88,7 +89,7 @@ namespace AudioAnalysisTools.Indices
         /// </param>
         public static SpectralIndexValues[] ImportFromDictionary(Dictionary<string, double[,]> dictionaryOfSpectra)
         {
-            return dictionaryOfSpectra.FromTwoDimensionalArray<SpectralIndexValues, double>(CachedSetters, TwoDimensionalArray.Rotate90AntiClockWise);
+            return dictionaryOfSpectra.FromTwoDimensionalArray(CachedSetters, TwoDimensionalArray.Rotate90AntiClockWise);
         }
 
         /// <summary>
@@ -97,9 +98,6 @@ namespace AudioAnalysisTools.Indices
         /// </summary>
         public static void CheckExistenceOfSpectralIndexValues(Dictionary<string, IndexProperties> indexProperties)
         {
-            var siv = new SpectralIndexValues();
-            double[] dummyArray = null;
-
             foreach (var kvp in indexProperties)
             {
                 if (!kvp.Value.IsSpectralIndex)
@@ -107,7 +105,7 @@ namespace AudioAnalysisTools.Indices
                     continue;
                 }
 
-                var success = siv.TrySetPropertyValue(kvp.Key, dummyArray);
+                var success = CachedSelectors.ContainsKey(kvp.Key);
                 if (!success)
                 {
                     LoggedConsole.WriteWarnLine(
@@ -116,15 +114,14 @@ namespace AudioAnalysisTools.Indices
             }
         }
 
-        public static Dictionary<string, Func<SpectralIndexBase, double[]>> CachedSelectors => CachedSelectorsInternal;
+        public static Dictionary<string, Func<SpectralIndexBase, double[]>> CachedSelectors { get; }
 
-        public static Dictionary<string, Action<SpectralIndexValues, double[]>> CachedSetters => CachedSettersInternal;
+        public static Dictionary<string, Action<SpectralIndexValues, double[]>> CachedSetters { get; }
 
-        public static string[] Keys = CachedSelectors.Keys.ToArray();
+        public static string[] Keys { get; }
 
         public static Image CreateImageOfSpectralIndices(SpectralIndexValues spectralIndices)
         {
-
             var images = new List<Image>();
             foreach (var key in Keys)
             {
@@ -144,7 +141,7 @@ namespace AudioAnalysisTools.Indices
         /// </summary>
         /// <remarks>
         /// This property was added when we started generating lots of results that used
-        /// different parameters - we needed a way to diambiguate them.
+        /// different parameters - we needed a way to disambiguate them.
         /// </remarks>
         public IndexCalculateConfig Configuration { get; }
 

tests/Acoustics.Test/AnalysisPrograms/Concatenation/ConcatenationTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ConcatenationTests.cs" company="QutEcoacoustics">
+// <copyright file="ConcatenationTests.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
 
@@ -78,7 +78,7 @@ namespace Acoustics.Test.AnalysisPrograms.Concatenation
                 IndexPropertiesConfig = indexPropertiesConfig.FullName,
                 FalseColourSpectrogramConfig = testConfig.FullName,
                 ColorMap1 = LDSpectrogramRGB.DefaultColorMap1,
-                ColorMap2 = "BGN-POW-EVN", // POW was depracated post May 2017
+                ColorMap2 = "BGN-PMN-EVN", // POW was depracated post May 2017
                 ConcatenateEverythingYouCanLayYourHandsOn = true, // join everything found
                 TimeSpanOffsetHint = TimeSpan.FromHours(8),
                 DrawImages = true,
@@ -133,7 +133,7 @@ namespace Acoustics.Test.AnalysisPrograms.Concatenation
                 IndexPropertiesConfig = indexPropertiesConfig.FullName,
                 FalseColourSpectrogramConfig = testConfig.FullName,
                 ColorMap1 = LDSpectrogramRGB.DefaultColorMap1,
-                ColorMap2 = "BGN-POW-EVN", // POW was depracated post May 2017
+                ColorMap2 = "BGN-PMN-EVN", // POW was depracated post May 2017
                 ConcatenateEverythingYouCanLayYourHandsOn = false, // 24 hour blocks only
                 TimeSpanOffsetHint = TimeSpan.FromHours(8),
                 DrawImages = true,
@@ -190,7 +190,7 @@ namespace Acoustics.Test.AnalysisPrograms.Concatenation
                 IndexPropertiesConfig = indexPropertiesConfig.FullName,
                 FalseColourSpectrogramConfig = testConfig.FullName,
                 ColorMap1 = LDSpectrogramRGB.DefaultColorMap1,
-                ColorMap2 = "BGN-POW-EVN", // POW was depracated post May 2017
+                ColorMap2 = "BGN-PMN-EVN", // POW was depracated post May 2017
                 ConcatenateEverythingYouCanLayYourHandsOn = false, // 24 hour blocks only
                 TimeSpanOffsetHint = TimeSpan.FromHours(8),
                 DrawImages = true,
@@ -250,7 +250,7 @@ namespace Acoustics.Test.AnalysisPrograms.Concatenation
             var config = Yaml.Deserialize<LdSpectrogramConfig>(defaultConfigFile);
 
             // make changes to config file as required for test
-            config.ColorMap1 = "BGN-ENT-POW";
+            config.ColorMap1 = "BGN-ENT-PMN";
             config.ColorMap2 = "ACI-RNG-EVN";
 
             // write new config
@@ -285,7 +285,7 @@ namespace Acoustics.Test.AnalysisPrograms.Concatenation
             Assert.That.FileExists(outputDataDir.CombineFile(prefix + "Towsey.Acoustic.Indices.csv"));
             Assert.That.FileNotExists(outputDataDir.CombineFile(prefix + "SummaryIndex.csv"));
 
-            var imageFileInfo1 = outputDataDir.CombineFile(prefix + "BGN-ENT-POW.png");
+            var imageFileInfo1 = outputDataDir.CombineFile(prefix + "BGN-ENT-PMN.png");
             Assert.IsTrue(imageFileInfo1.Exists);
 
             var imageFileInfo2 = outputDataDir.CombineFile(prefix + "ACI-RNG-EVN.png");