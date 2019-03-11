QutEcoacoustics/audio-analysis

commit 209745daa7702c8bbbccaf500330439b7bbddce5

Fixes bug with event index calculation
There was a logic error in ConvertEventsToSummaryIndices. Because we changed the meaning of EventStartSeconds that method no longer works for both segment and recording level results. Since we want to pase out segment results, I removed the three usages of this method inside Analysis methods.

Also fixes bugs with converting events to event indices.

Also fixes some log file copying bugs.

Closes #220

src/Acoustics.Shared/Logging/Logging.cs
@@ -92,6 +92,7 @@ namespace Acoustics.Shared.Logging
             string logFilePath = null;
             if (enableFileLogger)
             {
+                this.LogFileName = new PatternString("log_ %utcdate{yyyyMMddTHHmmssZ}.txt").Format();
                 var fileAppender = new RollingFileAppender()
                 {
                     AppendToFile = false,
@@ -105,7 +106,7 @@ namespace Acoustics.Shared.Logging
                     Name = nameof(RollingFileAppender),
 
                     // ReSharper disable StringLiteralTypo
-                    File = new PatternString("Logs/log_%utcdate{yyyyMMddTHHmmssZ}.txt").Format(),
+                    File = "Logs/" + this.LogFileName,
 
                     // ReSharper restore StringLiteralTypo
 
@@ -191,6 +192,8 @@ namespace Acoustics.Shared.Logging
                 _ = this.CleanLogs(logFilePath);
             }
         }
+
+        public string LogFileName { get; private set; }
 
         internal MemoryAppender MemoryAppender { get; }
 

src/Acoustics.Shared/Logging/LoggedConsole.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="LoggedConsole.cs" company="QutEcoacoustics">
+// <copyright file="LoggedConsole.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
 
@@ -6,13 +6,12 @@
 namespace System
 {
     using System.IO;
-
+    using System.Text;
+    using System.Threading.Tasks;
     using Acoustics.Shared;
     using Acoustics.Shared.Logging;
     using DotSpinners;
     using log4net;
-    using Text;
-    using Threading.Tasks;
 
     /// <summary>
     /// This class is designed to be an abstraction to the system console.

src/AnalysisBase/IAnalyser2.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IAnalyser2.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
@@ -95,8 +95,14 @@ namespace AnalysisBase
         List<FileInfo> WriteSpectrumIndicesFiles(DirectoryInfo destination, string fileNameBase, IEnumerable<SpectralIndexBase> results);
 
         /// <summary>
-        /// Allows Events to be rendered as Summary Indices
+        /// Allows Events to be rendered as Summary Indices.
         /// </summary>
+        /// <remarks>
+        /// This method used to be used on every segment but we are phasing out
+        /// segment-level results. Thus it is recommended it only be used at the end of the analysis
+        /// when the full result set (and when <paramref name="duration"/> equals the full recording length)
+        /// is available.
+        /// </remarks>
         /// <param name="events">The events to process.</param>
         /// <param name="unitTime">The unit time of the summary indices to produce.</param>
         /// <param name="duration">The duration of audio for the period analyzed that produced <c>events</c>.</param>

src/AnalysisPrograms/AED.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AED.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
@@ -292,11 +292,7 @@ namespace AnalysisPrograms
 
             if (analysisSettings.AnalysisDataSaveBehavior)
             {
-                var unitTime = TimeSpan.FromMinutes(1.0);
-                analysisResults.SummaryIndices = this.ConvertEventsToSummaryIndices(analysisResults.Events, unitTime, analysisResults.SegmentAudioDuration, 0);
-
-                this.WriteSummaryIndicesFile(segmentSettings.SegmentSummaryIndicesFile, analysisResults.SummaryIndices);
-                analysisResults.SummaryIndicesFile = segmentSettings.SegmentSummaryIndicesFile;
+                // noop
             }
 
             // save image of sonograms
@@ -361,7 +357,7 @@ namespace AnalysisPrograms
 
         public override void WriteSummaryIndicesFile(FileInfo destination, IEnumerable<SummaryIndexBase> results)
         {
-            Csv.WriteToCsv(destination, results);
+            Csv.WriteToCsv(destination, results.Cast<EventIndex>());
         }
     }
 }

src/AnalysisPrograms/AnalyseLongRecordings/AnalyseLongRecording.cs
@@ -392,21 +392,21 @@ namespace AnalysisPrograms.AnalyseLongRecordings
         }
 
         /// <summary>
-        /// Generic organization of resources after a run
+        /// Generic organization of resources after a run.
         /// </summary>
         private static void Cleanup(Arguments args, FileInfo configFile)
         {
-            if (args.WhenExitCopyConfig)
+            if (args.WhenExitCopyConfig && args.Config.NotNull())
             {
                 configFile.CopyTo(Path.Combine(args.Output.FullName, Path.GetFileName(args.Config)), true);
             }
 
-            if (args.WhenExitCopyLog)
+            if (args.WhenExitCopyLog && MainEntry.Logging.LogFileName.NotNull())
             {
                 var logDirectory = LoggedConsole.LogFolder;
-                var logFile = Path.Combine(logDirectory, "log.txt");
+                var logFile = Path.Combine(logDirectory, MainEntry.Logging.LogFileName);
 
-                File.Copy(logFile, Path.Combine(args.Output.FullName, "log.txt"), true);
+                File.Copy(logFile, Path.Combine(args.Output.FullName, MainEntry.Logging.LogFileName), true);
             }
         }
     }

src/AnalysisPrograms/GroundParrotRecogniser.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="GroundParrotRecogniser.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
@@ -331,10 +331,7 @@ namespace AnalysisPrograms
 
             if (analysisSettings.AnalysisDataSaveBehavior)
             {
-                var unitTime = TimeSpan.FromMinutes(1.0);
-                analysisResults.SummaryIndices = this.ConvertEventsToSummaryIndices(analysisResults.Events, unitTime, analysisResults.SegmentAudioDuration, 0);
-
-                this.WriteSummaryIndicesFile(segmentSettings.SegmentSummaryIndicesFile, analysisResults.SummaryIndices);
+                // noop
             }
 
             // save image of sonograms
@@ -355,7 +352,7 @@ namespace AnalysisPrograms
 
         public override void WriteSummaryIndicesFile(FileInfo destination, IEnumerable<SummaryIndexBase> results)
         {
-            Csv.WriteToCsv(destination, results);
+            Csv.WriteToCsv(destination, results.Cast<EventIndex>());
         }
 
         public override List<FileInfo> WriteSpectrumIndicesFiles(DirectoryInfo destination, string fileNameBase, IEnumerable<SpectralIndexBase> results)

src/AnalysisPrograms/KoalaMale.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="KoalaMale.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
@@ -331,14 +331,7 @@ namespace AnalysisPrograms
 
             if (analysisSettings.AnalysisDataSaveBehavior)
             {
-                TimeSpan unitTime = TimeSpan.FromMinutes(1.0);
-                analysisResults.SummaryIndices = this.ConvertEventsToSummaryIndices(
-                    analysisResults.Events,
-                    unitTime,
-                    analysisResults.SegmentAudioDuration,
-                    0);
-
-                this.WriteSummaryIndicesFile(segmentSettings.SegmentSummaryIndicesFile, analysisResults.SummaryIndices);
+                // noop
             }
 
             if (analysisSettings.AnalysisImageSaveBehavior.ShouldSave(analysisResults.Events.Length))
@@ -376,7 +369,7 @@ namespace AnalysisPrograms
 
         public override void WriteSummaryIndicesFile(FileInfo destination, IEnumerable<SummaryIndexBase> results)
         {
-            Csv.WriteToCsv(destination, results);
+            Csv.WriteToCsv(destination, results.Cast<EventIndex>());
         }
 
         private static Image DrawSonogram(

commit ee00f00a2e3a99721f0027ce0fbe7d4317283379

Fixes log naming bug
Bug was introduced in 209745daa7702c8bbbccaf500330439b7bbddce5 for #220. I accidently added a space in the log file names :dizzy_face:

I've also added more tests so that our log cleaning function gets tested now.

src/Acoustics.Shared/Logging/Logging.cs
@@ -15,7 +15,6 @@ namespace Acoustics.Shared.Logging
     using log4net.Appender;
     using log4net.Core;
     using log4net.Layout;
-    using log4net.Layout.Pattern;
     using log4net.Repository.Hierarchy;
     using log4net.Util;
     using static log4net.Appender.ManagedColoredConsoleAppender;
@@ -24,10 +23,13 @@ namespace Acoustics.Shared.Logging
     {
         public const string CleanLogger = "CleanLogger";
         public const string LogFileOnly = "LogFileOnly";
+        private const string LogPrefix = "log_";
 
         private readonly Logger rootLogger;
+
         // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
         private readonly Logger cleanLogger;
+
         // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
         private readonly Logger noConsoleLogger;
         private readonly AppenderSkeleton standardConsoleAppender;
@@ -92,7 +94,7 @@ namespace Acoustics.Shared.Logging
             string logFilePath = null;
             if (enableFileLogger)
             {
-                this.LogFileName = new PatternString("log_ %utcdate{yyyyMMddTHHmmssZ}.txt").Format();
+                this.LogFileName = new PatternString(LogPrefix + "%utcdate{yyyyMMddTHHmmssZ}.txt").Format();
                 var fileAppender = new RollingFileAppender()
                 {
                     AppendToFile = false,
@@ -256,13 +258,13 @@ namespace Acoustics.Shared.Logging
         {
             Contract.RequiresNotNull(logFilePath);
             const int threshold = 60;
-            const int target = 50;
+            int target = 50;
 
             void CleanFiles()
             {
                 var logsPath = Path.GetDirectoryName(logFilePath) ??
                                throw new InvalidOperationException("Could not resolve logs directory path: " + logFilePath);
-                var files = Directory.GetFiles(logsPath, "log_*.txt");
+                var files = Directory.GetFiles(logsPath, LogPrefix + "*.txt");
                 if (files.Length > threshold)
                 {
                     var sorted = new SortedDictionary<DateTime, List<string>>();
@@ -271,12 +273,21 @@ namespace Acoustics.Shared.Logging
                         var name = Path.GetFileName(file);
 
                         // assuming a format of log_20180717T130822Z.1.txt
-                        var datePart = name.Substring(4, name.IndexOf(".", StringComparison.Ordinal) - 4);
-                        var date = DateTime.ParseExact(
+                        int prefixLength = LogPrefix.Length;
+                        var datePart = name.Substring(prefixLength, name.IndexOf(".", StringComparison.Ordinal) - prefixLength);
+                        var success = DateTime.TryParseExact(
                             datePart,
                             "yyyyMMddTHHmmssZ",
                             CultureInfo.InvariantCulture,
-                            DateTimeStyles.AssumeUniversal);
+                            DateTimeStyles.AssumeUniversal,
+                            out var date);
+
+                        if (!success)
+                        {
+                            // the default date value will ensure value is added into sorted list
+                            // and it will be deleted first!
+                            LoggedConsole.WriteWarnLine($"Log file with name `{name}` has invalid date format and is being cleaned");
+                        }
 
                         if (sorted.ContainsKey(date))
                         {

tests/Acoustics.Test/Shared/LoggingTests/LoggingTests.cs
@@ -6,7 +6,10 @@ namespace Acoustics.Test.Shared.LoggingTests
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
+    using System.Threading.Tasks;
+    using Acoustics.Shared;
     using Acoustics.Shared.ConfigFile;
     using Acoustics.Shared.Logging;
     using Fasterflect;
@@ -57,6 +60,45 @@ namespace Acoustics.Test.Shared.LoggingTests
 
                 Assert.AreEqual(expectedMessageCount, TestSetup.TestLogging.MemoryAppender.GetEvents().Length);
             }
+        }
+
+        [TestMethod]
+        public void TestLogFileIsCreated()
+        {
+            var logging = new Logging(false, Level.Info, quietConsole: false);
+
+            var expectedPath = Path.Combine(LoggedConsole.LogFolder, logging.LogFileName);
+            Assert.That.PathExists(expectedPath);
+
+            StringAssert.StartsWith(logging.LogFileName, "log_20");
+        }
+
+        [TestMethod]
+        public async Task TestLogFilesAreCleaned()
+        {
+            var logDirectory = LoggedConsole.LogFolder;
+
+            // get count
+            var files = Directory.GetFiles(logDirectory);
+            var oldCount = files.Length;
+
+            // it should allow up to 60 log files, before it cleans 10
+            var delta = 60 - oldCount + 1;
+
+            // "do" because we need to create a new log at least once for this test to trigger
+            // its cleaning logic
+            do
+            {
+                var log = new Logging(false, Level.Info, quietConsole: false);
+                delta--;
+            } while (delta > 0);
+
+            // wait for delete async task to fire
+            await Task.Delay(100);
+
+            // get count
+            files = Directory.GetFiles(logDirectory);
+            Assert.AreEqual(50, files.Length);
         }
     }
 }