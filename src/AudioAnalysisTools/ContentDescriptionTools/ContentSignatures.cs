e1465f57c15410ea6cbcdc1160bdf7fbf10f4677
Michael Towsey <m.towsey@qut.edu.au>
2019-11-12 15:56:46 +1000

    Delete unnecesary files
    Issue #252 Delete five classes that were originally used to define different templates but hwich are now described in the yml and jason templates files. Other changes are small changes required to run software on different computer.

--- src/AudioAnalysisTools/ContentDescriptionTools/ContentSignatures.cs
+++ src/AudioAnalysisTools/ContentDescriptionTools/ContentSignatures.cs
@@ -1,4 +1,4 @@
-// <copyright file="ContentDescription.cs" company="QutEcoacoustics">
+// <copyright file="ContentSignatures.cs" company="QutEcoacoustics">
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
 
