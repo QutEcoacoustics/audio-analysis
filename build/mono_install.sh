a64e5bce9a26fb58afcddf4645384e6cab312951
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-03-03 18:57:28 +1000

    First (failing attempt) to make Azure Pipelines work
    This is a combination of 14 commits.
    
    Update azure-pipelines.yml for Azure Pipelines
    
    CI attempt
    
    fix indentation
    
    change variable expression syntax
    
    attempt
    
    attempt
    
    fix displayName
    
    Update title
    
    try setting vmImage
    
    try runtime expression
    
    Fix cache, try again with displayname
    
    lock down .net sdk, add nuget config to restore
    
    fixed sdk version

--- build/mono_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# http://www.mono-project.com/download/#download-lin-debian
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian stretch main" | sudo tee /etc/apt/sources.list.d/mono-official.list
-sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y mono-complete --allow-unauthenticated
