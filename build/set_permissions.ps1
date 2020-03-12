b27e4ec93566456dbf0f2343dd5e70661ee3651b
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-03-12 22:35:48 +1000

    Fix permissions of included binaries

---
+++ build/set_permissions.ps1
@@ -0,0 +1,23 @@
+
+
+$tools = @(
+    "ffmpeg",
+    "ffprobe",
+    "sox",
+    "soxi",
+    "wvunpack"
+)
+
+foreach ($tool in $tools) {
+    $files = Get-ChildItem "$PSScriptRoot/../lib/audio-utils" -Include "$tool*" -Recurse -File
+        | Where-Object { $_.Name -in $tools -or $_.Extension -eq ".exe" }
+
+    foreach ($file in $files) {
+        if (!$IsWindows) {
+            chmod a+x $file
+        }
+
+        git update-index --chmod=+x $file
+
+    }
+}
