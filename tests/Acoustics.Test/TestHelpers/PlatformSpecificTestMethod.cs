7a913e479b6b08498d1fef2716604e9997375383
Anthony Trusknger <anthony.truskinger@gmail.com>
2020-03-18 16:28:11 +1000

    [WIP] Add negatated platform specific test option
    If the platform string is prefixed with an exclamation mark, then the test is NOT run on the platform specified and run on all other platforms.

--- tests/Acoustics.Test/TestHelpers/PlatformSpecificTestMethod.cs
+++ tests/Acoustics.Test/TestHelpers/PlatformSpecificTestMethod.cs
@@ -17,7 +17,12 @@ namespace Acoustics.Test.TestHelpers
         /// <summary>
         /// Initializes a new instance of the <see cref="PlatformSpecificTestMethod"/> class.
         /// </summary>
-        /// <param name="platform">The platform to run the test on.</param>
+        /// <param name="platform">
+        /// The platform to run the test on.
+        /// If the value is prefixed with an `!` (exclaimation mark)
+        /// then the provided platform is NOT tested against and all others are
+        /// tested.
+        /// </param>
         public PlatformSpecificTestMethod(string platform)
             : this(platform, null)
         {
@@ -26,12 +31,20 @@ namespace Acoustics.Test.TestHelpers
         /// <summary>
         /// Initializes a new instance of the <see cref="PlatformSpecificTestMethod"/> class.
         /// </summary>
-        /// <param name="platform">The platform to run the test on.</param>
+        /// <param name="platform">
+        /// The platform to run the test on.
+        /// If the value is prefixed with an `!` (exclaimation mark)
+        /// then the provided platform is NOT tested against and all others are
+        /// tested.
+        /// </param>
         /// <param name="displayName">The friendly name for this test.</param>
         public PlatformSpecificTestMethod(string platform, string displayName)
             : base(displayName)
         {
-            this.Platform = platform.ToUpper() switch
+            this.IgnorePlatform = platform.StartsWith("!");
+            this.Platform = platform
+                .Substring(this.IgnorePlatform ? 1 : 0)
+                .ToUpper() switch
             {
                 "WINDOWS" => OSPlatform.Windows,
                 "OSX" => OSPlatform.OSX,
@@ -47,6 +60,15 @@ namespace Acoustics.Test.TestHelpers
         public OSPlatform Platform { get; }
 
         /// <summary>
+        /// Gets a value indicating whether this test method ignores the provided
+        /// <see cref="Platform"/> value or whether it runs the test only on that
+        /// platform.
+        /// </summary>
+        /// <value>True if the <see cref="Platform"/> should be ignored, false if
+        /// <see cref="Platform"/> should be the only platform tested.</value>
+        public bool IgnorePlatform { get; }
+
+        /// <summary>
         /// Gets or sets an optional reason to add into the test result message.
         /// </summary>
         public string Reason { get; set; }
@@ -54,7 +76,14 @@ namespace Acoustics.Test.TestHelpers
         /// <inheritdoc />
         public override TestResult[] Execute(ITestMethod testMethod)
         {
-            if (!RuntimeInformation.IsOSPlatform(this.Platform))
+            var platformMatches = RuntimeInformation.IsOSPlatform(this.Platform);
+
+            //                       | platformMatches==true | platformMatches==false |
+            // IgnorePlatform==true  |        Ignore         |        Run             |
+            // IgnorePlatform==false |        Run            |        Ignore          |
+
+            // euqlaity operates likes XNOR
+            if (platformMatches == this.IgnorePlatform)
             {
                 var message = $"Test not executed. The current platform is not {this.Platform}{this.Reason?.Prepend(" ")}.";
                 return new[]
