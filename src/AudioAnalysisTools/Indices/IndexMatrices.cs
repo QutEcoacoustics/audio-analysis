QutEcoacoustics/audio-analysis

commit a29aad1d2ac6c241b0c580e50c5b993fcd49a635

Removes Zio

src/AudioAnalysisTools/Indices/IndexMatrices.cs
@@ -22,7 +22,7 @@ namespace AudioAnalysisTools.Indices
     using StandardSpectrograms;
     using log4net;
     using TowseyLibrary;
-    using Zio;
+    using Acoustics.Shared.Contracts;
     using MoreLinq;
 
     public static class IndexMatrices
@@ -422,10 +422,10 @@ namespace AudioAnalysisTools.Indices
         /// </summary>
         public static double[,] ReadSpectrogram(FileInfo csvFile, out int binCount)
         {
-            return ReadSpectrogram(csvFile.ToFileEntry(), out binCount);
+            return ReadSpectrogram(csvFile, out binCount);
         }
 
-        public static double[,] ReadSpectrogram(FileEntry csvFile, out int binCount, TwoDimensionalArray transform = TwoDimensionalArray.None)
+        public static double[,] ReadSpectrogram(FileInfo csvFile, out int binCount, TwoDimensionalArray transform = TwoDimensionalArray.None)
         {
             double[,] matrix = Csv.ReadMatrixFromCsv<double>(csvFile, transform);
             binCount = matrix.GetLength(1);
@@ -451,11 +451,6 @@ namespace AudioAnalysisTools.Indices
         }
 
         public static Dictionary<string, double[,]> ReadSpectralIndices(DirectoryInfo ipdir, string fileName, string analysisTag, string[] keys)
-        {
-            return ReadSpectralIndices(ipdir.ToDirectoryEntry(), fileName, analysisTag, keys);
-        }
-
-        public static Dictionary<string, double[,]> ReadSpectralIndices(DirectoryEntry ipdir, string fileName, string analysisTag, string[] keys)
         {
             // parallel reading of CSV files
             var readData = keys.AsParallel()

src/Acoustics.Shared/Acoustics.Shared.csproj
@@ -38,7 +38,7 @@
       <PrivateAssets>all</PrivateAssets>
       <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
     </PackageReference>
+    <PackageReference Include="System.IO.Abstractions" Version="9.0.5" />
     <PackageReference Include="YamlDotNet" Version="8.1.0" />
-    <PackageReference Include="Zio" Version="0.7.6" />
   </ItemGroup>
 </Project>

src/Acoustics.Shared/AnalysisIo.cs
@@ -5,14 +5,13 @@
 namespace Acoustics.Shared
 {
     using System;
+    using System.IO;
+    using System.IO.Abstractions;
     using Acoustics.Shared.Contracts;
-
-    using Zio;
-    using FileSystemEntry = Zio.FileSystemEntry;
 
     public class AnalysisIo
     {
-        public AnalysisIo((IFileSystem FileSystem, FileSystemEntry Base) input, (IFileSystem FileSystem, DirectoryEntry Base) output, (IFileSystem FileSystem, DirectoryEntry Base)? temp)
+        public AnalysisIo((IFileSystem FileSystem, IDirectoryInfo Base) input, (IFileSystem FileSystem, IDirectoryInfo Base) output, (IFileSystem FileSystem, IDirectoryInfo Base)? temp)
         {
             this.Input = input.FileSystem;
             this.InputBase = input.Base;
@@ -21,14 +20,14 @@ namespace Acoustics.Shared
             this.OutputBase = output.Base;
 
             this.Temp = (temp ?? input).FileSystem;
-            this.TempBase = (DirectoryEntry)(temp ?? input).Base;
+            this.TempBase = (temp ?? input).Base;
         }
 
-        public FileSystemEntry InputBase { get; }
+        public IDirectoryInfo InputBase { get; }
 
-        public DirectoryEntry OutputBase { get; }
+        public IDirectoryInfo OutputBase { get; }
 
-        public DirectoryEntry TempBase { get; }
+        public IDirectoryInfo TempBase { get; }
 
         public IFileSystem Input { get; }
 
@@ -40,7 +39,7 @@ namespace Acoustics.Shared
         {
             Contract.Requires(this.InputBase != null, $"{nameof(this.InputBase)} must not be null");
 
-            if (this.Input.DirectoryExists(this.InputBase.Path))
+            if (this.Input.Directory.Exists(this.InputBase.FullName))
             {
                 return new AnalysisIoInputDirectory((this.Input, this.InputBase), (this.Output, this.OutputBase), (this.Temp, this.TempBase));
             }
@@ -52,11 +51,11 @@ namespace Acoustics.Shared
     public class AnalysisIoInputDirectory
         : AnalysisIo
     {
-        internal AnalysisIoInputDirectory((IFileSystem FileSystem, FileSystemEntry Base) input, (IFileSystem FileSystem, DirectoryEntry Base) output, (IFileSystem FileSystem, DirectoryEntry Base)? temp)
+        internal AnalysisIoInputDirectory((IFileSystem FileSystem, IDirectoryInfo Base) input, (IFileSystem FileSystem, IDirectoryInfo Base) output, (IFileSystem FileSystem, IDirectoryInfo Base)? temp)
             : base(input, output, temp)
         {
         }
 
-        public new DirectoryEntry InputBase => (DirectoryEntry)base.InputBase;
+        public new DirectoryInfo InputBase => new DirectoryInfo(base.InputBase.FullName);
     }
 }

src/Acoustics.Shared/ConfigFile/ConfigFile.cs
@@ -19,8 +19,6 @@ namespace Acoustics.Shared.ConfigFile
 
     using log4net;
 
-    using Zio;
-
     public static partial class ConfigFile
     {
         public const string ProfilesKey = nameof(IProfiles<object>.Profiles);
@@ -55,11 +53,11 @@ namespace Acoustics.Shared.ConfigFile
         {
             if (!path.IsNullOrEmpty())
             {
-                var success = TryResolve(path, searchPaths.Select(x => x.ToDirectoryEntry()), out FileEntry configFile);
+                var success = TryResolve(path, searchPaths, out FileInfo configFile);
 
                 if (success)
                 {
-                    return configFile.ToFileInfo();
+                    return configFile;
                 }
             }
 
@@ -98,18 +96,18 @@ namespace Acoustics.Shared.ConfigFile
                 throw new ArgumentException("Try to resolve config failed, because supplied file argument was null or empty.", nameof(file));
             }
 
-            var success = TryResolve(file, searchPaths.Select(x => x.ToDirectoryEntry()), out FileEntry configFile);
+            var success = TryResolve(file, searchPaths, out FileInfo configFile);
 
             if (success)
             {
-                return configFile.ToFileInfo();
+                return configFile;
             }
 
             var message = NotFoundMessage(file, searchPaths);
             throw new ConfigFileException(message, file);
         }
 
-        public static bool TryResolve(string file, IEnumerable<DirectoryEntry> searchPaths, out FileEntry configFile)
+        public static bool TryResolve(string file, IEnumerable<DirectoryInfo> searchPaths, out FileInfo configFile)
         {
             configFile = null;
             if (string.IsNullOrWhiteSpace(file))
@@ -123,7 +121,7 @@ namespace Acoustics.Shared.ConfigFile
             var fullPath = Path.GetFullPath(file);
             if (File.Exists(fullPath))
             {
-                configFile = fullPath.ToFileEntry();
+                configFile = fullPath.ToFileInfo();
                 return true;
             }
 
@@ -183,10 +181,10 @@ namespace Acoustics.Shared.ConfigFile
 
             if (found)
             {
-                FileEntry defaultConfig = null;
+                FileInfo defaultConfig = null;
                 if (TryResolveInConfigFolder(defaultName, ref defaultConfig))
                 {
-                    var file = defaultConfig.ToFileInfo();
+                    var file = defaultConfig;
                     Log.Info($"Supplied config file not found, but a default was found and returned (`{file}`)");
                     {
                         fileInfo = file;
@@ -204,13 +202,13 @@ namespace Acoustics.Shared.ConfigFile
             return false;
         }
 
-        private static bool TryResolveInConfigFolder(string file, ref FileEntry configFile)
+        private static bool TryResolveInConfigFolder(string file, ref FileInfo configFile)
         {
             // config files are always packaged with the app so use a physical file system
             var defaultConfigFile = Path.GetFullPath(Path.Combine(ConfigFolder, file));
             if (File.Exists(defaultConfigFile))
             {
-                configFile = defaultConfigFile.ToFileEntry();
+                configFile = defaultConfigFile.ToFileInfo();
                 return true;
             }
 
@@ -220,7 +218,7 @@ namespace Acoustics.Shared.ConfigFile
                 var nestedDefaultConfigFile = Path.Combine(directory, file);
                 if (File.Exists(nestedDefaultConfigFile))
                 {
-                    configFile = nestedDefaultConfigFile.ToFileEntry();
+                    configFile = nestedDefaultConfigFile.ToFileInfo();
                     return true;
                 }
             }

src/Acoustics.Shared/Csv/Csv.cs
@@ -12,17 +12,16 @@ namespace Acoustics.Shared.Csv
 {
     using System;
     using System.Collections.Generic;
-    using SixLabors.ImageSharp;
     using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Reflection;
-    using Contracts;
+    using Acoustics.Shared.Contracts;
     using CsvHelper;
     using CsvHelper.Configuration;
     using CsvHelper.TypeConversion;
     using log4net;
-    using Zio;
+    using SixLabors.ImageSharp;
 
     /// <summary>
     /// Generic methods for reading and writing Csv file.
@@ -316,11 +315,6 @@ Member Name: {tce.MemberMapData.Member.Name}
         }
 
         public static T[,] ReadMatrixFromCsv<T>(FileInfo source, TwoDimensionalArray transform = TwoDimensionalArray.None)
-        {
-            return ReadMatrixFromCsv<T>(source.ToFileEntry(), transform);
-        }
-
-        public static T[,] ReadMatrixFromCsv<T>(FileEntry source, TwoDimensionalArray transform = TwoDimensionalArray.None)
         {
             Contract.Requires(source != null);
 

src/Acoustics.Shared/Extensions/ZioExtensions.cs
@@ -1,156 +1,0 @@
-// <copyright file="ZioExtensions.cs" company="QutEcoacoustics">
-// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
-// </copyright>
-
-// ReSharper disable once CheckNamespace
-namespace Zio
-{
-    using System;
-    using System.IO;
-    using System.Text;
-    using Acoustics.Shared.Contracts;
-
-    using FileSystems;
-    using SixLabors.ImageSharp;
-    using SixLabors.ImageSharp.Formats;
-    using SixLabors.ImageSharp.Formats.Png;
-    using Path = System.IO.Path;
-
-    public static class ZioExtensions
-    {
-        private static readonly PhysicalFileSystem FileSystem = new PhysicalFileSystem();
-
-        public static UPath ToUPath(this FileSystemInfo file)
-        {
-            return FileSystem.ConvertPathFromInternal(file.FullName);
-        }
-
-        public static string ToOsPath(this UPath path)
-        {
-            return FileSystem.ConvertPathToInternal(path);
-        }
-
-        public static DirectoryEntry ToDirectoryEntry(this DirectoryInfo directory)
-        {
-            return new DirectoryEntry(FileSystem, directory.ToUPath());
-        }
-
-        public static FileEntry ToFileEntry(this FileInfo file)
-        {
-            return new FileEntry(FileSystem, file.ToUPath());
-        }
-
-        public static DirectoryEntry ToDirectoryEntry(this string directory)
-        {
-            var path = Path.GetFullPath(directory);
-            return new DirectoryEntry(FileSystem, FileSystem.ConvertPathFromInternal(path));
-        }
-
-        public static FileEntry ToFileEntry(this string file)
-        {
-            var path = Path.GetFullPath(file);
-
-            return new FileEntry(FileSystem, FileSystem.ConvertPathFromInternal(path));
-        }
-
-        public static FileInfo ToFileInfo(this FileEntry file)
-        {
-            Contract.Requires(file != null);
-            Contract.Requires(
-                file.FileSystem is PhysicalFileSystem,
-                $"To convert the path {file} back to a physical filesystem, it must be from a physical file system");
-
-            return new FileInfo(file.Path.ToOsPath());
-        }
-
-        public static DirectoryInfo ToDirectoryInfo(this DirectoryEntry directory)
-        {
-            Contract.Requires(directory != null);
-            Contract.Requires(
-                directory.FileSystem is PhysicalFileSystem,
-                $"To convert the path {directory} back to a physical filesystem, it must be from a physical file system");
-
-            return new DirectoryInfo(directory.Path.ToOsPath());
-        }
-
-        public static DirectoryEntry Combine(this DirectoryEntry directoryInfo, params string[] str)
-        {
-            Contract.Requires(directoryInfo != null);
-            Contract.Requires(str != null && str.Length > 0);
-
-            UPath merged = directoryInfo.Path;
-            foreach (var fragment in str)
-            {
-                merged = merged / fragment;
-            }
-
-            return new DirectoryEntry(directoryInfo.FileSystem, merged);
-        }
-
-        public static FileEntry CombineFile(this DirectoryEntry directoryInfo, params string[] str)
-        {
-            Contract.Requires(directoryInfo != null);
-            Contract.Requires(str != null && str.Length > 0);
-
-            UPath merged = directoryInfo.Path;
-            foreach (var fragment in str)
-            {
-                merged = merged / fragment;
-            }
-
-            return new FileEntry(directoryInfo.FileSystem, merged);
-        }
-
-        /// <summary>
-        /// Adapter method that converts a DirectoryInfo or FileInfo to a default Zio PhysicalFileSystem and equivalent
-        /// UPath.
-        /// </summary>
-        public static (IFileSystem FileSystem, UPath Path) ToZio(this FileSystemInfo file)
-        {
-            return (FileSystem, FileSystem.ConvertPathFromInternal(file.FullName));
-        }
-
-        /// <summary>
-        /// Return a default physical file system that can act as a crutch while we migrate
-        /// </summary>
-        /// <param name="path">Any UPath. This parameter has no effect.</param>
-        /// <returns>A stati</returns>
-        public static PhysicalFileSystem DefaultFileSystem(this UPath path)
-        {
-            return FileSystem;
-        }
-
-        public static void Save(this Image bitmap, IFileSystem fileSystem, UPath path)
-        {
-            var extension = path.GetExtensionWithDot();
-
-            IImageEncoder format;
-            switch (extension)
-            {
-                case ".png":
-                    format = new PngEncoder();
-                    break;
-                default:
-                    throw new NotSupportedException();
-            }
-
-            using var fileStream = fileSystem.CreateFile(path);
-            bitmap.Save(fileStream, format);
-        }
-
-        public static StreamReader OpenText(this IFileSystem fileSystem, UPath path)
-        {
-            using (var stream = fileSystem.OpenFile(path, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                return new StreamReader(stream, Encoding.UTF8, true);
-            }
-        }
-
-        public static StreamReader OpenText(this FileEntry file)
-        {
-            var stream = file.FileSystem.OpenFile(file.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-            return new StreamReader(stream, Encoding.UTF8, true);
-        }
-    }
-}

src/Acoustics.Shared/FileNameHelpers.cs
@@ -9,7 +9,7 @@ namespace Acoustics.Shared
     using System.Linq;
     using System.Text.RegularExpressions;
 
-    using Zio;
+    using Acoustics.Shared.Contracts;
 
     /// <summary>
     /// A set of helper methods used to create/read consistently encoded filenames
@@ -103,15 +103,6 @@ namespace Acoustics.Shared
 
         public static void ParseAnalysisFileName(
             FileInfo file,
-            out string originalBaseName,
-            out string analysisTag,
-            out string[] otherSegments)
-        {
-            ParseAnalysisFileName(file.Name, out originalBaseName, out analysisTag, out otherSegments);
-        }
-
-        public static void ParseAnalysisFileName(
-            FileEntry file,
             out string originalBaseName,
             out string analysisTag,
             out string[] otherSegments)

src/Acoustics.Shared/Json.cs
@@ -14,7 +14,7 @@ namespace Acoustics.Shared
 
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
-    using Zio;
+    using Acoustics.Shared.Contracts;
 
     public static class Json
     {
@@ -52,12 +52,12 @@ namespace Acoustics.Shared
 
         public static T Deserialize<T>(FileInfo file)
         {
-            return Deserialize<T>(file.ToFileEntry());
+            return Deserialize<T>(file.FullName);
         }
 
-        public static T Deserialize<T>(FileEntry file)
+        public static T Deserialize<T>(string file)
         {
-            using (var stream = file.OpenText())
+            using (var stream = File.OpenText(file))
             using (var reader = new JsonTextReader(stream))
             {
                 return Serializer.Deserialize<T>(reader);

commit 9b444ff01e0d1e58a4110c0fcfdc219406d0905f

Fixes stack overflow exception in ReadSpectrogram
The changes in a29aad1d2ac6c241b0c580e50c5b993fcd49a635 introduced a self-calling version of the the ReadSpectrogram method when the overload was simplified.
This led to a stackoverflow and the difficult to debug variable unit test symptom we were seeing.

Work done for #196

src/AudioAnalysisTools/Indices/IndexMatrices.cs
@@ -420,11 +420,6 @@ namespace AudioAnalysisTools.Indices
         /// and the first column contains row/time names.
         /// Note: no rotation of data is done!.
         /// </summary>
-        public static double[,] ReadSpectrogram(FileInfo csvFile, out int binCount)
-        {
-            return ReadSpectrogram(csvFile, out binCount);
-        }
-
         public static double[,] ReadSpectrogram(FileInfo csvFile, out int binCount, TwoDimensionalArray transform = TwoDimensionalArray.None)
         {
             double[,] matrix = Csv.ReadMatrixFromCsv<double>(csvFile, transform);