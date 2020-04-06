8f2315cac4420cebbf79417cc6d581ee9a06eeab
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-04-06 22:53:06 +1000

    Add debugging information to git version script
    [release]
    
    Work done for #196

--- src/git_version.ps1
+++ src/git_version.ps1
@@ -29,7 +29,10 @@ $self_contained = if ($self_contained -eq 'true') { 'true' } else { 'false' }
 
 $commit_hash = git show -s --format="%H"
 
-$branch = ((git log -n 1 --pretty=%d HEAD) | Select-String "-> ([\w-]+)").Matches[0].Groups[1].Value
+$git_log = git log -n 1 --pretty=%d HEAD
+Write-output "git log output: $git_log"
+$branch = ($git_log | Select-String "-> ([\w-]+)").Matches[0].Groups[1].Value
+Write-output "branch: $branch"
 
 $describe = git describe --dirty --abbrev --long --always
 
