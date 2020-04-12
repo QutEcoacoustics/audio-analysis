f9292e322a7ce2c9a761a22e429a894b8d71b534
towsey <m.towsey@qut.edu.au>
2020-04-12 21:04:06 +1000

    Create new Track class
    Issue #297 Create new Track class to replace the old SpectralTrack class.

---
+++ src/AudioAnalysisTools/Events/Tracks/Track.cs
@@ -0,0 +1,30 @@
+// <copyright file="Track.cs" company="QutEcoacoustics">
+// All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
+// </copyright>
+
+namespace AudioAnalysisTools.Events.Interfaces
+{
+    using System.Collections.Generic;
+    using AudioAnalysisTools.Events.Drawing;
+    using SixLabors.ImageSharp.PixelFormats;
+    using SixLabors.ImageSharp.Processing;
+
+    public class Track : ITrack
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Track"/> class.
+        /// Constructor.
+        /// </summary>
+        public Track()
+        {
+        }
+
+        public ISet<ISpectralPoint> Points => throw new System.NotImplementedException();
+
+        public void Draw<T>(IImageProcessingContext graphics, EventRenderingOptions options)
+            where T : struct, IPixel<T>
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
