500010ea4e1af2e55e2b1b5db69225edfd16ad73
towsey <m.towsey@qut.edu.au>
2020-05-15 13:05:25 +1000

    Changed namespaces for test classes
    Issue #319

--- tests/Acoustics.Test/AnalysisPrograms/Recognizers/GenericRecognizerTests.cs
+++ tests/Acoustics.Test/AnalysisPrograms/Recognizers/GenericRecognizerTests.cs
@@ -2,7 +2,7 @@
 // All code in this file and all associated files are the copyright and property of the QUT Ecoacoustics Research Group (formerly MQUTeR, and formerly QUT Bioacoustics Research Group).
 // </copyright>
 
-namespace Acoustics.Test.AnalysisPrograms.Recognizers.GenericRecognizer
+namespace Acoustics.Test.AnalysisPrograms.Recognizers
 {
     using System;
     using System.Collections.Generic;
