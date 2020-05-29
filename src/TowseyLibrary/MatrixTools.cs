d77674b388785505972ddc6b430e0c54fe66b0c4
Michael Towsey <m.towsey@qut.edu.au>
2020-05-29 10:26:53 +1000

    Update src/TowseyLibrary/MatrixTools.cs
    Co-authored-by: Anthony Truskinger <anthony.truskinger@gmail.com>

--- src/TowseyLibrary/MatrixTools.cs
+++ src/TowseyLibrary/MatrixTools.cs
@@ -287,7 +287,7 @@ namespace TowseyLibrary
 
         /// <summary>
         /// Concatenate two matrices.
-        /// WARNING: This method assumes that the two matrices have the same number of rows.
+        /// WARNING: This method requires that the two matrices have the same number of rows.
         ///          They will be joined by adding the columns of M2 to the columns of M1.
         /// </summary>
         public static double[,] ConcatenateTwoMatrices(double[,] matrix1, double[,] matrix2)
