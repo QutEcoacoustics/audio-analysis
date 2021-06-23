ae34d1c4d7537e6002e2bd35b29f85d0c1967e0a
Michael Towsey <m.towsey@qut.edu.au>
2021-06-23 14:37:09 +1000

    Update HarmonicAlgorithmTests.cs
    These changes are an unexplained hangover from a previous branch. I am sure they were previously fixed.

--- tests/Acoustics.Test/AudioAnalysisTools/HarmonicAnalysis/HarmonicAlgorithmTests.cs
+++ tests/Acoustics.Test/AudioAnalysisTools/HarmonicAnalysis/HarmonicAlgorithmTests.cs
@@ -77,7 +77,7 @@ namespace Acoustics.Test.AudioAnalysisTools.HarmonicAnalysis
                 "440_harmonic");
 
             this.SaveImage(
-                SpectrogramTools.GetSonogramPlusCharts(this.spectrogram, events, plots, null));
+                SpectrogramTools.GetSonogramPlusCharts(this.spectrogram, events, plots, null, "440_harmonic"));
 
             Assert.AreEqual(1, events.Count);
             Assert.IsInstanceOfType(events.First(), typeof(HarmonicEvent));
@@ -118,7 +118,7 @@ namespace Acoustics.Test.AudioAnalysisTools.HarmonicAnalysis
                 "1000_harmonic");
 
             this.SaveImage(
-                SpectrogramTools.GetSonogramPlusCharts(this.spectrogram, events, plots, null));
+                SpectrogramTools.GetSonogramPlusCharts(this.spectrogram, events, plots, null, "1000_harmonic"));
 
             Assert.AreEqual(1, events.Count);
             Assert.IsInstanceOfType(events.First(), typeof(HarmonicEvent));
